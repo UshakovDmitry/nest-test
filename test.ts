1b07efcaa24c32e7fc21af1fd1ca68077d64ee9b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:29:31 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,27 @@
-'"@nestjs/common"' has no exported member named 'HttpService'. Did you mean 'HttpServer'?ts(2724)
+npm install @nestjs/axios axios
+
+
+import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq.service';
+import { HttpModule } from '@nestjs/axios'; // <-- Измененный импорт
+
+@Module({
+  imports: [HttpModule.register({})],  // Используйте .register({}), если вам нужна дополнительная конфигурация
+  providers: [RabbitMQService],
+  exports: [RabbitMQService]
+})
+export class RabbitMQModule {}
+
+
+
+
+
+
+
+import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios'; // <-- Измененный импорт
+
+@Injectable()
+export class RabbitMQService {
+  // ... (оставьте как есть)
+}
