2ef1edf4d1f2dd20e8e704298b777ffce40e5f1d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:35:23 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,18 +1,13 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> npm run start
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TransportModule } from './transport/transport.module';
+import { LoadersModule } from './loaders/loaders.module';
+import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
 
-> tsm-api@0.0.1 start
-> nest start
-
-src/app.module.ts:6:10 - error TS2724: '"./rabbitmq/rabbitmq.module"' has no exported member named 'RabbitmqModule'. Did you mean 'RabbitMQModule'?
-
-6 import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
-           ~~~~~~~~~~~~~~
-
-  src/rabbitmq/rabbitmq.module.ts:12:14
-    12 export class RabbitMQModule {}
-                    ~~~~~~~~~~~~~~
-    'RabbitMQModule' is declared here.
-
-Found 1 error(s).
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api>
+@Module({
+  imports: [TransportModule, LoadersModule, RabbitmqModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
