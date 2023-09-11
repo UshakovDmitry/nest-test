1be1ee10fc6464808e1c8b472b2689f329d2b0b0
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:11:27 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,6 +1,39 @@
 
 
 MODULE.TS
+import { Module } from '@nestjs/common';
+import { TransportService } from './transport.service';
+import { TransportController } from './transport.controller';
 
+@Module({
+  controllers: [TransportController],
+  providers: [TransportService],
+})
+export class TransportModule {}
+
+
+
+COTROLLER.TS
+
+import { Controller, Get } from '@nestjs/common';
+import { TransportService } from './transport.service';
+
+@Controller('transport')
+export class TransportController {
+  constructor(private readonly transportService: TransportService) {}
+  @Get()
+  async getTransport() {
+    return await this.transportService.getTransport();
+  }
+}
 
 SERVICE.TS
+import { Module } from '@nestjs/common';
+import { TransportService } from './transport.service';
+import { TransportController } from './transport.controller';
+
+@Module({
+  controllers: [TransportController],
+  providers: [TransportService],
+})
+export class TransportModule {}
