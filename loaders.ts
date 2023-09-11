df780e7c3a78cd20090518b9a20590bb5628d27f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:10:10 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,1 +1,62 @@
 
+MODULE.TS
+import { Module } from '@nestjs/common';
+import { LoadersService } from './loaders.service';
+import { LoadersController } from './loaders.controller';
+
+@Module({
+  controllers: [LoadersController],
+  providers: [LoadersService],
+})
+export class LoadersModule {}
+
+
+COTROLLER.TS
+import { Controller, Get } from '@nestjs/common';
+import { LoadersService } from './loaders.service';
+
+@Controller('loaders')
+export class LoadersController {
+  constructor(private readonly loadersService: LoadersService) {}
+
+  @Get()
+  getLoaders() {
+    return this.loadersService.getLoaders();
+  }
+}
+
+
+
+SERVICE.TS
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class LoadersService {
+  private LOADERS: any[] = [
+    {
+      fullName: 'Иванов Иван Иванович',
+      warehouseNumber: '1',
+      schedule: 'Пн-Пт 9:00-18:00',
+      transportNumber: 'A 123 AA 77',
+      isActive: true,
+    },
+    {
+      fullName: 'Кукин Иван Иванович',
+      warehouseNumber: '1',
+      schedule: 'Пн-Пт 9:00-18:00',
+      transportNumber: 'A 123 AA 77',
+      isActive: true,
+    },
+    {
+      fullName: 'Сидоренко Иван Иванович',
+      warehouseNumber: '1',
+      schedule: 'Пн-Пт 9:00-18:00',
+      transportNumber: 'A 123 AA 77',
+      isActive: true,
+    },
+  ];
+
+  getLoaders(): any[] {
+    return this.LOADERS;
+  }
+}
