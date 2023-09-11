c4b74ec78458989bbc4bd293df749f68d0cee8cd
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 09:48:20 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -33,3 +33,18 @@ export class TransportService {
     return this.TRANSPORT;
   }
 }
+
+
+
+///////////
+import { Module } from '@nestjs/common';
+import { TransportService } from './transport.service';
+import { TransportController } from './transport.controller';
+
+@Module({
+  controllers: [TransportController],
+  providers: [TransportService],
+})
+export class TransportModule {}
+
+
