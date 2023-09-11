737ae8f4eec6346095ce3863f00f6d5d08d06f8b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:37:33 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,19 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TransportModule } from './transport/transport.module';
-import { LoadersModule } from './loaders/loaders.module';
-import { RabbitmqModule } from './rabbitmq/rabbitmq.module';
-
-@Module({
-  imports: [TransportModule, LoadersModule, RabbitmqModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
-
-
-
-'"./rabbitmq/rabbitmq.module"' has no exported member named 'RabbitmqModule'. Did you mean 'RabbitMQModule'?ts(2724)
-rabbitmq.module.ts(12, 14): 'RabbitMQModule' is declared here.
-import RabbitmqModule
+[
+    {
+        "payload_bytes": 1437,
+        "redelivered": true,
+        "exchange": "TmsExchange",
+        "routing_key": "tms1c",
+        "message_count": 0,
+        "properties": {
+            "headers": {
+                "JMSXDeliveryCount": 1,
+                "JMS_AMQP_HEADER": true,
+                "JMS_AMQP_HEADERDURABLE": true,
+                "JMS_AMQP_ORIGINAL_ENCODING": 6,
+                "NATIVE_MESSAGE_ID": "ID:AMQP_UUID:9388997e-8283-40aa-8c93-dafb9637c56d",
+                "SenderCode": "office",
+                "content_type": "application/json",
+                "integ_sender_code": "office"
+            }
+        },
+        "payload": "{\r\r\n\t\"Number\": \"№№00015933\",\r\r\n\t\"Date\": \"01.08.2023 9:01:14\",\r\r\n\t\"Organization\": \"TOO Gulser Computers (Гулсер Компьютерс)\",\r\r\n\t\"DocumentStatus\": \"Доставляется\",\r\r\n\t\"Driver\": \"Бархудар Асан Арыпұлы\",\r\r\n\t\"ISR\": \"\",\r\r\n\t\"Informal_Document\": \"Акт регистрации брака TL300000000014 от 01.04.2022 18:36:05\",\r\r\n\t\"SKU_Weight\": \"17\",\r\r\n\t\"ArrayStrings\": [\r\r\n\t\t{\r\r\n\t\t\t\"Shipping_Point\": \"Талгар 3, ул. Абылай хана, дом 111 (склад товара, ожидающего ремонта)\",\r\r\n\t\t\t\"Goods\": \"Телевизор KIVI 55U710KB  Smart 4K UHD\",\r\r\n\t\t\t\"Quantity\": \"1\",\r\r\n\t\t\t\"Item_Status\": \"Оформлена\",\r\r\n\t\t\t\"Pickup_Point\": \"1\",\r\r\n\t\t\t\"Delivery_Point\": \"2\",\r\r\n\t\t\t\"Pickup_Latitude\": \"43,305732\",\r\r\n\t\t\t\"Pickup_Longitude\": \"77,241999\",\r\r\n\t\t\t\"Delivery_Latitude\": \"43,245609\",\r\r\n\t\t\t\"Delivery_Longitude\": \"76,90425\",\r\r\n\t\t\t\"Pickup_Time\": \"01.01.0001 9:43:53\",\r\r\n\t\t\t\"Delivery_Time\": \"01.01.0001 10:46:44\"\r\r\n\t\t}\r\r\n\t],\r\r\n\t\"ContactInformation\": {\r\r\n\t\t\"City\": \"Алматы\",\r\r\n\t\t\"Delivery_Condition\": \"Доставка\",\r\r\n\t\t\"Date_Time_delivery\": \"2023-08-05 До 18:00\",\r\r\n\t\t\"Time_Window\": \"09:00-18:00\",\r\r\n\t\t\"Latitude\": \"43,245609\",\r\r\n\t\t\"Longitude\": \"76,90425\",\r\r\n\t\t\"Street\": \"нет данных\",\r\r\n\t\t\"Home\": \"нет данных\",\r\r\n\t\t\"Phone\": \"+7(778)021-1316\",\r\r\n\t\t\"Apartment\": \"нет данных\",\r\r\n\t\t\"Contractor\": \"нет данных\"\r\r\n\t}\r\r\n}",
+        "payload_encoding": "string"
+    }
+]
