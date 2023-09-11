1a81067cbee6048389f1b9ac8d94103261084c7d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:16:58 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,33 +1,3 @@
- readFromQueue(): void {
-    const username: string = 'tms';
-    const password: string = '26000567855499290979';
+npm install amqplib @nestjs/platform-express
 
-    fetch('http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get', {
-      method: 'POST',
-      mode: 'no-cors',
-      headers: {
-        Authorization: `Basic ${btoa(`${username}:${password}`)}`,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        count: 1,
-        ackmode: 'ack_requeue_true',
-        encoding: 'auto',
-        truncate: 50000,
-      }),
-    })
-      .then(async (response) => {
-        if (response.ok) {
-          console.log(response.json());
-          return await response.json();
-        } else {
-          throw new Error('Возникла ошибка при получении данных');
-        }
-      })
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }
+
