e99a99e9fcae3d566f5c075014f30f314fc8be2b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-11 10:42:54 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,58 @@
-[
-    {
-        "payload_bytes": 1437,
-        "redelivered": true,
-        "exchange": "TmsExchange",
-        "routing_key": "tms1c",
-        "message_count": 0,
-        "properties": {
-            "headers": {
-                "JMSXDeliveryCount": 1,
-                "JMS_AMQP_HEADER": true,
-                "JMS_AMQP_HEADERDURABLE": true,
-                "JMS_AMQP_ORIGINAL_ENCODING": 6,
-                "NATIVE_MESSAGE_ID": "ID:AMQP_UUID:9388997e-8283-40aa-8c93-dafb9637c56d",
-                "SenderCode": "office",
-                "content_type": "application/json",
-                "integ_sender_code": "office"
-            }
-        },
-        "payload": "{\r\r\n\t\"Number\": \"№№00015933\",\r\r\n\t\"Date\": \"01.08.2023 9:01:14\",\r\r\n\t\"Organization\": \"TOO Gulser Computers (Гулсер Компьютерс)\",\r\r\n\t\"DocumentStatus\": \"Доставляется\",\r\r\n\t\"Driver\": \"Бархудар Асан Арыпұлы\",\r\r\n\t\"ISR\": \"\",\r\r\n\t\"Informal_Document\": \"Акт регистрации брака TL300000000014 от 01.04.2022 18:36:05\",\r\r\n\t\"SKU_Weight\": \"17\",\r\r\n\t\"ArrayStrings\": [\r\r\n\t\t{\r\r\n\t\t\t\"Shipping_Point\": \"Талгар 3, ул. Абылай хана, дом 111 (склад товара, ожидающего ремонта)\",\r\r\n\t\t\t\"Goods\": \"Телевизор KIVI 55U710KB  Smart 4K UHD\",\r\r\n\t\t\t\"Quantity\": \"1\",\r\r\n\t\t\t\"Item_Status\": \"Оформлена\",\r\r\n\t\t\t\"Pickup_Point\": \"1\",\r\r\n\t\t\t\"Delivery_Point\": \"2\",\r\r\n\t\t\t\"Pickup_Latitude\": \"43,305732\",\r\r\n\t\t\t\"Pickup_Longitude\": \"77,241999\",\r\r\n\t\t\t\"Delivery_Latitude\": \"43,245609\",\r\r\n\t\t\t\"Delivery_Longitude\": \"76,90425\",\r\r\n\t\t\t\"Pickup_Time\": \"01.01.0001 9:43:53\",\r\r\n\t\t\t\"Delivery_Time\": \"01.01.0001 10:46:44\"\r\r\n\t\t}\r\r\n\t],\r\r\n\t\"ContactInformation\": {\r\r\n\t\t\"City\": \"Алматы\",\r\r\n\t\t\"Delivery_Condition\": \"Доставка\",\r\r\n\t\t\"Date_Time_delivery\": \"2023-08-05 До 18:00\",\r\r\n\t\t\"Time_Window\": \"09:00-18:00\",\r\r\n\t\t\"Latitude\": \"43,245609\",\r\r\n\t\t\"Longitude\": \"76,90425\",\r\r\n\t\t\"Street\": \"нет данных\",\r\r\n\t\t\"Home\": \"нет данных\",\r\r\n\t\t\"Phone\": \"+7(778)021-1316\",\r\r\n\t\t\"Apartment\": \"нет данных\",\r\r\n\t\t\"Contractor\": \"нет данных\"\r\r\n\t}\r\r\n}",
-        "payload_encoding": "string"
-    }
-]
+export interface QueueMessage {
+  payload_bytes: number;
+  redelivered: boolean;
+  exchange: string;
+  routing_key: string;
+  message_count: number;
+  properties: MessageProperties;
+  payload: Payload;
+  payload_encoding: string;
+}
+
+export interface MessageProperties {
+  headers: {
+    [key: string]: any;
+  };
+}
+
+export interface Payload {
+  Number: string;
+  Date: string;
+  Organization: string;
+  DocumentStatus: string;
+  Driver: string;
+  ISR: string;
+  Informal_Document: string;
+  SKU_Weight: string;
+  ArrayStrings: ArrayString[];
+  ContactInformation: ContactInformation;
+}
+
+export interface ArrayString {
+  Shipping_Point: string;
+  Goods: string;
+  Quantity: string;
+  Item_Status: string;
+  Pickup_Point: string;
+  Delivery_Point: string;
+  Pickup_Latitude: string;
+  Pickup_Longitude: string;
+  Delivery_Latitude: string;
+  Delivery_Longitude: string;
+  Pickup_Time: string;
+  Delivery_Time: string;
+}
+
+export interface ContactInformation {
+  City: string;
+  Delivery_Condition: string;
+  Date_Time_delivery: string;
+  Time_Window: string;
+  Latitude: string;
+  Longitude: string;
+  Street: string;
+  Home: string;
+  Phone: string;
+  Apartment: string;
+  Contractor: string;
+}
