38be9c16867c83a1094dd057816e02e0dc669bad
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 14:38:51 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,16 +1,63 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Message, MessageSchema } from './message.entity';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+  ],
+})
+export class DatabaseModule {}
+
+
+
+
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
 > tms-api@0.0.1 start
 > nest start
 
-src/database/database.module.ts:7:7 - error TS2345: Argument of type '{ useNewUrlParser: boolean; }' is not assignab
-le to parameter of type 'MongooseModuleOptions'.
-  Object literal may only specify known properties, and 'useNewUrlParser' does not exist in type 'MongooseModuleOpti
-ons'.
+[Nest] 19844  - 13.09.2023, 14:38:10     LOG [NestFactory] Starting Nest application...
+[Nest] 19844  - 13.09.2023, 14:38:10     LOG [InstanceLoader] DatabaseModule dependencies initialized +34ms
+[Nest] 19844  - 13.09.2023, 14:38:10     LOG [InstanceLoader] TypeOrmModule dependencies initialized +0ms
+[Nest] 19844  - 13.09.2023, 14:38:10   ERROR [ExceptionHandler] Nest can't resolve dependencies of the MessageModel
+(?). Please make sure that the argument DatabaseConnection at index [0] is available in the MongooseModule context.
 
-7       useNewUrlParser: true,
-        ~~~~~~~~~~~~~~~
+Potential solutions:
+- Is MongooseModule a valid NestJS module?
+- If DatabaseConnection is a provider, is it part of the current MongooseModule?
+- If DatabaseConnection is exported from a separate @Module, is that module imported within MongooseModule?
+  @Module({
+    imports: [ /* the Module containing DatabaseConnection */ ]
+  })
 
-Found 1 error(s).
+Error: Nest can't resolve dependencies of the MessageModel (?). Please make sure that the argument DatabaseConnectio
+n at index [0] is available in the MongooseModule context.
 
+Potential solutions:
+- Is MongooseModule a valid NestJS module?
+- If DatabaseConnection is a provider, is it part of the current MongooseModule?
+- If DatabaseConnection is exported from a separate @Module, is that module imported within MongooseModule?
+  @Module({
+    imports: [ /* the Module containing DatabaseConnection */ ]
+  })
+
+    at Injector.lookupComponentInParentModules (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend
+\node_modules\@nestjs\core\injector\injector.js:254:19)
+    at Injector.resolveComponentInstance (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_
+modules\@nestjs\core\injector\injector.js:207:33)
+    at resolveParam (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\
+injector\injector.js:128:38)
+    at async Promise.all (index 0)
+    at Injector.resolveConstructorParams (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_
+modules\@nestjs\core\injector\injector.js:143:27)
+    at Injector.loadInstance (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nes
+tjs\core\injector\injector.js:70:13)
+    at Injector.loadProvider (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nes
+tjs\core\injector\injector.js:97:9)
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\injector\insta
+nce-loader.js:56:13
+    at async Promise.all (index 3)
+    at InstanceLoader.createInstancesOfProviders (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backe
+nd\node_modules\@nestjs\core\injector\instance-loader.js:55:9)
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
