be5f6313fecd8bb8b540aaa8264827898277bc4d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:31:34 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,18 +1,23 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Message, MessageDocument } from './message.entity';
+...
+import { DatabaseService } from '../database/database.service';
 
 @Injectable()
-export class DatabaseService {
-  constructor(@InjectModel(Message.name) private messageModel: Model<MessageDocument>) {}
+export class RabbitMQService {
+  ...
+  constructor(private readonly httpService: HttpService, private readonly dbService: DatabaseService) {}
 
-  async create(data: any): Promise<Message> {
-    const createdMessage = new this.messageModel(data);
-    return createdMessage.save();
-  }
+  async readFromQueue(): Promise<QueueMessage> {
+    ...
+    if (response.status === 200 && response.data.length > 0) {
+        const data = response.data[0];
+        const payload = JSON.parse(data.payload);
+        
+        // Сохраняем в MongoDB
+        await this.dbService.create(payload);
 
-  async findAll(): Promise<Message[]> {
-    return this.messageModel.find().exec();
+        return payload;
+    } else {
+        throw new Error('Возникла ошибка при получении данных');
+    }
   }
 }
