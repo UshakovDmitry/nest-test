eff739452ac2c0fdb9f56f96687f93eda0bdd98b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:58:55 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,69 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+Добавим новый метод в rabbitmq.service.ts:
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Message } from '../message/message.schema';
 
-> tms-api@0.0.1 start
-> nest start
+@Injectable()
+export class RabbitMQService {
+  private client: ClientProxy;
 
-src/rabbitmq/rabbitmq.service.ts:28:17 - error TS2339: Property 'on' does not exist on type 'ClientProxy'.
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<Message>
+  ) {
+    this.client = ClientProxyFactory.create({
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://tms:26000567855499290979@rabbitmq.next.local`],
+        queue: 'TmsQueue',
+        queueOptions: { durable: false },
+      },
+    });
+  }
 
-28     this.client.on('data', async (data) => {
-                   ~~
+  async readFromQueue(): Promise<any> {
+    return this.client.send('get_message', {}).toPromise();
+  }
 
-Found 1 error(s).
+  async getAllMessages(): Promise<any[]> {
+    return this.messageModel.find().exec();
+  }
+}
 
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
+
+
+
+
+rabbitmq.module.ts:
+
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RabbitMQService } from './rabbitmq.service';
+import { RabbitMQController } from './rabbitmq.controller';
+import { MessageModule } from '../message/message.module'; // Путь может отличаться в зависимости от структуры вашего проекта
+
+@Module({
+  imports: [MessageModule],
+  providers: [RabbitMQService],
+  controllers: [RabbitMQController]
+})
+export class RabbitMQModule {}
+
+
+
+
+rabbitmq.controller.ts:
+
+import { Controller, Get } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq.service';
+
+@Controller('all-message')
+export class RabbitMQController {
+  constructor(private readonly rabbitMQService: RabbitMQService) {}
+
+  @Get()
+  async findAll(): Promise<any[]> {
+    return this.rabbitMQService.getAllMessages();
+  }
+}
