33c9522e3fc77edaa59287197104420d7c1abbb8
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:07:40 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -2,12 +2,13 @@
 MODULE.TS
 import { Module } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.service';
-import { RabbitMQController } from './rabbitmq.controller'; 
+import { RabbitMQController } from './rabbitmq.controller';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule.register({})],
-  controllers: [RabbitMQController], 
+  controllers: [RabbitMQController],
+  providers: [RabbitMQService],
   exports: [RabbitMQService],
 })
 export class RabbitMQModule {}
@@ -17,16 +18,19 @@ export class RabbitMQModule {}
 COTROLLER.TS
 import { Controller, Get } from '@nestjs/common';
 import { RabbitMQService } from './rabbitmq.service';
+import { ApiTags } from '@nestjs/swagger';
 
-@Controller('rabbitmq')
+@ApiTags('rabbitmq')
+@Controller('/api/rabbitmq')
 export class RabbitMQController {
   constructor(private readonly rabbitMQService: RabbitMQService) {}
 
-  @Get('read')
+  @Get('/read')
   async readFromQueue() {
     return await this.rabbitMQService.readFromQueue();
   }
 }
+
 
 
 
@@ -79,50 +83,3 @@ export class RabbitMQService {
     }
   }
 }
-
-
-app module
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TransportModule } from './transport/transport.module';
-import { LoadersModule } from './loaders/loaders.module';
-import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
-
-@Module({
-  imports: [TransportModule, LoadersModule, RabbitMQModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api> npm run start
-
-> tms-api@0.0.1 start
-> nest start
-
-[Nest] 14052  - 12.09.2023, 12:34:19     LOG [NestFactory] Starting Nest application...
-[Nest] 14052  - 12.09.2023, 12:34:19   ERROR [ExceptionHandler] Nest cannot export a provider/module that is not a part of the currently processed module (RabbitMQModule). Ple
-ase verify whether the exported RabbitMQService is available in this particular context.
-
-Possible Solutions:
-- Is RabbitMQService part of the relevant providers/imports within RabbitMQModule?
-
-Error: Nest cannot export a provider/module that is not a part of the currently processed module (RabbitMQModule). Please verify whether the exported RabbitMQService is availa
-ble in this particular context.
-
-Possible Solutions:
-- Is RabbitMQService part of the relevant providers/imports within RabbitMQModule?
-
-    at Module.validateExportedProvider (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\injector\module.js:308:19)
-    at addExportedUnit (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\injector\module.js:276:67)
-    at Module.addExportedProvider (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\injector\module.js:287:9)
-    at NestContainer.addExportedProvider (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\injector\container.js:168:19)
-    at DependenciesScanner.insertExportedProvider (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\scanner.js:291:24)
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\scanner.js:148:50
-    at Array.forEach (<anonymous>)
-    at DependenciesScanner.reflectExports (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\scanner.js:148:17)
-    at DependenciesScanner.scanModulesForDependencies (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\scanner.js:101:18
-)
-    at DependenciesScanner.scan (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api\node_modules\@nestjs\core\scanner.js:31:9)
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\tsm-api>
