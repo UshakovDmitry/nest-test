65a647af58a83c57424a8f79cf0ed98642d605c8
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:39:26 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,61 +1,19 @@
-import { Injectable, HttpService } from '@nestjs/common';
-import { QueueMessage } from './rabbitmq.interface';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MessageDocument, MESSAGE_SCHEMA_NAME } from '../message/message.schema';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-@Injectable()
-export class RabbitMQService {
-  private readonly username: string = 'tms';
-  private readonly password: string = '26000567855499290979';
+> tms-api@0.0.1 start
+> nest start
 
-  constructor(
-    private readonly httpService: HttpService,
-    @InjectModel(MESSAGE_SCHEMA_NAME) private readonly messageModel: Model<MessageDocument>
-  ) {}
+src/rabbitmq/rabbitmq.service.ts:1:22 - error TS2724: '"@nestjs/common"' has no exported member named 'HttpService'.
+ Did you mean 'HttpServer'?
 
-  async readFromQueue(): Promise<QueueMessage> {
-    try {
-      const response = await this.httpService
-        .post(
-          'http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get',
-          {
-            count: 1,
-            ackmode: 'ack_requeue_true',
-            encoding: 'auto',
-            truncate: 50000,
-          },
-          {
-            headers: {
-              Authorization: `Basic ${Buffer.from(
-                `${this.username}:${this.password}`,
-              ).toString('base64')}`,
-              'Content-Type': 'application/json',
-            },
-          },
-        )
-        .toPromise();
+1 import { Injectable, HttpService } from '@nestjs/common';
+                       ~~~~~~~~~~~
+src/rabbitmq/rabbitmq.service.ts:5:54 - error TS2307: Cannot find module '../message/message.schema' or its correspo
+nding type declarations.
 
-      if (response.status === 200 && response.data.length > 0) {
-        const data = response.data[0];
-        const payload = JSON.parse(data.payload);
+5 import { MessageDocument, MESSAGE_SCHEMA_NAME } from '../message/message.schema';
+                                                       ~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-        // Сохраняем сообщение в базе данных
-        const message = new this.messageModel(payload);
-        await message.save();
+Found 2 error(s).
 
-        return payload;
-      } else {
-        throw new Error('Возникла ошибка при получении данных');
-      }
-    } catch (error) {
-      console.error(error);
-      throw error;
-    }
-  }
-
-  // Дополнительный метод для получения всех сообщений из MongoDB
-  async getAllMessages(): Promise<QueueMessage[]> {
-    return this.messageModel.find().exec();
-  }
-}
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
