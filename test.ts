834c68949075b82b101034cf3e5f509b150792dd
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:03:37 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,10 @@
-npm install --save @nestjs/mongoose mongoose amqplib amqp-connection-manager
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm install --save @nestjs/mongoose mongoos
+e amqplib amqp-connection-manager
+
+added 21 packages, and audited 750 packages in 33s
+
+118 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
