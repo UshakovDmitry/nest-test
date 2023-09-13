c8d548280a1037613018214a845843a9af0c6fa6
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 14:59:51 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,39 @@
-import { Module } from '@nestjs/common';
-
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-
-
-@Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
-
+{
+    "Number": "№№00015933",
+    "Date": "01.08.2023 9:01:14",
+    "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+    "DocumentStatus": "Доставляется",
+    "Driver": "Бархудар Асан Арыпұлы",
+    "ISR": "",
+    "Informal_Document": "Акт регистрации брака TL300000000014 от 01.04.2022 18:36:05",
+    "SKU_Weight": "17",
+    "ArrayStrings": [
+        {
+            "Shipping_Point": "Талгар 3, ул. Абылай хана, дом 111 (склад товара, ожидающего ремонта)",
+            "Goods": "Телевизор KIVI 55U710KB  Smart 4K UHD",
+            "Quantity": "1",
+            "Item_Status": "Оформлена",
+            "Pickup_Point": "1",
+            "Delivery_Point": "2",
+            "Pickup_Latitude": "43,305732",
+            "Pickup_Longitude": "77,241999",
+            "Delivery_Latitude": "43,245609",
+            "Delivery_Longitude": "76,90425",
+            "Pickup_Time": "01.01.0001 9:43:53",
+            "Delivery_Time": "01.01.0001 10:46:44"
+        }
+    ],
+    "ContactInformation": {
+        "City": "Алматы",
+        "Delivery_Condition": "Доставка",
+        "Date_Time_delivery": "2023-08-05 До 18:00",
+        "Time_Window": "09:00-18:00",
+        "Latitude": "43,245609",
+        "Longitude": "76,90425",
+        "Street": "нет данных",
+        "Home": "нет данных",
+        "Phone": "+7(778)021-1316",
+        "Apartment": "нет данных",
+        "Contractor": "нет данных"
+    }
+}
