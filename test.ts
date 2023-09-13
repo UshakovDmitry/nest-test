6b6b433d3e2cd9963fd9fd073c26a022d69cf0ea
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:02:31 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,39 +1,57 @@
-{
-    "Number": "№№00015933",
-    "Date": "01.08.2023 9:01:14",
-    "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-    "DocumentStatus": "Доставляется",
-    "Driver": "Бархудар Асан Арыпұлы",
-    "ISR": "",
-    "Informal_Document": "Акт регистрации брака TL300000000014 от 01.04.2022 18:36:05",
-    "SKU_Weight": "17",
-    "ArrayStrings": [
-        {
-            "Shipping_Point": "Талгар 3, ул. Абылай хана, дом 111 (склад товара, ожидающего ремонта)",
-            "Goods": "Телевизор KIVI 55U710KB  Smart 4K UHD",
-            "Quantity": "1",
-            "Item_Status": "Оформлена",
-            "Pickup_Point": "1",
-            "Delivery_Point": "2",
-            "Pickup_Latitude": "43,305732",
-            "Pickup_Longitude": "77,241999",
-            "Delivery_Latitude": "43,245609",
-            "Delivery_Longitude": "76,90425",
-            "Pickup_Time": "01.01.0001 9:43:53",
-            "Delivery_Time": "01.01.0001 10:46:44"
-        }
-    ],
-    "ContactInformation": {
-        "City": "Алматы",
-        "Delivery_Condition": "Доставка",
-        "Date_Time_delivery": "2023-08-05 До 18:00",
-        "Time_Window": "09:00-18:00",
-        "Latitude": "43,245609",
-        "Longitude": "76,90425",
-        "Street": "нет данных",
-        "Home": "нет данных",
-        "Phone": "+7(778)021-1316",
-        "Apartment": "нет данных",
-        "Contractor": "нет данных"
-    }
+// message.schema.ts
+import { Document, Schema } from 'mongoose';
+
+const ArrayStringSchema = new Schema({
+  Shipping_Point: String,
+  Goods: String,
+  Quantity: String,
+  Item_Status: String,
+  Pickup_Point: String,
+  Delivery_Point: String,
+  Pickup_Latitude: String,
+  Pickup_Longitude: String,
+  Delivery_Latitude: String,
+  Delivery_Longitude: String,
+  Pickup_Time: String,
+  Delivery_Time: String
+});
+
+const ContactInformationSchema = new Schema({
+  City: String,
+  Delivery_Condition: String,
+  Date_Time_delivery: String,
+  Time_Window: String,
+  Latitude: String,
+  Longitude: String,
+  Street: String,
+  Home: String,
+  Phone: String,
+  Apartment: String,
+  Contractor: String
+});
+
+export const MessageSchema = new Schema({
+  Number: String,
+  Date: String,
+  Organization: String,
+  DocumentStatus: String,
+  Driver: String,
+  ISR: String,
+  Informal_Document: String,
+  SKU_Weight: String,
+  ArrayStrings: [ArrayStringSchema],
+  ContactInformation: ContactInformationSchema
+});
+
+export interface Message extends Document {
+  Number: string;
+  Date: string;
+  Organization: string;
+  DocumentStatus: string;
+  Driver: string;
+  ISR: string;
+  Informal_Document: string;
+  SKU_Weight: string;
+  ArrayStrings: typeof ArrayStringSchema[];
+  ContactInformation: typeof ContactInformationSchema;
 }
