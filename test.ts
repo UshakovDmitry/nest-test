82e8a31684cb897bf70f95b70ea66890fcf08eec
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:34:21 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,50 +1,14 @@
 
-
-import { Injectable } from '@nestjs/common';
-import { Client, ClientProxy, Transport } from '@nestjs/microservices';
-
-@Injectable()
-export class RabbitMQService {
-  private client: ClientProxy;
-
-  constructor() {
-    this.client = new ClientProxy({
-      transport: Transport.RMQ,
-      options: {
-        urls: [`amqp://tms:26000567855499290979@rabbitmq.next.local`],
-        queue: 'TmsQueue',
-        queueOptions: { durable: false }
-      },
-    });
-  }
-
-  async readFromQueue(): Promise<any> {
-    // Просто отправьте сообщение на сервер
-    return this.client.send('get_message', {}).toPromise();
-  }
-}
-
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
 > tms-api@0.0.1 start
 > nest start
 
-src/rabbitmq/rabbitmq.module.ts:9:19 - error TS2511: Cannot create an instance of an abstract class.
-
-  9     this.client = new ClientProxy({
-                      ~~~~~~~~~~~~~~~~~
- 10       transport: Transport.RMQ,
-    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-...
- 15       },
-    ~~~~~~~~
- 16     });
-    ~~~~~~
-
-Found 1 error(s).
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
-
-
-
-      
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [NestFactory] Starting Nest application...
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] MongooseModule dependencies initialized +26ms
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] AppModule dependencies initialized +0ms
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +11ms
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] MongooseModule dependencies initialized +9ms
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [RoutesResolver] AppController {/}: +25ms
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [RouterExplorer] Mapped {/, GET} route +2ms
+[Nest] 19932  - 13.09.2023, 15:33:33     LOG [NestApplication] Nest application successfully started +2ms
