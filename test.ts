55f3bbfc564f7b91a25f902aafa0af517b361133
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:48:27 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MessageSchema } from './message/message.shema';
 
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
-> tms-api@0.0.1 start
-> nest start
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/rabbitmq'),
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
 
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [NestFactory] Starting Nest application...
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] MongooseModule dependencies initialized +26ms
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] AppModule dependencies initialized +0ms
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +11ms
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [InstanceLoader] MongooseModule dependencies initialized +9ms
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [RoutesResolver] AppController {/}: +25ms
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [RouterExplorer] Mapped {/, GET} route +2ms
-[Nest] 19932  - 13.09.2023, 15:33:33     LOG [NestApplication] Nest application successfully started +2ms
+
