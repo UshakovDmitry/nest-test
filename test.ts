3cf8c624f7b8d4ea279479ce71df771e7627d0d4
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:52:52 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,142 +1,28 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-export type MessageDocument = Message & Document;
-
-@Schema()
-export class ArrayString {
-  @Prop()
-  Shipping_Point: string;
-
-  @Prop()
-  Goods: string;
-
-  @Prop()
-  Quantity: string;
-
-  @Prop()
-  Item_Status: string;
-
-  @Prop()
-  Pickup_Point: string;
-
-  @Prop()
-  Delivery_Point: string;
-
-  @Prop()
-  Pickup_Latitude: string;
-
-  @Prop()
-  Pickup_Longitude: string;
-
-  @Prop()
-  Delivery_Latitude: string;
-
-  @Prop()
-  Delivery_Longitude: string;
-
-  @Prop()
-  Pickup_Time: string;
-
-  @Prop()
-  Delivery_Time: string;
-}
-
-@Schema()
-export class ContactInfo {
-  @Prop()
-  City: string;
-
-  @Prop()
-  Delivery_Condition: string;
-
-  @Prop()
-  Date_Time_delivery: string;
-
-  @Prop()
-  Time_Window: string;
-
-  @Prop()
-  Latitude: string;
-
-  @Prop()
-  Longitude: string;
-
-  @Prop()
-  Street: string;
-
-  @Prop()
-  Home: string;
-
-  @Prop()
-  Phone: string;
-
-  @Prop()
-  Apartment: string;
-
-  @Prop()
-  Contractor: string;
-}
-
-@Schema()
-export class Message {
-  @Prop()
-  Number: string;
-
-  @Prop()
-  Date: string;
-
-  @Prop()
-  Organization: string;
-
-  @Prop()
-  DocumentStatus: string;
-
-  @Prop()
-  Driver: string;
-
-  @Prop()
-  ISR: string;
-
-  @Prop()
-  Informal_Document: string;
-
-  @Prop()
-  SKU_Weight: string;
-
-  @Prop({ type: [ArrayString], default: [] })
-  ArrayStrings: ArrayString[];
-
-  @Prop()
-  ContactInformation: ContactInfo;
-}
-
-export const MessageSchema = SchemaFactory.createForClass(Message);
+> tms-api@0.0.1 start
+> nest start
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { Message, MessageSchema } from './message.entity';
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-  ],
-})
-export class DatabaseModule {}
+C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\schema.js:1412
+        throw new TypeError('Invalid schema configuration: ' +
+              ^
+TypeError: Invalid schema configuration: `ArrayString` is not a valid type within the array `ArrayStrings`.See https
+://bit.ly/mongoose-schematypes for a list of valid schema types.
+    at Schema.interpretAsType (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mon
+goose\lib\schema.js:1412:15)
+    at Schema.path (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\s
+chema.js:1065:27)
+    at Schema.add (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\sc
+hema.js:784:14)
+    at new Schema (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\sc
+hema.js:137:10)
+    at Function.createForClass (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@n
+estjs\mongoose\dist\factories\schema.factory.js:12:16)
+    at Object.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\database\message
+.entity.ts:114:44)
+    at Module._compile (node:internal/modules/cjs/loader:1103:14)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
+    at Module.load (node:internal/modules/cjs/loader:981:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
