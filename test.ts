53f3632bc6002c1a6896234c8a7bf46ea32c1557
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:56:50 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,12 +1,19 @@
-// src/rabbitmq/rabbitmq.module.ts
-import { Module, HttpModule } from '@nestjs/common';
-import { RabbitMQService } from './rabbitmq.service';
-import { RabbitMQController } from './rabbitmq.controller';
-import { DatabaseModule } from '../database/database.module';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-@Module({
-  imports: [HttpModule, DatabaseModule],
-  providers: [RabbitMQService],
-  controllers: [RabbitMQController],
-})
-export class RabbitMQModule {}
+> tms-api@0.0.1 start
+> nest start
+
+src/rabbitmq/rabbitmq.module.ts:28:18 - error TS2724: '"@nestjs/common"' has no exported member named 'HttpModule'.
+Did you mean 'HttpCode'?
+
+28 import { Module, HttpModule } from '@nestjs/common';
+                    ~~~~~~~~~~
+
+  node_modules/@nestjs/common/decorators/http/http-code.decorator.d.ts:11:25
+    11 export declare function HttpCode(statusCode: number): MethodDecorator;
+                               ~~~~~~~~
+    'HttpCode' is declared here.
+
+Found 1 error(s).
+
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
