72e4236b3ba48ae5b8430e112b182305819548a8
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:24:51 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,110 +1,41 @@
-// message.schema.ts
-import { Document, Schema } from 'mongoose';
-
-const ArrayStringSchema = new Schema({
-  Shipping_Point: String,
-  Goods: String,
-  Quantity: String,
-  Item_Status: String,
-  Pickup_Point: String,
-  Delivery_Point: String,
-  Pickup_Latitude: String,
-  Pickup_Longitude: String,
-  Delivery_Latitude: String,
-  Delivery_Longitude: String,
-  Pickup_Time: String,
-  Delivery_Time: String
-});
-
-const ContactInformationSchema = new Schema({
-  City: String,
-  Delivery_Condition: String,
-  Date_Time_delivery: String,
-  Time_Window: String,
-  Latitude: String,
-  Longitude: String,
-  Street: String,
-  Home: String,
-  Phone: String,
-  Apartment: String,
-  Contractor: String
-});
-
-export const MessageSchema = new Schema({
-  Number: String,
-  Date: String,
-  Organization: String,
-  DocumentStatus: String,
-  Driver: String,
-  ISR: String,
-  Informal_Document: String,
-  SKU_Weight: String,
-  ArrayStrings: [ArrayStringSchema],
-  ContactInformation: ContactInformationSchema
-});
-
-export interface Message extends Document {
-  Number: string;
-  Date: string;
-  Organization: string;
-  DocumentStatus: string;
-  Driver: string;
-  ISR: string;
-  Informal_Document: string;
-  SKU_Weight: string;
-  ArrayStrings: typeof ArrayStringSchema[];
-  ContactInformation: typeof ContactInformationSchema;
-}
-
-
-
-// message.service.ts
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Message } from './message.schema';
-
-@Injectable()
-export class MessageService {
-  constructor(@InjectModel('Message') private messageModel: Model<Message>) {}
-
-  async create(messageDto: any): Promise<Message> {
-    const createdMessage = new this.messageModel(messageDto);
-    return createdMessage.save();
-  }
-}
-
-
-
-
-
-// app.module.ts
-@Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }])
-  ],
-  ...
-})
-
-
-  PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
 > tms-api@0.0.1 start
 > nest start
 
-src/app.module.ts:5:31 - error TS2307: Cannot find module './message/message.schema' or its corresponding type decla
-rations.
+src/rabbitmq/rabbitmq.service.ts:11:19 - error TS2350: Only a void function can be called with the 'new' keyword.
 
-5 import { MessageSchema } from './message/message.schema';
-                                ~~~~~~~~~~~~~~~~~~~~~~~~~~
-src/message/message.service.ts:4:25 - error TS2307: Cannot find module './message.schema' or its corresponding type
-declarations.
+ 11     this.client = new Client({
+                      ~~~~~~~~~~~~
+ 12       transport: Transport.RMQ,
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+...
+ 21       },
+    ~~~~~~~~
+ 22     });
+    ~~~~~~
+src/rabbitmq/rabbitmq.service.ts:25:34 - error TS2304: Cannot find name 'QueueMessage'.
 
-4 import { Message } from './message.schema';
-                          ~~~~~~~~~~~~~~~~~~
+25   async readFromQueue(): Promise<QueueMessage> {
+                                    ~~~~~~~~~~~~
+src/rabbitmq/rabbitmq.service.ts:25:34 - error TS4055: Return type of public method from exported class has or is us
+ing private name 'QueueMessage'.
 
-Found 2 error(s).
+25   async readFromQueue(): Promise<QueueMessage> {
+                                    ~~~~~~~~~~~~
+src/rabbitmq/rabbitmq.service.ts:26:29 - error TS2304: Cannot find name 'QueueMessage'.
 
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
-  
+26     return this.client.send<QueueMessage, void>('get_message', {}).toPromise();
+                               ~~~~~~~~~~~~
+src/rabbitmq/rabbitmq.service.ts:26:64 - error TS2345: Argument of type '{}' is not assignable to parameter of type
+'void'.
+
+26     return this.client.send<QueueMessage, void>('get_message', {}).toPromise();
+                                                                  ~~
+src/rabbitmq/rabbitmq.service.ts:25:34 - error TS4055: Return type of public method from exported class has or is us
+ing private name 'QueueMessage'.
+
+25   async readFromQueue(): Promise<QueueMessage> {
+                                    ~~~~~~~~~~~~
+
+Found 6 error(s).
