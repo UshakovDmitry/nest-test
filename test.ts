7e3c3089389a76e127c098d211ab33696bdca496
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 14:52:03 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,2 +1,14 @@
-MongooseModule.forRoot('mongodb://localhost:27017/yourdbname'),
+import { Module } from '@nestjs/common';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+
+
+@Module({
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
 
