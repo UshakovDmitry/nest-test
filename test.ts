3f88f0d6b2e4d74aeb2067176da2e5cf55618900
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 14:33:30 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,49 +1,38 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-> tms-api@0.0.1 start
-> nest start
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest', { useNewUrlParser: true }),
+    // другие модули
+  ],
+  // providers и exports если они есть
+})
+export class DatabaseModule {}
 
-[Nest] 2944  - 13.09.2023, 12:59:41     LOG [NestFactory] Starting Nest application...
-[Nest] 2944  - 13.09.2023, 12:59:41     LOG [InstanceLoader] DatabaseModule dependencies initialized +29ms
-[Nest] 2944  - 13.09.2023, 12:59:41     LOG [InstanceLoader] TypeOrmModule dependencies initialized +1ms
-[Nest] 2944  - 13.09.2023, 12:59:41   ERROR [ExceptionHandler] Nest can't resolve dependencies of the MessageModel (
-?). Please make sure that the argument DatabaseConnection at index [0] is available in the MongooseModule context.
 
-Potential solutions:
-- Is MongooseModule a valid NestJS module?
-- If DatabaseConnection is a provider, is it part of the current MongooseModule?
-- If DatabaseConnection is exported from a separate @Module, is that module imported within MongooseModule?
-  @Module({
-    imports: [ /* the Module containing DatabaseConnection */ ]
-  })
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MessageSchema } from './message.entity';
+import { MessageService } from './message.service';
 
-Error: Nest can't resolve dependencies of the MessageModel (?). Please make sure that the argument DatabaseConnectio
-n at index [0] is available in the MongooseModule context.
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }])],
+  providers: [MessageService],
+  exports: [MessageService],
+})
+export class MessageModule {}
 
-Potential solutions:
-- Is MongooseModule a valid NestJS module?
-- If DatabaseConnection is a provider, is it part of the current MongooseModule?
-- If DatabaseConnection is exported from a separate @Module, is that module imported within MongooseModule?
-  @Module({
-    imports: [ /* the Module containing DatabaseConnection */ ]
-  })
 
-    at Injector.lookupComponentInParentModules (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend
-\node_modules\@nestjs\core\injector\injector.js:254:19)
-    at Injector.resolveComponentInstance (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_
-modules\@nestjs\core\injector\injector.js:207:33)
-    at resolveParam (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\
-injector\injector.js:128:38)
-    at async Promise.all (index 0)
-    at Injector.resolveConstructorParams (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_
-modules\@nestjs\core\injector\injector.js:143:27)
-    at Injector.loadInstance (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nes
-tjs\core\injector\injector.js:70:13)
-    at Injector.loadProvider (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nes
-tjs\core\injector\injector.js:97:9)
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\injector\insta
-nce-loader.js:56:13
-    at async Promise.all (index 3)
-    at InstanceLoader.createInstancesOfProviders (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backe
-nd\node_modules\@nestjs\core\injector\instance-loader.js:55:9)
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
+
+
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Message } from './message.entity';
+
+@Injectable()
+export class MessageService {
+  constructor(@InjectModel('Message') private readonly messageModel: Model<Message>) {}
+  // остальные методы
+}
