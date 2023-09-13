7d2cb2e30823487af4e4b37f9afec26e015bda45
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 16:09:52 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,43 +1,23 @@
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
 > tms-api@0.0.1 start
 > nest start
 
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [NestFactory] Starting Nest application...
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [InstanceLoader] MongooseModule dependencies initialized +26ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [InstanceLoader] ClientsModule dependencies initialized +1ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +9ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [InstanceLoader] MongooseModule dependencies initialized +6ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [InstanceLoader] AppModule dependencies initialized +6ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [RoutesResolver] RabbitMQController {/all-messages}: +25ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [RouterExplorer] Mapped {/all-messages, GET} route +2ms
-[Nest] 20628  - 13.09.2023, 16:06:45     LOG [NestApplication] Nest application successfully started +3ms
-[Nest] 20628  - 13.09.2023, 16:06:45   ERROR [ClientProxy] Disconnected from RMQ. Trying to reconnect.
-[Nest] 20628  - 13.09.2023, 16:06:45   ERROR [ClientProxy] Object:
-{
-  "err": {
-    "code": 406,
-    "classId": 50,
-    "methodId": 10
-  }
-}
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [NestFactory] Starting Nest application...
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [InstanceLoader] MongooseModule dependencies initialized +27ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [InstanceLoader] ClientsModule dependencies initialized +0ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +9ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [InstanceLoader] AppModule dependencies initialized +6ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [RoutesResolver] RabbitMQController {/all-messages}: +24ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [RouterExplorer] Mapped {/all-messages, GET} route +3ms
+[Nest] 1948  - 13.09.2023, 16:08:36     LOG [NestApplication] Nest application successfully started +3ms
 
 
-C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\amqplib\lib\channel.js:133
-      var closeFrameError = new Error(e);
-                            ^
-Error: Operation failed: QueueDeclare; 406 (PRECONDITION-FAILED) with message "PRECONDITION_FAILED - inequivalent ar
-g 'durable' for queue 'TmsQueue' in vhost '/': received 'false' but current is 'true'"
-    at reply (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\amqplib\lib\channel.
-js:133:29)
-    at ConfirmChannel.C.accept (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\am
-qplib\lib\channel.js:416:7)
-    at Connection.mainAccept (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\amqp
-lib\lib\connection.js:63:33)
-    at Socket.go (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\amqplib\lib\conn
-ection.js:486:48)
-    at Socket.emit (node:events:526:28)
-    at emitReadable_ (node:internal/streams/readable:578:12)
-    at processTicksAndRejections (node:internal/process/task_queues:82:21)
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
+
+
+как теперь следить за тем что записывается в бд?
+в compass она пустая 
+                                                                                 
