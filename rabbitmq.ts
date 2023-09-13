60994b7848c69209dd4b366dc1c230b3cb76850b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 10:32:29 +0600

    Update rabbitmq.ts

--- rabbitmq.ts
+++ rabbitmq.ts
@@ -1,1 +1,93 @@
+$ npm i --save amqplib amqp-connection-manager @nestjs/microservices
+
+
+// MODULE.TS
+import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq.service';
+import { RabbitMQController } from './rabbitmq.controller'; 
+import { HttpModule } from '@nestjs/axios';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+
+@Module({
+  imports: [
+    HttpModule.register({}),
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local:5672'],
+          queue: 'TmsQueue',
+          queueOptions: {
+            durable: true
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [RabbitMQController], 
+  providers: [RabbitMQService],
+})
+export class RabbitMQModule {}
+
+
+
+
+
+// SERVICE.TS
+import { Injectable } from '@nestjs/common';
+import { Client, ClientProxy, Transport, ClientProxyFactory } from '@nestjs/microservices';
+import { QueueMessage } from './rabbitmq.interface';
+
+@Injectable()
+export class RabbitMQService {
+  @Client({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local:5672'],
+      queue: 'TmsQueue',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  })
+  client: ClientProxy;
+
+  async readFromQueue(): Promise<QueueMessage> {
+    return this.client.send<QueueMessage>('read', {}).toPromise();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { RabbitMQService } from './rabbitmq.service';
+import { QueueMessage } from './rabbitmq.interface';
+
+@Controller('rabbitmq')
+export class RabbitMQController {
+  constructor(private readonly rabbitMQService: RabbitMQService) {}
+
+  @Get('read')
+  async readFromQueue() {
+    return await this.rabbitMQService.readFromQueue();
+  }
+
+  @MessagePattern('read')
+  handleMessage(@Payload() data: QueueMessage) {
+    console.log(data); // ваше сообщение из очереди RabbitMQ
+    return data; 
+  }
+}
 
