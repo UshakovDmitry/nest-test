b7f5c3e38d7c9fd6b06024ee9c379f9f5cc4e6c6
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:51:02 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,18 +1,33 @@
-import { Module } from '@nestjs/common';
-import { MessageSchema } from './message/message.shema';
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Message } from './message.interface';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+@Injectable()
+export class MessageService {
+  constructor(@InjectModel('Message') private readonly messageModel: Model<Message>) {}
 
-@Module({
-  imports: [
-    MongooseModule.forRoot('mongodb://localhost/rabbitmq'),
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-  ],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
+  async create(data: any): Promise<Message> {
+    const createdMessage = new this.messageModel(data);
+    return createdMessage.save();
+  }
+
+  async findAll(): Promise<Message[]> {
+    return this.messageModel.find().exec();
+  }
+}
+
+
+
+
+
+import { Document } from 'mongoose';
+
+export interface Message extends Document {
+  // ваша структура данных, например:
+  content: string;
+  timestamp: Date;
+  // добавьте дополнительные поля при необходимости
+}
 
 
