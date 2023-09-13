9edf27d02f8bc38a9c9c17793acc5bd2613043dc
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:49:51 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,27 +1,115 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
-
-> tms-api@0.0.1 start
-> nest start
+{
 
 
-C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\reflect-metadata\Reflect.js:553
-                var decorated = decorator(target, propertyKey, descriptor);
-                                ^
-Error: Cannot determine a type for the "Message.ContactInformation" field (union/intersection/ambiguous type was use
-d). Make sure your property is decorated with a "@Prop({ type: TYPE_HERE })" decorator.
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\mongoose\dist\decor
-ators\prop.decorator.js:21:23
-    at DecorateProperty (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\reflect-m
-etadata\Reflect.js:553:33)
-    at Reflect.decorate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\reflect-m
-etadata\Reflect.js:123:24)
-    at __decorate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\dist\database\message.entity
-.js:4:92)
-    at Object.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\database\message
-.entity.ts:36:3)
-    at Module._compile (node:internal/modules/cjs/loader:1103:14)
-    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
-    at Module.load (node:internal/modules/cjs/loader:981:32)
-    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
-    at Module.require (node:internal/modules/cjs/loader:1005:19)
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
+	"Number": "№№00015933",
+
+
+	"Date": "01.08.2023 9:01:14",
+
+
+	"Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+
+
+	"DocumentStatus": "Доставляется",
+
+
+	"Driver": "Бархудар Асан Арыпұлы",
+
+
+	"ISR": "",
+
+
+	"Informal_Document": "Акт регистрации брака TL300000000014 от 01.04.2022 18:36:05",
+
+
+	"SKU_Weight": "17",
+
+
+	"ArrayStrings": [
+
+
+		{
+
+
+			"Shipping_Point": "Талгар 3, ул. Абылай хана, дом 111 (склад товара, ожидающего ремонта)",
+
+
+			"Goods": "Телевизор KIVI 55U710KB  Smart 4K UHD",
+
+
+			"Quantity": "1",
+
+
+			"Item_Status": "Оформлена",
+
+
+			"Pickup_Point": "1",
+
+
+			"Delivery_Point": "2",
+
+
+			"Pickup_Latitude": "43,305732",
+
+
+			"Pickup_Longitude": "77,241999",
+
+
+			"Delivery_Latitude": "43,245609",
+
+
+			"Delivery_Longitude": "76,90425",
+
+
+			"Pickup_Time": "01.01.0001 9:43:53",
+
+
+			"Delivery_Time": "01.01.0001 10:46:44"
+
+
+		}
+
+
+	],
+
+
+	"ContactInformation": {
+
+
+		"City": "Алматы",
+
+
+		"Delivery_Condition": "Доставка",
+
+
+		"Date_Time_delivery": "2023-08-05 До 18:00",
+
+
+		"Time_Window": "09:00-18:00",
+
+
+		"Latitude": "43,245609",
+
+
+		"Longitude": "76,90425",
+
+
+		"Street": "нет данных",
+
+
+		"Home": "нет данных",
+
+
+		"Phone": "+7(778)021-1316",
+
+
+		"Apartment": "нет данных",
+
+
+		"Contractor": "нет данных"
+
+
+	}
+
+
+}
