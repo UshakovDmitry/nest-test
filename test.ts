9fd5ed72503f6242a5517b9ebc20bb705f338c01
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 12:35:51 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,15 +1,36 @@
-...
-import { DatabaseService } from '../database/database.service';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-@ApiTags('rabbitmq')
-@Controller('/api/rabbitmq')
-export class RabbitMQController {
-  constructor(private readonly rabbitMQService: RabbitMQService, private readonly dbService: DatabaseService) {}
+> tms-api@0.0.1 start
+> nest start
 
-  ...
+src/rabbitmq/rabbitmq.service.ts:14:9 - error TS2552: Cannot find name 'response'. Did you mean 'Response'?
 
-  @Get('/all-messages')
-  async getAllMessages() {
-    return await this.dbService.findAll();
-  }
-}
+14     if (response.status === 200 && response.data.length > 0) {
+           ~~~~~~~~
+
+  node_modules/typescript/lib/lib.dom.d.ts:18813:13
+    18813 declare var Response: {
+                      ~~~~~~~~
+    'Response' is declared here.
+src/rabbitmq/rabbitmq.service.ts:14:36 - error TS2552: Cannot find name 'response'. Did you mean 'Response'?
+
+14     if (response.status === 200 && response.data.length > 0) {
+                                      ~~~~~~~~
+
+  node_modules/typescript/lib/lib.dom.d.ts:18813:13
+    18813 declare var Response: {
+                      ~~~~~~~~
+    'Response' is declared here.
+src/rabbitmq/rabbitmq.service.ts:15:20 - error TS2552: Cannot find name 'response'. Did you mean 'Response'?
+
+15       const data = response.data[0];
+                      ~~~~~~~~
+
+  node_modules/typescript/lib/lib.dom.d.ts:18813:13
+    18813 declare var Response: {
+                      ~~~~~~~~
+    'Response' is declared here.
+
+Found 3 error(s).
+
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
