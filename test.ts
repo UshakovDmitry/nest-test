f35136dfbb81e964a5f3ea096788f46f3ecf0b37
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-13 15:29:06 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,6 +3,17 @@ PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run
 > tms-api@0.0.1 start
 > nest start
 
+src/rabbitmq/rabbitmq.module.ts:9:19 - error TS2511: Cannot create an instance of an abstract class.
+
+  9     this.client = new ClientProxy({
+                      ~~~~~~~~~~~~~~~~~
+ 10       transport: Transport.RMQ,
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+...
+ 15       },
+    ~~~~~~~~
+ 16     });
+    ~~~~~~
 src/rabbitmq/rabbitmq.service.ts:11:19 - error TS2350: Only a void function can be called with the 'new' keyword.
 
  11     this.client = new Client({
@@ -14,28 +25,12 @@ src/rabbitmq/rabbitmq.service.ts:11:19 - error TS2350: Only a void function can 
     ~~~~~~~~
  22     });
     ~~~~~~
-src/rabbitmq/rabbitmq.service.ts:25:34 - error TS2304: Cannot find name 'QueueMessage'.
-
-25   async readFromQueue(): Promise<QueueMessage> {
-                                    ~~~~~~~~~~~~
-src/rabbitmq/rabbitmq.service.ts:25:34 - error TS4055: Return type of public method from exported class has or is us
-ing private name 'QueueMessage'.
-
-25   async readFromQueue(): Promise<QueueMessage> {
-                                    ~~~~~~~~~~~~
-src/rabbitmq/rabbitmq.service.ts:26:29 - error TS2304: Cannot find name 'QueueMessage'.
-
-26     return this.client.send<QueueMessage, void>('get_message', {}).toPromise();
-                               ~~~~~~~~~~~~
-src/rabbitmq/rabbitmq.service.ts:26:64 - error TS2345: Argument of type '{}' is not assignable to parameter of type
+src/rabbitmq/rabbitmq.service.ts:26:55 - error TS2345: Argument of type '{}' is not assignable to parameter of type
 'void'.
 
-26     return this.client.send<QueueMessage, void>('get_message', {}).toPromise();
-                                                                  ~~
-src/rabbitmq/rabbitmq.service.ts:25:34 - error TS4055: Return type of public method from exported class has or is us
-ing private name 'QueueMessage'.
+26     return this.client.send<any, void>('get_message', {}).toPromise();
+                                                         ~~
 
-25   async readFromQueue(): Promise<QueueMessage> {
-                                    ~~~~~~~~~~~~
+Found 3 error(s).
 
-Found 6 error(s).
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
