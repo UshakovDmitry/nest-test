21da9792a8842fd3cb795fea7a9b2f85fbaa0b23
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 12:47:18 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,30 +1,22 @@
-// message.module.ts
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MessageService } from './message.service';
-import { MessageSchema } from './message.schema';
+ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-3)
+$ npm run start
 
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }])
-  ],
-  providers: [MessageService],
-  exports: [MessageService]  // Убедитесь, что сервис экспортируется
-})
-export class MessageModule {}
+> tms-api@0.0.1 start
+> nest start
 
-
-
-
-// rabbitmq.module.ts
-import { Module } from '@nestjs/common';
-import { RabbitMQService } from './rabbitmq.service';
-import { RabbitMQController } from './rabbitmq.controller';
-import { MessageModule } from '../message/message.module';  // Добавьте этот импорт
-
-@Module({
-  imports: [MessageModule],  // Добавьте этот импорт в массив
-  providers: [RabbitMQService, RabbitMQController],
-})
-export class RabbitMQModule {}
-
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [NestFactory] Starting Nest application...
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MongooseModule dependencies initialized +32ms
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] ClientsModule dependencies initialized +1ms
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +10ms
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MessageModule dependencies initialized +1ms
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
+[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] AppModule dependencies initialized +1ms
+[Nest] 18168  - 14.09.2023, 12:46:10     LOG [NestMicroservice] Nest microservice successfully started +87ms
+Microservices started
+[Nest] 18168  - 14.09.2023, 12:46:10     LOG [RoutesResolver] RabbitMQController {/all-messages}: +16ms
+[Nest] 18168  - 14.09.2023, 12:46:10     LOG [RouterExplorer] Mapped {/all-messages, GET} route +3ms
+[Nest] 18168  - 14.09.2023, 12:46:10     LOG [NestApplication] Nest application successfully started +2ms
+Application is listening on port 4000
+[Nest] 18168  - 14.09.2023, 12:46:58   ERROR [Server] There is no matching event handler defined in the remote s
+ervice. Event pattern: undefined
