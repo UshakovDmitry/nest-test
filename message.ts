cecd9a2458af2098b28a72b9b55599610ba6cc49
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 12:12:27 +0600

    Update message.ts

--- message.ts
+++ message.ts
@@ -1,5 +1,58 @@
-SWAGGER_USER=tms
-SWAGGER_PASSWORD=123456789
-RABBITMQ_URL=amqp://tms:26000567855499290979@rabbitmq.next.local
-PORT=4000
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+import * as compression from 'compression';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { config } from 'dotenv';
+
+// Инициализация dotenv
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || ''],
+      queue: 'TmsQueue',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  // SWAGGER CONFIGURATION
+  app.use(
+    ['/swagger', '/swagger-stats'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER || '']: process.env.SWAGGER_PASSWORD || '',
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Описание всех контроллеров REST API')
+    .setDescription(
+      'Внимание! Некоторые методы могут изменять данные в базе данных!',
+    )
+    .setVersion('1.0')
+    .addTag('API для TMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.enableCors();
+  app.use(compression());
+
+  await app.startAllMicroservicesAsync();
+  console.log('Microservices started');
+
+  const port = parseInt(process.env.PORT || '4000', 10);
+  await app.listen(port);
+  console.log(`Application is listening on port ${port}`);
+}
+bootstrap();
 
