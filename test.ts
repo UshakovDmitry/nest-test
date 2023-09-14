b2d050c09e71b81e8af3e11a51cc4abed21d9a80
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 12:25:20 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,46 @@
-вот как выглядит консоль когда ссобщений нет в очереди
-ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-3)
-$ npm run start
+import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq.service';
+import { MessageService } from '../message/message.service';
+import { RabbitMQController } from './rabbitmq.controller';
 
-> tms-api@0.0.1 start
-> nest start
-
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [NestFactory] Starting Nest application...
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] MongooseModule dependencies initialized +27ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] ClientsModule dependencies initialized +0ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +9ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] AppModule dependencies initialized +1ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [NestMicroservice] Nest microservice successfully started +85ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [RoutesResolver] RabbitMQController {/all-messages}: +16ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [RouterExplorer] Mapped {/all-messages, GET} route +3ms
-[Nest] 21496  - 14.09.2023, 11:54:24     LOG [NestApplication] Nest application successfully started +2ms
+@Module({
+  providers: [RabbitMQService, MessageService, RabbitMQController],
+})
+export class RabbitMQModule {}
 
 
-и вот что происходит когда в очереди появляется сообщение
 
-[Nest] 21496  - 14.09.2023, 11:55:32   ERROR [Server] There is no matching event handler defined in the remote service. Event pattern: und
-efined
+
+import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq/rabbitmq.service';
+import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
+import { MessageService } from './message/message.service';
+import { MessageSchema } from './message/message.shema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { connectMongoose } from './connect-mongoose';
+import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+
+@Module({
+  imports: [
+    RabbitMQModule,
+    MongooseModule.forRoot(connectMongoose()),
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
+          queue: 'TmsQueue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [RabbitMQController],
+  providers: [RabbitMQService, MessageService],
+})
+export class AppModule {}
