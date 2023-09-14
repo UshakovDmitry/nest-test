ff1936e59881448c6a55699f90190c08c16cb840
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 13:39:59 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,40 +1,108 @@
-ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-
-3)
-$ npm run start
+Overview
+Queued messages last minute 
+13:38:4013:38:5013:39:0013:39:1013:39:2013:39:300.01.0
+Ready
+0
+Unacked
+0
+Total
+0
+Message rates last minute 
+13:38:3013:38:4013:38:5013:39:0013:39:1013:39:200.0 /s1.0 /s
+Publish
+0.00/s
+Deliver (manual ack)
+0.00/s
+Deliver (auto ack)
+0.00/s
+Consumer ack
+0.00/s
+Redelivered
+0.00/s
+Get (manual ack)
+0.00/s
+Get (auto ack)
+0.00/s
+Get (empty)
+0.00/s
+Details
+Features	
+arguments:	
+x-queue-type:	classic
+durable:	true
+Policy	ha-fed
+Operator policy	
+Effective policy definition	
+federation-upstream-set:	all
+ha-mode:	exactly
+ha-params:	5
+ha-sync-mode:	automatic
+message-ttl:	3465687905
+Node	rabbit@dkrclstrnd1
+Mirrors	rabbit@dkrclstrnd4
+rabbit@dkrclstrnd3
+rabbit@dkrclstrnd2
+rabbit@dkrclstrnd5
+State	idle
+Consumers	1
+Consumer capacity 	100%
+Total	Ready	Unacked	In memory	Persistent	Transient, Paged Out
+Messages 	0	0	0	0	0	0
+Message body bytes 	0 B	0 B	0 B	0 B	0 B	0 B
+Process memory 	17 KiB
+Consumers (1)
+Channel 	Consumer tag	Ack required	Exclusive	Prefetch count	Active 	Activity status	Arguments
+10.0.0.138:57162 (1)
+amq.ctag-efltliz6jnNjcsKmhKMTnA	○	○	0	●	up	
+Bindings (2)
+From	Routing key	Arguments	
+(Default exchange binding)
+TmsExchange
+tms1c	
+⇓
 
-> tms-api@0.0.1 start
-> nest start
+This queue
 
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [NestFactory] Starting Nest application...
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] MongooseModule dependenci
-es initialized +29ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] ClientsModule dependencie
-s initialized +0ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] MongooseCoreModule depend
-encies initialized +9ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] MongooseModule dependenci
-es initialized +7ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] MessageModule dependencie
-s initialized +0ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] RabbitMQModule dependenci
-es initialized +1ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [InstanceLoader] AppModule dependencies in
-itialized +0ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [NestMicroservice] Nest microservice succe
-ssfully started +83ms
-Microservices started
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [RoutesResolver] RabbitMQController {/all-
-messages}: +16ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [RouterExplorer] Mapped {/all-messages, GE
-T} route +3ms
-[Nest] 23228  - 14.09.2023, 13:29:33     LOG [NestApplication] Nest application success
-fully started +2ms
-Application is listening on port 4000
-[Nest] 23228  - 14.09.2023, 13:29:33   ERROR [Server] There is no matching event handle
-r defined in the remote service. Event pattern: undefined
-[Nest] 23228  - 14.09.2023, 13:31:35   ERROR [Server] There is no matching event handle
-r defined in the remote service. Event pattern: undefined
-[Nest] 23228  - 14.09.2023, 13:31:43   ERROR [Server] There is no matching event handle
-r defined in the remote service. Event pattern: undefined
-[Nest] 23228  - 14.09.2023, 13:32:03   ERROR [Server] There is no matching event handle
-r defined in the remote service. Event pattern: undefined
+Add binding to this queue
+From exchange:
+ *
+Routing key:
+Arguments:
+=	
+
+String
+Publish message
+Message will be published to the default exchange with routing key TmsQueue, routing it to this queue.
+Delivery mode:
+
+1 - Non-persistent
+Headers: 
+=	
+
+String
+Properties: 
+=	
+Payload:
+Get messages
+Warning: getting messages from a queue is a destructive action. 
+
+Ack Mode:
+
+Nack message requeue true
+Encoding:
+
+Auto string / base64
+ 
+Messages:
+1
+Delete
+Purge
+Runtime Metrics (Advanced)
+Reductions (per second) last minute 
+13:38:3013:38:4013:38:5013:39:0013:39:1013:39:200.0 /s1.0 /s
+Reductions
+0.00/s
+Minimum binary virtual heap size in words (min_bin_vheap_size)	46422
+Minimum heap size in words (min_heap_size)	233
+Maximum generational collections before fullsweep (fullsweep_after)	65535
+Number of minor GCs (minor_gcs)	22418
