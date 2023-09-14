836f74024645568b53ffe8fd9023123d310e0be8
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 09:54:56 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,30 +1,61 @@
-import { Injectable } from '@nestjs/common';
-import { EventPattern, Payload, Ctx, RmqContext, MessagePattern } from '@nestjs/microservices';
-import { MessageService } from '../message/message.service';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
 
-@Injectable()
-export class RabbitMQService {
-  constructor(private messageService: MessageService) {}
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+import * as compression from 'compression';
 
-  @EventPattern('get_message')
-  async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
-    const channel = context.getChannelRef();
-    const originalMsg = context.getMessage();
-    await this.messageService.create(data);
-    channel.ack(originalMsg);
-  }
+process.env.SWAGGER_USER = 'tms';
+process.env.SWAGGER_PASSWORD = '123456789';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // -- SWAGGER -- //
+  app.use(
+    ['/swagger', '/swagger-stats'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Описание всех контроллеров REST API')
+    .setDescription(
+      'Внимание! Некоторые методы могут изменять данные в базе данных!',
+    )
+    .setVersion('1.0')
+    .addTag('API для TMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  // -- CORS -- //
+  app.enableCors();
+  // -- COMPRESS -- //
+  app.use(compression());
+  // -- LISTEN -- //
+  await app.listen(4000);
 }
+bootstrap();
+
 
 
 
 
 import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq/rabbitmq.service';
+import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
+import { MessageService } from './message/message.service';
+import { MessageSchema } from './message/message.shema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsModule, Transport } from '@nestjs/microservices';
-import { RabbitMQService } from './rabbitmq.service';
-import { MessageService } from '../message/message.service';
+import { connectMongoose } from './connect-mongoose';
 
 @Module({
   imports: [
+    MongooseModule.forRoot(connectMongoose()),
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
     ClientsModule.register([
       {
         name: 'RABBITMQ_SERVICE',
@@ -32,12 +63,52 @@ import { MessageService } from '../message/message.service';
         options: {
           urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
           queue: 'TmsQueue',
-          queueOptions: { durable: true },
+          queueOptions: {
+            durable: true,
+          },
         },
       },
     ]),
   ],
+  controllers: [RabbitMQController],
   providers: [RabbitMQService, MessageService],
 })
-export class RabbitMQModule {}
+export class AppModule {}
+
+
+
+
+import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq/rabbitmq.service';
+import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
+import { MessageService } from './message/message.service';
+import { MessageSchema } from './message/message.shema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { connectMongoose } from './connect-mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(connectMongoose()),
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
+          queue: 'TmsQueue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [RabbitMQController],
+  providers: [RabbitMQService, MessageService],
+})
+export class AppModule {}
+
+
 
