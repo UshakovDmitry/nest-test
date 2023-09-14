451d1e3cf14852c1b037943daf5ef6e737f634a7
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 12:50:32 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,22 +1,14 @@
-ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-3)
-$ npm run start
 
-> tms-api@0.0.1 start
-> nest start
+Ack Mode: Nack message requeue true
+Encoding: Auto string / base64
+Messages: 1
 
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [NestFactory] Starting Nest application...
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MongooseModule dependencies initialized +32ms
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] ClientsModule dependencies initialized +1ms
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +10ms
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] MessageModule dependencies initialized +1ms
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
-[Nest] 18168  - 14.09.2023, 12:46:09     LOG [InstanceLoader] AppModule dependencies initialized +1ms
-[Nest] 18168  - 14.09.2023, 12:46:10     LOG [NestMicroservice] Nest microservice successfully started +87ms
-Microservices started
-[Nest] 18168  - 14.09.2023, 12:46:10     LOG [RoutesResolver] RabbitMQController {/all-messages}: +16ms
-[Nest] 18168  - 14.09.2023, 12:46:10     LOG [RouterExplorer] Mapped {/all-messages, GET} route +3ms
-[Nest] 18168  - 14.09.2023, 12:46:10     LOG [NestApplication] Nest application successfully started +2ms
-Application is listening on port 4000
-[Nest] 18168  - 14.09.2023, 12:46:58   ERROR [Server] There is no matching event handler defined in the remote s
-ervice. Event pattern: undefined
+
+
+Warning: getting messages from a queue is a destructive action. 
+Clicking "Get Message(s)" will consume messages from the queue. If requeue is set the message will be put back into the queue in place, but "redelivered" will be set on the message.
+
+If requeue is not set messages will be removed from the queue.
+
+Furthermore, message payloads will be truncated to 50000 bytes.
+
