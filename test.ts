36a30988b0ccb9825f6c318ee437a7212af6a669
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 12:53:45 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,140 @@
+Message 1
+The server reported 0 messages remaining.
 
-Ack Mode: Nack message requeue true
-Encoding: Auto string / base64
-Messages: 1
+Exchange	TmsExchange
+Routing Key	tms1c
+Redelivered	○
+Properties	
+headers:	
+JMSXDeliveryCount:	1
+JMS_AMQP_HEADER:	true
+JMS_AMQP_HEADERDURABLE:	true
+JMS_AMQP_ORIGINAL_ENCODING:	6
+NATIVE_MESSAGE_ID:	ID:AMQP_UUID:fe636d99-0288-4c6c-90ed-37445d5b7117
+SenderCode:	office
+content_type:	application/json
+integ_sender_code:	office
+Payload
+1506 bytes
+Encoding: string
+[
 
 
+	{
 
-Warning: getting messages from a queue is a destructive action. 
-Clicking "Get Message(s)" will consume messages from the queue. If requeue is set the message will be put back into the queue in place, but "redelivered" will be set on the message.
 
-If requeue is not set messages will be removed from the queue.
+		"Number": "№GLO000079",
 
-Furthermore, message payloads will be truncated to 50000 bytes.
 
+		"Date": "01.08.2023 9:50:26",
+
+
+		"Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+
+
+		"DocumentStatus": "Доставляется",
+
+
+		"Driver": "Трункин Алексей Максимович",
+
+
+		"ISR": "",
+
+
+		"Informal_Document": "Акт регистрации брака GLO00000000047 от 01.02.2023 18:52:29",
+
+
+		"SKU_Weight": "0,4",
+
+
+		"ArrayStrings": [
+
+
+			{
+
+
+				"Shipping_Point": "Жетыкара, ТД \"Континенталь\", 53А",
+
+
+				"Goods": "Сетевой фильтр SMART SM-06B-1.5M XH-ED06K-1.5M черный 10А выключатель 6роз 1.5м",
+
+
+				"Quantity": "1",
+
+
+				"Item_Status": "Оформлена",
+
+
+				"Pickup_Point": "1",
+
+
+				"Delivery_Point": "2",
+
+
+				"Pickup_Latitude": "61,189797",
+
+
+				"Pickup_Longitude": "52,183911",
+
+
+				"Delivery_Latitude": "43,227672",
+
+
+				"Delivery_Longitude": "76,833993",
+
+
+				"Pickup_Time": "01.01.0001 9:44:29",
+
+
+				"Delivery_Time": "01.01.0001 10:10:11"
+
+
+			}
+
+
+		],
+
+
+		"ContactInformation": {
+
+
+			"City": "Костанай",
+
+
+			"Delivery_Condition": "Доставка",
+
+
+			"Date_Time_delivery": "2023-08-05 До 18:00",
+
+
+			"Time_Window": "09:00-18:00",
+
+
+			"Latitude": "53,212172",
+
+
+			"Longitude": "63,638317",
+
+
+			"Street": "нет данных",
+
+
+			"Home": "нет данных",
+
+
+			"Phone": "+7(714)292-7030",
+
+
+			"Apartment": "нет данных",
+
+
+			"Contractor": "нет данных"
+
+
+		}
+
+
+	}
+
+
+]
