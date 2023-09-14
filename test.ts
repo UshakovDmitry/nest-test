3c4a08b7abd5ec7e086c12fab38844cc795d20d2
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 09:58:06 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,54 +1,16 @@
-import { NestFactory, Reflector } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as basicAuth from 'express-basic-auth';
-import * as compression from 'compression';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-process.env.SWAGGER_USER = 'tms';
-process.env.SWAGGER_PASSWORD = '123456789';
+> tms-api@0.0.1 start
+> nest start
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
-      queue: 'TmsQueue',
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  // -- SWAGGER -- //
-  app.use(
-    ['/swagger', '/swagger-stats'],
-    basicAuth({
-      challenge: true,
-      users: {
-        [process.env.SWAGGER_USER]: process.env.SWAGGER_PASSWORD,
-      },
-    }),
-  );
-  const config = new DocumentBuilder()
-    .setTitle('Описание всех контроллеров REST API')
-    .setDescription(
-      'Внимание! Некоторые методы могут изменять данные в базе данных!',
-    )
-    .setVersion('1.0')
-    .addTag('API для TMS')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
-
-  // -- CORS -- //
-  app.enableCors();
-  // -- COMPRESS -- //
-  app.use(compression());
-
-  await app.startAllMicroservicesAsync();
-  // -- LISTEN -- //
-  await app.listen(4000);
-}
-bootstrap();
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [NestFactory] Starting Nest application...
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [InstanceLoader] MongooseModule dependencies initialized +28ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [InstanceLoader] ClientsModule dependencies initialized +0ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +10ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [InstanceLoader] AppModule dependencies initialized +1ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [NestMicroservice] Nest microservice successfully started +84ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [RoutesResolver] RabbitMQController {/all-messages}: +16ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [RouterExplorer] Mapped {/all-messages, GET} route +2ms
+[Nest] 848  - 14.09.2023, 09:57:34     LOG [NestApplication] Nest application successfully started +3ms
+[Nest] 848  - 14.09.2023, 09:57:34   ERROR [Server] There is no matching event handler defined in the remote service. Event pattern: undefined
