451e1b069e312dec04a5f60551c99b53b259f1dd
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 14:47:52 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,60 +1,22 @@
-import { Module } from '@nestjs/common';
-import { RabbitMQService } from './rabbitmq/rabbitmq.service';
-import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
-import { MessageModule } from './message/message.module';
-import { MessageSchema } from './message/message.shema';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { connectMongoose } from './connect-mongoose';
-import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
+@EventPattern()
+async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    console.log('Получено сообщение:', originalMsg);
 
-@Module({
-  imports: [
-    RabbitMQModule,
-    MessageModule,
-    MongooseModule.forRoot(connectMongoose()),
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-    ClientsModule.register([
-      {
-        name: 'RABBITMQ_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
-          queue: 'TmsQueue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
-  ],
-  controllers: [RabbitMQController],
-  providers: [RabbitMQService],
-})
-export class AppModule {}
+    try {
+        if (data && data.data) {
+            console.log('Сообщение получено:', data.data);
+            await this.messageService.create(data.data);
+            console.log('Сообщение сохранено');
 
-
-
-
-ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-3)
-$ npm run start
-
-> tms-api@0.0.1 start
-> nest start
-
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [NestFactory] Starting Nest application...
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] MongooseModule dependencies initialized +27ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] ClientsModule dependencies initialized +0ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +11ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] MongooseModule dependencies initialized +11ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] MessageModule dependencies initialized +0ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] RabbitMQModule dependencies initialized +1ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [InstanceLoader] AppModule dependencies initialized +0ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [NestMicroservice] Nest microservice successfully started +103ms
-Microservices started
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [RoutesResolver] RabbitMQController {/all-messages}: +15ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [RouterExplorer] Mapped {/all-messages, GET} route +3ms
-[Nest] 19560  - 14.09.2023, 14:30:46     LOG [NestApplication] Nest application successfully started +2ms
-Application is listening on port 4000
-[Nest] 19560  - 14.09.2023, 14:36:27   ERROR [Server] There is no matching event handler defined in the remote service. Event pa
-ttern: undefined
+            console.log('Отправка подтверждения для сообщения:', originalMsg);
+            channel.ack(originalMsg);
+            console.log('Подтверждение отправлено.');
+        } else {
+            console.error('Некорректный формат сообщения');
+        }
+    } catch (error) {
+        console.error('Ошибка при сохранении', error);
+    }
+}
