ad3a775b483ed818d64d00d582c0c65ab5618633
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 14:56:39 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -5,8 +5,8 @@ import { MessageService } from '../message/message.service';
 @Injectable()
 export class RabbitMQService {
   constructor(private messageService: MessageService) {}
-
-  @EventPattern('#')
+  console.log('RabbitMQService');
+  @EventPattern('TmsQueue')
   async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
     const channel = context.getChannelRef();
     const originalMsg = context.getMessage();
@@ -39,30 +39,29 @@ $ npm run start
 > tms-api@0.0.1 start
 > nest start
 
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [NestFactory] Starting Nest application...
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] MongooseModule dependenci
-es initialized +28ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] ClientsModule dependencie
-s initialized +0ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] MongooseCoreModule depend
-encies initialized +10ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] MongooseModule dependenci
-es initialized +6ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] MessageModule dependencie
-s initialized +1ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] RabbitMQModule dependenci
-es initialized +0ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [InstanceLoader] AppModule dependencies in
-itialized +1ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [NestMicroservice] Nest microservice succe
-ssfully started +83ms
-Microservices started
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [RoutesResolver] RabbitMQController {/all-
-messages}: +17ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [RouterExplorer] Mapped {/all-messages, GE
-T} route +2ms
-[Nest] 17640  - 14.09.2023, 14:52:22     LOG [NestApplication] Nest application success
-fully started +2ms
-Application is listening on port 4000
-[Nest] 17640  - 14.09.2023, 14:52:34   ERROR [Server] There is no matching event handle
-r defined in the remote service. Event pattern: undefined
+src/rabbitmq/rabbitmq.service.ts:8:3 - error TS1434: Unexpected keyword or identifier.
+
+8   console.log('RabbitMQService');
+    ~~~~~~~
+src/rabbitmq/rabbitmq.service.ts:8:10 - error TS1068: Unexpected token. A constructor,
+method, accessor, or property was expected.
+
+8   console.log('RabbitMQService');
+           ~
+src/rabbitmq/rabbitmq.service.ts:8:15 - error TS1003: Identifier expected.
+
+8   console.log('RabbitMQService');
+                ~~~~~~~~~~~~~~~~~
+src/rabbitmq/rabbitmq.service.ts:10:9 - error TS2389: Function implementation name must
+ be 'log'.
+
+10   async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
+           ~~~~~~~~~~
+
+Found 4 error(s).
+
+
+ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-
+3)
+$
+
