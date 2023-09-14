c71dcc0813d35b19e0fc00096f3f085cbbca8ba5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 14:07:06 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,32 +1,97 @@
-$ npm run start
+4
 
-> tms-api@0.0.1 start
-> nest start
+I'm trying to handle the message published on topic test_ack from online MQTT broker using microservices. But I'm getting the error.
 
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [NestFactory] Starting Nest application...
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] MongooseModule dependenci
-es initialized +28ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] ClientsModule dependencie
-s initialized +0ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] MongooseCoreModule depend
-encies initialized +10ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] MongooseModule dependenci
-es initialized +7ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] MessageModule dependencie
-s initialized +0ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] RabbitMQModule dependenci
-es initialized +1ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [InstanceLoader] AppModule dependencies in
-itialized +0ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [NestMicroservice] Nest microservice succe
-ssfully started +83ms
-Microservices started
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [RoutesResolver] RabbitMQController {/all-
-messages}: +18ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [RouterExplorer] Mapped {/all-messages, GE
-T} route +3ms
-[Nest] 23992  - 14.09.2023, 14:01:45     LOG [NestApplication] Nest application success
-fully started +2ms
-Application is listening on port 4000
-[Nest] 23992  - 14.09.2023, 14:01:45   ERROR [Server] There is no matching event handle
-r defined in the remote service. Event pattern: undefined
+There is no matching event handler defined in the remote service.
+
+My Code:
+
+main.ts
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Transport } from '@nestjs/common/enums/transport.enum';
+var url = 'mqtt://test.mosquitto.org';
+
+async function bootstrap() {
+    const app = await NestFactory.createMicroservice(AppModule, {
+        transport: Transport.MQTT,
+        options: {
+            url: url
+        }
+    });
+    await app.listenAsync();
+}
+bootstrap();
+app.controller.ts
+
+import { Controller } from '@nestjs/common';
+import { MessagePattern } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+    constructor() {}
+
+    @MessagePattern('test') 
+    ackMessageTestData(data:unknown) {
+        console.log(data.toString());
+        return 'Message Received';
+    }
+}
+
+
+
+
+
+
+As I don't have edit permission, I will post it as a new answer. As mentioned in the above answer. We have to use @EventPattern('test_ack').
+
+The published message should be in format {data: 'Your message'} and should be serialized before publishing as mentioned here.
+
+client.publish('test_ack', JSON.stringify({data: 'test data'}))
+
+
+
+
+Bug Report
+Current behavior
+When @EventPattern is supplied with a wildcard, e.g. sensor/+/status, a There is no matching event handler defined in the remote service error is logged (similar: #2236). However, when supplying the full topic string, e.g. sensor/1/status, the event is caught as normal.
+
+Input Code
+@EventPattern('sensor/+/status')
+async handleStatus(data: Record<string, string>) {
+    console.log(data);
+}
+Expected behavior
+Receiving a message with topic sensor/1/status should be handled by both handlers with pattern sensor/+/status and handlers with pattern sensor/#.
+
+Possible Solution
+When receiving an event, check for wildcards in the registered patterns, such as + and #, before matching to handlers. It might have similar solution to #2447
+
+Environment
+
+Nest version: 6.6.7
+ 
+For Tooling issues:
+- Node version: 10.16.0
+- Platform:  Windows
+
+Others:
+
+@Nosfistis Nosfistis added the needs triage label on Sep 13, 2019
+@hiepthai hiepthai mentioned this issue on Sep 14, 2019
+feat(microservices) add matchMqttPattern to server mqtt #2954
+ Merged
+3 tasks
+@hiepthai
+This comment has been minimized.
+Show comment
+@wesselvdv
+wesselvdv commented on Jun 12, 2020
+I believe this can be closed no?
+
+@kamilmysliwiec kamilmysliwiec closed this as completed on Jun 12, 2020
+@kamilmysliwiec
+Member
+kamilmysliwiec commented on Jun 12, 2020
+Correct @wesselvdv. This feature is already available in the framework.
