4d2e0d463fc8a3c4c50fc17db37b1572d27364d7
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 16:17:14 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -47,37 +47,38 @@ export class RabbitMQService {
 
 
 
-ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-
-3)
-$ npm run start
+ async readFromQueue(): Promise<QueueMessage> {
+    try {
+      const response = await this.httpService
+        .post(
+          'http://rabbitmq.next.local/api/queues/%2F/TmsQueue/get',
+          {
+            count: 1,
+            ackmode: 'ack_requeue_true',
+            encoding: 'auto',
+            truncate: 50000,
+          },
+          {
+            headers: {
+              Authorization: `Basic ${Buffer.from(
+                `${this.username}:${this.password}`,
+              ).toString('base64')}`,
+              'Content-Type': 'application/json',
+            },
+          },
+        )
+        .toPromise();
 
-> tms-api@0.0.1 start
-> nest start
+      if (response.status === 200 && response.data.length > 0) {
+        const data = response.data[0];
+        const payload = JSON.parse(data.payload);
 
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [NestFactory] Starting Nest application...
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] MongooseModule dependencie
-s initialized +28ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] ClientsModule dependencies
- initialized +0ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] MongooseCoreModule depende
-ncies initialized +10ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] MongooseModule dependencie
-s initialized +6ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] MessageModule dependencies
- initialized +1ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] RabbitMQModule dependencie
-s initialized +6ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [InstanceLoader] AppModule dependencies ini
-tialized +1ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [NestMicroservice] Nest microservice succes
-sfully started +86ms
-Microservices started
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [RoutesResolver] RabbitMQController {/all-m
-essages}: +16ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [RouterExplorer] Mapped {/all-messages, GET
-} route +2ms
-[Nest] 3616  - 14.09.2023, 15:54:31     LOG [NestApplication] Nest application successf
-ully started +2ms
-Application is listening on port 4000
-[Nest] 3616  - 14.09.2023, 15:54:34   ERROR [Server] There is no matching event handler
- defined in the remote service. Event pattern: undefined
+        return payload;
+      } else {
+        throw new Error('Возникла ошибка при получении данных');
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
