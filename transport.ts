68fa1ed79efd45fc4bdf570fa78bc579adcd6177
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 17:49:28 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,19 +1,61 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+import { Controller, Logger } from '@nestjs/common';
+import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
 
-> tms-api@0.0.1 start
-> nest start
+@Controller()
+export class MessageController {
+  private readonly logger = new Logger(MessageController.name);
 
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [NestFactory] Starting Nest application...
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [AmqpConnection] Trying to connect to RabbitMQ broker (default)
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [InstanceLoader] AppModule dependencies initialized +12ms
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [InstanceLoader] RabbitMqModule dependencies initialized +1ms
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [InstanceLoader] DiscoveryModule dependencies initialized +0ms
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [AmqpConnection] Successfully connected to RabbitMQ broker (default)
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [RabbitMQModule] Successfully connected to RabbitMQ
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [AmqpConnection] Successfully connected a RabbitMQ channel "AmqpConnection"
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [InstanceLoader] RabbitMQModule dependencies initialized +1ms
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [RoutesResolver] MessageController {/}: +24ms
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [RabbitMQModule] Initializing RabbitMQ Handlers
-[Nest] 12484  - 14.09.2023, 17:38:15     LOG [NestApplication] Nest application successfully started +2ms
-Application is listening on port 4000
+  @RabbitSubscribe({
+    exchange: 'TmsExchange',
+    routingKey: 'tms1c', // Или другой ключ маршрутизации, который вы используете
+    queue: 'TmsQueue', // Имя очереди
+  })
+  public async handleMessage(message: any) {
+    console.log('Received message:', message);
+    this.logger.log(`Received message: ${JSON.stringify(message)}`);
+  }
+}
 
+
+
+данные из плагина просмотра очереди
+
+Features	
+arguments:	
+x-queue-type:	classic
+durable:	true
+Policy	ha-fed
+Operator policy	
+Effective policy definition	
+federation-upstream-set:	all
+ha-mode:	exactly
+ha-params:	5
+ha-sync-mode:	automatic
+message-ttl:	3465687905
+Node	rabbit@dkrclstrnd1
+Mirrors	rabbit@dkrclstrnd4
+rabbit@dkrclstrnd3
+rabbit@dkrclstrnd2
+rabbit@dkrclstrnd5
+
+
+
+
+From	Routing key	Arguments	
+(Default exchange binding)
+TmsExchange
+tms1c
+
+
+
+Warning: getting messages from a queue is a destructive action. 
+
+Ack Mode:
+
+Nack message requeue true
+Encoding:
+
+Auto string / base64
+ 
+Messages:
+1
