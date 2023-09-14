0e4a576b983a0d0ef1f4a750416813bcaa84e852
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 12:18:36 +0600

    Update message.ts

--- message.ts
+++ message.ts
@@ -1,20 +1,148 @@
-ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-3)
-$ npm run start
+rabbitmq.service
 
-> tms-api@0.0.1 start
-> nest start
+import { Injectable } from '@nestjs/common';
+import {
+  MessagePattern,
+  Payload,
+  Ctx,
+  RmqContext,
+} from '@nestjs/microservices';
+import { MessageService } from '../message/message.service';
 
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [NestFactory] Starting Nest application...
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [InstanceLoader] MongooseModule dependencies initialized +27ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [InstanceLoader] ClientsModule dependencies initialized +0ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +10ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [InstanceLoader] AppModule dependencies initialized +1ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [NestMicroservice] Nest microservice successfully started +85ms
-Microservices started
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [RoutesResolver] RabbitMQController {/all-messages}: +17ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [RouterExplorer] Mapped {/all-messages, GET} route +2ms
-[Nest] 24904  - 14.09.2023, 12:13:11     LOG [NestApplication] Nest application successfully started +3ms
-Application is listening on port 4000
-[Nest] 24904  - 14.09.2023, 12:13:47   ERROR [Server] There is no matching event handler defined in the remote service. Event pattern: und
-efined
+@Injectable()
+export class RabbitMQService {
+  constructor(private messageService: MessageService) {}
+
+  @MessagePattern()
+  async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    console.log('Сообщение получено:', data);
+    try {
+      await this.messageService.create(data);
+      console.log('Сообщение сохранено');
+      channel.ack(originalMsg);
+    } catch (error) {
+      console.error('Ошибка при сохранении', error);
+    }
+  }
+}
+
+
+rabbitmq.module
+import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RabbitMQService } from './rabbitmq.service';
+import { MessageService } from '../message/message.service';
+import { RabbitMQController } from './rabbitmq.controller';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
+          queue: 'TmsQueue',
+          queueOptions: { durable: true },
+        },
+      },
+    ]),
+  ],
+  providers: [RabbitMQService, MessageService, RabbitMQController],
+})
+export class RabbitMQModule {}
+
+app.module
+import { Module } from '@nestjs/common';
+import { RabbitMQService } from './rabbitmq/rabbitmq.service';
+import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
+import { MessageService } from './message/message.service';
+import { MessageSchema } from './message/message.shema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { connectMongoose } from './connect-mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(connectMongoose()),
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+    ClientsModule.register([
+      {
+        name: 'RABBITMQ_SERVICE',
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
+          queue: 'TmsQueue',
+          queueOptions: {
+            durable: true,
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [RabbitMQController],
+  providers: [RabbitMQService, MessageService],
+})
+export class AppModule {}
+
+
+main.ts
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as basicAuth from 'express-basic-auth';
+import * as compression from 'compression';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { config } from 'dotenv';
+
+// Инициализация dotenv
+config();
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: [process.env.RABBITMQ_URL || ''],
+      queue: 'TmsQueue',
+      queueOptions: {
+        durable: true,
+      },
+    },
+  });
+
+  // SWAGGER CONFIGURATION
+  app.use(
+    ['/swagger', '/swagger-stats'],
+    basicAuth({
+      challenge: true,
+      users: {
+        [process.env.SWAGGER_USER || '']: process.env.SWAGGER_PASSWORD || '',
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('Описание всех контроллеров REST API')
+    .setDescription(
+      'Внимание! Некоторые методы могут изменять данные в базе данных!',
+    )
+    .setVersion('1.0')
+    .addTag('API для TMS')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.enableCors();
+  app.use(compression());
+
+  await app.startAllMicroservices();
+  console.log('Microservices started');
+
+  const port = parseInt(process.env.PORT || '4000', 10);
+  await app.listen(port);
+  console.log(`Application is listening on port ${port}`);
+}
+bootstrap();
