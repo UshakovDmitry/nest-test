905c719bbf314ddcd7134b7482fcdb1af244f087
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 15:48:08 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,108 +1,51 @@
-[Nest] 7856  - 14.09.2023, 15:05:50   ERROR [Server] There is no matching event handler
- defined in the remote service. Event pattern: undefined
+import { Injectable } from '@nestjs/common';
+import {
+  EventPattern,
+  Payload,
+  Ctx,
+  RmqContext,
+  Client,
+  ClientProxy,
+  Transport,
+} from '@nestjs/microservices';
+import { MessageService } from '../message/message.service';
 
-Queue TmsQueue in virtual host /
-Overview
-Queued messages last minute 
-15:36:4015:36:5015:37:0015:37:1015:37:2015:37:300.01.0
-Ready
-0
-Unacked
-0
-Total
-0
-Message rates last minute 
-15:36:3015:36:4015:36:5015:37:0015:37:1015:37:200.0 /s1.0 /s
-Publish
-0.00/s
-Deliver (manual ack)
-0.00/s
-Deliver (auto ack)
-0.00/s
-Consumer ack
-0.00/s
-Redelivered
-0.00/s
-Get (manual ack)
-0.00/s
-Get (auto ack)
-0.00/s
-Get (empty)
-0.00/s
-Details
-Features	
-arguments:	
-x-queue-type:	classic
-durable:	true
-Policy	ha-fed
-Operator policy	
-Effective policy definition	
-federation-upstream-set:	all
-ha-mode:	exactly
-ha-params:	5
-ha-sync-mode:	automatic
-message-ttl:	3465687905
-Node	rabbit@dkrclstrnd1
-Mirrors	rabbit@dkrclstrnd4
-rabbit@dkrclstrnd3
-rabbit@dkrclstrnd2
-rabbit@dkrclstrnd5
-State	idle
-Consumers	1
-Consumer capacity 	100%
-Total	Ready	Unacked	In memory	Persistent	Transient, Paged Out
-Messages 	0	0	0	0	0	0
-Message body bytes 	0 B	0 B	0 B	0 B	0 B	0 B
-Process memory 	17 KiB
-Consumers (1)
-Channel 	Consumer tag	Ack required	Exclusive	Prefetch count	Active 	Activity status	Arguments
-10.0.0.138:44130 (1)
-amq.ctag-XEWHQumiEoeElTMmv9DsSw	○	○	0	●	up	
-Bindings (2)
-From	Routing key	Arguments	
-(Default exchange binding)
-TmsExchange
-tms1c	
-⇓
+@Injectable()
+export class RabbitMQService {
+  private client: ClientProxy;
 
-This queue
+  constructor(private readonly messageService: MessageService) {
+    this.client = new ClientProxy({
+      transport: Transport.RMQ,
+      options: {
+        urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
+        queue: 'TmsQueue',
+        queueOptions: {
+          durable: true,
+        },
+      },
+    });
+  }
 
-Add binding to this queue
-From exchange:
- *
-Routing key:
-Arguments:
-=	
+  @EventPattern('TmsQueue')
+  async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
 
-String
-Publish message
-Message will be published to the default exchange with routing key TmsQueue, routing it to this queue.
-Delivery mode:
+    console.log('Сообщение получено:', data);
 
-1 - Non-persistent
-Headers: 
-=	
+    try {
+      await this.messageService.create(data);
+      console.log('Сообщение сохранено');
+      channel.ack(originalMsg); // Подтверждаем успешное получение и обработку сообщения
+    } catch (error) {
+      console.error('Ошибка при сохранении', error);
+      channel.nack(originalMsg); // Отправляем neg-ack в случае ошибки, чтобы сообщение могло быть переотправлено или обработано иначе
+    }
+  }
 
-String
-Properties: 
-=	
-Payload:
-Get messages
-Warning: getting messages from a queue is a destructive action. 
-
-Ack Mode:
-
-Nack message requeue true
-Encoding:
-
-Auto string / base64
- 
-Messages:
-1
-Delete
-Purge
-Runtime Metrics (Advanced)
-
-
-
-
+  async sendToQueue(data: any) {
+    await this.client.connect();
+    return this.client.emit('TmsQueue', data).toPromise();
+  }
+}
