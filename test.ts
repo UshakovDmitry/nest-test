5a4a9c105f335585c5e56ef9f4b939984391c15a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-14 11:55:53 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,23 @@
-@EventPattern('get_message')
-async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
-  const channel = context.getChannelRef();
-  const originalMsg = context.getMessage();
-  console.log('Received message:', data);
-  try {
-    await this.messageService.create(data);
-    console.log('Message saved successfully');
-    channel.ack(originalMsg);
-  } catch (error) {
-    console.error('Error saving message:', error);
-  }
-}
+вот как выглядит консоль когда ссобщений нет в очереди
+ushakov.dmitriy@DIT-104 MINGW64 ~/Desktop/alser.dispatcherworkplaceui/backend (develop-3)
+$ npm run start
+
+> tms-api@0.0.1 start
+> nest start
+
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [NestFactory] Starting Nest application...
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] MongooseModule dependencies initialized +27ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] ClientsModule dependencies initialized +0ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +9ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] MongooseModule dependencies initialized +7ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [InstanceLoader] AppModule dependencies initialized +1ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [NestMicroservice] Nest microservice successfully started +85ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [RoutesResolver] RabbitMQController {/all-messages}: +16ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [RouterExplorer] Mapped {/all-messages, GET} route +3ms
+[Nest] 21496  - 14.09.2023, 11:54:24     LOG [NestApplication] Nest application successfully started +2ms
+
+
+и вот что происходит когда в очереди появляется сообщение
+
+[Nest] 21496  - 14.09.2023, 11:55:32   ERROR [Server] There is no matching event handler defined in the remote service. Event pattern: und
+efined
