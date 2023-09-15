fcef1ab439569b7b4b576ea1639da61345776ab9
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 10:10:55 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,2 +1,27 @@
-From exchange: TmsExchange
-Routing key: tms1c
+Message rates last minute 
+10:09:4010:09:5010:10:0010:10:1010:10:2010:10:300.0 /s1.0 /s
+Publish (In)
+0.00/s
+Publish (Out)
+0.00/s
+Details
+Type	direct
+Features	
+durable:	true
+Policy	ha-fed
+Bindings
+This exchange
+
+⇓
+
+To	Routing key	Arguments	
+TmsQueue
+tms1c	
+Add binding from this exchange
+
+To queue
+:	
+ *
+Routing key:
+Arguments:
+=	
