3463d2ffe92708bc2debaa7fdcb5789afce89f2a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 10:57:18 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,40 +1,22 @@
-// all-exceptions.filter.ts
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
-import { Request, Response } from 'express';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-@Catch()
-export class AllExceptionsFilter implements ExceptionFilter {
-  catch(exception: unknown, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status =
-      exception instanceof HttpException
-        ? exception.getStatus()
-        : 500;
+> tms-api@0.0.1 start
+> nest start
 
-    console.error(exception);  // Логирование ошибки в консоль
-
-    response.status(status).json({
-      statusCode: status,
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    });
-  }
-}
+[Nest] 10760  - 15.09.2023, 10:55:35     LOG [NestFactory] Starting Nest application...
+[Nest] 10760  - 15.09.2023, 10:55:35     LOG [AmqpConnection] Trying to connect to RabbitMQ broker (default)
+[Nest] 10760  - 15.09.2023, 10:55:35     LOG [InstanceLoader] AppModule dependencies initialized +13ms
+[Nest] 10760  - 15.09.2023, 10:55:35     LOG [InstanceLoader] DiscoveryModule dependencies initialized +1ms
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [AmqpConnection] Successfully connected to RabbitMQ broker (default)
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [RabbitMQModule] Successfully connected to RabbitMQ
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [AmqpConnection] Successfully connected a RabbitMQ channel "AmqpConnection"
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [RoutesResolver] AppController {/}: +17ms
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [RabbitMQModule] Initializing RabbitMQ Handlers
+[Nest] 10760  - 15.09.2023, 10:55:36     LOG [NestApplication] Nest application successfully started +3ms
+Микросервис запущен
 
 
-
-// main.ts
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { AllExceptionsFilter } from './all-exceptions.filter';  // Добавьте эту строку
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new AllExceptionsFilter());  // Добавьте эту строку
-  await app.listen(3000); 
-  console.log('Микросервис запущен');
-}
-
-bootstrap();
+ничего не изменилось
+по прежнему Consumers (0)
+и никаких логов
