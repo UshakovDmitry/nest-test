50d90f0be0796af86491ada595f2cf8f0c7abe99
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 15:29:58 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,17 +3,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MessageService } from './message.service';
 import { MessageSchema } from './message.shema';
 import { MessageController } from './message.controller';
+
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
   ],
   providers: [MessageService],
   exports: [MessageService],
-  сontrollers: [MessageController],
+  controllers: [MessageController],  // Используйте 'controllers' вместо 'сontrollers'
 })
 export class MessageModule {}
-
-
-Argument of type '{ imports: DynamicModule[]; providers: (typeof MessageService)[]; exports: (typeof MessageService)[]; сontrollers: (typeof MessageController)[]; }' is not assignable to parameter of type 'ModuleMetadata'.
-  Object literal may only specify known properties, but 'сontrollers' does not exist in type 'ModuleMetadata'. Did you mean to write 'controllers'?ts(2345)
-(property) сontrollers: (typeof MessageController)[]
