5984cf0bd90ef3509a1933773a7b47de22df06f9
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 14:49:30 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -62,6 +62,41 @@ export class MessageService {
 
 
 
+/////
+// ... остальной код
+
+async getAllMessages(): Promise<any[]> {
+    return await this.messageModel.find().exec();
+}
+
+////
+
+
+
+
+
+message.controller.ts:
+
+import { Controller, Get } from '@nestjs/common';
+import { MessageService } from './message.service';
+
+@Controller('messages')
+export class MessageController {
+  constructor(private readonly messageService: MessageService) {}
+
+  @Get()
+  async getAllMessages() {
+    return this.messageService.getAllMessages();
+  }
+}
+
+
+
+
+
+
+
+
 
 
 
