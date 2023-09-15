1385f8f10bb717a5cae62997483bd7596386438a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 15:51:06 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,53 +1,239 @@
-import { Injectable } from '@nestjs/common';
-import { MessageService } from '../message/message.service';
-import { Observer } from '../observer';
-import { messageSubject } from '../listener-rabbitMQ';
+ C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-@Injectable()
-export class RabbitMQService implements Observer {
-  constructor(private readonly messageService: MessageService) {
-    messageSubject.addObserver(this);
-  }
+> tms-api@0.0.1 start
+> nest start
 
-  public update(message: string): void {
-    this.saveMessageToDb(message);
-  }
-
-  private async saveMessageToDb(message: string): Promise<void> {
-    try {
-      console.log('Saving message to database:', message);
-      await this.messageService.saveMessage(message);
-    } catch (error) {
-      console.log('Error saving message to database:', error);
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [NestFactory] Starting Nest application...
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MongooseModule dependencies initialized +25ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +10ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MongooseModule dependencies initialized +6ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MessageModule dependencies initialized +1ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] AppModule dependencies initialized +1ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [NestMicroservice] Nest microservice successfully started +95ms
+Микросервис запущен
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RoutesResolver] MessageController {/messages}: +17ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, GET} route +3ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, POST} route +1ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RoutesResolver] MessageController {/messages}: +0ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, GET} route +0ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, POST} route +1ms
+[Nest] 24496  - 15.09.2023, 15:50:39     LOG [NestApplication] Nest application successfully started +2ms
+Приложение слушаетсчя на 4000
+Received message: [
+        {
+                "Number": "№№00015934",
+                "Date": "14.08.2023 15:53:04",
+                "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+                "DocumentStatus": "Оформлена",
+                "Driver": "",
+                "ISR": "(747)2667569",
+                "Informal_Document": "Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12",
+                "SKU_Weight": "",
+                "ArrayStrings": [
+                        {
+                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
+                                "Goods": "",
+                                "Quantity": "1",
+                                "Item_Status": "",
+                                "Pickup_Point": "0",
+                                "Delivery_Point": "0",
+                                "Pickup_Latitude": "0",
+                                "Pickup_Longitude": "0",
+                                "Delivery_Latitude": "0",
+                                "Delivery_Longitude": "0",
+                                "Pickup_Time": "01.01.0001 0:00:00",
+                                "Delivery_Time": "01.01.0001 0:00:00"
+                        },
+                        {
+                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
+                                "Goods": "",
+                                "Quantity": "1",
+                                "Item_Status": "",
+                                "Pickup_Point": "0",
+                                "Delivery_Point": "0",
+                                "Pickup_Latitude": "0",
+                                "Pickup_Longitude": "0",
+                                "Delivery_Latitude": "0",
+                                "Delivery_Longitude": "0",
+                                "Pickup_Time": "01.01.0001 0:00:00",
+                                "Delivery_Time": "01.01.0001 0:00:00"
+                        }
+                ],
+                "ContactInformation": {
+                        "City": "Алматы",
+                        "Delivery_Condition": "Доставка",
+                        "Date_Time_delivery": "2023-08-16 К До 20:00",
+                        "Time_Window": "15:00-18:00",
+                        "Latitude": "43,3189165",
+                        "Longitude": "76,93994950000001",
+                        "Street": "нет данных",
+                        "Home": "74",
+                        "Phone": "(747)2667569",
+                        "Apartment": "нет данных",
+                        "Contractor": "АЛЕКСЕЙ ТРУНКИН"
+                }
+        }
+]
+Saving message to database: [
+        {
+                "Number": "№№00015934",
+                "Date": "14.08.2023 15:53:04",
+                "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+                "DocumentStatus": "Оформлена",
+                "Driver": "",
+                "ISR": "(747)2667569",
+                "Informal_Document": "Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12",
+                "SKU_Weight": "",
+                "ArrayStrings": [
+                        {
+                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
+                                "Goods": "",
+                                "Quantity": "1",
+                                "Item_Status": "",
+                                "Pickup_Point": "0",
+                                "Delivery_Point": "0",
+                                "Pickup_Latitude": "0",
+                                "Pickup_Longitude": "0",
+                                "Delivery_Latitude": "0",
+                                "Delivery_Longitude": "0",
+                                "Pickup_Time": "01.01.0001 0:00:00",
+                                "Delivery_Time": "01.01.0001 0:00:00"
+                        },
+                        {
+                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
+                                "Goods": "",
+                                "Quantity": "1",
+                                "Item_Status": "",
+                                "Pickup_Point": "0",
+                                "Delivery_Point": "0",
+                                "Pickup_Latitude": "0",
+                                "Pickup_Longitude": "0",
+                                "Delivery_Latitude": "0",
+                                "Delivery_Longitude": "0",
+                                "Pickup_Time": "01.01.0001 0:00:00",
+                                "Delivery_Time": "01.01.0001 0:00:00"
+                        }
+                ],
+                "ContactInformation": {
+                        "City": "Алматы",
+                        "Delivery_Condition": "Доставка",
+                        "Date_Time_delivery": "2023-08-16 К До 20:00",
+                        "Time_Window": "15:00-18:00",
+                        "Latitude": "43,3189165",
+                        "Longitude": "76,93994950000001",
+                        "Street": "нет данных",
+                        "Home": "74",
+                        "Phone": "(747)2667569",
+                        "Apartment": "нет данных",
+                        "Contractor": "АЛЕКСЕЙ ТРУНКИН"
+                }
+        }
+]
+[
+  {
+    Number: '№№00015934',
+    Date: '14.08.2023 15:53:04',
+    Organization: 'TOO Gulser Computers (Гулсер Компьютерс)',
+    DocumentStatus: 'Оформлена',
+    Driver: '',
+    ISR: '(747)2667569',
+    Informal_Document: 'Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12',
+    SKU_Weight: '',
+    ArrayStrings: [ [Object], [Object] ],
+    ContactInformation: {
+      City: 'Алматы',
+      Delivery_Condition: 'Доставка',
+      Date_Time_delivery: '2023-08-16 К До 20:00',
+      Time_Window: '15:00-18:00',
+      Latitude: '43,3189165',
+      Longitude: '76,93994950000001',
+      Street: 'нет данных',
+      Home: '74',
+      Phone: '(747)2667569',
+      Apartment: 'нет данных',
+      Contractor: 'АЛЕКСЕЙ ТРУНКИН'
     }
   }
-}
-
-
-
-
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-
-@Injectable()
-export class MessageService {
-  constructor(
-    @InjectModel('Message') private readonly messageModel: Model<any>,
-  ) {}
-
-  saveMessage(data: any): any {
-    try {
-      const parsedData = typeof data === 'string' ? JSON.parse(data) : data;
-      const message = new this.messageModel(parsedData); 
-      return message.save();
-    } catch (error) {
-      console.error('Error saving message:', error);
-      throw error;
+] parsedData
+Saving message to database: [
+        {
+                "Number": "№№00015934",
+                "Date": "14.08.2023 15:53:04",
+                "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+                "DocumentStatus": "Оформлена",
+                "Driver": "",
+                "ISR": "(747)2667569",
+                "Informal_Document": "Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12",
+                "SKU_Weight": "",
+                "ArrayStrings": [
+                        {
+                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
+                                "Goods": "",
+                                "Quantity": "1",
+                                "Item_Status": "",
+                                "Pickup_Point": "0",
+                                "Delivery_Point": "0",
+                                "Pickup_Latitude": "0",
+                                "Pickup_Longitude": "0",
+                                "Delivery_Latitude": "0",
+                                "Delivery_Longitude": "0",
+                                "Pickup_Time": "01.01.0001 0:00:00",
+                                "Delivery_Time": "01.01.0001 0:00:00"
+                        },
+                        {
+                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
+                                "Goods": "",
+                                "Quantity": "1",
+                                "Item_Status": "",
+                                "Pickup_Point": "0",
+                                "Delivery_Point": "0",
+                                "Pickup_Latitude": "0",
+                                "Pickup_Longitude": "0",
+                                "Delivery_Latitude": "0",
+                                "Delivery_Longitude": "0",
+                                "Pickup_Time": "01.01.0001 0:00:00",
+                                "Delivery_Time": "01.01.0001 0:00:00"
+                        }
+                ],
+                "ContactInformation": {
+                        "City": "Алматы",
+                        "Delivery_Condition": "Доставка",
+                        "Date_Time_delivery": "2023-08-16 К До 20:00",
+                        "Time_Window": "15:00-18:00",
+                        "Latitude": "43,3189165",
+                        "Longitude": "76,93994950000001",
+                        "Street": "нет данных",
+                        "Home": "74",
+                        "Phone": "(747)2667569",
+                        "Apartment": "нет данных",
+                        "Contractor": "АЛЕКСЕЙ ТРУНКИН"
+                }
+        }
+]
+[
+  {
+    Number: '№№00015934',
+    Date: '14.08.2023 15:53:04',
+    Organization: 'TOO Gulser Computers (Гулсер Компьютерс)',
+    DocumentStatus: 'Оформлена',
+    Driver: '',
+    ISR: '(747)2667569',
+    Informal_Document: 'Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12',
+    SKU_Weight: '',
+    ArrayStrings: [ [Object], [Object] ],
+    ContactInformation: {
+      City: 'Алматы',
+      Delivery_Condition: 'Доставка',
+      Date_Time_delivery: '2023-08-16 К До 20:00',
+      Time_Window: '15:00-18:00',
+      Latitude: '43,3189165',
+      Longitude: '76,93994950000001',
+      Street: 'нет данных',
+      Home: '74',
+      Phone: '(747)2667569',
+      Apartment: 'нет данных',
+      Contractor: 'АЛЕКСЕЙ ТРУНКИН'
     }
   }
-
-  async getAllMessages(): Promise<any[]> {
-    return await this.messageModel.find().exec();
-  }
-}
+] parsedData
