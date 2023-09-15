b71a4c023e1aa7c86f360773e69d22ba379d9e77
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 09:51:59 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,17 +1,9 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
-
-> tms-api@0.0.1 start
-> nest start
-
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [NestFactory] Starting Nest application...
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [AmqpConnection] Trying to connect to RabbitMQ broker (default)
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [InstanceLoader] AppModule dependencies initialized +12ms
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [InstanceLoader] DiscoveryModule dependencies initialized +0ms
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [AmqpConnection] Successfully connected to RabbitMQ broker (default)
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [RabbitMQModule] Successfully connected to RabbitMQ
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [AmqpConnection] Successfully connected a RabbitMQ channel "AmqpConnection"
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [RabbitMQModule] Initializing RabbitMQ Handlers
-[Nest] 21292  - 15.09.2023, 09:49:02     LOG [NestMicroservice] Nest microservice successfully started +69ms
-Микросервис запущен
-[Nest] 21292  - 15.09.2023, 09:49:25   ERROR [Server] There is no matching event handler defined in the remote service. Event pattern: undefined
+@RabbitSubscribe({
+  exchange: 'TmsExchange',
+  routingKey: 'tms1c',
+  queue: 'TmsQueue',
+})
+public async handleMessage(message: any) {
+  console.log('Received full message:', JSON.stringify(message));
+  this.logger.log(`Received message: ${JSON.stringify(message)}`);
+}
