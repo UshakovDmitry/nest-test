aa57c4632cbeefd6cb0812461dcc4ac4933866ca
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 09:48:35 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,2 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
-
-> tms-api@0.0.1 start
-> nest start
-
-src/main.ts:19:14 - error TS2554: Expected 0 arguments, but got 1.
-
-19   app.listen(() => console.log('Микросервис запущен'));
-                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-Found 1 error(s).
-
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
+await app.listen();
+console.log('Микросервис запущен');
