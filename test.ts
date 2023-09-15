03fbafa28a725128f897a8b15d4f8e2052855d6d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 10:28:42 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,17 +1,4 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
-
-> tms-api@0.0.1 start
-> nest start
-
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [NestFactory] Starting Nest application...
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [AmqpConnection] Trying to connect to RabbitMQ broker (default)
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [InstanceLoader] AppModule dependencies initialized +12ms
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [InstanceLoader] DiscoveryModule dependencies initialized +1ms
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [AmqpConnection] Successfully connected to RabbitMQ broker (default)
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [RabbitMQModule] Successfully connected to RabbitMQ
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [AmqpConnection] Successfully connected a RabbitMQ channel "AmqpConnection"
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [RabbitMQModule] Initializing RabbitMQ Handlers
-[Nest] 8612  - 15.09.2023, 10:14:23     LOG [NestMicroservice] Nest microservice successfully started +213ms
-Микросервис запущен
-[Nest] 8612  - 15.09.2023, 10:14:23   ERROR [Server] There is no matching event handler defined in the remote service. Event pattern: undefined
+Consumers (1)
+Channel 	Consumer tag	Ack required	Exclusive	Prefetch count	Active 	Activity status	Arguments
+10.0.0.138:43696 (1)
+amq.ctag-vuIbBdXvJ6B4Tqs3zYRzTQ	○	○	0	●	up
