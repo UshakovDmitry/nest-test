eb8f71a03973fb44692afad6c7b3386c49bfbd75
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 11:13:04 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,22 +1,18 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
+Собственно код по отправке сообщения:
 
-> tms-api@0.0.1 start
-> nest start
+Фабрика_XDTO = ПолучитьФабрикуXDTO();
+        
+ОбъектСообщение = Фабрика_XDTO.Создать(Фабрика_XDTO.Тип("http://rest_rmq", "message_out"));
+        
+ОбъектСообщение.routing_key           = ПараметрыПодключения.КлючМаршрутизации;
+ОбъектСообщение.payload_encoding      = "string"; // вариант "base64"
+ОбъектСообщение.properties            = Фабрика_XDTO.Создать(Фабрика_XDTO.Тип("http://rest_rmq", "message_properties"));
 
-[Nest] 10760  - 15.09.2023, 10:55:35     LOG [NestFactory] Starting Nest application...
-[Nest] 10760  - 15.09.2023, 10:55:35     LOG [AmqpConnection] Trying to connect to RabbitMQ broker (default)
-[Nest] 10760  - 15.09.2023, 10:55:35     LOG [InstanceLoader] AppModule dependencies initialized +13ms
-[Nest] 10760  - 15.09.2023, 10:55:35     LOG [InstanceLoader] DiscoveryModule dependencies initialized +1ms
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [AmqpConnection] Successfully connected to RabbitMQ broker (default)
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [RabbitMQModule] Successfully connected to RabbitMQ
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [AmqpConnection] Successfully connected a RabbitMQ channel "AmqpConnection"
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [RoutesResolver] AppController {/}: +17ms
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [RabbitMQModule] Initializing RabbitMQ Handlers
-[Nest] 10760  - 15.09.2023, 10:55:36     LOG [NestApplication] Nest application successfully started +3ms
-Микросервис запущен
+ОбъектСообщение.payload               = ТекстСообщения;
+        
+ТекстJSON = ОбъектXDTO_ПолучитьJSON(ОбъектСообщение, Фабрика_XDTO);
 
-
-ничего не изменилось
-по прежнему Consumers (0)
-и никаких логов
+// %2f это vhost по умолчанию "/"
+АдресРесурса = СтрШаблон("/api/exchanges/%1/%2/publish", "%2f", ПараметрыПодключения.ТочкаОбмена);
+        
+Ответ = ОтправитьHTTPЗапрос(ПараметрыПодключения, АдресРесурса, "POST", ТекстJSON);
