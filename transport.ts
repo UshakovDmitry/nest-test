0b12ef37ee0bd9b9384cb174beeeffb00294a787
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 11:22:38 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,18 +1,53 @@
-Собственно код по отправке сообщения:
+// src/rabbitmq/rabbitmq.service.ts
 
-Фабрика_XDTO = ПолучитьФабрикуXDTO();
-        
-ОбъектСообщение = Фабрика_XDTO.Создать(Фабрика_XDTO.Тип("http://rest_rmq", "message_out"));
-        
-ОбъектСообщение.routing_key           = ПараметрыПодключения.КлючМаршрутизации;
-ОбъектСообщение.payload_encoding      = "string"; // вариант "base64"
-ОбъектСообщение.properties            = Фабрика_XDTO.Создать(Фабрика_XDTO.Тип("http://rest_rmq", "message_properties"));
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import * as amqp from 'amqplib';
 
-ОбъектСообщение.payload               = ТекстСообщения;
-        
-ТекстJSON = ОбъектXDTO_ПолучитьJSON(ОбъектСообщение, Фабрика_XDTO);
+@Injectable()
+export class RabbitmqService implements OnModuleInit {
+  private readonly logger = new Logger(RabbitmqService.name);
+  private connection: amqp.Connection;
+  private channel: amqp.Channel;
 
-// %2f это vhost по умолчанию "/"
-АдресРесурса = СтрШаблон("/api/exchanges/%1/%2/publish", "%2f", ПараметрыПодключения.ТочкаОбмена);
-        
-Ответ = ОтправитьHTTPЗапрос(ПараметрыПодключения, АдресРесурса, "POST", ТекстJSON);
+  async onModuleInit() {
+    this.connection = await amqp.connect('amqp://tms:26000567855499290979@rabbitmq.next.local');
+    this.channel = await this.connection.createChannel();
+    await this.channel.assertQueue('TmsQueue');
+    this.consume();
+  }
+
+  async consume() {
+    await this.channel.consume('TmsQueue', (msg) => {
+      if (msg !== null) {
+        this.logger.log(`Received message: ${msg.content.toString()}`);
+        this.channel.ack(msg);
+      }
+    });
+  }
+}
+
+
+// src/app.module.ts
+
+import { Module } from '@nestjs/common';
+import { RabbitmqService } from './rabbitmq/rabbitmq.service';
+
+@Module({
+  providers: [RabbitmqService],
+})
+export class AppModule {}
+
+
+
+// src/main.ts
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+  console.log('Микросервис запущен');
+}
+
+bootstrap();
