7585ae97eecd963e9e0d9828dfdee37c6c776f44
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 14:45:02 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,48 +1,6 @@
- listener.rabbitMQ.ts
-const amqp = require('amqplib/callback_api');
+rabbitmq.service.ts:
 
-const arrMessage = [];
-
-amqp.connect(
-  'amqp://tms:26000567855499290979@rabbitmq.next.local',
-  function (error0, connection) {
-    if (error0) {
-      throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-      if (error1) {
-        throw error1;
-      }
-      // Название очереди
-      const queue = 'TmsQueue';
-
-      channel.assertQueue(queue, {
-        durable: true,
-      });
-
-      // Здесть мы получаем сообщения из очереди и добавляем в массив arrMessage
-      channel.consume(
-        queue,
-        function (msg: any) {
-          console.log('Сообщение :', msg.content.toString());
-          arrMessage.push(msg.content.toString());
-        },
-        {
-          noAck: true,
-        },
-      );
-    });
-  },
-);
-
-export function getAllMessagesFromRabbitMQ() {
-  return arrMessage;
-}
-
-
-а вот rabbitmq.service.ts
 import { Injectable } from '@nestjs/common';
-
 import { MessageService } from '../message/message.service';
 import { getAllMessagesFromRabbitMQ } from '../listener-rabbitMQ';
 
@@ -50,39 +8,62 @@ import { getAllMessagesFromRabbitMQ } from '../listener-rabbitMQ';
 export class RabbitMQService {
   constructor(private readonly messageService: MessageService) {
     try {
-      console.log('list', getAllMessagesFromRabbitMQ());
-      setTimeout(() => {
-        console.log('list', getAllMessagesFromRabbitMQ());
-      }, 20000);
+      // При создании сервиса выводим текущий список сообщений из RabbitMQ
+      this.listenForNewMessages();
     } catch (error) {
+      // Если возникли ошибки, выводим их в консоль
       console.log('Ошибка при подключении к RabbitMQ', error);
+    }
+  }
+
+  private async listenForNewMessages() {
+    const messages = getAllMessagesFromRabbitMQ();
+    for (const message of messages) {
+      // Попробуйте сохранить сообщение в базу данных
+      try {
+        await this.messageService.saveMessage(message); // предположим, что у вас есть метод saveMessage в вашем messageService
+      } catch (error) {
+        console.log('Ошибка при сохранении сообщения в базе данных', error);
+      }
     }
   }
 }
 
-app.module.ts
-import { Module } from '@nestjs/common';
-import { RabbitMQService } from './rabbitmq/rabbitmq.service';
-// import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
-import { MessageModule } from './message/message.module';
-import { MessageSchema } from './message/message.shema';
-import { MongooseModule } from '@nestjs/mongoose';
-import { connectMongoose } from './connect-mongoose';
-import { RabbitMQModule } from './rabbitmq/rabbitmq.module';
-
-@Module({
-  imports: [
-    RabbitMQModule,
-    MessageModule,
-    MongooseModule.forRoot(connectMongoose()),
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-  ],
-  controllers: [],
-  providers: [RabbitMQService],
-})
-export class AppModule {}
 
 
 
-сделай рефакторинг 
+
+
+
+message.service.ts (Пример):
+
+
+
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MessageDocument } from './message.schema';
+
+@Injectable()
+export class MessageService {
+  constructor(
+    @InjectModel('Message') private readonly messageModel: Model<MessageDocument>
+  ) {}
+
+  async saveMessage(content: string) {
+    const newMessage = new this.messageModel({ content });
+    return await newMessage.save();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 
