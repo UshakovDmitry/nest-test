8cfa499f925e9b1913e6d8a552ff74bfa1a536d1
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 10:49:10 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,17 +1,21 @@
-@RabbitSubscribe({
-  exchange: 'TmsExchange',
-  routingKey: 'tms1c',
-  queue: 'TmsQueue',
-})
-public async handleMessage(message: any, amqpMsg: Message) {
-  console.log('Received full message:', JSON.stringify(message));
-  this.logger.log(`Received message: ${JSON.stringify(message)}`);
-  amqpMsg.ack();
-}
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-Cannot find name 'Message'.ts(2304)
-Parameter 'amqpMsg' of public method from exported class has or is using private name 'Message'.ts(4073)
-type Message = /*unresolved*/ any
+> tms-api@0.0.1 start
+> nest start
+
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [NestFactory] Starting Nest application...
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [AmqpConnection] Trying to connect to RabbitMQ broker (default)
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [InstanceLoader] AppModule dependencies initialized +14ms
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [InstanceLoader] DiscoveryModule dependencies initialized +0ms
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [AmqpConnection] Successfully connected to RabbitMQ broker (default)
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [RabbitMQModule] Successfully connected to RabbitMQ
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [AmqpConnection] Successfully connected a RabbitMQ channel "AmqpConnection"
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [RoutesResolver] AppController {/}: +16ms
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [RabbitMQModule] Initializing RabbitMQ Handlers
+[Nest] 18988  - 15.09.2023, 10:47:46     LOG [NestApplication] Nest application successfully started +3ms
+Микросервис запущен
+
 
 и вот мой код
 
