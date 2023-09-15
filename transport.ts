06088d2aab0b80867ca62e12aa8b9b84b8aab076
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 11:37:44 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,53 +1,16 @@
-// src/rabbitmq/rabbitmq.service.ts
 
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import * as amqp from 'amqplib';
-
-@Injectable()
-export class RabbitmqService implements OnModuleInit {
-  private readonly logger = new Logger(RabbitmqService.name);
-  private connection: amqp.Connection;
-  private channel: amqp.Channel;
-
-  async onModuleInit() {
-    this.connection = await amqp.connect('amqp://tms:26000567855499290979@rabbitmq.next.local');
-    this.channel = await this.connection.createChannel();
-    await this.channel.assertQueue('TmsQueue');
-    this.consume();
-  }
-
-  async consume() {
-    await this.channel.consume('TmsQueue', (msg) => {
-      if (msg !== null) {
-        this.logger.log(`Received message: ${msg.content.toString()}`);
-        this.channel.ack(msg);
-      }
-    });
-  }
-}
-
-
-// src/app.module.ts
-
-import { Module } from '@nestjs/common';
-import { RabbitmqService } from './rabbitmq/rabbitmq.service';
-
-@Module({
-  providers: [RabbitmqService],
-})
-export class AppModule {}
-
-
-
-// src/main.ts
-
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
-  console.log('Микросервис запущен');
-}
-
-bootstrap();
+Virtual Host	Name	Pattern	Apply to	Definition	Priority
+/	ha-fed	.*	all	
+federation-upstream-set:	all
+ha-mode:	exactly
+ha-params:	5
+ha-sync-mode:	automatic
+message-ttl:	3465687905
+1
+/	ha-fed3	vfr.*	queues	
+ha-mode:	nodes
+ha-params:	rabbithvclow1@dkrclstrnd1
+rabbithvchi1@dkrclstrnd2
+rabbitvmware1@dkrclstrnd3
+ha-sync-mode:	automatic
+3
