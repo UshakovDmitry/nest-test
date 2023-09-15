2bb2e6b595662cf6727ff7eba3f6387b46ec026a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-15 09:47:45 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,78 +1,13 @@
-npm install @nestjs/microservices @nestjs/common @nestjs/core reflect-metadata rxjs
-npm install @golevelup/nestjs-rabbitmq
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
+> tms-api@0.0.1 start
+> nest start
 
+src/main.ts:19:14 - error TS2554: Expected 0 arguments, but got 1.
 
-import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { AppModule } from './app.module';
+19   app.listen(() => console.log('Микросервис запущен'));
+                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.RMQ,
-      options: {
-        urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
-        queue: 'TmsQueue',
-        queueOptions: {
-          durable: true
-        },
-      },
-    },
-  );
-  app.listen(() => console.log('Microservice is listening'));
-}
+Found 1 error(s).
 
-bootstrap();
-
-
-
-///////////////////////////////////////////
-
-
-import { Module } from '@nestjs/common';
-import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
-import { AppController } from './app.controller';
-
-@Module({
-  imports: [
-    RabbitMQModule.forRoot(RabbitMQModule, {
-      exchanges: [
-        {
-          name: 'TmsExchange',
-          type: 'direct',
-        },
-      ],
-      uri: 'amqp://tms:26000567855499290979@rabbitmq.next.local',
-    }),
-  ],
-  controllers: [AppController],
-})
-export class AppModule {}
-
-
-///////////////////////////////////////////
-
-
-import { Controller, Logger } from '@nestjs/common';
-import { RabbitSubscribe } from '@golevelup/nestjs-rabbitmq';
-
-@Controller()
-export class AppController {
-  private readonly logger = new Logger(AppController.name);
-
-  @RabbitSubscribe({
-    exchange: 'TmsExchange',
-    routingKey: 'tms1c', 
-    queue: 'TmsQueue',
-  })
-  public handleIncomingMessage(payload: any) {
-    this.logger.log(`Received message: ${JSON.stringify(payload)}`);
-  }
-}
-
-
-///////////////////////////////////////////
-
-
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
