8ffa67225eeae12091292258401d618b9cc4f8ca
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-18 11:53:22 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,149 +1,57 @@
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MessageSchema } from '../message/message.shema';
-
-@Injectable()
-export class MessageService {
-  constructor(
-    @InjectModel('Message') private messageModel: Model<MessageSchema>,
-  ) {}
-
-  async saveMessage(messageData: any) {
-    try {
-      const parsedData =
-        typeof messageData === 'string' ? JSON.parse(messageData) : messageData;
-      console.log(parsedData, 'messageData!');
-
-      const createdMessage = new this.messageModel(parsedData);
-
-      return createdMessage.save();
-    } catch (error) {
-      console.error('Ошибка сохранения в бд:', error);
-      throw error;
-    }
+transformToTransportRequest(data: any) {
+    return {
+      number: String(data.Number),
+      status: String(data.DocumentStatus),
+      ISR: String(data.ISR),
+      document: String(data.Informal_Document),
+      shipping_point: {
+        address: String(data.ArrayStrings[0].Shipping_Point),
+        coordinates: `${data.ArrayStrings[0].Pickup_Latitude}, ${data.ArrayStrings[0].Pickup_Longitude}`,
+      },
+      contractor: {
+        name: String(data.ContactInformation.Contractor),
+        phone: String(data.ContactInformation.Phone),
+      },
+      delivery_time: String(data.ContactInformation.Date_Time_delivery),
+      delivery_point: {
+        address: `${data.ContactInformation.City}, ${data.ContactInformation.Street}, ${data.ContactInformation.Home}, ${data.ContactInformation.Apartment}}`,
+        coordinates: `${data.ContactInformation.Latitude}, ${data.ContactInformation.Longitude}`,
+      },
+      sku_weight: Number(data.SKU_Weight),
+    };
   }
 
-  async getAllMessages(): Promise<any[]> {
-    return await this.messageModel.find().exec();
+  getData(): void {
+    fetch('http://localhost:4000/messages', {
+      method: 'GET',
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          console.log('Данные получены');
+          return await response.json();
+        } else {
+          throw new Error('Возникла ошибка при получении данных');
+        }
+      })
+      .then((data) => {
+        const transformedData = this.transformToTransportRequest(data);
+        console.log(transformedData, 'transformedData');
+        this.model.transportRequests.push(transformedData,"vvv");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
-}
-
-'MessageSchema' refers to a value, but is being used as a type here. Did you mean 'typeof MessageSchema'?ts(2749)
-Parameter 'messageModel' of constructor from exported class has or is using private name 'MessageSchema'.ts(4063)
-type MessageSchema = /*unresolved*/ any
 
 
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-
-export type MessageDocument = Document & Message;
-@Schema()
-class ArrayStrings {
-  @Prop()
-  Shipping_Point: string;
-
-  @Prop()
-  Goods: string;
-
-  @Prop()
-  Quantity: string;
-
-  @Prop()
-  Item_Status: string;
-
-  @Prop()
-  Pickup_Point: string;
-
-  @Prop()
-  Delivery_Point: string;
-
-  @Prop()
-  Pickup_Latitude: string;
-
-  @Prop()
-  Pickup_Longitude: string;
-
-  @Prop()
-  Delivery_Latitude: string;
-
-  @Prop()
-  Delivery_Longitude: string;
-
-  @Prop()
-  Pickup_Time: string;
-
-  @Prop()
-  Delivery_Time: string;
-}
-
-@Schema()
-class ContactInformation {
-  @Prop()
-  City: string;
-
-  @Prop()
-  Delivery_Condition: string;
-
-  @Prop()
-  Date_Time_delivery: string;
-
-  @Prop()
-  Time_Window: string;
-
-  @Prop()
-  Latitude: string;
-
-  @Prop()
-  Longitude: string;
-
-  @Prop()
-  Street: string;
-
-  @Prop()
-  Home: string;
-
-  @Prop()
-  Phone: string;
-
-  @Prop()
-  Apartment: string;
-
-  @Prop()
-  Contractor: string;
-}
-@Schema({ versionKey: false })
-export class Message {
-  @Prop()
-  Number: string;
-
-  @Prop()
-  Date: string;
-
-  @Prop()
-  Organization: string;
-
-  @Prop()
-  DocumentStatus: string;
-
-  @Prop()
-  Driver: string;
-
-  @Prop()
-  ISR: string;
-
-  @Prop()
-  Informal_Document: string;
-
-  @Prop()
-  SKU_Weight: string;
-
-  @Prop()
-  ArrayStrings: ArrayStrings[];
-
-  @Prop()
-  ContactInformation: ContactInformation;
-}
-
-export const MessageSchema = SchemaFactory.createForClass(Message);
-
+applications.viewmodel.ts:52 TypeError: Cannot read properties of undefined (reading '0')
+    at TransportRequestsViewModel.transformToTransportRequest (applications.viewmodel.ts:18:30)
+    at applications.viewmodel.ts:47:38
+(anonymous)	@	applications.viewmodel.ts:52
+Promise.catch (async)		
+getData	@	applications.viewmodel.ts:51
+TransportRequestsViewModel	@	applications.viewmodel.ts:9
+setup	@	applications.component.vue:30
+Promise.then (async)		
+(anonymous)	@	applications.component.vue:14
+Show 22 more frames
