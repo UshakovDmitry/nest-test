bcb717bb554b329d4cc5eccba86463dc65fcfb70
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-18 10:03:35 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,112 +1,27 @@
-[
-    {
-        "_id": "650420f9a51d74bf510284b8",
-        "ArrayStrings": [],
-        "__v": 0
+В файле listener-rabbitMQ:
+
+channel.consume(
+    queue,
+    function (msg: any) {
+      const messageObj = JSON.parse(msg.content.toString());
+      console.log('Сообщение из TmsQueue', messageObj);
+      messageSubject.notifyObservers(messageObj);
     },
     {
-        "_id": "650420f9a51d74bf510284b9",
-        "ArrayStrings": [],
-        "__v": 0
+      noAck: true,
     },
-    {
-        "_id": "65042165a51d74bf510284bd",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "65042165a51d74bf510284be",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "650426fba9aec2b30f372954",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "650426fba9aec2b30f372955",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "650428f32a6cbde49402475a",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "650428f32a6cbde49402475b",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c28396f8ba59c51fcb83",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c28396f8ba59c51fcb84",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c2a7eef887ca82fbbeb1",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c2a7eef887ca82fbbeb2",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c30110500d227e070d40",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c30110500d227e070d41",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c30910500d227e070d44",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c30910500d227e070d45",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c44310500d227e070d48",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c44310500d227e070d49",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c45dafe0e59b175f875c",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507c45dafe0e59b175f875d",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507ca3a2b2baa2eb53054ca",
-        "ArrayStrings": [],
-        "__v": 0
-    },
-    {
-        "_id": "6507ca3a2b2baa2eb53054cb",
-        "ArrayStrings": [],
-        "__v": 0
-    }
-]
+);
+
+
+В вашем message.service, вы уже делаете проверку:
+
+const parsedData =
+    typeof messageData === 'string' ? JSON.parse(messageData) : messageData;
+
+
+
+В файле message.shema:
+
+export const MessageSchema = new Schema({
+  // ... остальные поля ...
+}, { versionKey: false });
