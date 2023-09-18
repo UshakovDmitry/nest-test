2a087a8399a90e60a4f2fa023cd03c207c56d673
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-18 10:17:29 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,46 +1,78 @@
-import { Schema } from 'mongoose';
-
-const ArrayStringsSchema = new Schema({
-  Shipping_Point: { type: String, default: '' },
-  Goods: { type: String, default: '' },
-  Quantity: { type: String, default: '' },
-  Item_Status: { type: String, default: '' },
-  Pickup_Point: { type: String, default: '' },
-  Delivery_Point: { type: String, default: '' },
-  Pickup_Latitude: { type: String, default: '' },
-  Pickup_Longitude: { type: String, default: '' },
-  Delivery_Latitude: { type: String, default: '' },
-  Delivery_Longitude: { type: String, default: '' },
-  Pickup_Time: { type: String, default: '' },
-  Delivery_Time: { type: String, default: '' },
-});
-
-const ContactInformationSchema = new Schema({
-  City: { type: String, default: '' },
-  Delivery_Condition: { type: String, default: '' },
-  Date_Time_delivery: { type: String, default: '' },
-  Time_Window: { type: String, default: '' },
-  Latitude: { type: String, default: '' },
-  Longitude: { type: String, default: '' },
-  Street: { type: String, default: 'нет данных' },
-  Home: { type: String, default: 'нет данных' },
-  Phone: { type: String, default: '' },
-  Apartment: { type: String, default: 'нет данных' },
-  Contractor: { type: String, default: '' },
-});
-
-export const MessageSchema = new Schema(
-  {
-    Number: { type: String, default: '' },
-    Date: { type: Date, default: Date.now },
-    Organization: { type: String, default: '' },
-    DocumentStatus: { type: String, default: '' },
-    Driver: { type: String, default: '' },
-    ISR: { type: String, default: '' },
-    Informal_Document: { type: String, default: '' },
-    SKU_Weight: { type: String, default: '' },
-    ArrayStrings: { type: [ArrayStringsSchema], default: [] },
-    ContactInformation: { type: ContactInformationSchema, default: {} },
-  },
-  { versionKey: false },
-);
+    {
+        "Number": "",
+        "Organization": "",
+        "DocumentStatus": "",
+        "Driver": "",
+        "ISR": "",
+        "Informal_Document": "",
+        "SKU_Weight": "",
+        "ContactInformation": {
+            "City": "",
+            "Delivery_Condition": "",
+            "Date_Time_delivery": "",
+            "Time_Window": "",
+            "Latitude": "",
+            "Longitude": "",
+            "Street": "нет данных",
+            "Home": "нет данных",
+            "Phone": "",
+            "Apartment": "нет данных",
+            "Contractor": "",
+            "_id": "6507cf287c6f1b90d3c37e0c"
+        },
+        "_id": "6507cd94aadddf5037287447",
+        "ArrayStrings": [],
+        "Date": "2023-09-18T04:16:40.908Z"
+    },
+    {
+        "_id": "6507ceda7c6f1b90d3c37ded",
+        "Number": "",
+        "Organization": "",
+        "DocumentStatus": "",
+        "Driver": "",
+        "ISR": "",
+        "Informal_Document": "",
+        "SKU_Weight": "",
+        "ContactInformation": {
+            "City": "",
+            "Delivery_Condition": "",
+            "Date_Time_delivery": "",
+            "Time_Window": "",
+            "Latitude": "",
+            "Longitude": "",
+            "Street": "нет данных",
+            "Home": "нет данных",
+            "Phone": "",
+            "Apartment": "нет данных",
+            "Contractor": "",
+            "_id": "6507ceda7c6f1b90d3c37dec"
+        },
+        "Date": "2023-09-18T04:15:22.567Z",
+        "ArrayStrings": []
+    },
+    {
+        "_id": "6507ceda7c6f1b90d3c37def",
+        "Number": "",
+        "Organization": "",
+        "DocumentStatus": "",
+        "Driver": "",
+        "ISR": "",
+        "Informal_Document": "",
+        "SKU_Weight": "",
+        "ContactInformation": {
+            "City": "",
+            "Delivery_Condition": "",
+            "Date_Time_delivery": "",
+            "Time_Window": "",
+            "Latitude": "",
+            "Longitude": "",
+            "Street": "нет данных",
+            "Home": "нет данных",
+            "Phone": "",
+            "Apartment": "нет данных",
+            "Contractor": "",
+            "_id": "6507ceda7c6f1b90d3c37dee"
+        },
+        "Date": "2023-09-18T04:15:22.572Z",
+        "ArrayStrings": []
+    }
