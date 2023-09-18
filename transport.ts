13db65e3a8a8c6477cfe3e398b2dcbe4e78f8dff
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-18 10:37:20 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,18 +1,114 @@
-  import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
 
-export type CatDocument = HydratedDocument<Cat>;
+export type MessageDocument = Document & Message;
 
-@Schema()
-export class Cat {
-  @Prop()
-  name: string;
+@Schema({ versionKey: false })
+export class Message {
+  @Prop({ default: '' })
+  Number: string;
 
-  @Prop()
-  age: number;
+  @Prop({ default: Date.now })
+  Date: Date;
 
-  @Prop()
-  breed: string;
+  @Prop({ default: '' })
+  Organization: string;
+
+  @Prop({ default: '' })
+  DocumentStatus: string;
+
+  @Prop({ default: '' })
+  Driver: string;
+
+  @Prop({ default: '' })
+  ISR: string;
+
+  @Prop({ default: '' })
+  Informal_Document: string;
+
+  @Prop({ default: '' })
+  SKU_Weight: string;
+
+  @Prop({ default: [] })
+  ArrayStrings: ArrayStrings[];
+
+  @Prop({ default: {} })
+  ContactInformation: ContactInformation;
 }
 
-export const CatSchema = SchemaFactory.createForClass(Cat);
+@Schema()
+class ArrayStrings {
+  @Prop({ default: '' })
+  Shipping_Point: string;
+
+  @Prop({ default: '' })
+  Goods: string;
+
+  @Prop({ default: '' })
+  Quantity: string;
+
+  @Prop({ default: '' })
+  Item_Status: string;
+
+  @Prop({ default: '' })
+  Pickup_Point: string;
+
+  @Prop({ default: '' })
+  Delivery_Point: string;
+
+  @Prop({ default: '' })
+  Pickup_Latitude: string;
+
+  @Prop({ default: '' })
+  Pickup_Longitude: string;
+
+  @Prop({ default: '' })
+  Delivery_Latitude: string;
+
+  @Prop({ default: '' })
+  Delivery_Longitude: string;
+
+  @Prop({ default: '' })
+  Pickup_Time: string;
+
+  @Prop({ default: '' })
+  Delivery_Time: string;
+}
+
+@Schema()
+class ContactInformation {
+  @Prop({ default: '' })
+  City: string;
+
+  @Prop({ default: '' })
+  Delivery_Condition: string;
+
+  @Prop({ default: '' })
+  Date_Time_delivery: string;
+
+  @Prop({ default: '' })
+  Time_Window: string;
+
+  @Prop({ default: '' })
+  Latitude: string;
+
+  @Prop({ default: '' })
+  Longitude: string;
+
+  @Prop({ default: 'нет данных' })
+  Street: string;
+
+  @Prop({ default: 'нет данных' })
+  Home: string;
+
+  @Prop({ default: '' })
+  Phone: string;
+
+  @Prop({ default: 'нет данных' })
+  Apartment: string;
+
+  @Prop({ default: '' })
+  Contractor: string;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
