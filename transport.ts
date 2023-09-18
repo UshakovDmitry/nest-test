d0f01348ad2a1fc1f623754e07a21054de2e1692
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-18 10:38:00 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,114 +1,21 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
 
-export type MessageDocument = Document & Message;
+> tms-api@0.0.1 start
+> nest start
 
-@Schema({ versionKey: false })
-export class Message {
-  @Prop({ default: '' })
-  Number: string;
 
-  @Prop({ default: Date.now })
-  Date: Date;
-
-  @Prop({ default: '' })
-  Organization: string;
-
-  @Prop({ default: '' })
-  DocumentStatus: string;
-
-  @Prop({ default: '' })
-  Driver: string;
-
-  @Prop({ default: '' })
-  ISR: string;
-
-  @Prop({ default: '' })
-  Informal_Document: string;
-
-  @Prop({ default: '' })
-  SKU_Weight: string;
-
-  @Prop({ default: [] })
-  ArrayStrings: ArrayStrings[];
-
-  @Prop({ default: {} })
+C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\message\message.shema.ts:82
   ContactInformation: ContactInformation;
-}
-
-@Schema()
-class ArrayStrings {
-  @Prop({ default: '' })
-  Shipping_Point: string;
-
-  @Prop({ default: '' })
-  Goods: string;
-
-  @Prop({ default: '' })
-  Quantity: string;
-
-  @Prop({ default: '' })
-  Item_Status: string;
-
-  @Prop({ default: '' })
-  Pickup_Point: string;
-
-  @Prop({ default: '' })
-  Delivery_Point: string;
-
-  @Prop({ default: '' })
-  Pickup_Latitude: string;
-
-  @Prop({ default: '' })
-  Pickup_Longitude: string;
-
-  @Prop({ default: '' })
-  Delivery_Latitude: string;
-
-  @Prop({ default: '' })
-  Delivery_Longitude: string;
-
-  @Prop({ default: '' })
-  Pickup_Time: string;
-
-  @Prop({ default: '' })
-  Delivery_Time: string;
-}
-
-@Schema()
-class ContactInformation {
-  @Prop({ default: '' })
-  City: string;
-
-  @Prop({ default: '' })
-  Delivery_Condition: string;
-
-  @Prop({ default: '' })
-  Date_Time_delivery: string;
-
-  @Prop({ default: '' })
-  Time_Window: string;
-
-  @Prop({ default: '' })
-  Latitude: string;
-
-  @Prop({ default: '' })
-  Longitude: string;
-
-  @Prop({ default: 'нет данных' })
-  Street: string;
-
-  @Prop({ default: 'нет данных' })
-  Home: string;
-
-  @Prop({ default: '' })
-  Phone: string;
-
-  @Prop({ default: 'нет данных' })
-  Apartment: string;
-
-  @Prop({ default: '' })
-  Contractor: string;
-}
-
-export const MessageSchema = SchemaFactory.createForClass(Message);
+                      ^
+ReferenceError: Cannot access 'ContactInformation' before initialization
+    at Object.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\message\message.shema.ts:82:23)
+    at Module._compile (node:internal/modules/cjs/loader:1103:14)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
+    at Module.load (node:internal/modules/cjs/loader:981:32)
+    at Function.Module._load (node:internal/modules/cjs/loader:822:12)
+    at Module.require (node:internal/modules/cjs/loader:1005:19)
+    at require (node:internal/modules/cjs/helpers:102:18)
+    at Object.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\message\message.module.ts:4:1)
+    at Module._compile (node:internal/modules/cjs/loader:1103:14)
+    at Object.Module._extensions..js (node:internal/modules/cjs/loader:1157:10)
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend>
