2caafacea85b2f202d6199acb6e21bf480fa3e6f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-18 09:58:52 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,239 +1,112 @@
- C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> npm run start
-
-> tms-api@0.0.1 start
-> nest start
-
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [NestFactory] Starting Nest application...
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MongooseModule dependencies initialized +25ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MongooseCoreModule dependencies initialized +10ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MongooseModule dependencies initialized +6ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] MessageModule dependencies initialized +1ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] RabbitMQModule dependencies initialized +0ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [InstanceLoader] AppModule dependencies initialized +1ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [NestMicroservice] Nest microservice successfully started +95ms
-Микросервис запущен
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RoutesResolver] MessageController {/messages}: +17ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, GET} route +3ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, POST} route +1ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RoutesResolver] MessageController {/messages}: +0ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, GET} route +0ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [RouterExplorer] Mapped {/messages, POST} route +1ms
-[Nest] 24496  - 15.09.2023, 15:50:39     LOG [NestApplication] Nest application successfully started +2ms
-Приложение слушаетсчя на 4000
-Received message: [
-        {
-                "Number": "№№00015934",
-                "Date": "14.08.2023 15:53:04",
-                "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-                "DocumentStatus": "Оформлена",
-                "Driver": "",
-                "ISR": "(747)2667569",
-                "Informal_Document": "Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12",
-                "SKU_Weight": "",
-                "ArrayStrings": [
-                        {
-                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
-                                "Goods": "",
-                                "Quantity": "1",
-                                "Item_Status": "",
-                                "Pickup_Point": "0",
-                                "Delivery_Point": "0",
-                                "Pickup_Latitude": "0",
-                                "Pickup_Longitude": "0",
-                                "Delivery_Latitude": "0",
-                                "Delivery_Longitude": "0",
-                                "Pickup_Time": "01.01.0001 0:00:00",
-                                "Delivery_Time": "01.01.0001 0:00:00"
-                        },
-                        {
-                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
-                                "Goods": "",
-                                "Quantity": "1",
-                                "Item_Status": "",
-                                "Pickup_Point": "0",
-                                "Delivery_Point": "0",
-                                "Pickup_Latitude": "0",
-                                "Pickup_Longitude": "0",
-                                "Delivery_Latitude": "0",
-                                "Delivery_Longitude": "0",
-                                "Pickup_Time": "01.01.0001 0:00:00",
-                                "Delivery_Time": "01.01.0001 0:00:00"
-                        }
-                ],
-                "ContactInformation": {
-                        "City": "Алматы",
-                        "Delivery_Condition": "Доставка",
-                        "Date_Time_delivery": "2023-08-16 К До 20:00",
-                        "Time_Window": "15:00-18:00",
-                        "Latitude": "43,3189165",
-                        "Longitude": "76,93994950000001",
-                        "Street": "нет данных",
-                        "Home": "74",
-                        "Phone": "(747)2667569",
-                        "Apartment": "нет данных",
-                        "Contractor": "АЛЕКСЕЙ ТРУНКИН"
-                }
-        }
-]
-Saving message to database: [
-        {
-                "Number": "№№00015934",
-                "Date": "14.08.2023 15:53:04",
-                "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-                "DocumentStatus": "Оформлена",
-                "Driver": "",
-                "ISR": "(747)2667569",
-                "Informal_Document": "Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12",
-                "SKU_Weight": "",
-                "ArrayStrings": [
-                        {
-                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
-                                "Goods": "",
-                                "Quantity": "1",
-                                "Item_Status": "",
-                                "Pickup_Point": "0",
-                                "Delivery_Point": "0",
-                                "Pickup_Latitude": "0",
-                                "Pickup_Longitude": "0",
-                                "Delivery_Latitude": "0",
-                                "Delivery_Longitude": "0",
-                                "Pickup_Time": "01.01.0001 0:00:00",
-                                "Delivery_Time": "01.01.0001 0:00:00"
-                        },
-                        {
-                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
-                                "Goods": "",
-                                "Quantity": "1",
-                                "Item_Status": "",
-                                "Pickup_Point": "0",
-                                "Delivery_Point": "0",
-                                "Pickup_Latitude": "0",
-                                "Pickup_Longitude": "0",
-                                "Delivery_Latitude": "0",
-                                "Delivery_Longitude": "0",
-                                "Pickup_Time": "01.01.0001 0:00:00",
-                                "Delivery_Time": "01.01.0001 0:00:00"
-                        }
-                ],
-                "ContactInformation": {
-                        "City": "Алматы",
-                        "Delivery_Condition": "Доставка",
-                        "Date_Time_delivery": "2023-08-16 К До 20:00",
-                        "Time_Window": "15:00-18:00",
-                        "Latitude": "43,3189165",
-                        "Longitude": "76,93994950000001",
-                        "Street": "нет данных",
-                        "Home": "74",
-                        "Phone": "(747)2667569",
-                        "Apartment": "нет данных",
-                        "Contractor": "АЛЕКСЕЙ ТРУНКИН"
-                }
-        }
-]
 [
-  {
-    Number: '№№00015934',
-    Date: '14.08.2023 15:53:04',
-    Organization: 'TOO Gulser Computers (Гулсер Компьютерс)',
-    DocumentStatus: 'Оформлена',
-    Driver: '',
-    ISR: '(747)2667569',
-    Informal_Document: 'Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12',
-    SKU_Weight: '',
-    ArrayStrings: [ [Object], [Object] ],
-    ContactInformation: {
-      City: 'Алматы',
-      Delivery_Condition: 'Доставка',
-      Date_Time_delivery: '2023-08-16 К До 20:00',
-      Time_Window: '15:00-18:00',
-      Latitude: '43,3189165',
-      Longitude: '76,93994950000001',
-      Street: 'нет данных',
-      Home: '74',
-      Phone: '(747)2667569',
-      Apartment: 'нет данных',
-      Contractor: 'АЛЕКСЕЙ ТРУНКИН'
+    {
+        "_id": "650420f9a51d74bf510284b8",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "650420f9a51d74bf510284b9",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "65042165a51d74bf510284bd",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "65042165a51d74bf510284be",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "650426fba9aec2b30f372954",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "650426fba9aec2b30f372955",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "650428f32a6cbde49402475a",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "650428f32a6cbde49402475b",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c28396f8ba59c51fcb83",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c28396f8ba59c51fcb84",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c2a7eef887ca82fbbeb1",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c2a7eef887ca82fbbeb2",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c30110500d227e070d40",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c30110500d227e070d41",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c30910500d227e070d44",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c30910500d227e070d45",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c44310500d227e070d48",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c44310500d227e070d49",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c45dafe0e59b175f875c",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507c45dafe0e59b175f875d",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507ca3a2b2baa2eb53054ca",
+        "ArrayStrings": [],
+        "__v": 0
+    },
+    {
+        "_id": "6507ca3a2b2baa2eb53054cb",
+        "ArrayStrings": [],
+        "__v": 0
     }
-  }
-] parsedData
-Saving message to database: [
-        {
-                "Number": "№№00015934",
-                "Date": "14.08.2023 15:53:04",
-                "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-                "DocumentStatus": "Оформлена",
-                "Driver": "",
-                "ISR": "(747)2667569",
-                "Informal_Document": "Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12",
-                "SKU_Weight": "",
-                "ArrayStrings": [
-                        {
-                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
-                                "Goods": "",
-                                "Quantity": "1",
-                                "Item_Status": "",
-                                "Pickup_Point": "0",
-                                "Delivery_Point": "0",
-                                "Pickup_Latitude": "0",
-                                "Pickup_Longitude": "0",
-                                "Delivery_Latitude": "0",
-                                "Delivery_Longitude": "0",
-                                "Pickup_Time": "01.01.0001 0:00:00",
-                                "Delivery_Time": "01.01.0001 0:00:00"
-                        },
-                        {
-                                "Shipping_Point": "Алматы ул.Кабдолова 1/4 ТРК Grand Park",
-                                "Goods": "",
-                                "Quantity": "1",
-                                "Item_Status": "",
-                                "Pickup_Point": "0",
-                                "Delivery_Point": "0",
-                                "Pickup_Latitude": "0",
-                                "Pickup_Longitude": "0",
-                                "Delivery_Latitude": "0",
-                                "Delivery_Longitude": "0",
-                                "Pickup_Time": "01.01.0001 0:00:00",
-                                "Delivery_Time": "01.01.0001 0:00:00"
-                        }
-                ],
-                "ContactInformation": {
-                        "City": "Алматы",
-                        "Delivery_Condition": "Доставка",
-                        "Date_Time_delivery": "2023-08-16 К До 20:00",
-                        "Time_Window": "15:00-18:00",
-                        "Latitude": "43,3189165",
-                        "Longitude": "76,93994950000001",
-                        "Street": "нет данных",
-                        "Home": "74",
-                        "Phone": "(747)2667569",
-                        "Apartment": "нет данных",
-                        "Contractor": "АЛЕКСЕЙ ТРУНКИН"
-                }
-        }
 ]
-[
-  {
-    Number: '№№00015934',
-    Date: '14.08.2023 15:53:04',
-    Organization: 'TOO Gulser Computers (Гулсер Компьютерс)',
-    DocumentStatus: 'Оформлена',
-    Driver: '',
-    ISR: '(747)2667569',
-    Informal_Document: 'Заказ покупателя ППО 000000080238 от 14.08.2023 15:46:12',
-    SKU_Weight: '',
-    ArrayStrings: [ [Object], [Object] ],
-    ContactInformation: {
-      City: 'Алматы',
-      Delivery_Condition: 'Доставка',
-      Date_Time_delivery: '2023-08-16 К До 20:00',
-      Time_Window: '15:00-18:00',
-      Latitude: '43,3189165',
-      Longitude: '76,93994950000001',
-      Street: 'нет данных',
-      Home: '74',
-      Phone: '(747)2667569',
-      Apartment: 'нет данных',
-      Contractor: 'АЛЕКСЕЙ ТРУНКИН'
-    }
-  }
-] parsedData
