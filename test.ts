f950b7a01986ea12c61686d80a4b311307355c2f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-20 16:14:00 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,162 +1,116 @@
-<input type="checkbox" id="switch" /><label for="switch">Toggle</label>
+<template>
+  <div class="filters">
+    <div class="filter-online">
+      <IconComponent
+        :сonfig="{
+          name: 'filter',
+          color: '#23362D4D',
+          width: 24,
+          height: 24,
+        }"
+      ></IconComponent>
+      <span class="filter-online__text">Сперва онлайн</span>
+    </div>
+    <div class="search">
+      <search-field-component
+        :placeholder="'Введите имя или машину'"
+      ></search-field-component>
+    </div>
+    <div class="set-city">
+      <dropdown-component
+        :items="cities"
+        :width="350"
+        :currentValue="'Выберите город'"
+        @onSelect="emits('selectCity', $event)"
+      ></dropdown-component>
+    </div>
+    <div class="geofence-switch">
+      <toggle-component></toggle-component>
+      <p>Показывать геозоны</p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import DropdownComponent from '../../../components/global/dropdown/dropdown.vue';
+import SearchFieldComponent from '../../../components/global/fields/search-field/search-field.vue';
+import IconComponent from '../../../components/global/icon/icon.component.vue';
+import ToggleComponent from '../../../components/global/toggle/toggle.vue';
 
+defineProps<{
+  cities: string[];
+}>();
 
-input[type=checkbox]{
-  height: 0;
-  width: 0;
-  visibility: hidden;
+const emits = defineEmits(['selectCity', 'downloadLoadersAsXLSX']);
+</script>
+<style scoped>
+.filters {
+  width: 100%;
+  height: 64px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  /* border: 1px solid rgb(230, 100, 40); */
+  padding: 0 20px;
 }
 
-label {
-  cursor: pointer;
-  text-indent: -9999px;
+
+.date_picker {
+  --dp-border-radius: 16px;
+  /* --dp-cell-border-radius: 4px;  */
   width: 200px;
-  height: 100px;
-  background: grey;
-  display: block;
-  border-radius: 100px;
-  position: relative;
 }
 
-label:after {
-  content: '';
-  position: absolute;
-  top: 5px;
-  left: 5px;
-  width: 90px;
-  height: 90px;
-  background: #fff;
-  border-radius: 90px;
-  transition: 0.3s;
+.date_picker .dp__outer_menu_wrap .dp--menu-wrapper {
+  --dp-border-radius: 4px;
+  --dp-cell-border-radius: 4px;
 }
 
-input:checked + label {
-  background: #bada55;
-}
-
-input:checked + label:after {
-  left: calc(100% - 5px);
-  transform: translateX(-100%);
-}
-
-label:active:after {
-  width: 130px;
-}
-
-// centering
-body {
+.search {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  gap: 8px;
+min-width: 600px;
 }
 
 
-
-
-
-
-
-
-
-
-
-
-/////////////////////////
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<label class="toggle">
-  <input class="toggle-checkbox" type="checkbox">
-  <div class="toggle-switch"></div>
-  <span class="toggle-label">Wi-fi</span>
-</label>
-
-<hr>
-
-<label class="toggle">
-  <input class="toggle-checkbox" type="checkbox" checked>
-  <div class="toggle-switch"></div>
-  <span class="toggle-label">Bluetooth</span>
-</label>
-
-
-
-
-
-*,
-*:before,
-*:after {
+.set-date {
+  display: flex;
+  height: 42px;
+  padding: 6px 16px;
+  align-items: center;
+  gap: 8px;
+  border-radius: 80px;
   box-sizing: border-box;
+  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
+  background: #fff;
 }
 
-body {
-  font-family: -apple-system, ".SFNSText-Regular", "Helvetica Neue", "Roboto", "Segoe UI", sans-serif;
+.filter-online {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  justify-content: center;
+  border-radius: 58px;
+  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
+  background: #fff;
+  padding: 8px 16px 8px 8px;
+  
 }
-
-.toggle {
-  cursor: pointer;
-  display: inline-block;
+.filter-online__text {
+  color: var(--text-dark, #23362d);
+  font-family: Rubik;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 20px;
+  letter-spacing: 0.25px;
 }
-
-.toggle-switch {
-  display: inline-block;
-  background: #ccc;
-  border-radius: 16px;
-  width: 58px;
-  height: 32px;
-  position: relative;
-  vertical-align: middle;
-  transition: background 0.25s;
+.geofence-switch {
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  justify-content: center;
 }
-.toggle-switch:before, .toggle-switch:after {
-  content: "";
-}
-.toggle-switch:before {
-  display: block;
-  background: linear-gradient(to bottom, #fff 0%, #eee 100%);
-  border-radius: 50%;
-  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.25);
-  width: 24px;
-  height: 24px;
-  position: absolute;
-  top: 4px;
-  left: 4px;
-  transition: left 0.25s;
-}
-.toggle:hover .toggle-switch:before {
-  background: linear-gradient(to bottom, #fff 0%, #fff 100%);
-  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.5);
-}
-.toggle-checkbox:checked + .toggle-switch {
-  background: #56c080;
-}
-.toggle-checkbox:checked + .toggle-switch:before {
-  left: 30px;
-}
-
-.toggle-checkbox {
-  position: absolute;
-  visibility: hidden;
-}
-
-.toggle-label {
-  margin-left: 5px;
-  position: relative;
-  top: 2px;
-}
-
+</style>
