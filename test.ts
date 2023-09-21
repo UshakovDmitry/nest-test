1d1175e7bd97e22693ce9c59fccb44787bab2648
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-21 11:55:04 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,10 @@
-      {
-        path: 'couriers/:id',
-        name: 'CourierDetail',
-        component: CourierDetailComponent,
-      },
+  <router-link to="/couriers">
+      <IconComponent
+        :сonfig="{
+          name: 'peopleIcon',
+          color: isActive('/couriers') ? '#01A254' : '#23362D4D',
+          width: 24,
+          height: 24,
+        }"
+      >
+      </IconComponent>
