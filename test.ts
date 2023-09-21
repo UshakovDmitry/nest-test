08d5ad248e8a6f02e32488ab4a73047d1e5a11af
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-21 09:58:33 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,39 +1,11 @@
-import { httpProvider } from "../providers/http.provider";
-import { urls } from "../resourse/config/urls";
-
-export function useGetApi(method: string, params?: string | undefined) {
-    console.log(method, 'method');
-    
-    function getURL(apiName: string): string {
-        const p: string = params ? params : "";
-        
-        // Изменяем проверку окружения
-        const isProduction = Boolean(import.meta.env.PROD);
-        
-        if (isProduction) {
-            return urls[apiName].production + p;
-        }
-        return urls[apiName].development + p;
-    }
-
-    function fetch(apiName: string) {
-        const URL: string = getURL(apiName);
-        const METHOD: string = "GET";
-
-        return httpProvider(URL, METHOD);
-    }
-
-    const methods: { [key: string]: Function } = {
-        getMessages: () => fetch("messages/getMessages"),
-    };
-
-    return methods[method]();
-}
-
-// Убираем getToken из объекта urls
-export const urls: any = {
-    getMessages: {
-        production: 'https://tms.next.local/api',
-        development: 'http://127.0.0.1:5173/api',
-    },
-};
+getHTTP.service.ts:16 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'development')
+    at getURL (getHTTP.service.ts:16:30)
+    at fetch (getHTTP.service.ts:20:29)
+    at Object.getMessages (getHTTP.service.ts:27:28)
+    at useGetApi (getHTTP.service.ts:30:26)
+    at TransportRequestsViewModel.getData (applications.viewmodel.ts:14:28)
+    at new TransportRequestsViewModel (applications.viewmodel.ts:10:10)
+    at setup (applications.component.vue:31:3)
+    at callWithErrorHandling (runtime-core.esm-bundler.js:158:18)
+    at setupStatefulComponent (runtime-core.esm-bundler.js:7236:25)
+    at setupComponent (runtime-core.esm-bundler.js:7197:36)
