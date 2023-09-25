87bf85fb75189231344cbc2ad9ded605abae9ba2
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-25 17:35:21 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,118 @@
-https://courier.yandex.ru/vrs/api/v1/result/mvrp/22cc3628-26f81dfa-a91c4ad4-15a49418
+export interface IContactInformation {
+  city: string;
+  deliveryCondition: string;
+  dateTimeDelivery: string;
+  timeWindow: string;
+  latitude: string;
+  longitude: string;
+  street: string;
+  home: string;
+  phone: string;
+  apartment: string;
+  contractor: string;
+  id: string;
+}
 
-'"@nestjs/common"' has no exported member named 'HttpModule'. Did you mean 'HttpCode'?ts(2724)
-http-code.decorator.d.ts(11, 25): 'HttpCode' is declared here.
-import HttpModule
+import { type IOrder } from './order.interface';
+
+export interface IDriver {
+  id: number;
+  status: string;
+  fullName: string;
+  phone: string;
+  car_model: string;
+  car_number: string;
+  all_orders: number | string;
+  completed_orders: number | string;
+  delayed_orders: number | string;
+  orders_on_time: number | string;
+  current_order: string;
+  current_location: string;
+  isActive?: boolean;
+  orders: IOrder[];
+}
+
+export interface ILoader {
+  fullName: string;
+  warehouseNumber: string;
+  schedule: string;
+  carNumber: string;
+  isActive: boolean;
+}
+export interface IOrder {
+  shippingPoint: string;
+  goods: string;
+  quantity: string;
+  itemStatus: string;
+  pickupPoint: string;
+  deliveryPoint: string;
+  pickupLatitude: string;
+  pickupLongitude: string;
+  deliveryLatitude: string;
+  deliveryLongitude: string;
+  pickupTime: string;
+  deliveryTime: string;
+}
+
+export interface ITransport {
+  model: number | string;
+  number: number | string;
+  type: string;
+  volume: string;
+  loadCapacity: number | string;
+  city: number | string;
+  isActive: boolean;
+  hasDriver: boolean;
+  schedule: string;
+}
+
+import { type IContactInformation } from './contactInformation.interface';
+import { type IOrder } from './order.interface';
+
+export interface ITransportRequest {
+  id: string;
+  number: string;
+  date: string;
+  organization: string;
+  documentStatus: string;
+  driver: string;
+  ISR: string;
+  informalDocument: string;
+  skuWeight: string;
+  orders: IOrder[];
+  contactInformation: IContactInformation;
+}
+
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "useDefineForClassFields": true,
+    "module": "ESNext",
+    "lib": ["ES2020", "DOM", "DOM.Iterable"],
+    "skipLibCheck": true,
+    "moduleResolution": "bundler",
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "preserve",
+    "strict": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noFallthroughCasesInSwitch": true
+  },
+  "include": ["src/**/*.ts", "src/**/*.d.ts", "src/**/*.tsx", "src/**/*.vue"],
+  "exclude": ["node_modules", "src/**/*.vue.js"],
+  "references": [{ "path": "./tsconfig.node.json" }]
+}
+
+
+
+
+src/domain/entities/ContactInformation.ts(1,42): error TS2307: Cannot find module '../interfaces/contactInformation.interface' or its corresponding type declarations.
+   src/domain/entities/Driver.ts(1,29): error TS2307: Cannot find module '../interfaces/order.interface' or its corresponding type declarations.
+   src/domain/entities/Order.ts(1,29): error TS2307: Cannot find module '../interfaces/order.interface' or its corresponding type declarations.
+  src/domain/entities/TransportRequest.ts(2,29): error TS2307: Cannot find module '../interfaces/order.interface' or its corresponding type declarations.
+  src/domain/entities/TransportRequest.ts(4,42): error TS2307: Cannot find module '../interfaces/contactInformation.interface' or its corresponding type declarations.
+  src/domain/interfaces/driver.interface.ts(1,29): error TS2307: Cannot find module './order.interface' or its corresponding type declarations.
+   src/domain/interfaces/transportRequest.interface.ts(1,42): error TS2307: Cannot find module './contactInformation.interface' or its corresponding type declarations.
+  src/domain/interfaces/transportRequest.interface.ts(2,29): error TS2307: Cannot find module './order.interface' or its corresponding type declarations.
