e5c689065905d4c89bf7c140d39622aa4927b991
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-26 16:25:46 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,46 +1,28 @@
-Request URL:
-http://tms.next.local/api/messages
-Request Method:
-GET
-Status Code:
-502 Bad Gateway
-Remote Address:
-10.0.0.101:80
-Referrer Policy:
-strict-origin-when-cross-origin
-Content-Length:
-559
-Content-Type:
-text/html
-Date:
-Mon, 18 Sep 2023 10:05:07 GMT
-Server:
-nginx/1.25.1
-Accept:
-*/*
-Accept-Encoding:
-gzip, deflate
-Accept-Language:
-en-US,en;q=0.9,ru;q=0.8,ru-RU;q=0.7,en-GB;q=0.6,bg;q=0.5
-Connection:
-keep-alive
-Cookie:
-_ga=GA1.1.1724716743.1684227843; _ga_5RG0MJMH57=GS1.1.1692674301.5.0.1692674301.60.0.0; _ga_64PLKRVRJ0=GS1.1.1692691950.6.0.1692691950.0.0.0
-Host:
-tms.next.local
-Referer:
-http://tms.next.local/applications
-Sec-Ch-Ua:
-"Not/A)Brand";v="99", "Google Chrome";v="115", "Chromium";v="115"
-Sec-Ch-Ua-Mobile:
-?1
-Sec-Ch-Ua-Platform:
-"Android"
-Sec-Fetch-Dest:
-empty
-Sec-Fetch-Mode:
-cors
-Sec-Fetch-Site:
-same-origin
-User-Agent:
-Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Mobile Safari/537.36
+async getMessagesByDrivers(): Promise<any[]> {
+    return await this.messageModel.aggregate([
+        {
+            $group: {
+                _id: '$Driver',
+                orders: {
+                    $push: {
+                        number: "$Number",
+                        date: "$Date",
+                        organization: "$Organization",
+                        documentStatus: "$DocumentStatus",
+                        informal_document: "$Informal_Document",
+                        sku_weight: "$SKU_Weight",
+                        details: "$ArrayStrings",
+                        contactInformation: "$ContactInformation"
+                    }
+                }
+            }
+        },
+        {
+            $project: {
+                driver: '$_id',
+                orders: 1,
+                _id: 0
+            }
+        }
+    ]).exec();
+}
