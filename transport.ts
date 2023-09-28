3419c0ef544c0cb6baaedea245298118be1471f3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-28 11:44:40 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,193 +1,66 @@
-мне нужно реализовать POST запрос который будет теперь дополнительно дату и возвращаться водителей у которых поле DateCreated совпадает в бд
-реализуй и напиши пример запроса для проверки в постмане
-
 drivers.controller.ts
-import { Controller, Get } from '@nestjs/common';
-import { DriversService } from './drivers.service';
-import { ApiTags } from '@nestjs/swagger';
 
-@ApiTags('drivers')
+import { Controller, Get, Post, Body } from '@nestjs/common';
+
+// ... остальной код ...
+
 @Controller('/api/drivers')
 export class DriversController {
-  constructor(private readonly driversService: DriversService) {}
+    // ... остальной код ...
 
-  @Get()
-  async getDrivers() {
-    return await this.driversService.getDrivers();
-  }
-}
-
-drivers.service.ts
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MessageDocument } from '../schemas/message.shema';
-import { DBService } from '../db/db.service'; 
-@Injectable()
-export class DriversService {
-  constructor(
-    @InjectModel('Message') private messageModel: Model<MessageDocument>,
-    private readonly dbService: DBService, 
-  ) {}
-
-  async getDrivers() {
-    return await this.dbService.getDriversTest(); 
-  }
+    @Post('/by-date')
+    async getDriversByDate(@Body('DateCreated') date: string) {
+        return await this.driversService.getDriversByDate(date);
+    }
 }
 
 
 
 
 
-db.service.ts
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MessageDocument } from '../schemas/message.shema';
+
+
+
+// ... остальной код ...
 
 @Injectable()
 export class DBService {
-  constructor(
-    @InjectModel('Message') private messageModel: Model<MessageDocument>,
-  ) {}
+    // ... остальной код ...
 
-  async saveMessage(messageData: any) {
-    try {
-      const parsedData =
-        typeof messageData === 'string' ? JSON.parse(messageData) : messageData;
-        // TODO - Создание поля DateCreated для дальнейшей сортировки по дате
-      const currentDate = new Date();
-      parsedData.DateCreated = `${currentDate.getDate()}-${
-        currentDate.getMonth() + 1
-      }-${currentDate.getFullYear()}`;
-
-      const createdMessage = new this.messageModel(parsedData);
-      return createdMessage.save();
-    } catch (error) {
-      console.error('Ошибка сохранения в бд:', error);
-      throw error;
+    async getDriversByDate(date: string): Promise<any[]> {
+        return await this.messageModel.find({ DateCreated: date }).exec();
     }
-  }
-
-  async getAllMessages(): Promise<any[]> {
-    return await this.messageModel.find().exec();
-  }
-
 }
 
-  
-схема записи
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
-import * as mongoose from 'mongoose';
 
-export type MessageDocument = Document & Message;
 
-@Schema()
-class ArrayStrings {
-  @Prop()
-  Shipping_Point: string;
-  @Prop()
-  Goods: string;
-  @Prop()
-  Quantity: string;
-  @Prop()
-  Item_Status: string;
-  @Prop()
-  Pickup_Point: string;
-  @Prop()
-  Delivery_Point: string;
-  @Prop()
-  Pickup_Latitude: string;
-  @Prop()
-  Pickup_Longitude: string;
-  @Prop()
-  Delivery_Latitude: string;
-  @Prop()
-  Delivery_Longitude: string;
-  @Prop()
-  Pickup_Time: string;
-  @Prop()
-  Delivery_Time: string;
+
+
+
+
+
+
+
+// ... остальной код ...
+
+@Injectable()
+export class DriversService {
+    // ... остальной код ...
+
+    async getDriversByDate(date: string) {
+        return await this.dbService.getDriversByDate(date); 
+    }
 }
 
-@Schema()
-class ContactInformation {
-  @Prop()
-  City: string;
-  @Prop()
-  Delivery_Condition: string;
-  @Prop()
-  Date_Time_delivery: string;
-  @Prop()
-  Time_Window: string;
-  @Prop()
-  Latitude: string;
-  @Prop()
-  Longitude: string;
-  @Prop()
-  Street: string;
-  @Prop()
-  Home: string;
-  @Prop()
-  Phone: string;
-  @Prop()
-  Apartment: string;
-  @Prop()
-  Contractor: string;
+
+
+
+
+
+
+{
+    "DateCreated": "28-10-2023"
 }
 
-@Schema({ versionKey: false })
-class StructureQuantities {
-  @Prop()
-  TotalWeight: string;
-  @Prop()
-  TotalAmount: string;
-}
 
-@Schema({ versionKey: false })
-class ArrayChronologies {
-  @Prop()
-  PPO: string;
 
-  @Prop([String])
-  Chronology: string[];
-}
-
-@Schema({ versionKey: false })
-export class Message {
-  @Prop()
-  Number: string;
-  @Prop()
-  Date: string;
-  @Prop()
-  DateCreated: string;
-  @Prop()
-  Organization: string;
-  @Prop()
-  DocumentStatus: string;
-  @Prop()
-  Driver: string;
-  @Prop()
-  ISR: string;
-  @Prop()
-  NuberPPO: string;
-  @Prop()
-  TypePayment: string;
-  @Prop()
-  loanAgreementStatus: string;
-  @Prop()
-  Informal_Document: string;
-  @Prop()
-  FilterContractor: string;
-  @Prop({ type: mongoose.Schema.Types.Array })
-  ArrayStrings: ArrayStrings[];
-  @Prop()
-  ContactInformation: ContactInformation;
-  @Prop()
-  StructureQuantities: StructureQuantities;
-  @Prop({ type: mongoose.Schema.Types.Array })
-  ArrayChronologies: ArrayChronologies[];
-}
-
-export const MessageSchema = SchemaFactory.createForClass(Message);
