46e7807d999c719e773bf88505989a5c107c5310
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-28 14:49:01 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,66 +1,18 @@
-drivers.controller.ts
 
-import { Controller, Get, Post, Body } from '@nestjs/common';
-
-// ... остальной код ...
-
-@Controller('/api/drivers')
-export class DriversController {
-    // ... остальной код ...
-
-    @Post('/by-date')
-    async getDriversByDate(@Body('DateCreated') date: string) {
-        return await this.driversService.getDriversByDate(date);
-    }
-}
+    const prepareCollection = collection.map((item) => item.ArrayStrings);
+      console.log('prepareCollection', prepareCollection);
 
 
+prepareCollection  [
 
-
-
-
-
-
-// ... остальной код ...
-
-@Injectable()
-export class DBService {
-    // ... остальной код ...
-
-    async getDriversByDate(date: string): Promise<any[]> {
-        return await this.messageModel.find({ DateCreated: date }).exec();
-    }
-}
-
-
-
-
-
-
-
-
-
-
-// ... остальной код ...
-
-@Injectable()
-export class DriversService {
-    // ... остальной код ...
-
-    async getDriversByDate(date: string) {
-        return await this.dbService.getDriversByDate(date); 
-    }
-}
-
-
-
-
-
-
-
-{
-    "DateCreated": "28-10-2023"
-}
-
-
-
+    [
+        {
+            "name" : "Lbvf"
+        }
+    ],
+    [
+           {
+            "name" : "Lbvf"
+        }
+    ]
+]
