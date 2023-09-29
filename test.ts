98e1210542d10136b22e3b15e0668082b9cc338e
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-09-29 16:18:43 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,3 @@
-  goToTransportRequestDetail(row: any) {
-    router.push({
-      name: 'ApplicationDetail',
-      params: { id: row.number },
-      query: {
-        number: row.number,
-      },
-    });
-  }
-
-
 import router from '../../router';
 import { ApplicationDetail } from './application-detail.model';
 
@@ -20,10 +9,48 @@ export class ApplicationDetailViewModel {
     this.getTransportRequestByNumber();
   }
 
-  getTransportRequestByNumber() {}
+  async getTransportRequestByNumber() {
+    try {
+      const response = await fetch('/api/getTransportRequests/getByNumber', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ number: this.model.number }) // или из router.currentRoute.query.number, если модель еще не обновлена
+      });
+
+      if (!response.ok) {
+        console.error('Сетевой ответ не был ok.', response.statusText);
+        return;
+      }
+
+      const data = await response.json();
+      console.log(data); // Здесь будут данные от сервера
+      // TODO: Обновите вашу модель данными от сервера, если это необходимо
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    }
+  }
 
   test() {
     console.log('test');
   }
-    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
