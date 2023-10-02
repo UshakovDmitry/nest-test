876a8766719a91c5100249d10cad92e80ddc7eeb
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-02 09:42:48 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,72 +1,52 @@
-       for (const driver of collectionDriverNames) {
-      const driverTransportRequest = {
-        number: '',
-        date: '',
-        dateCreated: '',
-        organization: '',
-        documentStatus: '',
-        ISR: '',
-        nuberPPO: '',
-        informalDocument: '',
-        filterContractor: '',
-        loanAgreementStatus: '',
-        typePayment: '',
-        // quantities: {
-        //   totalWeight: '',
-        //   totalAmount: '',
-        // },
-        chronologies: [],
-        contactInformation: {},
-        orders: [],
-      };
-
-      const driverOne = {
-        driver: '',
+for (const driver of collectionDriverNames) {
+    const driverOne = {
+        driver: driver, // можно сразу присвоить значение
         сarNumber: '',
         carModel: '',
         transportRequests: [],
-      };
-      driverOne.transportRequests.push(driverTransportRequest);
+    };
 
-      driverOne.driver = driver;
-
-      const collection = await this.messageModel
+    const collection = await this.messageModel
         .find({ Driver: driver })
         .exec();
-      for (const item of collection) {
+
+    for (const item of collection) {
+        const driverTransportRequest = {
+            number: item.Number,
+            date: item.Date,
+            dateCreated: item.DateCreated,
+            organization: item.Organization,
+            documentStatus: item.DocumentStatus,
+            ISR: item.ISR,
+            nuberPPO: item.NumberPPO,
+            informalDocument: item.Informal_Document,
+            filterContractor: item.FilterContractor,
+            loanAgreementStatus: item.loanAgreementStatus,
+            typePayment: item.TypePayment,
+            chronologies: item.ArrayChronologies,
+            contactInformation: item.ContactInformation,
+            orders: []
+        };
+
         driverOne.сarNumber = item.NumberCar;
         driverOne.carModel = item.CarModel;
-        driverTransportRequest.number = item.Number;
-        driverTransportRequest.date = item.Date;
-        driverTransportRequest.dateCreated = item.DateCreated;
-        driverTransportRequest.organization = item.Organization;
-        driverTransportRequest.documentStatus = item.DocumentStatus;
-        driverTransportRequest.ISR = item.ISR;
-        driverTransportRequest.nuberPPO = item.NumberPPO;
-        driverTransportRequest.informalDocument = item.Informal_Document;
-        driverTransportRequest.filterContractor = item.FilterContractor;
-        driverTransportRequest.loanAgreementStatus = item.loanAgreementStatus;
-        driverTransportRequest.typePayment = item.TypePayment;
-        // driverTransportRequest.quantities.totalWeight = item.StructureQuantities.TotalWeight;
-        // driverTransportRequest.quantities.totalAmount = item.StructureQuantities.TotalAmount;
-        driverTransportRequest.chronologies = item.ArrayChronologies;
-        driverTransportRequest.contactInformation = item.ContactInformation;
-      }
-      console.log('collection', collection);
 
-      const prepareOrders = collection.map((item) => item.ArrayStrings);
+        const prepareOrders = collection.map((item) => item.ArrayStrings);
+        const flatOrders = prepareOrders.flat();
+        const hashSummSet = new Set();
 
-      const flatOrders = prepareOrders.flat();
+        flatOrders.forEach((order) => {
+            const hs = hashSumm(JSON.stringify(order));
+            if (!hashSummSet.has(hs)) {
+                hashSummSet.add(hs);
+                driverTransportRequest.orders.push(order);
+            }
+        });
 
-      const hashSummSet = new Set();
-      flatOrders.forEach((order) => {
-        const hs = hashSumm(JSON.stringify(order));
-        if (!hashSummSet.has(hs)) {
-          hashSummSet.add(hs);
-          driverTransportRequest.orders.push(order);
-        }
-      });
-
-      drivers.push(driverOne);
+        driverOne.transportRequests.push(driverTransportRequest);
     }
-    return drivers;
+
+    drivers.push(driverOne);
+}
+
+return drivers;
