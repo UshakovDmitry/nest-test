d323d84b92b873f171b1d284ff05a636cf20eece
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-02 10:37:38 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,13 +2,12 @@
   <div class="chronologies">
     <div
       class="chronologies__item"
-      v-for="(status, index) in allStatuses"
+      v-for="(status, index) in defaultStatusesPPO"
       :key="index"
-      :style="{
-        width: '25%', /* Подразумевается равное распределение между статусами, можете адаптировать под свои нужды */
-        backgroundColor: chronologies.includes(status) ? 'green' : 'gray',
-      }"
-    >{{ status }}</div>
+      :style="{ done: chronologies.includes(status) }"
+    >
+      {{ status }}
+    </div>
   </div>
 </template>
 
@@ -18,11 +17,37 @@ defineProps({
     type: Array,
     required: true,
   },
+  defaultStatusesPPO: {
+    type: Array,
+    required: true,
+  },
 });
 
-const allStatuses = ['оформлена', 'доставляется', 'доставлен', 'завершен'];
 </script>
 
 <style scoped>
-/* ... ваш стиль ... */
+.chronologies {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 10px;
+  width: 100%;
+  height: 10px;
+  margin-top: 20px;
+}
+.chronologies__item {
+  height: 10px;
+  border-radius: 10px;
+}
+.done {
+  background-color: #259451;
+}
 </style>
+
+
+
+Type '{ done: boolean; }' is not assignable to type 'StyleValue'.
+  Object literal may only specify known properties, and 'done' does not exist in type 'CSSProperties | StyleValue[]'.ts(2322)
+runtime-dom.d.ts(321, 5): The expected type comes from property 'style' which is declared here on type 'HTMLAttributes & ReservedProps & Record<string, unknown>'
+(property) done: boolean
