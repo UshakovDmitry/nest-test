1dda229afe2f20a35e866bb816e40d90465ceeb5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-02 16:02:12 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -44,3 +44,51 @@ async getAllDrivers() {
 
     return drivers;
 }
+
+
+
+
+
+
+
+
+No overload matches this call.
+  Overload 1 of 2, '(pipeline?: PipelineStage[], options?: AggregateOptions): Aggregate<any[]>', gave the following error.
+    Argument of type '({ $sort: { Driver: number; }; $group?: undefined; } | { $group: { _id: string; transportRequests: { $push: { number: string; date: string; dateCreated: string; organization: string; documentStatus: string; ... 8 more ...; orders: string; }; }; сarNumber: { ...; }; carModel: { ...; }; }; $sort?: undefined; })[]' is not assignable to parameter of type 'PipelineStage[]'.
+      Type '{ $sort: { Driver: number; }; $group?: undefined; } | { $group: { _id: string; transportRequests: { $push: { number: string; date: string; dateCreated: string; organization: string; documentStatus: string; ... 8 more ...; orders: string; }; }; сarNumber: { ...; }; carModel: { ...; }; }; $sort?: undefined; }' is not assignable to type 'PipelineStage'.
+        Type '{ $sort: { Driver: number; }; $group?: undefined; }' is not assignable to type 'PipelineStage'.
+          Type '{ $sort: { Driver: number; }; $group?: undefined; }' is not assignable to type 'Sort'.
+            Types of property '$sort' are incompatible.
+              Type '{ Driver: number; }' is not assignable to type 'Record<string, 1 | Meta | -1>'.
+                Property 'Driver' is incompatible with index signature.
+                  Type 'number' is not assignable to type '1 | Meta | -1'.
+  Overload 2 of 2, '(pipeline: PipelineStage[]): Aggregate<any[]>', gave the following error.
+    Argument of type '({ $sort: { Driver: number; }; $group?: undefined; } | { $group: { _id: string; transportRequests: { $push: { number: string; date: string; dateCreated: string; organization: string; documentStatus: string; ... 8 more ...; orders: string; }; }; сarNumber: { ...; }; carModel: { ...; }; }; $sort?: undefined; })[]' is not assignable to parameter of type 'PipelineStage[]'.ts(2769)
+const aggregation: ({
+    $sort: {
+        Driver: number;
+    };
+    $group?: undefined;
+} | {
+    $group: {
+        _id: string;
+        transportRequests: {
+            $push: {
+                number: string;
+                date: string;
+                dateCreated: string;
+                organization: string;
+                documentStatus: string;
+                ... 8 more ...;
+                orders: string;
+            };
+        };
+        сarNumber: {
+            ...;
+        };
+        carModel: {
+            ...;
+        };
+    };
+    $sort?: undefined;
+})[]
