95f38eca21c1eece863d521d0227ff0913162556
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-02 11:55:50 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,13 @@
 "Date_Time_delivery": "2023-09-30 До 20:00",
+
+
+const dateTimeDelivery = "2023-09-30 До 20:00";
+
+// Используя метод split
+const date = dateTimeDelivery.split(" ")[0];
+console.log(date); // "2023-09-30"
+
+// Используя регулярные выражения
+const regexMatch = dateTimeDelivery.match(/^(\d{4}-\d{2}-\d{2})/);
+const extractedDate = regexMatch ? regexMatch[1] : null;
+console.log(extractedDate); // "2023-09-30"
