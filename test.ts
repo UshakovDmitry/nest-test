b46ea7cf436f724f8b0da8bdb7744ad09666522c
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-03 14:52:37 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,66 +1,5 @@
-<template>
-  <div class="pagination">
-    <div
-      class="first-page-btn"
-      data-test="first-page-btn"
-      @click="goToFirstPage"
-    >
-      <IconComponent
-        :сonfig="{
-          name: 'doubleArrowRight',
-          color: '#23362D4D',
-          width: 28,
-          height: 28,
-        }"
-      ></IconComponent>
-    </div>
-    <div class="prev-page-btn" data-test="prev-page-btn" @click="goToPrevPage">
-      <IconComponent
-        :сonfig="{
-          name: 'keyboardLeft',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-
-    <button
-      v-for="page in displayedPages"
-      :key="page"
-      class="page-btn"
-      data-test="page-btn"
-      @click="goToPage(page)"
-      :class="{ active: currentPage.value === page }"
-    >
-      {{ page }}
-    </button>
-
-    <div class="next-page-btn" data-test="next-page-btn" @click="goToNextPage">
-      <IconComponent
-        :сonfig="{
-          name: 'keyboardRight',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-    <div class="last-page-btn" data-test="last-page-btn" @click="goToLastPage">
-      <IconComponent
-        :сonfig="{
-          name: 'doubleArrowRight',
-          color: '#23362D4D',
-          width: 28,
-          height: 28,
-        }"
-      ></IconComponent>
-    </div>
-  </div>
-</template>
-
 <script setup lang="ts">
-import { ref, watch ,computed} from 'vue';
+import { ref, watch ,computed } from 'vue';
 import IconComponent from '../icon/icon.component.vue';
 
 const props = defineProps({
@@ -102,6 +41,18 @@ const goToPage = (page: number) => {
   emit('page-change', page);
 };
 
+const goToPrevPage = () => {
+  if (currentPage.value > 1) {
+    goToPage(currentPage.value - 1);
+  }
+};
+
+const goToNextPage = () => {
+  if (currentPage.value < props.totalPages) {
+    goToPage(currentPage.value + 1);
+  }
+};
+
 const goToFirstPage = () => {
   goToPage(1);
 };
@@ -119,15 +70,5 @@ watch(
 </script>
 
 <style scoped>
-
+  /* ваш стиль */
 </style>
-
-Property 'goToPrevPage' does not exist on type '{ $: ComponentInternalInstance; $data: {}; $props: { "onPage-change"?: (value: number) => any; readonly totalPages: number; readonly initialPage: number; readonly currentCity?: string; ... 11 more ...; style?: unknown; }; ... 10 more ...; $watch<T extends string | ((...args: any) => any)>(source: T, cb: T extends (....'. Did you mean 'goToPage'?ts(2551)
-any
-
-Property 'goToPrevPage' does not exist on type '{ $: ComponentInternalInstance; $data: {}; $props: { "onPage-change"?: (value: number) => any; readonly totalPages: number; readonly initialPage: number; readonly currentCity?: string; ... 11 more ...; style?: unknown; }; ... 10 more ...; $watch<T extends string | ((...args: any) => any)>(source: T, cb: T extends (....'. Did you mean 'goToPage'?ts(2551)
-any
-Property 'value' does not exist on type 'number'. Did you mean 'valueOf'?ts(2551)
-lib.es5.d.ts(585, 5): 'valueOf' is declared here.
-any
-Property 'goToNextPage' does not exist on type '{ $: ComponentInternalInstance; $data: {}; $props: { "onPage-change"?: (value: number) => any; readonly totalPages: number; readonly initialPage: number; readonly currentCity?: string; ... 11 more ...; style?: unknown; }; ... 10 more ...; $watch<T extends string | ((...args: any) => any)>(source: T, cb: T extends (....'. Did you mean 'goToPage'?ts(2551)
