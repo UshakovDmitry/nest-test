48c364b73dbbe8baf56b618f1cac8a9bcc6c7e90
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-03 09:27:53 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,78 +1,4 @@
-totalPrice: data.StructureQuantities ? String(data.StructureQuantities.TotalAmount) : "0",
-totalWeight: data.StructureQuantities ? String(data.StructureQuantities.TotalWeight) : "0",
-
-
-
-
-{
-    "_id": "65166aa64a53e0d6011408da",
-    "Number": "№№00015684",
-    "Date": "27.09.2023 11:03:39",
-    "DateCreated": "29-9-2023",
-    "Organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-    "DocumentStatus": "Повреждение товара",
-    "Driver": "Чунаев Марат Чакенович",
-    "ISR": "240783461",
-    "NuberPPO": "8149234",
-    "TypePayment": "Кредит",
-    "loanAgreementStatus": "ОПЛАЧЕН",
-    "Informal_Document": "Заказ покупателя ППО",
-    "FilterContractor": "Alser",
-    "ArrayStrings": [
-        {
-            "NuberPPO": "8149234",
-            "PPOStatus": "Сделка завершена",
-            "SKU": "1330426",
-            "Goods": "Умная Колонка Яндекс.Станция Лайт, Мята (YNDX-00025 Green)",
-            "Count": "1",
-            "ShippingAddress": "Almaty, Rayymbeka, 127/147",
-            "Brand": "Яндекс",
-            "Weight": "0,4",
-            "Price": "27 990",
-            "Item_Status": "Забран",
-            "Pickup_Point": "1",
-            "Delivery_Point": "2",
-            "Pickup_Latitude": "12",
-            "Pickup_Longitude": "15",
-            "Delivery_Latitude": "15",
-            "Delivery_Longitude": "14",
-            "Pickup_Time": "01.01.0001 9:00:00",
-            "Delivery_Time": "01.01.0001 15:00:00"
-        }
-    ],
-    "ContactInformation": {
-        "City": "Алматы",
-        "Delivery_Condition": "Доставка",
-        "Date_Time_delivery": "2023-02-03 До 20:00",
-        "Time_Window": "нет данных",
-        "Latitude": "43,253029",
-        "Longitude": "76,938656",
-        "Street": "Кайсар Плаза",
-        "Home": "115",
-        "Phone": "(706)4192015",
-        "Apartment": "Кайсар Плаза",
-        "Contractor": "Таирова Жанета",
-        "_id": "651699e0a26df6599121d3b0"
-    },
-    "StructureQuantities": {
-        "TotalWeight": "0.4",
-        "TotalAmount": "27990",
-        "_id": "651699e0a26df6599121d3b1"
-    },
-    "ArrayChronologies": [
-        {
-            "PPO": "8149234",
-            "Chronology": [
-                "Оформлен",
-                "Доставляется до клиента (на складе отгрузки)",
-                "Доставляется",
-                "Сделка завершена"
-            ]
-        }
-    ],
-    "CarModel": "Gazel",
-    "NumberCar": "M121240"
-}
+Это мой код обработки данных с сервера
 
 import { useGetApi } from '../../domain/services/getHTTP.service';
 import router from '../../router';
@@ -163,15 +89,13 @@ export class TransportRequestsViewModel {
         coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
       },
       quantitties: {
-        totalPrice: String(data.StructureQuantities.TotalAmount),
-        totalWeight: String(data.StructureQuantities.TotalWeight),
+        totalPrice: data.quantities ? String(data.quantities.TotalAmount) : "---",
+        totalWeight: data.quantities ? String(data.quantities.TotalWeight) : "---",
       },
     };
   }
 }
 
-applications.viewmodel.ts:90 Uncaught (in promise) TypeError: Cannot read properties of undefined (reading 'TotalAmount')
-    at TransportRequestsViewModel.transformToTransportForTable (applications.viewmodel.ts:90:53)
-    at applications.viewmodel.ts:21:14
-    at Array.forEach (<anonymous>)
-    at TransportRequestsViewModel.getTransportRequests (applications.viewmodel.ts:16:14)
+в браузере пишет что рекурсивность на 22 строчке
+
+applications.viewmodel.ts:22 [Vue warn]: Maximum recursive updates exceeded in component <applications.component>. This means you have a reactive effect that is mutating its own dependencies and thus recursively triggering itself. Possible sources include component template, render function, updated hook or watcher source function.
