b4b2e0e8b59c9962601a1b7702e114360d0f7d0a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-03 15:14:37 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,19 +1,106 @@
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\frontend> npm run build
+import { useGetApi } from '../../domain/services/getHTTP.service';
+import router from '../../router';
+import { type TransportRequestsModel } from './applications.model';
+// import { ITransportRequest } from '../../domain/interfaces/transportRequest.interface';
 
-> alser.dispatcherworkplaceui@0.0.0 build
-> vue-tsc && vite build
+export class TransportRequestsViewModel {
+  model: TransportRequestsModel;
 
-src/components/global/paginator/paginator.vue:34:37 - error TS2551: Property 'value' does not exist on type 'number'. Did you mean 'valueOf'?
+  constructor(model: any) {
+    this.model = model;
+    this.getTransportRequests();
+  }
 
-34       :class="{ active: currentPage.value === page }"
-                                       ~~~~~
+  async getTransportRequests(): Promise<void> {
+    const response = await useGetApi('getTransportRequests');
+    console.log(response.length, 'кол-во заявок');
+    response.forEach((data: any) => {
+      const transformedData = this.transformToTransportRequest(data);
+      const transformedDataForCities = this.setCitiesList(transformedData);
+this.model.cities.push(transformedDataForCities);      
+      const transformedDataForTable =
+        this.transformToTransportForTable(transformedData);
+      this.model.transportRequests.push(transformedDataForTable);
+    });
+  }
 
-  node_modules/typescript/lib/lib.es5.d.ts:585:5
-    585     valueOf(): number;
-            ~~~~~~~~~~~~~~~~~~
-    'valueOf' is declared here.
+  setCitiesList(data) {
+    return data.contactInformation.City;
+  }
 
+  selectCity(city: string): void {
+    console.log(city);
+  }
 
-Found 1 error in src/components/global/paginator/paginator.vue:34
+  downloadLoadersAsXLSX(): void {
+    alert('Функционал в разработке');
+  }
 
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\frontend>
+  goToTransportRequestDetail(row: any) {
+    console.log('router');
+
+    router.push({
+      name: 'ApplicationDetail',
+      params: { id: row.number },
+      query: {
+        number: row.number,
+      },
+    });
+  }
+
+  transformToTransportRequest(data: any) {
+    return {
+      number: String(data.Number),
+      status: String(data.DocumentStatus),
+      ISR: String(data.ISR),
+      document: String(data.Informal_Document),
+      carModel: String(data.CarModel),
+      carNumber: String(data.NuberCar),
+      numberPPO: String(data.NuberPPO),
+      organization: String(data.Organization),
+      typePayment: String(data.TypePayment),
+      loanAgreementStatus: String(data.loanAgreementStatus),
+      quantities: data.StructureQuantities,
+      chronologies: data.ArrayChronologies,
+      contactInformation: data.ContactInformation,
+      orders: data.ArrayStrings,
+      dataCreated: String(data.DateCreated),
+      date: String(data.Date),
+      driver: String(data.Driver),
+      fiterContractor: String(data.FilterContractor),
+    };
+  }
+
+  transformToTransportForTable(data: any) {
+    return {
+      number: String(data.number),
+      status: String(data.status),
+      ISR: String(data.ISR),
+      document: String(data.document),
+      // shippingAddress: {
+      //   address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
+      //   coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
+      // },
+      recipient: {
+        name: String(data.contactInformation.Contractor),
+        phone: String(data.contactInformation.Phone),
+      },
+      deliveryTime: {
+        date: String(data.date),
+        time: String(data.contactInformation.Date_Time_delivery),
+      },
+      deliveryAddress: {
+        address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
+        coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
+      },
+      quantitties: {
+        totalPrice: data.quantities
+          ? String(data.quantities.TotalAmount)
+          : '-/-/-',
+        totalWeight: data.quantities
+          ? String(data.quantities.TotalWeight)
+          : '---',
+      },
+    };
+  }
+}
