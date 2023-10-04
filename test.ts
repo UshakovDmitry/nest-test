b299de70364921c3e62b3de0ba99e036b02a80b7
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-04 15:22:54 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,31 @@
+
+
+
+
 import { lastValueFrom } from 'rxjs';
+
+
+
+
+
+
+
+
+
+async getCorrectCityName(city: string) {
+    console.log(city, 'ПРИХОДИТ ГОРОД В getCorrectCityName');
+    
+    const apiKey = '06c9301e-6663-4182-b060-81da5969b5f3';
+    const url = `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${apiKey}&geocode=${city}`;
+    console.log(url, 'url');
+
+    try {
+      const response$ = this.httpService.get(url);
+      const response = await lastValueFrom(response$); // используйте lastValueFrom здесь
+      console.log(response.data);
+      return response.data; 
+    } catch (error) {
+      console.error('Ошибка при выполнении GET-запроса:', error);
+      throw error;
+    }
+}
