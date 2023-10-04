33fc150a95686573c626b1423b3de454e7b0c72f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-04 16:19:26 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,92 +1,34 @@
-у меня есть вот такой компонент для отображения инпута для дальнейшей реализации поиска
+<input
+  type="text"
+  data-test="search-input-PPO"
+  v-model="searchValue"
+  @input="updateSearch"
+  :placeholder="props.placeholder"
+  class="search-input"
+/>
 
-<template>
-  <!-- Контейнер для поля поиска и иконки -->
-  <div class="search-container">
-    <div class="icon-wrapper">
-      <IconComponent
-        :сonfig="{
-          name: 'search',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-    <!-- Поле ввода -->
-    <input
-      type="text"
-      data-test="search-input-PPO"
-      :value="searchValue"
-      @input="updateSearch"
-      :placeholder="props.placeholder"
-      class="search-input"
-    />
-  </div>
-</template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import IconComponent from '../../../global/icon/icon.component.vue';
-const props = defineProps<{
-  placeholder: string;
-}>();
-// Определяем эмиттеры для событий
-const emits = defineEmits(['onSearch']);
 
-// Состояние для хранения значения поля поиска
-const searchValue = ref('');
-
-// Функция для обновления значения поиска
 const updateSearch = (event: Event) => {
   // Получаем текущее значение из поля ввода
   const value = (event.target as HTMLInputElement).value;
 
-  // Обновляем локальное состояние
-  searchValue.value = value;
-
   // Эмитим событие с новым значением для родительского компонента
-  emits('onSearch', value);
+  emits('update:modelValue', value);
 };
+
+
+
+<template>
+  <!-- ... другой код ... -->
+  <search-component v-model="searchTerm"></search-component>
+  <div>Текущий поисковый запрос: {{ searchTerm }}</div>
+  <!-- ... другой код ... -->
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+// ... другие импорты ...
+
+const searchTerm = ref('');
 </script>
-
-<style scoped>
-.search-container {
-  /* Расположение иконки и поля ввода в одной строке */
-  display: flex;
-  align-items: center;
-  position: relative;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  border-radius: 80px;
-  width: 100%;
-  overflow: hidden;
-}
-
-.icon-wrapper {
-  /* Позиционирование иконки внутри поля ввода */
-  position: absolute;
-  left: 8px; /* Расстояние от левого края поля ввода */
-  z-index: 1; /* Поверх поля ввода */
-}
-
-.search-input {
-  border-radius: 58px;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  background: #fff;
-  border-radius: 16px;
-  padding: 12px 16px;
-  padding-left: 40px; /* Добавляем отступ слева, чтобы текст не перекрывал иконку */
-  font-size: 14px;
-  border: none;
-  outline: transparent;
-  color: hsl(152, 21%, 17%);
-  width: 100%;
-  position: relative;
-  z-index: 0; /* Под иконкой */
-}
-</style>
-
-и есть страница где он используется 
-я хочу выводить на экран страницы то что ввожу в поиске 
-реализуй это 
-какие проспы и функции я должен передать 
