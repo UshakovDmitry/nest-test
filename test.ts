4b62154e73bb1cfb638ef6ef421e15e4c88a3428
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-04 11:54:16 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,67 +1,34 @@
-Я хочу отображать элемент с текстом Распределена если config.value.number равен true
-иначе отображать элемент с текстом Не распределена если config.value.number равен false
-
-
-
 <template>
-    <div class="cell__wrapper">
-     <p class="number"> {{ config.value.number }}</p>
-     <div class="status"> 
-        <IconComponent
-          :сonfig="{
-            name: 'error',
-            color: '#4caf50',
-            width: 20,
-            height: 20,
-          }"></IconComponent>
-       <p>Распределена</p></div>
+  <div class="cell__wrapper">
+    <p class="number">{{ config.value.number }}</p>
+    <div class="status">
+      <IconComponent
+        :сonfig="{
+          name: config.value.number ? 'success' : 'error', // Заменил на условие для выбора иконки
+          color: config.value.number ? '#4caf50' : 'red',  // Заменил на условие для выбора цвета
+          width: 20,
+          height: 20,
+        }"
+      ></IconComponent>
+      <p v-if="config.value.number">Распределена</p>
+      <p v-else>Не распределена</p>
     </div>
-  </template>
-  
-  <script setup lang="ts">
+  </div>
+</template>
+
+<script setup lang="ts">
 import IconComponent from '../../icon/icon.component.vue';
 
-  defineProps<{
-    config: {
-      type: number;
-      value: any;
-    };
-  }>();
-  </script>
-  
-  <style scoped>
-  .cell__wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: center;
-    gap: 5px;
-    width: 100%;
-    max-width: 250px;
-    height: 100%;
-    padding: 0 0 0 30px;
-    font-size: 14px;
-    line-height: 1.2;
-    color: #23362d;
-    box-sizing: border-box;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: normal; 
-  }
+defineProps<{
+  config: {
+    type: number;
+    value: any;
+  };
+}>();
+</script>
 
-  .number {
-    word-break: break-word; /* Переносит строку на любом символе */
-    max-height: 2.4em; /* Максимальная высота для двух строк, основываясь на вашем line-height */
-    overflow: hidden; /* Скрывает все, что выходит за пределы */
-    display: -webkit-box;
-    -webkit-line-clamp: 2; /* Отображает максимум 2 строки */
-    -webkit-box-orient: vertical;
-  }
-  .status {
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    gap: 5px;
-  }
-  </style>
-  
+<!-- Тут ваш CSS код, который остается без изменений -->
+
+
+
+    
