b320b121c6d554b747bf6ce8b2c0edbd1463a6e5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-04 15:55:57 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,119 +1,46 @@
-import { useGetApi } from '../../domain/services/getHTTP.service';
-import router from '../../router';
-import { type TransportRequestsModel } from './applications.model';
-// import { ITransportRequest } from '../../domain/interfaces/transportRequest.interface';
+setSearchTerm(term: string) {
+  this.searchTerm = term;
+  this.filterRequests();
+}
 
-export class TransportRequestsViewModel {
-  model: TransportRequestsModel;
 
-  constructor(model: any) {
-    this.model = model;
-    this.getTransportRequests();
-  }
 
-  async getTransportRequests(): Promise<void> {
-    const response = await useGetApi('getTransportRequests');
-    console.log(response.length, 'кол-во заявок');
-    // console.log(response, 'response'); 
-    response.forEach((data: any) => {
-      
-      const transformedData = this.transformToTransportRequest(data);
-      const city = this.setCitiesList(transformedData);
-      // Проверяем наличие города в списке и добавляем, если его нет
-      if (!this.model.cities.includes(city)) {
-        this.model.cities.push(city);
-      }
-      const transformedDataForTable =
-        this.transformToTransportForTable(transformedData);
-      console.log(transformedDataForTable, 'transformedDataForTable');
 
-      this.model.transportRequests.unshift(transformedDataForTable);
-    });
-  }
 
-  setCitiesList(data) {
-    return data.contactInformation.City;
-  }
 
-  selectCity(city: string): void {
-    console.log(city);
-  }
 
-  downloadLoadersAsXLSX(): void {
-    alert('Функционал в разработке');
-  }
-
-  goToTransportRequestDetail(row: any) {
-    console.log('router');
-
-    router.push({
-      name: 'ApplicationDetail',
-      params: { id: row.request.number },
-      query: {
-        number: row.request.number,
-      },
-    });
-  }
-
-  transformToTransportRequest(data: any) {
-    return {
-      request: {
-        number: String(data.Number),
-        status: Boolean(data.distribution),
-      },
-      status: String(data.DocumentStatus),
-      ISR: {
-        number:String(data.ISR),
-        status: String(data.loanAgreementStatus),
-      },
-      document: String(data.Informal_Document),
-      carModel: String(data.CarModel),
-      carNumber: String(data.NuberCar),
-      numberPPO: String(data.NuberPPO),
-      organization: String(data.Organization),
-      typePayment: String(data.TypePayment),
-      loanAgreementStatus: String(data.loanAgreementStatus),
-      quantities: data.StructureQuantities,
-      chronologies: data.ArrayChronologies,
-      contactInformation: data.ContactInformation,
-      orders: data.ArrayStrings,
-      dataCreated: String(data.DateCreated),
-      date: String(data.Date),
-      driver: String(data.Driver),
-      fiterContractor: String(data.FilterContractor),
-    };
-  }
-
-  transformToTransportForTable(data: any) {
-    return {
-      request: data.request,
-      status: String(data.status),
-      ISR: data.ISR,
-      document: String(data.document),
-      // shippingAddress: {
-      //   address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
-      //   coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
-      // },
-      recipient: {
-        name: String(data.contactInformation.Contractor),
-        phone: String(data.contactInformation.Phone),
-      },
-      deliveryTime: {
-        date: String(data.date),
-        time: String(data.contactInformation.Date_Time_delivery),
-      },
-      deliveryAddress: {
-        address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
-        coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
-      },
-      quantitties: {
-        totalPrice: data.quantities
-          ? String(data.quantities.TotalAmount)
-          : '-/-/-',
-        totalWeight: data.quantities
-          ? String(data.quantities.TotalWeight)
-          : '---',
-      },
-    };
+filterRequests() {
+  if (this.searchTerm) {
+    this.filteredRequests = this.model.transportRequests.filter(request => 
+      request.request.number.includes(this.searchTerm)
+    );
+  } else {
+    this.filteredRequests = [...this.model.transportRequests];
   }
 }
+
+
+
+
+
+
+export class TransportRequestsViewModel {
+  ...
+  searchTerm: string = ''; 
+  filteredRequests: any[] = [];
+
+  ...
+}
+
+
+
+
+
+
+
+
+
+<!-- Это ваш новый инпут для поиска -->
+<div class="search-input">
+  <input v-model="searchTerm" @input="filterRequests" placeholder="Введите номер для поиска..." />
+</div>
