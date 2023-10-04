54a8f49cc447e5d22bd90b01ec5673a208faebac
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-04 10:59:42 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,124 +1,14 @@
-    {
-        "driver": "Бегимбетов Жаслан Бесембаевич",
-        "carNumber": "",
-        "carModel": "",
-        "transportRequests": [
-            {
-                "number": "№№00148286",
-                "date": "02.10.2023 20:15:40",
-                "dateCreated": "4-10-2023",
-                "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-                "documentStatus": "Доставляется",
-                "ISR": "288355994",
-                "informalDocument": "Заказ покупателя ППО",
-                "filterContractor": "Alser",
-                "loanAgreementStatus": "ПРИНЯТ НАМИ",
-                "typePayment": "Кредит",
-                "chronologies": [
-                    {
-                        "PPO": "8779120",
-                        "Chronology": [
-                            "Оформлен",
-                            "Доставляется до клиента (на складе отгрузки)",
-                            "Доставляется"
-                        ]
-                    }
-                ],
-                "contactInformation": {
-                    "City": "Алматы",
-                    "Delivery_Condition": "Доставка",
-                    "Date_Time_delivery": "2023-10-04 До 20:00",
-                    "Time_Window": "нет данных",
-                    "Latitude": "43.227133",
-                    "Longitude": "76.931335",
-                    "Street": "56",
-                    "Home": "32",
-                    "Phone": "(747)6181656",
-                    "Apartment": "56",
-                    "Contractor": "Айгерим",
-                    "_id": "651ce52a13f963bee3f2be34"
-                },
-                "orders": [
-                    {
-                        "NuberPPO": "8779120",
-                        "PPOStatus": "Доставляется",
-                        "SKU": "1386916",
-                        "Goods": "Водоочиститель Кувшин Аквафор Прованс А5 модель Р140А5FM (белый) (из PMMA)",
-                        "Count": "1",
-                        "ShippingAddress": "",
-                        "Brand": "АКВАФОР",
-                        "Weight": "1",
-                        "Price": "6 290",
-                        "Item_Status": "Забран",
-                        "Pickup_Point": "0",
-                        "Delivery_Point": "21",
-                        "Pickup_Latitude": "43,366674",
-                        "Pickup_Longitude": "76,970839",
-                        "Delivery_Latitude": "43,227133",
-                        "Delivery_Longitude": "76,931335",
-                        "Pickup_Time": "01.01.0001 9:00:00",
-                        "Delivery_Time": "01.01.0001 15:49:02"
-                    }
-                ]
-            },
-            {
-                "number": "№№00148793",
-                "date": "03.10.2023 23:35:41",
-                "dateCreated": "4-10-2023",
-                "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
-                "documentStatus": "Доставляется",
-                "ISR": "288609756",
-                "informalDocument": "Заказ покупателя ППО",
-                "filterContractor": "Alser",
-                "loanAgreementStatus": "ПРИНЯТ НАМИ",
-                "typePayment": "Кредит",
-                "chronologies": [
-                    {
-                        "PPO": "8781574",
-                        "Chronology": [
-                            "Оформлен",
-                            "Доставляется до клиента (на складе отгрузки)"
-                        ]
-                    }
-                ],
-                "contactInformation": {
-                    "City": "Алматы",
-                    "Delivery_Condition": "Доставка",
-                    "Date_Time_delivery": "2023-10-04 До 20:00",
-                    "Time_Window": "нет данных",
-                    "Latitude": "43,324348",
-                    "Longitude": "76,950909",
-                    "Street": "нет данных",
-                    "Home": "28",
-                    "Phone": "(700)3040600",
-                    "Apartment": "нет данных",
-                    "Contractor": "Бекмұратқызы Салтанат",
-                    "_id": "651ceb2dea74dd30daacebdb"
-                },
-                "orders": [
-                    {
-                        "NuberPPO": "8781574",
-                        "PPOStatus": "Доставляется до клиента (на складе отгрузки)",
-                        "SKU": "1373294",
-                        "Goods": "Веб-камера Logitech StreamCam White 1080p/60fps/78° (960-001297)",
-                        "Count": "1",
-                        "ShippingAddress": "",
-                        "Brand": "logitech",
-                        "Weight": "0,2",
-                        "Price": "66 290",
-                        "Item_Status": "Забран",
-                        "Pickup_Point": "0",
-                        "Delivery_Point": "0",
-                        "Pickup_Latitude": "43,366674",
-                        "Pickup_Longitude": "76,970839",
-                        "Delivery_Latitude": "0",
-                        "Delivery_Longitude": "0",
-                        "Pickup_Time": "01.01.0001 0:00:00",
-                        "Delivery_Time": "01.01.0001 0:00:00"
-                    }
-                ]
-            },
-           
+const updatedDrivers = drivers.map(driver => {
+  const countDoneOrders = driver.transportRequests.filter(req => req.documentStatus === "Доставлено").length;
+  const countProcessOrders = driver.transportRequests.filter(req => req.documentStatus === "Доставляется").length;
+  
+  return {
+    ...driver,
+    countDoneOrdreds: String(countDoneOrders),
+    countProccesOrdreds: String(countProcessOrders)
+  };
+});
+
        
         ]
     },
