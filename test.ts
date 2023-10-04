1e525251c3c62f756b2ae25c6248111e995b3643
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-04 15:48:34 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,31 +1,28 @@
-
-
-
-
-import { lastValueFrom } from 'rxjs';
-
-
-
-
-
-
-
-
-
-async getCorrectCityName(city: string) {
-    console.log(city, 'ПРИХОДИТ ГОРОД В getCorrectCityName');
-    
-    const apiKey = '06c9301e-6663-4182-b060-81da5969b5f3';
-    const url = `https://geocode-maps.yandex.ru/1.x/?format=json&apikey=${apiKey}&geocode=${city}`;
-    console.log(url, 'url');
-
-    try {
-      const response$ = this.httpService.get(url);
-      const response = await lastValueFrom(response$); // используйте lastValueFrom здесь
-      console.log(response.data);
-      return response.data; 
-    } catch (error) {
-      console.error('Ошибка при выполнении GET-запроса:', error);
-      throw error;
+{
+    "request": {
+        "number": "№№00148641",
+        "status": true
+    },
+    "status": "Доставляется",
+    "ISR": {
+        "number": "0003852065-1",
+        "status": "ПРИНЯТ НАМИ"
+    },
+    "document": "Заказ покупателя ППО",
+    "recipient": {
+        "name": "АЙНУР НУРДАУЛЕТОВНА",
+        "phone": "(775)2294550"
+    },
+    "deliveryTime": {
+        "date": "03.10.2023 17:25:41",
+        "time": "2023-10-04 После 15:00"
+    },
+    "deliveryAddress": {
+        "address": "Алматы, нет данных, 7, оплатит километраж, нет данных",
+        "coordinates": "43.334853, 76.808409"
+    },
+    "quantitties": {
+        "totalPrice": "214400",
+        "totalWeight": "66"
     }
 }
