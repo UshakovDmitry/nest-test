cd3b66262f0c3409e6572ec1070aa27d89efb490
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-05 15:53:50 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,9 @@
-
-﻿
-email-field.vue:1 
- Failed to load resource: the server responded with a status of 499 (Request has been forbidden by antivirus)
+Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue” was blocked because of a disallowed MIME type (“text/html”).
+auth
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue”. auth:13:45
+Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts” was blocked because of a disallowed MIME type (“text/html”).
+auth
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/spinner/spinner.vue”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/button/button.vue?vue&type=style&index=0&scoped=40d83cd2&lang.scss”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/password-field/password-field.vue?vue&type=style&index=0&scoped=72d3f84e&lang.scss”.
