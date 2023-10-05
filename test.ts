c849135239c56e8069c3190ad061656d15541d74
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-05 16:50:19 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,99 +1,61 @@
-Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue” was blocked because of a disallowed MIME type (“text/html”).
-<template>
-  <div
-    class="field"
-    :class="{ invalid: config.helper.isActive && config.input.isError }"
-  >
-    <label for="email-field" class="label grey-text">
-      {{ config.label }}
-    </label>
-    <input
-      :type="config.input.type"
-      name="email-field"
-      class="body-medium"
-      :class="{ disabled: config.input.isDisabled }"
-      :value="config.input.value"
-      :placeholder="config.input.placeholder"
-      @focusout="
-        config.setValue(($event.target as HTMLInputElement).value.trim());
-        !config.input.required && config.isEmpty();
-      "
-      @input="
-        config.setValue(($event.target as HTMLInputElement).value.trim());
-        emits('input', config.input.value);
-      "
-    />
-    <div
-      class="label-small red-text"
-      v-if="config.helper.isActive && config.input.isError"
-    >
-      {{ config.helper.value }}
-    </div>
-  </div>
-</template>
+GET
+http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts
 
-<script setup lang="ts">
-import { IEmailField } from './email-field.model';
+	
+GET
+	http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts
+Status
+499
+Request has been forbidden by antivirus
+VersionHTTP/1.1
+Transferred239 B (0 B size)
+Referrer Policystrict-origin-when-cross-origin
 
- defineProps<{
-  config: IEmailField;
-}>();
+	
+Cache-Control
+	no-store, no-cache, must-revalidate, max-age=0
+Content-Length
+	3412
+Content-Type
+	text/html; charset=utf-8
+Expires
+	Mon, 04 Dec 1999 21:29:02 GMT
+Pragma
+	no-cache
+	
+Accept
+	*/*
+Accept-Encoding
+	gzip, deflate, br
+Accept-Language
+	en-US,en;q=0.5
+Connection
+	keep-alive
+DNT
+	1
+Host
+	127.0.0.1:3000
+If-None-Match
+	W/"42b-y/q6sX2l3MDVFDGYlxgwm/PXt7Q"
+Referer
+	http://127.0.0.1:3000/src/pages/auth/auth.component.vue
+Sec-Fetch-Dest
+	script
+Sec-Fetch-Mode
+	cors
+Sec-Fetch-Site
+	same-origin
+User-Agent
+	Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/114.0
+Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts” was blocked because of a disallowed MIME type (“text/html”).
+auth
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/button/button.vue”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/password-field/password-field.vue”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/pages/auth/auth.model.ts”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/pages/auth/auth.viewmodel.ts”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/password-field/password-field.model.ts”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/pages/auth/auth.component.vue?vue&type=style&index=0&scoped=a624e9e6&lang.scss”. auth:13:45
+Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue?vue&type=style&index=0&scoped=3446098e&lang.css”. auth:13:45
 
-const emits = defineEmits(['input']);
-</script>
-
-<style scoped lang="scss">
-.field {
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  width: 100%;
-  height: 80px;
-}
-
-.label {
-  color: rgba(4, 48, 27, 0.3);
-
-/* Typography/Body Medium */
-font-family: Rubik;
-font-size: 14px;
-font-style: normal;
-font-weight: 400;
-line-height: 20px; /* 142.857% */
-letter-spacing: 0.5px;
-}
-
-input {
-  box-sizing: border-box;
-  background: inherit;
-  border: 1px solid var(--light);
-  border-radius: 16px;
-  width: 100%;
-  height: 40px;
-  padding: 12px;
-  outline: none;
-}
-
-input.disabled {
-  border: none;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 143%;
-  letter-spacing: 0.1px;
-  padding: 0;
-}
-
-.invalid {
-  input {
-    border-color: #c91e2d !important;
-
-    &::placeholder {
-      color: #c91e2d !important;
-    }
-  }
-
-  label {
-    color: #c91e2d !important;
-  }
-}
-</style>
+​
