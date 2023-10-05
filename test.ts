a436f57ebe94abe44093148ca3f66db460f91b27
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-05 16:24:53 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,9 +1,92 @@
-Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue” was blocked because of a disallowed MIME type (“text/html”).
-auth
-Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue”. auth:13:45
-Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts” was blocked because of a disallowed MIME type (“text/html”).
-auth
-Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.model.ts”. auth:13:45
-Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/spinner/spinner.vue”. auth:13:45
-Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/button/button.vue?vue&type=style&index=0&scoped=40d83cd2&lang.scss”. auth:13:45
-Loading failed for the module with source “http://127.0.0.1:3000/src/components/global/fields/password-field/password-field.vue?vue&type=style&index=0&scoped=72d3f84e&lang.scss”.
+export interface IEmailField {
+  label?: string;
+  input: {
+    type: string;
+    placeholder: string;
+    value: string;
+    isError: boolean;
+    isDisabled: boolean;
+    required: boolean;
+    body_key?: string;
+  };
+  helper: {
+    value: string;
+    isActive: boolean;
+  };
+  isEmpty: () => void;
+  checkValid?: () => void;
+  setValue: (value: string) => void;
+}
+
+export class EmailField implements IEmailField {
+  label?: string;
+  input: {
+    type: string;
+    placeholder: string;
+    value: string;
+    isError: boolean;
+    required: boolean;
+    isDisabled: boolean;
+    body_key?: string;
+  };
+
+  helper: {
+    value: string;
+    isActive: boolean;
+  };
+
+  constructor(object: {
+    label: string;
+    input: {
+      type: string;
+      placeholder: string;
+      value: string;
+      isError: boolean;
+      required: boolean;
+      isDisabled: boolean;
+      body_key: string;
+    };
+    helper: {
+      value: string;
+      isActive: boolean;
+    };
+  }) {
+    for (const key in object) {
+      this[key] = object[key];
+    }
+  }
+
+  setValue(value: string): void {
+    this.input.value = value;
+    this.clearError();
+    if (this.checkValid()) {
+      this.input.isError = true;
+      this.helper.isActive = true;
+      this.helper.value = 'Введите корректный email';
+    }
+  }
+
+  isEmpty(): void {
+    this.clearError();
+    if ((!this.input.value && !this.input.value.length) || this.checkValid()) {
+      this.input.isError = true;
+      this.helper.isActive = true;
+      this.helper.value = this.input.value.length
+        ? 'Введите корректный email'
+        : 'Поле не должно быть пустым';
+    }
+  }
+
+  // При вводе значений в инпут проверяем стал ли он валидным
+  checkValid(): boolean {
+    console.log('checkValid');
+    
+    return !this.input.value.includes('@');
+  }
+
+  clearError(): void {
+    this.input.isError = false;
+    this.helper.isActive = false;
+    this.helper.value = '';
+  }
+}
