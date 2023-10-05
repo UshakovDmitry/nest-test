62e4ffc44497f618904f508ace5eff586e207a98
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-05 16:45:55 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,99 @@
 Loading module from “http://127.0.0.1:3000/src/components/global/fields/email-field/email-field.vue” was blocked because of a disallowed MIME type (“text/html”).
+<template>
+  <div
+    class="field"
+    :class="{ invalid: config.helper.isActive && config.input.isError }"
+  >
+    <label for="email-field" class="label grey-text">
+      {{ config.label }}
+    </label>
+    <input
+      :type="config.input.type"
+      name="email-field"
+      class="body-medium"
+      :class="{ disabled: config.input.isDisabled }"
+      :value="config.input.value"
+      :placeholder="config.input.placeholder"
+      @focusout="
+        config.setValue(($event.target as HTMLInputElement).value.trim());
+        !config.input.required && config.isEmpty();
+      "
+      @input="
+        config.setValue(($event.target as HTMLInputElement).value.trim());
+        emits('input', config.input.value);
+      "
+    />
+    <div
+      class="label-small red-text"
+      v-if="config.helper.isActive && config.input.isError"
+    >
+      {{ config.helper.value }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IEmailField } from './email-field.model';
+
+ defineProps<{
+  config: IEmailField;
+}>();
+
+const emits = defineEmits(['input']);
+</script>
+
+<style scoped lang="scss">
+.field {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 100%;
+  height: 80px;
+}
+
+.label {
+  color: rgba(4, 48, 27, 0.3);
+
+/* Typography/Body Medium */
+font-family: Rubik;
+font-size: 14px;
+font-style: normal;
+font-weight: 400;
+line-height: 20px; /* 142.857% */
+letter-spacing: 0.5px;
+}
+
+input {
+  box-sizing: border-box;
+  background: inherit;
+  border: 1px solid var(--light);
+  border-radius: 16px;
+  width: 100%;
+  height: 40px;
+  padding: 12px;
+  outline: none;
+}
+
+input.disabled {
+  border: none;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 143%;
+  letter-spacing: 0.1px;
+  padding: 0;
+}
+
+.invalid {
+  input {
+    border-color: #c91e2d !important;
+
+    &::placeholder {
+      color: #c91e2d !important;
+    }
+  }
+
+  label {
+    color: #c91e2d !important;
+  }
+}
+</style>
