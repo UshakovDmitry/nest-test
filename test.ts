b5f3a80d51ef4b69d0fa3a10121b7b61fc9a0af7
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-05 10:42:13 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,107 +1,5 @@
-У меня есть страница где я использую VueDatePicker 
-моя задача вытаскивать из него диапазон дней выбранных пользователем
-<template>
-  <section class="transport">
-    <h1 class="transport__title">
-      Заявки на транспорт : {{ model.transportRequests.length }}/{{  model.filteredTransportRequests.length }} / {{ model.dateSelection }}
-    </h1>
-    <div class="transport__content">
-      <filters-panel-component
-        :cities="model.cities"
-        :dateSelection="model.dateSelection"
-        @search="viewModel.search($event)"
-        @select-city="viewModel.selectCity($event)"
-        @downloadLoadersAsXLSX="viewModel.downloadLoadersAsXLSX"
-      ></filters-panel-component>
-    </div>
-  </section>
-</template>
-<script setup lang="ts">
-import { ref, Ref } from 'vue';
-import { TransportRequestsModel } from './applications.model';
-import { TransportRequestsViewModel } from './applications.viewmodel';
-import TableComponent from '../../components/table/table-component.vue';
-import filtersPanelComponent from './components/filters-panel/filters-panel.component.vue';
-const model: Ref<TransportRequestsModel> = ref(new TransportRequestsModel());
-const viewModel: Ref<TransportRequestsViewModel> = ref(
-  new TransportRequestsViewModel(model.value),
-);
-</script>
-<style scoped>
-.transport__title {
-  align-self: stretch;
-  color: var(--text-dark, #23362d);
-  font-family: Rubik;
-  font-size: 22px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 28px;
-}
-.wrapper-toggle-btns {
-  width: 394px;
-  height: 44px;
-  display: flex;
-  flex-direction: row;
-  margin-top: 20px;
-}
-.wrapper-toggle-btns-item {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  cursor: pointer;
-}
-.isActiveTab {
-  color: var(--primary-light-mode-main, #00a153);
-  border-bottom: 3px solid var(--primary-light-mode-main, #00a153);
-}
-.transport {
-  padding: 0 20px;
-  width: 100%;
-  height: 100vh;
-  background-color: #f8f9fd;
-  margin-bottom: 20px;
-  box-sizing: border-box;
-}
+я хочу чтобы setTimeRange данные в таком виде ['"2023-09-30', '"2023-10-07']
 
-.transport__content {
-  margin-top: 24px;
-  padding: 0 16px 16px 16px;
-  border-radius: 16px;
-  height: 840px;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  gap: 16px;
-  background: #fff;
-  box-shadow:
-    0px 1px 3px 0px rgba(204, 204, 204, 0.3),
-    0px 4px 8px 3px rgba(204, 204, 204, 0.15);
-}
-
-.empty {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  margin-top: 50px;
-}
-.empty-text {
-  color: #b4b2b2;
-  font-family: Rubik;
-  font-size: 28px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 36px;
-}
-</style>
-Вот компонент VueDatePicker 
-реализуй логику 
-он находится внутри       <filters-panel-component>
 <template>
   <div class="filters">
     <div class="set-city">
@@ -123,6 +21,7 @@ const viewModel: Ref<TransportRequestsViewModel> = ref(
         range
         :placeholder="'Выберите дату'"
         class="date_picker"
+        @update:model-value="handleDate"
       ></VueDatePicker>
     </div>
     <div class="download">
@@ -149,18 +48,26 @@ import xlsxicon from '../../../../public/icons/XLSX.svg';
 
 defineProps<{
   cities: string[];
-  dateSelection: any;
 }>();
 
 const dateSelection = ref();
 
 onMounted(() => {
-  const startDate = new Date();
-  const endDate = new Date(new Date().setDate(startDate.getDate() - 7));
+  const endDate = new Date(); 
+  const startDate = new Date(new Date().setDate(endDate.getDate() - 7));
+
   dateSelection.value = [startDate, endDate];
 });
+const handleDate = (timeRange) => {
+  dateSelection.value = timeRange;
+  emits('setTimeRange', timeRange);
+  // do something else with the data
+}
 
-const emits = defineEmits(['selectCity', 'downloadLoadersAsXLSX', 'search']);
+
+
+
+const emits = defineEmits(['selectCity', 'downloadLoadersAsXLSX', 'search','setTimeRange']);
 </script>
 <style scoped>
 .filters {
@@ -227,4 +134,3 @@ const emits = defineEmits(['selectCity', 'downloadLoadersAsXLSX', 'search']);
   background: #fff;
 }
 </style>
-
