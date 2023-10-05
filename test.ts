e31dfce6f5330f17326b50cd26b4d862d91aa35d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-05 10:53:59 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,149 +1,226 @@
-я хочу чтобы setTimeRange данные в таком виде ['"2023-09-30', '"2023-10-07']
+У меня есть котроллер который возвращает все транспортные заявки а также по номеру
+./transportRequests.controller
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { TransportRequestsService } from './transportRequests.service';
+import { ApiTags } from '@nestjs/swagger';
 
-<template>
-  <div class="filters">
-    <div class="set-city">
-      <dropdown-component
-        :items="cities"
-        :width="350"
-        :currentValue="'Выберите город'"
-        @onSelect="emits('selectCity', $event)"
-      ></dropdown-component>
-    </div>
-    <div class="search">
-      <search-field-component
-        :placeholder="'Введите номер ППО или заявки на транспорт'"
-        @onSearch="emits('search', $event)"
-      ></search-field-component>
-      <VueDatePicker
-        v-model="dateSelection"
-        locale="ru"
-        range
-        :placeholder="'Выберите дату'"
-        class="date_picker"
-        @update:model-value="handleDate"
-      ></VueDatePicker>
-    </div>
-    <div class="download">
-      <button
-        class="download-btn"
-        data-test="download-btn"
-        @click="emits('downloadLoadersAsXLSX')"
-      >
-        Скачать файл
-        <img :src="xlsxicon" />
-      </button>
-    </div>
-  </div>
-</template>
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
+@ApiTags('getTransportRequests')
+@Controller('/api/getTransportRequests')
+export class TransportRequestsController {
+  constructor(
+    private readonly transportRequestsService: TransportRequestsService,
+  ) 
+  {}
 
-import VueDatePicker from '@vuepic/vue-datepicker';
-import '@vuepic/vue-datepicker/dist/main.css';
+  @Get()
+  async getAllTransportRequests() {
+    return this.transportRequestsService.getAllTransportRequests();
+  }
 
-import DropdownComponent from '../../../../components/global/dropdown/dropdown.vue';
-import SearchFieldComponent from '../../../../components/global/fields/search-field/search-field.vue';
-import xlsxicon from '../../../../public/icons/XLSX.svg';
-
-defineProps<{
-  cities: string[];
-}>();
-
-const dateSelection = ref();
-
-onMounted(() => {
-  const endDate = new Date(); 
-  const startDate = new Date(new Date().setDate(endDate.getDate() - 7));
-
-  dateSelection.value = [startDate, endDate];
-});
-const handleDate = (timeRange) => {
-  dateSelection.value = timeRange;
-  emits('setTimeRange', timeRange);
-  // do something else with the data
+  @Post('byNumber')
+async getTransportRequestByNumber(@Body('number') number: string) {
+    return this.transportRequestsService.getTransportRequestByNumber(number);
 }
 
-
-
-
-const emits = defineEmits(['selectCity', 'downloadLoadersAsXLSX', 'search','setTimeRange']);
-</script>
-<style scoped>
-.filters {
-  width: 100%;
-  height: 64px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  flex-direction: row;
-}
-.set-city {
-  width: 25%;
 }
 
-.date_picker {
-  --dp-border-radius: 16px;
-  /* --dp-cell-border-radius: 4px;  */
-  width: 200px;
+Я хочу добавить метод который будет принимать две даты и возвращать заявки из указанного диапазона
+
+./transportRequests.service
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MessageDocument } from '../schemas/message.shema';
+import { DBService } from '../db/db.service'; 
+
+
+@Injectable()
+export class TransportRequestsService {
+    constructor(
+        @InjectModel('Message') private messageModel: Model<MessageDocument>,
+        private readonly dbService: DBService, 
+      ) {}
+    
+        async getAllTransportRequests(): Promise<any[]> {
+            return await this.dbService.getAllTransportRequests();
+        }
+
+
+        async getTransportRequestByNumber(number: string): Promise<any> {
+          return await this.dbService.getTransportRequestByNumber(number);
+      }
 }
 
-.date_picker .dp__outer_menu_wrap .dp--menu-wrapper {
-  --dp-border-radius: 4px;
-  --dp-cell-border-radius: 4px;
+db.service
+Здесь у меня описаны методы с базоданных монго
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MessageDocument } from '../schemas/message.shema';
+import { HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
+
+@Injectable()
+export class DBService {
+  [x: string]: any;
+  constructor(
+    @InjectModel('Message') private messageModel: Model<MessageDocument>,
+    private readonly httpService: HttpService,
+  ) {}
+
+  async getDriversByDate(date: string) {
+    const drivers = await this.getAllDrivers();
+    const filteredDrivers = drivers
+      .map((driver) => {
+        const filteredRequests = driver.transportRequests.filter(
+          (transportRequest) => {
+            return (
+              transportRequest.contactInformation.Date_Time_delivery.split(
+                ' ',
+              )[0] === date
+            );
+          },
+        );
+
+        return { ...driver, transportRequests: filteredRequests };
+      })
+      .filter((driver) => driver.transportRequests.length > 0);
+    return filteredDrivers;
+  }
+
+  async getTransportRequestByNumber(number: string): Promise<any> {
+    return await this.messageModel.findOne({ Number: number }).exec();
+  }
 }
+вот так выглядят обьекты в бд
 
-.search {
-  width: 50%;
-  border-right: 1px solid rgba(35, 54, 45, 0.12);
-  border-left: 1px solid rgba(35, 54, 45, 0.12);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-}
-.download {
-  width: 25%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-}
-.download-btn {
-  border-radius: 100px;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  background: #fff;
-  width: 155px;
-  cursor: pointer;
-  box-sizing: border-box;
-  height: 38px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 8px;
-}
-.set-date {
-  display: flex;
-  width: 222px;
-  height: 42px;
-  padding: 6px 16px;
-  align-items: center;
-  gap: 8px;
-  border-radius: 80px;
-  box-sizing: border-box;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  background: #fff;
-}
-</style>
+_id
+651cfc4d1d80def42ca02fda
+Number
+"№№00148463"
+Date
+"03.10.2023 12:15:42"
+DateCreated
+"4-10-2023"
+Organization
+"TOO Gulser Computers (Гулсер Компьютерс)"
+DocumentStatus
+"Доставляется"
+CarModel
+""
+NumberCar
+""
+Driver
+"Әбілдаев Абылайхан Әшімұлы"
+ISR
+"(771)8975627"
+TypePayment
+"Кредит"
+loanAgreementStatus
+""
+IdYandex
+""
+distribution
+false
+Informal_Document
+"Заказ покупателя ППО"
+FilterContractor
+"Alser"
 
+ArrayStrings
+Array (1)
 
+0
+Object
+NuberPPO
+"8779739"
+PPOStatus
+"Доставляется до клиента (на складе отгрузки)"
+SKU
+"1375371"
+Goods
+"WMD-1280NDV-WH/Стиральная машина Dauscher"
+Count
+"1"
+ShippingAddress
+""
+Brand
+"DAUSCHER"
+Weight
+"68,3"
+Price
+"161 991"
+Item_Status
+""
+Pickup_Point
+"0"
+Delivery_Point
+"0"
+Pickup_Latitude
+"42,348907"
+Pickup_Longitude
+"69,530052"
+Delivery_Latitude
+"0"
+Delivery_Longitude
+"0"
+Pickup_Time
+"01.01.0001 0:00:00"
+Delivery_Time
+"01.01.0001 0:00:00"
 
+ContactInformation
+Object
+City
+"Шымкент"
+Delivery_Condition
+"Доставка"
+Date_Time_delivery
+"2023-10-05  До 20:00"
+Time_Window
+"15:00-18:00"
+Latitude
+"42.3420105"
+Longitude
+"69.62451"
+Street
+"42"
+Home
+"26"
+Phone
+"(771)8975627"
+Apartment
+"42"
+Contractor
+"Валентина Валентина"
+_id
+651cfc4d1d80def42ca02fdb
 
+StructureQuantities
+Object
+TotalWeight
+"68.3"
+TotalAmount
+"161991"
+_id
+651cfc4d1d80def42ca02fdc
 
+ArrayChronologies
+Array (1)
 
+0
+Object
+PPO
+"8779739"
 
-  const handleDate = (timeRange) => {
-  dateSelection.value = timeRange;
-  const formattedDates = timeRange.map(date => date.toISOString().split('T')[0]);
-  emits('setTimeRange', formattedDates);
-  // do something else with the data
-}
+Chronology
+Array (2)
+0
+"Оформлен"
+1
+"Доставляется до клиента (на складе отгрузки)"
+
+Ориентируйся на дату Date_Time_delivery
+"2023-10-05  До 20:00"
+Ее еще  нужно обработать убрав лишнее в строке(она стабильно приходит в таком виде!
+Реализуй и напиши код во всех файлах
