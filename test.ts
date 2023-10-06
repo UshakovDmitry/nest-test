a84fa1602173403956f71ed28815dcac6bfbf2ac
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-06 12:48:58 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,20 +1,10 @@
- const chooseStrategy = () => {
-        if (props.drivers && props.drivers.length > 0) {
-          return showDrivers(props.drivers);
-        } else if (props.address) {
-          return showAddress(props.address);
-        } else if (props.search) {
-          return determineAddress(props.city, props.street, props.house);
-        }
-      };
+const handleStrategy = async () => {
+  const strategy = chooseStrategy();
+  if (strategy) {
+    await strategy;
+  } else {
+    console.error("Не выбрана стратегия");
+  }
+};
 
-      const strategy = chooseStrategy();
-      if (strategy) {
-        strategy();
-      } else {
-        console.error("Не выбрана стратегия");
-      }
-
-This expression is not callable.
-  Type 'Promise<void>' has no call signatures.ts(2349)
-const strategy: Promise<void>
+handleStrategy();
