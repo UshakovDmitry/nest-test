ead8c5b8b7c7d38b8c2a5964e38d933ac030c90d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-06 10:35:17 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,173 +1,122 @@
 <template>
-  <button
-    :class="`${config.type} ${config.border} ${
-      config.mobileWidth ? 'mobileWidth' : ''
-    }`"
-    :style="`${
-      (config.type === 'filled' ? 'background:var(--' : 'color:var(--') +
-      config.color +
-      ')'
-    };
-        width:${config.width ? config.width : 'fit-content'}
-      `"
-    class="label-large"
-    :disabled="config.disabled || config.loading"
-    @click="emits('onClick')"
-  >
-    <span v-if="!config.loading">
-      <icon-component
-        v-if="config.prefix"
-        :сonfig="{
-          name: config.prefix,
-          color: config.iconColor || '#fff',
-          scale: 1.1,
-        }"
-      ></icon-component>
-      <icon-component
-        v-if="config.suffix"
-        :сonfig="{
-          name: config.suffix,
-          color: config.iconColor || '#fff',
-          scale: 1.1,
-        }"
-      ></icon-component>
-    </span>
-    <span class="hover"></span>
-    <spinner
-        v-if="config.loading"
-        :size="20"
-        :stroke="config.type === 'filled' ? '#fff' : config.color"
-      ></spinner>
-      {{ config.value }}
-  </button>
+  <!-- ... (не менялся) ... -->
 </template>
 
 <script setup lang="ts">
-// import IButton from '../../../domain/interfaces/button.interface';
-import { IButton } from '../../../domain/interfaces/button.interface';
-import IconComponent from '../icon/icon.component.vue';
-import Spinner from "../spinner/spinner.vue";
-
- defineProps<{
-  config: IButton;
-}>();
-// console.log(props.config);
-
-const emits = defineEmits(['onClick']);
+  // ... (не менялся) ...
 </script>
 
-<style scoped lang="scss">
-@import './button.scss';
-</style>
-
-'./button.scss
-
+<style scoped>
 button {
-    display: flex;
-    align-items: center;
-    width: fit-content;
-    padding: 0 24px;
-    height: 40px;
-    position: relative;
-    justify-content: center;
-  
-    span {
-      display: flex;
-      gap: 8px;
-      align-items: center;
-      white-space: nowrap;
-    }
-  }
-  
-  button.mobileWidth {
-    @media (max-width: 768px) {
-      width: 100% !important;
-    }
-  }
-  
-  // Filled button configurations
-  .filled {
-    border: none;
-    color: #fff;
-  
-    &:hover > .hover {
-      visibility: visible;
-    }
-  
-    &:disabled {
-      color: rgba(35, 54, 45, 0.3);
-      background: rgba(35, 54, 45, 0.3) !important;
-      cursor: not-allowed;
-    }
-  
-    &:disabled:hover > .hover {
-      visibility: hidden;
-    }
-  }
-  
-  // Outlined button configurations
-  .outlined {
-    background: #fff;
-    border: 1px solid;
-  
-    &:hover {
-      background: rgba(35, 54, 45, 0.08);
-    }
-  
-    &:disabled {
-      cursor: inherit;
-      color: rgba(35, 54, 45, 0.3) !important;
-    }
-  
-    &:disabled:hover {
-      background: #fff;
-      color: rgba(35, 54, 45, 0.3) !important;
-    }
-  }
-  
-  // Text button configurations
-  .text {
-    border: none;
-    background: inherit;
-    transition: .2s ease-out;
-  
-    &:hover {
-      transition: .2s ease-in;
-      background: rgba(35, 54, 45, 0.08);
-    }
-  
-    &:disabled {
-      color: rgba(35, 54, 45, 0.3) !important;
-      cursor: inherit;
-    }
-  }
-  
-  .text-without-padding{
-    @extend .text;
-    @media (max-width: 768px) {
-      padding: 0!important;
-    }
-  }
-  
-  .hover {
-    visibility: hidden;
-    position: absolute;
-    background: rgba(255, 255, 255, 0.08);
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
-  
-  .large {
-    border-radius: 16px;
-  }
-  
-  .small {
-    border-radius: 8px;
-  }
-  
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  padding: 0 24px;
+  height: 40px;
+  position: relative;
+  justify-content: center;
+}
 
-ПЕРЕПЕШИ СТИЛИ НА CSS
+button span {
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  white-space: nowrap;
+}
+
+button.mobileWidth {
+  width: fit-content;
+}
+
+@media (max-width: 768px) {
+  button.mobileWidth {
+    width: 100% !important;
+  }
+}
+
+.filled {
+  border: none;
+  color: #fff;
+}
+
+.filled:hover .hover {
+  visibility: visible;
+}
+
+.filled:disabled {
+  color: rgba(35, 54, 45, 0.3);
+  background: rgba(35, 54, 45, 0.3) !important;
+  cursor: not-allowed;
+}
+
+.filled:disabled:hover .hover {
+  visibility: hidden;
+}
+
+.outlined {
+  background: #fff;
+  border: 1px solid;
+}
+
+.outlined:hover {
+  background: rgba(35, 54, 45, 0.08);
+}
+
+.outlined:disabled {
+  cursor: inherit;
+  color: rgba(35, 54, 45, 0.3) !important;
+}
+
+.outlined:disabled:hover {
+  background: #fff;
+  color: rgba(35, 54, 45, 0.3) !important;
+}
+
+.text {
+  border: none;
+  background: inherit;
+  transition: 0.2s ease-out;
+}
+
+.text:hover {
+  transition: 0.2s ease-in;
+  background: rgba(35, 54, 45, 0.08);
+}
+
+.text:disabled {
+  color: rgba(35, 54, 45, 0.3) !important;
+  cursor: inherit;
+}
+
+.text-without-padding {
+  border: none;
+  background: inherit;
+  transition: 0.2s ease-out;
+}
+
+@media (max-width: 768px) {
+  .text-without-padding {
+    padding: 0 !important;
+  }
+}
+
+.hover {
+  visibility: hidden;
+  position: absolute;
+  background: rgba(255, 255, 255, 0.08);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.large {
+  border-radius: 16px;
+}
+
+.small {
+  border-radius: 8px;
+}
+</style>
 
 
