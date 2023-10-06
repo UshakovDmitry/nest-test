c04baef6ba8dbfa62eb3a0ba42931d7194bc15b5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-06 10:06:56 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,44 +1,130 @@
-// cities.module.ts
-import { HttpModule } from '@nestjs/axios';
+<template>
+  <div
+    :style="'height:' + config?.input?.height"
+    class="field"
+    :class="{ invalid: config.helper.isActive && config.input.isError }"
+  >
+    <label for="email-field" class="body-small grey-text">
+      <span v-if="config.input.required">{{config.label}} *</span></label
+    >
+    <input
+      v-if="!config.input.isTextarea"
+      :style="
+        'height:' +
+        (config?.input?.height
+          ? 'calc(' + config?.input?.height + '-40px'
+          : '40px')
+      "
+      :class="{ disabled: config.input.isDisabled }"
+      :type="config.input.type || 'text'"
+      :value="config.input.value"
+      :autocomplete="
+        config.input.type === 'password' ? 'new-password' : 'one-time-code'
+      "
+      name="email-field"
+      class="body-medium"
+      :placeholder="config.input.placeholder"
+      :readonly="config.input.isDisabled"
+      :maxLength="config.input.maxLength"
+      @input="
+        emits(
+          'input',
+          ($event.target as HTMLInputElement | HTMLTextAreaElement).value,
+        );
+        config.setValue(
+          ($event.target as HTMLInputElement | HTMLTextAreaElement).value,
+        );
+      "
+      @focusout="config.input.required && config.isEmpty()"
+      @focus="emits('onFocus')"
+      @blur="emits('onBlur')"
+    />
+    <textarea
+      v-if="config.input.isTextarea"
+      :value="config.input.value"
+      :style="'height:' + (config?.input?.height || '40px')"
+      class="body-medium"
+      :placeholder="config.input.placeholder"
+      @input="
+        emits(
+          'input',
+          ($event.target as HTMLInputElement | HTMLTextAreaElement).value,
+        );
+        config.setValue(
+          ($event.target as HTMLInputElement | HTMLTextAreaElement).value,
+        );
+      "
+      @focusout="config.input.required && config.isEmpty()"
+      @focus="emits('onFocus')"
+      @blur="emits('onBlur')"
+    ></textarea>
 
-@Module({
-  imports: [HttpModule],
-  controllers: [CitiesController],
-  providers: [CitiesService],
-})
-export class CitiesModule {}
+    <div
+      v-if="config.helper.isActive && config.input.isError"
+      class="label-small red-text"
+    >
+      {{ config.helper.value }}
+    </div>
+  </div>
+</template>
 
+<script setup lang="ts">
+import { IField } from './field.model';
 
-// cities.service.ts
-import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { firstValueFrom } from 'rxjs';
+defineProps<{
+  config: IField;
+}>();
+const emits = defineEmits(['input', 'onFocus', 'onBlur']);
+</script>
 
-@Injectable()
-export class CitiesService {
-  constructor(private readonly httpService: HttpService) {}
-
-  async getDistributedСities() {
-    const response = await firstValueFrom(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS/DistributionWorks'));
-    return response.data;
-  }
+<style scoped lang="scss">
+.field {
+  display: flex;
+  flex-direction: column;
+  gap: 4px;
+  width: 100%;
+  height: 80px;
 }
 
+input,
+textarea {
+  box-sizing: border-box;
+  background: inherit;
+  border: 1px solid var(--light);
+  border-radius: 16px;
+  width: 100%;
+  padding: 12px;
+  resize: none;
+}
 
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  /* display: none; <- Crashes Chrome on hover */
+  -webkit-appearance: none;
+  margin: 0;
+  /* <-- Apparently some margin are still there even though it's hidden */
+}
 
+input.disabled {
+  border: none;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 143%;
+  letter-spacing: 0.1px;
+  padding: 0;
+}
 
-// cities.controller.ts
-import { Controller, Get } from '@nestjs/common';
-import { CitiesService } from './cities.service';
-import { ApiTags } from '@nestjs/swagger';
+.invalid {
+  input {
+    border-color: var(--red) !important;
 
-@ApiTags('cities')
-@Controller('/api/getCities')
-export class CitiesController {
-  constructor(private readonly citiesService: CitiesService) {}
+    &::placeholder {
+      color: var(--red) !important;
+    }
+  }
 
-  @Get()
-  async getLoaders() {
-    return await this.citiesService.getDistributedСities();
+  label {
+    color: var(--red) !important;
   }
 }
+</style>
