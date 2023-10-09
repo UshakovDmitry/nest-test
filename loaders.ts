7d93e548a8d33e54204339f57d61c63869e72404
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-09 11:28:01 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,76 +1,30 @@
-couriers.service
-import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
-import { DBService } from '../db/db.service';
-import { firstValueFrom } from 'rxjs';
-
 @Injectable()
 export class CouriersService {
-  constructor(private readonly httpService: HttpService ,  private readonly dbService: DBService, ) {}
-
-
+  constructor(private readonly httpService: HttpService ,  private readonly dbService: DBService) {}
 
   async getCouriers() {
-    const response = await firstValueFrom(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS/Drivers'));
+    // ... (оставьте код без изменений)
 
     const data = await Promise.all(response.data.data.map(async (item: any) => {
-        const convertValue = (value: string, fieldName: string) => {
-            if (value === 'Да') return true;
-            if (value === 'Нет') return false;
-            return value; // Лучше добавить обработку неизвестных значений
-        };
+      // ... (оставьте код без изменений)
 
-        let carNumberFromDB = '';
+      let carNumberFromDB = '';
 
-        // Например, предполагая, что у вас есть метод для получения драйвера из базы данных:
-        const driverFromDB = await getDriverFromDatabase(item.Drivers);
-        if (driverFromDB) {
-            carNumberFromDB = driverFromDB.NumberCar;
-        }
+      const driverFromDB = await this.getDriverFromDatabase(item.Drivers);
+      if (driverFromDB) {
+          carNumberFromDB = driverFromDB.NumberCar;
+      }
 
-        return {
-            ...item,
-            carNumber: carNumberFromDB,
-            HardTimeWindow: convertValue(item.HardTimeWindow, 'HardTimeWindow'),
-            ReturnWarehouse: convertValue(item.ReturnWarehouse, 'ReturnWarehouse')
-        };
+      // ... (оставьте код без изменений)
     }));
 
     return data;
+  }
+
+  private async getDriverFromDatabase(driverName: string) {
+      return await this.dbService.collection('drivers').findOne({ Driver: driverName });
+  }
 }
-
-async function getDriverFromDatabase(driverName: string) {
-
-    return await dbService.collection('drivers').findOne({ Driver: driverName });
-}
-
-}
-
-couriers.module
-import { Module } from '@nestjs/common';
-import { CouriersService } from './couriers.service';
-import { CouriersController } from './couriers.controller';
-import { MessageSchema } from '../schemas/message.shema';
-import { MongooseModule } from '@nestjs/mongoose';
-import { HttpModule } from '@nestjs/axios';
-import { DBModule } from '../db/db.module';  
-
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-    DBModule  ,HttpModule],
-  controllers: [CouriersController],
-  providers: [CouriersService],
-})
-export class CouriersModule {}
-
-
-
-couriers.controller
-import { Controller, Get } from '@nestjs/common';
-import { CouriersService } from './couriers.service';
-import { ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Couriers')
 @Controller('/api/getCouriers')
@@ -78,18 +32,10 @@ export class CouriersController {
   constructor(private readonly couriersService: CouriersService) {}
 
   @Get()
-  getDistributedСities() {
+  getCouriers() {
     return this.couriersService.getCouriers();
   }
 }
-
-
-не получается 
-Unexpected token. A constructor, method, accessor, or property was expected.ts(1068)
-
-Cannot find name 'dbService'. Did you mean 'DBService'?ts(2552)
-any
-
 
 
 
