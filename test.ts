25a893e7ccc580ba8cc1decebd07a8770a06b6a4
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-09 17:32:50 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,20 +1,97 @@
-<template>
-  <label class="toggle">
-    <input class="toggle-checkbox" type="checkbox" :checked="isActive" disabled>
-    <div class="toggle-switch"></div>
-    <span class="toggle-label"></span>
-  </label>
-</template>
+import {
+  createRouter,
+  createWebHistory,
+  type RouteRecordRaw,
+} from 'vue-router';
+import Auth from '../pages/auth/auth.component.vue';
+import Layout from '../layout/dafault.vue';
+import Dashboard from '../pages/dashboard/dashboard.component.vue';
+import Map from '../pages/map/map.component.vue';
+import TransportRequests from '../pages/transport-requests/transportRequests.component.vue';
+import Transport from '../pages/transport/transport.component.vue';
+import Couriers from '../pages/couriers/couriers.component.vue';
+import CourierDetail from '../pages/courier-detail/courier-detail.component.vue';
+import TransportRequestsDetail from '../pages/transport-requests-detail/transport-requests-detail.component.vue';
 
-<script setup lang="ts">
-defineProps<{
-  id: number;
-  isActive: boolean;
-}>();
-</script>
+// Инициализация статуса авторизации, если он еще не установлен
+if (localStorage.getItem("isLogin") === null) {
+  localStorage.setItem("isLogin", "false");
+}
 
-<style scoped>
-/* ... ваш текущий стиль ... 
-*/
-</style>
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: Auth,
+    meta: {
+      public: true,
+      hasLayout: false,
+    },
+  },
+  {
+    path: '/',
+    component: Layout,
+    meta: { hasLayout: true },
+    children: [
+      {
+        path: '',
+        redirect: 'dashboard',
+      },
+      {
+        path: 'dashboard',
+        name: 'Dashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'map',
+        name: 'Map',
+        component: Map,
+      },
+      {
+        path: 'requests',
+        name: 'transportRequests',
+        component: TransportRequests,
+      },
+      {
+        path: 'requests/:id',
+        name: 'TransportRequestsDetail',
+        component: TransportRequestsDetail,
+      },
+      {
+        path: 'couriers',
+        name: 'Сouriers',
+        component: Couriers,
+      },
+      {
+        path: 'couriers/:id',
+        name: 'CourierDetail',
+        component: CourierDetail,
+      },
+      {
+        path: 'transport',
+        name: 'Transport',
+        component: Transport,
+      },
+    ],
+  },
+];
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem("isLogin") === "true";
+
+  if (!to.meta.public && !isAuthenticated) {
+    next("/auth");
+  } else if (to.path === "/auth" && isAuthenticated) {
+    next("/");
+  } else {
+    next();
+  }
+});
+
+export default router;
+Напиши код компонента для страницы 404 и условие в роутере
