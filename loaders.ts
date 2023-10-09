fd9d31b2617775d8e600ea793d2e8f3157ecdcfe
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-09 11:01:58 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,17 +1,17 @@
 async getCouriers() {
     const response = await firstValueFrom(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS/Drivers'));
-    
+
     const data = response.data.data.map((item: any) => {
-        const convertValue = (value: string) => {
+        const convertValue = (value: string, fieldName: string) => {
             if (value === 'Да') return true;
             if (value === 'Нет') return false;
-            throw new Error(`Unexpected value: ${value}`);
+            throw new Error(`Unexpected value for ${fieldName}: ${value}`);
         };
-        
+
         return {
             ...item,
-            HardTimeWindow: convertValue(item.HardTimeWindow),
-            ReturnWarehouse: convertValue(item.ReturnWarehouse)
+            HardTimeWindow: convertValue(item.HardTimeWindow, 'HardTimeWindow'),
+            ReturnWarehouse: convertValue(item.ReturnWarehouse, 'ReturnWarehouse')
         };
     });
 
