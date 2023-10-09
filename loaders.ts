c43333a1b554f20fd4efcdb81a72611818ef1604
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-09 10:56:14 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,53 +1,19 @@
-вот здесь 
-
-  async getCouriers() {
+async getCouriers() {
     const response = await firstValueFrom(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS/Drivers'));
-    return response.data.data;
-  }
-я получаю данные вот в таком виде
+    
+    const data = response.data.data.map((item: any) => {
+        const convertValue = (value: string) => {
+            if (value === 'Да') return true;
+            if (value === 'Нет') return false;
+            throw new Error(`Unexpected value: ${value}`);
+        };
+        
+        return {
+            ...item,
+            HardTimeWindow: convertValue(item.HardTimeWindow),
+            ReturnWarehouse: convertValue(item.ReturnWarehouse)
+        };
+    });
 
-[
-    {
-        "Drivers": "Наумов Сергей Александрович",
-        "HiringType": "Водитель-экспедитор",
-        "TimeWindow": "10:00:00-20:00:00",
-        "HardTimeWindow": "Да",
-        "ReturnWarehouse": "Да",
-        "City": "Алматы"
-    },
-    {
-        "Drivers": "Жайнақ Ержан Сембайұлы",
-        "HiringType": "Водитель-экспедитор",
-        "TimeWindow": "10:00:00-20:00:00",
-        "HardTimeWindow": "Да",
-        "ReturnWarehouse": "Да",
-        "City": "Алматы"
-    },
-    {
-        "Drivers": "Карпенко Александр Иванович",
-        "HiringType": "Водитель-экспедитор",
-        "TimeWindow": "10:00:00-21:00:00",
-        "HardTimeWindow": "Да",
-        "ReturnWarehouse": "Нет",
-        "City": "Алматы"
-    },
-    {
-        "Drivers": "Харитонов Александр Сергеевич",
-        "HiringType": "",
-        "TimeWindow": "09:00:00-20:00:00",
-        "HardTimeWindow": "Да",
-        "ReturnWarehouse": "Да",
-        "City": ""
-    },
-    {
-        "Drivers": "Кунц Алексей Иосифович",
-        "HiringType": "Водитель-экспедитор",
-        "TimeWindow": "09:00:00-22:00:00",
-        "HardTimeWindow": "Да",
-        "ReturnWarehouse": "Нет",
-        "City": "Актобе"
-    },
-  ]
-
-я хочу преобразовывать поля HardTimeWindow и ReturnWarehouse
-если 'Да' то возвращать true а если 'Нет' то false
+    return data;
+}
