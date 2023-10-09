4f60b26f0327012dcc3124042d10a5878de80372
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-09 11:24:40 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,4 +1,16 @@
-async getCouriers() {
+couriers.service
+import { HttpService } from '@nestjs/axios';
+import { Injectable } from '@nestjs/common';
+import { DBService } from '../db/db.service';
+import { firstValueFrom } from 'rxjs';
+
+@Injectable()
+export class CouriersService {
+  constructor(private readonly httpService: HttpService ,  private readonly dbService: DBService, ) {}
+
+
+
+  async getCouriers() {
     const response = await firstValueFrom(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS/Drivers'));
 
     const data = await Promise.all(response.data.data.map(async (item: any) => {
@@ -28,10 +40,58 @@ async getCouriers() {
 }
 
 async function getDriverFromDatabase(driverName: string) {
-    // Этот метод будет делать запрос к вашей базе данных и искать драйвера по имени.
-    // Это просто заглушка, вам нужно будет реализовать логику запроса на основе вашей базы данных и системы.
-    // Возвращаемое значение может быть объектом драйвера или null, если драйвер не найден.
 
-    // Например:
-    // return await db.collection('drivers').findOne({ Driver: driverName });
+    return await dbService.collection('drivers').findOne({ Driver: driverName });
 }
+
+}
+
+couriers.module
+import { Module } from '@nestjs/common';
+import { CouriersService } from './couriers.service';
+import { CouriersController } from './couriers.controller';
+import { MessageSchema } from '../schemas/message.shema';
+import { MongooseModule } from '@nestjs/mongoose';
+import { HttpModule } from '@nestjs/axios';
+import { DBModule } from '../db/db.module';  
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+    DBModule  ,HttpModule],
+  controllers: [CouriersController],
+  providers: [CouriersService],
+})
+export class CouriersModule {}
+
+
+
+couriers.controller
+import { Controller, Get } from '@nestjs/common';
+import { CouriersService } from './couriers.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Couriers')
+@Controller('/api/getCouriers')
+export class CouriersController {
+  constructor(private readonly couriersService: CouriersService) {}
+
+  @Get()
+  getDistributedСities() {
+    return this.couriersService.getCouriers();
+  }
+}
+
+
+не получается 
+Unexpected token. A constructor, method, accessor, or property was expected.ts(1068)
+
+Cannot find name 'dbService'. Did you mean 'DBService'?ts(2552)
+any
+
+
+
+
+
+
