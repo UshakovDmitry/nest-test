6310d863a90fda15f7398222a42e0934b70c1fea
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-10 09:45:45 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,36 +1,180 @@
-import { Sse } from '@nestjs/common';
-import { Observable, interval } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { useGetApi } from '../../domain/services/getHTTP.service';
+import { usePostApi } from '../../domain/services/postHTTP.service';
+import router from '../../router';
+import { type TransportRequestsModel } from './transportRequests.model';
+// import { ITransportRequest } from '../../domain/interfaces/transportRequest.interface';
+
+export class TransportRequestsViewModel {
+  model: TransportRequestsModel;
+
+  constructor(model: any) {
+    this.model = model;
+    this.getTransportRequests();
+  }
+
+  const eventSource = new EventSource('http://localhost:4000/api/getTransportRequests/sse');
+
+  eventSource.onmessage = (event) => {
+    console.log('New message', JSON.parse(event.data));
+    
+    if (JSON.parse(event.data).message === 'Pong') {
+      this.getTransportRequests(); // вызовите ваш существующий метод, когда получите понг
+    }
+  };
 
 
+  async getTransportRequests(): Promise<void> {
+    const response = await useGetApi('getTransportRequests');
+    console.log(response.length, 'кол-во заявок');
+    // console.log(response, 'response');
+    response.forEach((data: any) => {
+      const transformedData = this.transformToTransportRequest(data);
+      const city = this.setCitiesList(transformedData);
+      // Проверяем наличие города в списке и добавляем, если его нет
+      if (!this.model.cities.includes(city)) {
+        this.model.cities.push(city);
+      }
+      const transformedDataForTable =
+        this.transformToTransportForTable(transformedData);
 
-@ApiTags('getTransportRequests')
-@Controller('/api/getTransportRequests')
-export class TransportRequestsController {
-  // ... существующий код
-  
-  @Sse('sse')
-  sse(): Observable<any> {
-    return interval(1000).pipe(
-      map((_) => ({ data: { message: 'Pong' } }))
+      this.model.transportRequests.unshift(transformedDataForTable);
+    });
+  }
+
+  setTimeRange(timeRange) {
+    this.getTransportRequestsByDateRange(timeRange[0], timeRange[1]);
+  }
+
+  async getTransportRequestsByDateRange(
+    dateStart: string,
+    dateEnd: string,
+  ): Promise<void> {
+    const body = {
+      startDate: dateStart,
+      endDate: dateEnd,
+    };
+    try {
+      const response = usePostApi('getTransportRequestsbyDateRange', body,'sendFormData');
+
+
+      const data = await response;
+
+      // Очистить текущие данные перед добавлением новых
+      this.model.transportRequests = [];
+
+      data.forEach((dataItem: any) => {
+        const transformedData = this.transformToTransportRequest(dataItem);
+        const transformedDataForTable =
+          this.transformToTransportForTable(transformedData);
+        this.model.transportRequests.push(transformedDataForTable);
+      });
+      this.model.filteredTransportRequests = this.model.transportRequests;
+    } catch (error) {
+      console.error('Ошибка при получении данных:', error);
+    }
+  }
+
+  search(value) {
+    this.model.filteredTransportRequests = this.model.transportRequests.filter(
+      (item) => {
+        return item.request.number.toLowerCase().includes(value.toLowerCase());
+      },
     );
+  }
+
+  setCitiesList(data) {
+    return data.contactInformation.City;
+  }
+
+  selectCity(city: string): void {
+    console.log(city);
+  }
+
+  downloadLoadersAsXLSX(): void {
+    alert('Функционал в разработке');
+  }
+
+  goToTransportRequestDetail(row: any) {
+    console.log('router');
+
+    router.push({
+      name: 'TransportRequestsDetail',
+      params: { id: row.request.number },
+      query: {
+        number: row.request.number,
+      },
+    });
+  }
+
+  transformToTransportRequest(data: any) {
+    return {
+      request: {
+        number: String(data.Number),
+        status: Boolean(data.distribution),
+      },
+      status: String(data.DocumentStatus),
+      ISR: {
+        number: String(data.ISR),
+        status: String(data.loanAgreementStatus),
+      },
+      document: String(data.Informal_Document),
+      carModel: String(data.CarModel),
+      carNumber: String(data.NuberCar),
+      numberPPO: String(data.NuberPPO),
+      organization: String(data.Organization),
+      typePayment: String(data.TypePayment),
+      loanAgreementStatus: String(data.loanAgreementStatus),
+      quantities: data.StructureQuantities,
+      chronologies: data.ArrayChronologies,
+      contactInformation: data.ContactInformation,
+      orders: data.ArrayStrings,
+      dataCreated: String(data.DateCreated),
+      date: String(data.Date),
+      driver: String(data.Driver),
+      fiterContractor: String(data.FilterContractor),
+    };
+  }
+
+  transformToTransportForTable(data: any) {
+    return {
+      request: data.request,
+      status: String(data.status),
+      ISR: data.ISR,
+      document: String(data.document),
+      // shippingAddress: {
+      //   address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
+      //   coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
+      // },
+      recipient: {
+        name: String(data.contactInformation.Contractor),
+        phone: String(data.contactInformation.Phone),
+      },
+      deliveryTime: {
+        date: String(data.date),
+        time: String(data.contactInformation.Date_Time_delivery),
+      },
+      deliveryAddress: {
+        address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
+        coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
+      },
+      quantitties: {
+        totalPrice: data.quantities
+          ? String(data.quantities.TotalAmount)
+          : '-/-/-',
+        totalWeight: data.quantities
+          ? String(data.quantities.TotalWeight)
+          : '---',
+      },
+    };
   }
 }
 
 
+Unexpected keyword or identifier.ts(1434)
+Duplicate identifier 'eventSource'.ts(2300)
+Subsequent property declarations must have the same type.  Property 'eventSource' must be of type 'EventSource', but here has type 'any'.ts(2717)
+transportRequests.viewmodel.ts(15, 9): 'eventSource' was also declared here.
+(property) TransportRequestsViewModel.eventSource: EventSource
 
 
-
-
-
-
-
-const eventSource = new EventSource('/api/getTransportRequests/sse');
-
-eventSource.onmessage = (event) => {
-  console.log('New message', JSON.parse(event.data));
-  
-  if (JSON.parse(event.data).message === 'Pong') {
-    getTransportRequests(); // вызовите ваш существующий метод, когда получите понг
-  }
-};
+помоги реализовать на фронте
