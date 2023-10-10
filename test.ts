7d2ba9946eeac3a84f964cbecdd112255aaf9ac8
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-10 12:56:10 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,7 @@
-у меня есть массив   drivers 
-у каджого его элемента есть поле driver
-я хочу сравнивать приходящее значение и возвращать элемент у которого имя совпадает с name
-
 async getDriversByName(name: string) {
     const drivers = await this.getAllDrivers();
-    const filteredDrivers = drivers
-      .map((driver) => {
-        
-      })
+    
+    const filteredDrivers = drivers.filter(driver => driver.driver === name);
+    
     return filteredDrivers;
-  }
 }
