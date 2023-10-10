7627f2266938d5279bc72fe556ac995632268450
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-10 11:24:49 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,9 +1,11 @@
 <template>
   <div class="skeleton">
-    <div v-for="u in 9" class="skeleton__item">{{ u }}</div>
-  </div> 
+    <div v-for="u in 9" :key="u" class="skeleton__item"></div>
+  </div>
 </template>
+
 <script setup lang="ts"></script>
+
 <style scoped>
 .skeleton {
   display: flex;
@@ -17,18 +19,38 @@
   box-shadow:
     0px 1px 3px 0px rgba(204, 204, 204, 0.3),
     0px 4px 8px 3px rgba(204, 204, 204, 0.15);
+  overflow: hidden; /* Добавьте это, чтобы углы skeleton__item не выходили за пределы обертки */
+  padding: 16px 0;
 }
+
 .skeleton__item {
-  width: 100%;
+  width: 90%; /* уменьшено, чтобы лучше выглядело в контейнере */
   height: 80px;
-  background: #585151;
+  background: linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%);
+  background-size: 200% 100%;
+  animation: skeleton-animation 1.5s infinite;
   border-radius: 16px;
   margin-bottom: 16px;
+  opacity: 0; /* начальная прозрачность для анимации появления */
+  transform: translateY(20px); /* начальное положение для анимации появления */
+  animation: fadeInUp 0.6s forwards; /* анимация появления */
 }
 
+/* Анимация появления скелетон-загрузчика */
+@keyframes fadeInUp {
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+/* Анимация скелетон-загрузчика */
+@keyframes skeleton-animation {
+  0% {
+    background-position: 200% 0;
+  }
+  100% {
+    background-position: -200% 0;
+  }
+}
 </style>
-
-
-Ты гуру верстки
-сделай так чтобы была анимация пояления строк с классной анимаией для скелетона
-сделай полный рефакторинг скелетона 
