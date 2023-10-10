b1c24e75646de99510be866828560119e0ca4b20
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-10 12:55:13 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,56 +1,13 @@
-<template>
-  <div class="skeleton">
-    <div v-for="u in 9" :key="u" class="skeleton__item"></div>
-  </div>
-</template>
+у меня есть массив   drivers 
+у каджого его элемента есть поле driver
+я хочу сравнивать приходящее значение и возвращать элемент у которого имя совпадает с name
 
-<script setup lang="ts"></script>
-
-<style scoped>
-.skeleton {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 100%;
-  background: #ffffff;
-  border-radius: 16px;
-  box-shadow:
-    0px 1px 3px 0px rgba(204, 204, 204, 0.3),
-    0px 4px 8px 3px rgba(204, 204, 204, 0.15);
-  overflow: hidden; /* Добавьте это, чтобы углы skeleton__item не выходили за пределы обертки */
-  padding: 16px 0;
-}
-
-.skeleton__item {
-  width: 90%; /* уменьшено, чтобы лучше выглядело в контейнере */
-  height: 80px;
-  background: linear-gradient(90deg, #f0f0f0 25%, #e0e0e0 50%, #f0f0f0 75%);
-  background-size: 200% 100%;
-  animation: skeleton-animation 1.5s infinite;
-  border-radius: 16px;
-  margin-bottom: 16px;
-  opacity: 0; /* начальная прозрачность для анимации появления */
-  transform: translateY(20px); /* начальное положение для анимации появления */
-  animation: fadeInUp 0.6s forwards; /* анимация появления */
-}
-
-/* Анимация появления скелетон-загрузчика */
-@keyframes fadeInUp {
-  to {
-    opacity: 1;
-    transform: translateY(0);
+async getDriversByName(name: string) {
+    const drivers = await this.getAllDrivers();
+    const filteredDrivers = drivers
+      .map((driver) => {
+        
+      })
+    return filteredDrivers;
   }
 }
-
-/* Анимация скелетон-загрузчика */
-@keyframes skeleton-animation {
-  0% {
-    background-position: 200% 0;
-  }
-  100% {
-    background-position: -200% 0;
-  }
-}
-</style>
