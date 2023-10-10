bbdda568cb1175451c73996ae9cb876713fc7094
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-10 14:43:26 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,44 +1,36 @@
 sort(index: number): void {
+  const propertyToSortBy = this.getPropertyByIndex(index);
+
   switch (this.sortState) {
     case 'none':
     case 'descending':
       this.sortState = 'ascending';
-      this.model.filteredCouriers.sort((a, b) => a.courierFullName.localeCompare(b.courierFullName));
+      this.model.filteredCouriers.sort((a, b) => 
+        a[propertyToSortBy].length - b[propertyToSortBy].length
+      );
       break;
     case 'ascending':
       this.sortState = 'descending';
-      // Возвращаем исходное состояние:
-      this.model.filteredCouriers = [...this.model.couriers];
+      this.model.filteredCouriers.sort((a, b) => 
+        b[propertyToSortBy].length - a[propertyToSortBy].length
+      );
       break;
   }
 }
 
-
-async getCouriers(): Promise<void> {
-  ...
-  this.model.couriers = couriers;
-  this.model.filteredCouriers = [...this.model.couriers];
-  this.sortState = 'none';
-  ...
-}
-
-
-
-
-export class CouriersViewModel {
-  model: CouriersModel;
-  
-  // Добавьте эту строку:
-  sortState: 'none' | 'ascending' | 'descending' = 'none';
-
-  constructor(model: CouriersModel) {
-    ...
+getPropertyByIndex(index: number): keyof ICourier {
+  switch(index) {
+    case 0: return 'courierFullName';
+    case 1: return 'carNumber';
+    case 2: return 'hiringType';
+    case 3: return 'schedule';
+    case 4: return 'hardWindow';
+    case 5: return 'returnToWarehouse';
+    case 6: return 'city';
+    case 7: return 'goToDetail';
+    default: throw new Error(`Unknown index: ${index}`);
   }
-  
-  ...
 }
-
-
 
 
 
