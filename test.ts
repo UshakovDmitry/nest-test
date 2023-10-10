ca0da2143129f82fc20df04ee3cbb965cebfb69a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-10 11:21:51 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,7 +1,34 @@
-<script lang="ts">
-import { defineComponent } from "vue";
+<template>
+  <div class="skeleton">
+    <div v-for="u in 9" class="skeleton__item">{{ u }}</div>
+  </div> 
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.skeleton {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: #ffffff;
+  border-radius: 16px;
+  box-shadow:
+    0px 1px 3px 0px rgba(204, 204, 204, 0.3),
+    0px 4px 8px 3px rgba(204, 204, 204, 0.15);
+}
+.skeleton__item {
+  width: 100%;
+  height: 80px;
+  background: #585151;
+  border-radius: 16px;
+  margin-bottom: 16px;
+}
 
-export default defineComponent({
-  name: "NotFound",
-});
-</script>
+</style>
+
+
+Ты гуру верстки
+сделай так чтобы была анимация пояления строк с классной анимаией для скелетона
+сделай полный рефакторинг скелетона 
