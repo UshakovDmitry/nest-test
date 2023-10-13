8c2d9c395582b7b61a3fe6eaf34bd16c148cdfe7
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-13 10:52:46 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,3 +1,37 @@
+Ошибка при выполнении запроса к MongoDB: MongoServerError: PlanExecutor error during aggregation :: caused by :: can't convert from BSON type string to
+ Date
+    at Connection.onMessage (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\connection.ts:413:18)
+    at MessageStream.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\connection.ts:243:
+56)
+    at MessageStream.emit (node:events:526:28)
+    at processIncomingData (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\message_stream.ts:193:12
+)
+    at MessageStream._write (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\message_stream.ts:74:5)
+    at writeOrBuffer (node:internal/streams/writable:389:12)
+    at _write (node:internal/streams/writable:330:10)
+    at MessageStream.Writable.write (node:internal/streams/writable:334:10)
+    at Socket.ondata (node:internal/streams/readable:754:22)
+    at Socket.emit (node:events:526:28) {
+  ok: 0,
+  code: 16006,
+  codeName: 'Location16006',
+  [Symbol(errorLabels)]: Set(0) {}
+}
+[Nest] 14456  - 13.10.2023, 10:50:49   ERROR [ExceptionsHandler] PlanExecutor error during aggregation :: caused by :: can't convert from BSON type str
+ing to Date
+MongoServerError: PlanExecutor error during aggregation :: caused by :: can't convert from BSON type string to Date
+    at Connection.onMessage (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\connection.ts:413:18)
+    at MessageStream.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\connection.ts:243:
+56)
+    at MessageStream.emit (node:events:526:28)
+    at processIncomingData (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\message_stream.ts:193:12
+)
+    at MessageStream._write (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongodb\src\cmap\message_stream.ts:74:5)
+    at writeOrBuffer (node:internal/streams/writable:389:12)
+    at _write (node:internal/streams/writable:330:10)
+    at MessageStream.Writable.write (node:internal/streams/writable:334:10)
+    at Socket.ondata (node:internal/streams/readable:754:22)
+    at Socket.emit (node:events:526:28)
 async getTransportRequestsByDate(date: string): Promise<any[]> {
     try {
       // Создание пайплайна для агрегации
