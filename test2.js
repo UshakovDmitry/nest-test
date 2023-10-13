bcf8986107ebb228b0eb544de2e294094e0ebfd2
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-13 10:57:43 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,23 +1,24 @@
-async getTransportRequestsByDate(date: string): Promise<any[]> {
-    try {
-      // Создание пайплайна для агрегации
-      const pipeline = [
-        {
-          $match: {
-            "ContactInformation.Date_Time_delivery": {
-              $regex: new RegExp(`^${date}`), // Соответствие строке, начинающейся с переданной даты
-            },
-          },
-        },
-      ];
+async function fetchTransportRequestsByDate() {
+  // Получение текущей даты и форматирование её в нужный формат (год-месяц-день)
+  const today = new Date();
+  const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
-      // Выполнение агрегации с использованием созданного пайплайна
-      const results = await this.messageModel.aggregate(pipeline).exec();
-      return results;
-    } catch (error) {
-      console.error('Ошибка при выполнении запроса к MongoDB:', error);
-      throw error;
+  try {
+    // Запрос к серверу с сегодняшней датой в качестве параметра
+    const response = await fetch(`http://localhost:3000/api/transport-requests/date/${formattedDate}`);
+
+    if (!response.ok) {
+      throw new Error('Ошибка сети или сервера');
     }
+
+    const data = await response.json(); // Парсинг ответа сервера в JSON
+    console.log(data); // Вывод ответа сервера в консоль
+  } catch (error) {
+    console.error('Ошибка при выполнении запроса:', error);
   }
+}
+
+// Выполнение функции
+fetchTransportRequestsByDate();
 
 
