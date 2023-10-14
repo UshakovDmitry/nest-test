71606024c202922e6df856347929699dc5c613ad
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-14 13:57:26 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,15 +1,11 @@
-function getTotalTransportRequests(drivers) {
-    let totalRequests = 0;
-    
-    drivers.forEach(driver => {
-        totalRequests += driver.transportRequests.length;
-    });
-    
-    return totalRequests;
+ async getDriversStatsByDate (date) {
+  const drivers = await this.getDriversByDate(date);
+  let totalRequests = 0;
+  drivers.forEach((driver) => {
+    totalRequests += driver.transportRequests.length;
+  }
+  );
+  return {
+    totalRequests,
+  };
 }
-
-// Теперь вы можете вызвать функцию getTotalTransportRequests с вашим массивом водителей
-const drivers = [ /* ваш массив объектов водителей */ ];
-const totalRequests = getTotalTransportRequests(drivers);
-
-console.log(`Общее количество заявок у всех водителей: ${totalRequests}`);
