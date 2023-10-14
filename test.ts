9fe2574ee18e6c1ea49f816fd53190e15bd3eefa
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-14 16:48:26 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,28 +1,42 @@
-function updateDeliveryStatus(drivers) {
-    const currentTime = new Date(); // Получение текущего времени
+async  updateDeliveryStatus(drivers) {
+  const currentTime = new Date(); // Получение текущего времени
+  console.log(currentTime, 'currentTime');
+  
+  drivers.forEach(driver => {
+      driver.transportRequests.forEach(request => {
+          if (request.distribution === true && request.documentStatus === "Доставляется") { 
+              request.orders.forEach(order => { 
+                  const [_, deliveryTime] = order.Delivery_Time.split(' '); 
+                  const [deliveryHours, deliveryMinutes, deliverySeconds] = deliveryTime.split(':').map(Number); 
+                  const deliveryDateTime = new Date(); 
+                  deliveryDateTime.setHours(deliveryHours, deliveryMinutes, deliverySeconds);
+                  console.log(deliveryDateTime, 'deliveryDateTime');
+                  
 
-    drivers.forEach(driver => { // Проход по каждому водителю в массиве drivers
-        driver.transportRequests.forEach(request => { // Проход по каждому транспортному запросу в массиве transportRequests
-            if (request.distribution && request.documentStatus === "Доставляется") { // Проверка, удовлетворяет ли заявка условиям
-                request.orders.forEach(order => { // Проход по каждому заказу в массиве orders
-                    const [_, deliveryTime] = order.Delivery_Time.split(' '); // Извлечение времени доставки из строки Delivery_Time
-                    const [deliveryHours, deliveryMinutes, deliverySeconds] = deliveryTime.split(':').map(Number); // Разделение времени доставки на часы, минуты и секунды
-                    const deliveryDateTime = new Date(); // Создание объекта даты для времени доставки
-                    deliveryDateTime.setHours(deliveryHours, deliveryMinutes, deliverySeconds); // Установка времени доставки в объект даты
+                  if (currentTime > deliveryDateTime) { 
+                      request.documentStatus = "Доставляется с опазданием"; 
+                  } else {
+                      request.documentStatus = "Доставляется вовремя"; 
+                  }
+              });
+          }
+      });
+  });
 
-                    if (currentTime > deliveryDateTime) { // Сравнение текущего времени с временем доставки
-                        request.documentStatus = "Доставляется с опазданием"; // Обновление статуса, если текущее время больше времени доставки
-                    } else {
-                        request.documentStatus = "Доставляется вовремя"; // Обновление статуса, если текущее время меньше или равно времени доставки
-                    }
-                });
-            }
-        });
-    });
-
-    return drivers; // Возвращение обновленного массива drivers
+  return drivers; 
 }
 
-// Пример использования функции
-const updatedDrivers = updateDeliveryStatus(driversArray); // Обновление статусов и сохранение результатов в переменной
-console.log(updatedDrivers); // Вывод обновленного массива drivers в консоль
+
+
+
+
+
+
+
+
+
+
+
+
+2023-10-14T10:46:36.295Z currentTime
+2023-10-14T04:04:07.310Z deliveryDateTime
