d53cd36aab964f8cbf0aa624ea1f669b90c15903
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-14 13:30:57 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,24 +1,129 @@
-async function fetchTransportRequestsByDate() {
-  // Получение текущей даты и форматирование её в нужный формат (год-месяц-день)
-  const today = new Date();
-  const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-
-  try {
-    // Запрос к серверу с сегодняшней датой в качестве параметра
-    const response = await fetch(`http://localhost:3000/api/transport-requests/date/${formattedDate}`);
-
-    if (!response.ok) {
-      throw new Error('Ошибка сети или сервера');
-    }
-
-    const data = await response.json(); // Парсинг ответа сервера в JSON
-    console.log(data); // Вывод ответа сервера в консоль
-  } catch (error) {
-    console.error('Ошибка при выполнении запроса:', error);
-  }
-}
-
-// Выполнение функции
-fetchTransportRequestsByDate();
-
-
+[
+    {
+        "driver": "Кунц Алексей Иосифович",
+        "carNumber": "M121235",
+        "carModel": "Gazel",
+        "transportRequests": [
+            {
+                "number": "№№00153190",
+                "IdYandex": "df8167d-dc2362f3-a4bb24c-62c7fea4",
+                "distribution": true,
+                "date": "13.10.2023 0:46:34",
+                "dateCreated": "14-10-2023",
+                "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+                "documentStatus": "Доставляется",
+                "ISR": "290695810",
+                "informalDocument": "Заказ покупателя ППО",
+                "filterContractor": "Kaspi",
+                "loanAgreementStatus": "ПРИНЯТ НАМИ",
+                "typePayment": "Кредит",
+                "chronologies": [
+                    {
+                        "PPO": "8805880",
+                        "Chronology": [
+                            "Оформлен",
+                            "Доставляется до клиента (на складе отгрузки)"
+                        ]
+                    }
+                ],
+                "contactInformation": {
+                    "City": "Актобе",
+                    "Delivery_Condition": "Доставка",
+                    "Date_Time_delivery": "2023-10-14 До 20:00",
+                    "Time_Window": "нет данных",
+                    "Latitude": "50,287135",
+                    "Longitude": "57,223016",
+                    "Street": "4",
+                    "Home": "79",
+                    "Phone": "(775)8473732",
+                    "Apartment": "4",
+                    "Contractor": "Есжанова Гульнар",
+                    "_id": "652a340d659fe9648d0fe8bf"
+                },
+                "orders": [
+                    {
+                        "NuberPPO": "8805880",
+                        "PPOStatus": "Доставляется до клиента (на складе отгрузки)",
+                        "SKU": "1330427",
+                        "Goods": "Умная Колонка Яндекс.Станция Лайт, Фламинго (YNDX-00025 Pink)",
+                        "Count": "1",
+                        "ShippingAddress": "Aktobe, Virtualniy sklad, b/n",
+                        "Brand": "Яндекс",
+                        "Weight": "0,4",
+                        "Price": "28 990",
+                        "Item_Status": "Оформлена",
+                        "Pickup_Point": "2",
+                        "Delivery_Point": "7",
+                        "Pickup_Latitude": "50,290847",
+                        "Pickup_Longitude": "57,156415",
+                        "Delivery_Latitude": "50,287135",
+                        "Delivery_Longitude": "57,223016",
+                        "Pickup_Time": "01.01.0001 9:32:40",
+                        "Delivery_Time": "01.01.0001 11:25:18"
+                    }
+                ]
+            },
+            {
+                "number": "№№00153515",
+                "IdYandex": "df8167d-dc2362f3-a4bb24c-62c7fea4",
+                "distribution": true,
+                "date": "13.10.2023 17:46:51",
+                "dateCreated": "14-10-2023",
+                "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+                "documentStatus": "Доставляется",
+                "ISR": "290849389",
+                "informalDocument": "Заказ покупателя ППО",
+                "filterContractor": "Kaspi",
+                "loanAgreementStatus": "ПРИНЯТ НАМИ",
+                "typePayment": "Кредит",
+                "chronologies": [
+                    {
+                        "PPO": "8807645",
+                        "Chronology": [
+                            "Оформлен",
+                            "Доставляется до клиента (на складе отгрузки)"
+                        ]
+                    }
+                ],
+                "contactInformation": {
+                    "City": "Актобе",
+                    "Delivery_Condition": "Доставка",
+                    "Date_Time_delivery": "2023-10-14 До 20:00",
+                    "Time_Window": "нет данных",
+                    "Latitude": "50,31874",
+                    "Longitude": "57,130364",
+                    "Street": "6",
+                    "Home": "97",
+                    "Phone": "(747)2133273",
+                    "Apartment": "6",
+                    "Contractor": "Колесникова Виктория",
+                    "_id": "652a34509ae58a6682c9b057"
+                },
+                "orders": [
+                    {
+                        "NuberPPO": "8807645",
+                        "PPOStatus": "Доставляется до клиента (на складе отгрузки)",
+                        "SKU": "1411187",
+                        "Goods": "Пылесос Gorenje  VC1903GACBUCY (синий)",
+                        "Count": "1",
+                        "ShippingAddress": "Aktobe, Virtualniy sklad, b/n",
+                        "Brand": "GORENJE",
+                        "Weight": "7,3",
+                        "Price": "29 990",
+                        "Item_Status": "Оформлена",
+                        "Pickup_Point": "2",
+                        "Delivery_Point": "3",
+                        "Pickup_Latitude": "50,290847",
+                        "Pickup_Longitude": "57,156415",
+                        "Delivery_Latitude": "50,31874",
+                        "Delivery_Longitude": "57,130364",
+                        "Pickup_Time": "01.01.0001 9:32:40",
+                        "Delivery_Time": "01.01.0001 10:22:11"
+                    }
+                ]
+            }
+        ],
+        "countCompletedOrders": "0",
+        "countPendingOrders": "2",
+        "countAllOrders": "2"
+    },
