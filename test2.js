18b8e88aef94a27849bdd7f4c2e766d6fc2ee5da
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-14 14:09:43 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,1 +1,20 @@
-        "DocumentStatus": "Отказ клиента от заказа",
+
+  async getDriversStatsByDate(date) {
+    const drivers = await this.getDriversByDate(date);
+    let totalRequests = 0;
+    let couriers_online = 0;
+
+    drivers.forEach((driver) => {
+      // Количество всех заявок
+      totalRequests += driver.transportRequests.length;
+      // Количество курьеров у которых заполнены поля carNumber иcarModel
+      if (driver.carNumber.trim() !== '' && driver.carModel.trim() !== '') {
+        couriers_online++;
+      }
+    });
+
+    return {
+      totalRequests,
+      couriers_online,
+    };
+  }
