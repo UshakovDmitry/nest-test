9073c6beb97ec843b2530404b03e390e1edc3c84
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-14 14:10:39 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,20 +1,30 @@
-
-  async getDriversStatsByDate(date) {
+async getDriversStatsByDate(date) {
     const drivers = await this.getDriversByDate(date);
     let totalRequests = 0;
     let couriers_online = 0;
+    let count_failure = 0;
 
     drivers.forEach((driver) => {
-      // Количество всех заявок
-      totalRequests += driver.transportRequests.length;
-      // Количество курьеров у которых заполнены поля carNumber иcarModel
-      if (driver.carNumber.trim() !== '' && driver.carModel.trim() !== '') {
-        couriers_online++;
-      }
+        // Количество всех заявок
+        totalRequests += driver.transportRequests.length;
+        
+        // Количество курьеров у которых заполнены поля carNumber и carModel
+        if (driver.carNumber.trim() !== '' && driver.carModel.trim() !== '') {
+            couriers_online++;
+        }
+        
+        // Количество заявок со статусом "Отказ клиента от заказа"
+        driver.transportRequests.forEach((request) => {
+            if (request.documentStatus === "Отказ клиента от заказа") {
+                count_failure++;
+            }
+        });
     });
 
     return {
-      totalRequests,
-      couriers_online,
+        totalRequests,
+        couriers_online,
+        count_failure
     };
-  }
+}
+
