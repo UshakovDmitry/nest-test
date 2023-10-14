2a054fe575f886a0dc375e7dac6393052beda50d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-14 14:30:48 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,9 +1,9 @@
-rescheduling the delivery of my order
-  async getDriversStatsByDate(date) {
+async getDriversStatsByDate(date) {
     const drivers = await this.getDriversByDate(date);
     let totalRequests = 0;
     let couriers_online = 0;
     let count_failure = 0;
+    let rescheduling_delivery = 0;
 
     drivers.forEach((driver) => {
         // Количество всех заявок
@@ -14,23 +14,26 @@ rescheduling the delivery of my order
             couriers_online++;
         }
         
-        // Количество заявок со статусом "Отказ клиента от заказа"
+        // Количество заявок со статусом "Отказ клиента от заказа" и "Перенос доставки"
         driver.transportRequests.forEach((request) => {
             if (request.documentStatus === "Отказ клиента от заказа") {
                 count_failure++;
+            }
+            if (request.documentStatus === "Перенос доставки") {
+                rescheduling_delivery++;
             }
         });
     });
 
     // Рассчет процента отказов и округление до 2 знаков после запятой
     let failure_rate = totalRequests ? (count_failure / totalRequests) * 100 : 0;
-    failure_rate =parseFloat(failure_rate.toFixed(2));
+    failure_rate = parseFloat(failure_rate.toFixed(2));
 
     return {
         totalRequests,
         couriers_online,
         count_failure,
-        failure_rate
+        failure_rate,
+        rescheduling_delivery
     };
 }
-
