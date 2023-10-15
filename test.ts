1d3d1aea3c54b8707f491a42cbde52d437275fc5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-15 13:20:56 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,31 +1,29 @@
-async  updateDeliveryStatus(drivers) {
-  const currentTime = new Date(); // Получение текущего времени
-  console.log(currentTime, 'currentTime');
-  
-  drivers.forEach(driver => {
-      driver.transportRequests.forEach(request => {
-          if (request.distribution === true && request.documentStatus === "Доставляется") { 
-              request.orders.forEach(order => { 
-                  const [_, deliveryTime] = order.Delivery_Time.split(' '); 
-                  const [deliveryHours, deliveryMinutes, deliverySeconds] = deliveryTime.split(':').map(Number); 
-                  const deliveryDateTime = new Date(); 
-                  deliveryDateTime.setHours(deliveryHours, deliveryMinutes, deliverySeconds);
-                  console.log(deliveryDateTime, 'deliveryDateTime');
-                  
+  async getTransportRequests(): Promise<void> {
+    console.log('getTransportRequests');
+    
+    const today = new Date();
+    const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+    const body = {
+      date: formattedDate,
+    };
+    const response = await usePostApi('getTransportRequestsByDate', body);
+    console.log(response.length, 'кол-во заявок');
+    // console.log(response, 'response');
+    response.forEach((data: any) => {
+      const transformedData = this.transformToTransportRequest(data);
+      const city = this.setCitiesList(transformedData);
+      // Проверяем наличие города в списке и добавляем, если его нет
+      if (!this.model.cities.includes(city)) {
+        this.model.cities.push(city);
+      }
+      const transformedDataForTable =
+        this.transformToTransportForTable(transformedData);
 
-                  if (currentTime > deliveryDateTime) { 
-                      request.documentStatus = "Доставляется с опазданием"; 
-                  } else {
-                      request.documentStatus = "Доставляется вовремя"; 
-                  }
-              });
-          }
-      });
-  });
-
-  return drivers; 
-}
-
+      this.model.transportRequests.unshift(transformedDataForTable);
+      this.model.filteredTransportRequests = this.model.transportRequests;
+    });
+    this.model.isToday = true;
+  }
 
 
 
