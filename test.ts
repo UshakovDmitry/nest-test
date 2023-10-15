7c8d2af87a40dc08328e891dcd5e00bd89846b5a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-15 15:11:18 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -4,43 +4,55 @@
         v-for="(count, contractor) in filterContractors" 
         :key="contractor" 
         class="btn"
-        @click="emits('filterRequestsByContractor',contractor)"
+        :class="{ 'active': activeButton === contractor }" 
+        @click="buttonClicked(contractor)"
       >
         {{ contractor }} ({{ count }})
       </button>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { defineProps, defineEmits } from 'vue';
-  
-  defineProps({
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { defineProps, defineEmits } from 'vue';
+
+defineProps({
     filterContractors: Object,
-  });
-  
-  
+});
+
 const emits = defineEmits(['filterRequestsByContractor']);
-  </script>
-    <style scoped>
-    .wrapper {
-      display: flex;
-      flex-wrap: wrap;
-      gap: 12px;
-      /* margin-bottom: 20px; */
-    }
-    .btn {
-        background-color: #ffffff;
-        border: 1px solid #e5e5e5;
-        box-sizing: border-box;
-        border-radius: 40px;
-        padding: 8px 16px;
-        font-size: 14px;
-        line-height: 20px;
-        color: #000000;
-        cursor: pointer;
-    }
-    .btn:hover {
-        background-color: #f5f5f5;
-    }
-    </style>
-  
+
+const activeButton = ref(''); // реактивная переменная для хранения активной кнопки
+
+const buttonClicked = (contractor: string) => {
+    activeButton.value = contractor; // обновление активной кнопки
+    emits('filterRequestsByContractor', contractor); // отправка события
+};
+</script>
+
+<style scoped>
+.wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 12px;
+}
+
+.btn {
+    background-color: #ffffff;
+    border: 1px solid #e5e5e5;
+    box-sizing: border-box;
+    border-radius: 40px;
+    padding: 8px 16px;
+    font-size: 14px;
+    line-height: 20px;
+    color: #000000;
+    cursor: pointer;
+}
+
+.btn:hover,
+.btn.active {
+    background-color: green; /* или любой другой цвет для активной кнопки */
+    color: #ffffff;
+}
+</style>
+
