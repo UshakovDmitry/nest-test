45c40179ecc5402c511267dfa71b1682f5a85b32
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-15 15:41:57 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,5 +1,43 @@
-filterRequestsByCity(city: string): void {
+transformToTransportForTable(data: any) {
+    return {
+      request: data.request,
+      status: String(data.status),
+      ISR: data.ISR,
+      document: String(data.document),
+      filterContractor: String(data.filterContractor),
+      // shippingAddress: {
+      //   address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
+      //   coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
+      // },
+      recipient: {
+        name: String(data.contactInformation.Contractor),
+        phone: String(data.contactInformation.Phone),
+      },
+      deliveryTime: {
+        date: String(data.date),
+        time: String(data.contactInformation.Date_Time_delivery),
+      },
+      deliveryAddress: {
+        address: `${data.contactInformation.City}, ${data.contactInformation.Street}, ${data.contactInformation.Home}, ${data.contactInformation.Apartment}`,
+        coordinates: `${data.contactInformation.Latitude}, ${data.contactInformation.Longitude}`,
+      },
+      quantitties: {
+        totalPrice: data.quantities
+          ? String(data.quantities.TotalAmount)
+          : '-/-/-',
+        totalWeight: data.quantities
+          ? String(data.quantities.TotalWeight)
+          : '---',
+      },
+    };
+  }
+
+  filterRequestsByCity(city: string): void {
     this.model.filteredTransportRequests = this.model.transportRequests.filter(
-        request => request.city === city
+        request => request.deliveryAddress.address === city
     );
 }
+}
+
+
+нужно достать значение data.contactInformation.City через split
