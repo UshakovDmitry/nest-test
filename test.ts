6b6310bf6b7f730b9f0a0b78c79214c94f865405
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-15 13:36:41 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,38 +1,42 @@
-  async getTransportRequests(day: string): Promise<void> {
+ async getTransportRequests(day: string): Promise<void> {
     console.log('day', day);
   
     const today = new Date();
   
     if (day === "tomorrow") {
-      today.setDate(today.getDate() + 1);
-      this.model.isTomorrow = true;
-      this.model.isToday = false;
-      this.model.isYesterday = false;
+        today.setDate(today.getDate() + 1);
+        this.model.isTomorrow = true;
+        this.model.isToday = false;
+        this.model.isYesterday = false;
     } else if (day === "yesterday") {
-      today.setDate(today.getDate() - 1);
-      this.model.isTomorrow = false;
-      this.model.isToday = false;
+        today.setDate(today.getDate() - 1);
+        this.model.isTomorrow = false;
+        this.model.isToday = false;
+        this.model.isYesterday = true;
+    } else if (day === "today") {
+        this.model.isTomorrow = false;
+        this.model.isToday = true;
+        this.model.isYesterday = false;
     }
   
     const formattedDate = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
     const body = {
-      date: formattedDate,
+        date: formattedDate,
     };
     const response = await usePostApi('getTransportRequestsByDate', body);
     console.log(response.length, 'кол-во заявок');
     // console.log(response, 'response');
     this.model.transportRequests = [];
     response.forEach((data: any) => {
-      const transformedData = this.transformToTransportRequest(data);
-      const city = this.setCitiesList(transformedData);
-      // Проверяем наличие города в списке и добавляем, если его нет
-      if (!this.model.cities.includes(city)) {
-        this.model.cities.push(city);
-      }
-      const transformedDataForTable = this.transformToTransportForTable(transformedData);
+        const transformedData = this.transformToTransportRequest(data);
+        const city = this.setCitiesList(transformedData);
+        // Проверяем наличие города в списке и добавляем, если его нет
+        if (!this.model.cities.includes(city)) {
+            this.model.cities.push(city);
+        }
+        const transformedDataForTable = this.transformToTransportForTable(transformedData);
 
-      this.model.transportRequests.unshift(transformedDataForTable);
-      this.model.filteredTransportRequests = this.model.transportRequests;
+        this.model.transportRequests.unshift(transformedDataForTable);
+        this.model.filteredTransportRequests = this.model.transportRequests;
     });
-    
-  }
+}
