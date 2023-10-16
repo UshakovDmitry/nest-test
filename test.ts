4c85aa405f8f92c76fd774ac4c81bf2f28200093
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-16 13:26:36 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,22 +1,3 @@
-@Injectable()
-export class CouriersService {
-  constructor(
-    private readonly httpService: HttpService,
-    private readonly dbService: DBService,
-  ) {}
-
-  async getCouriersByDate(date: string) {
-    const driversByDate =  this.dbService.getDriversByDate(date);
-    const allCouriers = await this.getCouriers();
-    
-    // Фильтруем allCouriers, оставляя только тех водителей, которые присутствуют в driversByDate.
-    const filteredCouriers = allCouriers.filter(courier => 
-      driversByDate.some(driver => driver.driver === courier.Drivers)
-    );
-    
-    return filteredCouriers; // Возвращаем отфильтрованный список курьеров.
-  }
-
-  // ... (остальные методы)
-}
+Property 'some' does not exist on type 'Promise<any[]>'.ts(2339)
+couriers.service.ts(19, 21): Did you forget to use 'await'?
 
