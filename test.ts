488ef73118ee4ce57fceb7c44874cdacfcb276f9
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-17 18:03:37 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,58 +1,12 @@
-async updateDeliveryStatus(drivers) {
-  const currentTime = new Date();
-  currentTime.setMinutes(currentTime.getMinutes() + 15);
+http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver
 
-  drivers.forEach((driver) => {
-    driver.transportRequests.forEach((request) => {
-      let deliveryDateTime = null; // Переменная deliveryDateTime объявлена здесь
 
-      if (request.distribution === true && request.documentStatus === 'Доставляется') {
-        request.orders.forEach((order) => {
-          const [_, deliveryTime] = order.Delivery_Time.split(' ');
-          const [deliveryHours, deliveryMinutes, deliverySeconds] =
-            deliveryTime.split(':').map(Number);
-          deliveryDateTime = new Date();
-          deliveryDateTime.setHours(
-            deliveryHours,
-            deliveryMinutes,
-            deliverySeconds,
-          );
-
-          if (currentTime > deliveryDateTime) {
-            request.documentStatus = 'Доставляется с опазданием';
-          } else {
-            request.documentStatus = 'Доставляется вовремя';
-          }
-        });
-      }
-
-      if (request.distribution === true && request.documentStatus === 'Доставлено') {
-        let completedDateTime = null;
-        
-        request.orders.forEach((order) => {
-          if (order.completedDelivery) {
-            const [_, completedDelivery] = order.completedDelivery.split(' ');
-            const [completedHours, completedMinutes, completedSeconds] =
-              completedDelivery.split(':').map(Number);
-            completedDateTime = new Date();
-            completedDateTime.setHours(
-              completedHours,
-              completedMinutes,
-              completedSeconds,
-            );
-          }
-        });
-
-        if (completedDateTime && completedDateTime > deliveryDateTime) {
-          request.documentStatus = 'Доставлено с опазданием';
-        } else {
-          request.documentStatus = 'Доставлено вовремя';
-        }
-      }
-    });
-  });
-
-  return drivers;
+{
+    "DocNumber": "№MER000718",
+    "DateDoc": "20230926",
+    "TimeDelivery": "20231017180000",
+    "Driver": "Муравейников Сергей Николаевич",
+    "СarNumber": "A589",
+    "User": "660903402365"
+    "Сomment": "Корретировка диспетчером."
 }
-
-
