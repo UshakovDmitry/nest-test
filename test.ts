3a15fd56ed6802aa1718b0af800962ee26bd00e6
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-17 16:40:46 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,3 +1,58 @@
-Cannot find name 'deliveryDateTime'.ts(2304)
+async updateDeliveryStatus(drivers) {
+  const currentTime = new Date();
+  currentTime.setMinutes(currentTime.getMinutes() + 15);
+
+  drivers.forEach((driver) => {
+    driver.transportRequests.forEach((request) => {
+      let deliveryDateTime = null; // Переменная deliveryDateTime объявлена здесь
+
+      if (request.distribution === true && request.documentStatus === 'Доставляется') {
+        request.orders.forEach((order) => {
+          const [_, deliveryTime] = order.Delivery_Time.split(' ');
+          const [deliveryHours, deliveryMinutes, deliverySeconds] =
+            deliveryTime.split(':').map(Number);
+          deliveryDateTime = new Date();
+          deliveryDateTime.setHours(
+            deliveryHours,
+            deliveryMinutes,
+            deliverySeconds,
+          );
+
+          if (currentTime > deliveryDateTime) {
+            request.documentStatus = 'Доставляется с опазданием';
+          } else {
+            request.documentStatus = 'Доставляется вовремя';
+          }
+        });
+      }
+
+      if (request.distribution === true && request.documentStatus === 'Доставлено') {
+        let completedDateTime = null;
+        
+        request.orders.forEach((order) => {
+          if (order.completedDelivery) {
+            const [_, completedDelivery] = order.completedDelivery.split(' ');
+            const [completedHours, completedMinutes, completedSeconds] =
+              completedDelivery.split(':').map(Number);
+            completedDateTime = new Date();
+            completedDateTime.setHours(
+              completedHours,
+              completedMinutes,
+              completedSeconds,
+            );
+          }
+        });
+
+        if (completedDateTime && completedDateTime > deliveryDateTime) {
+          request.documentStatus = 'Доставлено с опазданием';
+        } else {
+          request.documentStatus = 'Доставлено вовремя';
+        }
+      }
+    });
+  });
+
+  return drivers;
+}
 
 
