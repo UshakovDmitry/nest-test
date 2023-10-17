16d7c17deef7e9332270e0889393e7d4c82a239d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-17 16:09:49 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,73 +1,72 @@
 async updateDeliveryStatus(drivers) {
-    const currentTime = new Date(); // Получение текущего времени
-    currentTime.setMinutes(currentTime.getMinutes() + 15); // Добавление 15 минут запаса на доставку
+  const currentTime = new Date();
+  currentTime.setMinutes(currentTime.getMinutes() + 15);
 
-    drivers.forEach((driver) => {
-      driver.transportRequests.forEach((request) => {
-        if (
-          request.distribution === true &&
-          request.documentStatus === 'Доставляется'
-        ) {
-          request.orders.forEach((order) => {
-            const [_, deliveryTime] = order.Delivery_Time.split(' ');
-            const [deliveryHours, deliveryMinutes, deliverySeconds] =
-              deliveryTime.split(':').map(Number);
-            const deliveryDateTime = new Date();
-            deliveryDateTime.setHours(
-              deliveryHours,
-              deliveryMinutes,
-              deliverySeconds,
-            );
+  drivers.forEach((driver) => {
+    driver.transportRequests.forEach((request) => {
+      if (request.distribution === true) {
+        let hasLateDelivery = false;
 
-            if (currentTime > deliveryDateTime) {
-              request.documentStatus = 'Доставляется с опазданием';
-            } else {
-              request.documentStatus = 'Доставляется вовремя';
-            }
-          });
+        request.orders.forEach((order) => {
+          const [_, deliveryTime] = order.Delivery_Time.split(' ');
+          const [deliveryHours, deliveryMinutes, deliverySeconds] =
+            deliveryTime.split(':').map(Number);
+          const deliveryDateTime = new Date();
+          deliveryDateTime.setHours(
+            deliveryHours,
+            deliveryMinutes,
+            deliverySeconds,
+          );
+
+          if (currentTime > deliveryDateTime) {
+            hasLateDelivery = true;
+          }
+        });
+
+        if (hasLateDelivery) {
+          request.documentStatus = 'Доставляется с опазданием';
+        } else {
+          request.documentStatus = 'Доставляется вовремя';
         }
-        if (
-          request.distribution === true &&
-          request.documentStatus === 'Доставлено'
-        )
-{
-  // const GoodTime = new Date(); 
-  //   GoodTime.setMinutes(GoodTime.getMinutes() + 15);
-  request.orders.forEach((order) => {
-    const [_, deliveryTime] = order.Delivery_Time.split(' ');
-    const [deliveryHours, deliveryMinutes, deliverySeconds] =
-      deliveryTime.split(':').map(Number);
-    const deliveryDateTime = new Date();
-    deliveryDateTime.setHours(
-      deliveryHours,
-      deliveryMinutes,
-      deliverySeconds,
-    );
+      }
 
-    const [_, completedDelivery] = order.completedDelivery.split(' ');
-    const [completedHours, completedMinutes, completedSeconds] =
-      completedDelivery.split(':').map(Number);
-    const completedDateTime = new Date();
-    completedDateTime.setHours(
-      completedHours,
-      completedMinutes,
-      completedSeconds,
-    );
-    if (deliveryDateTime > completedDateTime) {
-      request.documentStatus = 'Доставлено вовремя';
-    } else {
-      request.documentStatus = 'Доставлено с опазданием';
-    }
-  }
-  );
-}
-      });
+      if (request.distribution === true && request.documentStatus === 'Доставлено') {
+        let hasLateDelivery = false;
+
+        request.orders.forEach((order) => {
+          const [_, deliveryTime] = order.Delivery_Time.split(' ');
+          const [deliveryHours, deliveryMinutes, deliverySeconds] =
+            deliveryTime.split(':').map(Number);
+          const deliveryDateTime = new Date();
+          deliveryDateTime.setHours(
+            deliveryHours,
+            deliveryMinutes,
+            deliverySeconds,
+          );
+
+          const [_, completedDelivery] = order.completedDelivery.split(' ');
+          const [completedHours, completedMinutes, completedSeconds] =
+            completedDelivery.split(':').map(Number);
+          const completedDateTime = new Date();
+          completedDateTime.setHours(
+            completedHours,
+            completedMinutes,
+            completedSeconds,
+          );
+
+          if (deliveryDateTime > completedDateTime) {
+            hasLateDelivery = true;
+          }
+        });
+
+        if (hasLateDelivery) {
+          request.documentStatus = 'Доставлено с опазданием';
+        } else {
+          request.documentStatus = 'Доставлено вовремя';
+        }
+      }
     });
+  });
 
-    return drivers;
-  }
-
-
-
-
-Cannot redeclare block-scoped variable '_'.ts(2451)
+  return drivers;
+}
