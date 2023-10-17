1cacc31dd5d4744009bf47dc4607d3ca2c32f0cb
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-17 11:52:16 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,27 +1,95 @@
-API для разработчиков
-Данный компонент предназначен для интеграции продукта Gelios с другими информационными системами.
+// src/drivers/driver-location.model.ts
 
-Запрос в общем виде выглядит следующим образом:
+export class DriverLocation {
+  constructor(
+    public id: number,
+    public latitude: number,
+    public longitude: number,
+    // Другие поля, если необходимо
+  ) {}
+}
 
-admin.geliospro.com/sdk/?login=demo&pass=demo&svc=get_units&params={}
-Вместо пары логин и пароль также можно использовать токен:
 
-admin.geliospro.com/sdk/?token=111222111222111222111222&svc=get_units&params={}
-Все запросы на изменение данных требуют прав администратора и прав на редактирование сущности.
+// src/drivers/drivers.service.ts
+import { Injectable } from '@nestjs/common';
+import axios from 'axios'; // Установите axios, если он не установлен
 
-Компонент включает в себя следующие группы запросов:
+@Injectable()
+export class DriversService {
+  async getDriverLocations(login: string, pass: string) {
+    try {
+      // Выполните запрос к API GeliosPro для получения данных о координатах водителей
+      const response = await axios.get(
+        `https://admin.geliospro.com/sdk/?login=${login}&pass=${pass}&svc=get_units&params={}`
+      );
 
-Авторизация
-Пользователи
-Права
-Объекты
-Датчики
-Команды
-Отчеты
-Геообъекты
-Уведомления
-Маршруты
-Ретрансляторы
-Статистика
-Локатор
-—- Коды ошибок
+      // Обработайте ответ и верните данные о координатах водителей в нужном формате
+      // Возможно, вам придется парсить JSON-ответ и извлекать нужные данные
+
+      return response.data; // Верните данные о координатах водителей
+    } catch (error) {
+      throw new Error('Не удалось получить данные о водителях');
+    }
+  }
+}
+
+
+
+
+
+
+
+
+// src/drivers/drivers.controller.ts
+import { Controller, Get, Query } from '@nestjs/common';
+import { DriversService } from './drivers.service';
+
+@Controller('drivers')
+export class DriversController {
+  constructor(private readonly driversService: DriversService) {}
+
+  @Get('locations')
+  async getDriverLocations(
+    @Query('login') login: string,
+    @Query('pass') pass: string,
+  ) {
+    const driverLocations = await this.driversService.getDriverLocations(
+      login,
+      pass
+    );
+    return driverLocations;
+  }
+}
+
+
+
+
+
+
+
+
+// src/app.module.ts
+import { Module } from '@nestjs/common';
+import { DriversController } from './drivers/drivers.controller';
+import { DriversService } from './drivers/drivers.service';
+
+@Module({
+  imports: [],
+  controllers: [DriversController],
+  providers: [DriversService],
+})
+export class AppModule {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
