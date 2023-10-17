84dbb139d17f11edc209f9fb174f39a0702d2cfc
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-17 16:37:33 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,72 +1,69 @@
-async updateDeliveryStatus(drivers) {
-  const currentTime = new Date();
-  currentTime.setMinutes(currentTime.getMinutes() + 15);
 
-  drivers.forEach((driver) => {
-    driver.transportRequests.forEach((request) => {
-      if (request.distribution === true) {
-        let hasLateDelivery = false;
+  async updateDeliveryStatus(drivers) {
+    const currentTime = new Date(); // Получение текущего времени
+    currentTime.setMinutes(currentTime.getMinutes() + 15); // Добавление 15 минут запаса на доставку
 
-        request.orders.forEach((order) => {
-          const [unusedVar, deliveryTime] = order.Delivery_Time.split(' ');
-          const [deliveryHours, deliveryMinutes, deliverySeconds] =
-            deliveryTime.split(':').map(Number);
-          const deliveryDateTime = new Date();
-          deliveryDateTime.setHours(
-            deliveryHours,
-            deliveryMinutes,
-            deliverySeconds,
-          );
+    drivers.forEach((driver) => {
+      driver.transportRequests.forEach((request) => {
+        if (
+          request.distribution === true &&
+          request.documentStatus === 'Доставляется'
+        ) {
+          request.orders.forEach((order) => {
+            const [_, deliveryTime] = order.Delivery_Time.split(' ');
+            const [deliveryHours, deliveryMinutes, deliverySeconds] =
+              deliveryTime.split(':').map(Number);
+            const deliveryDateTime = new Date();
+            deliveryDateTime.setHours(
+              deliveryHours,
+              deliveryMinutes,
+              deliverySeconds,
+            );
 
-          if (currentTime > deliveryDateTime) {
-            hasLateDelivery = true;
-          }
-        });
-
-        if (hasLateDelivery) {
-          request.documentStatus = 'Доставляется с опазданием';
-        } else {
-          request.documentStatus = 'Доставляется вовремя';
+            if (currentTime > deliveryDateTime) {
+              request.documentStatus = 'Доставляется с опазданием';
+            } else {
+              request.documentStatus = 'Доставляется вовремя';
+            }
+          });
         }
-      }
-
-      if (request.distribution === true && request.documentStatus === 'Доставлено') {
-        let hasLateDelivery = false;
-
-        request.orders.forEach((order) => {
-          const [unusedVar, deliveryTime] = order.Delivery_Time.split(' ');
-          const [deliveryHours, deliveryMinutes, deliverySeconds] =
-            deliveryTime.split(':').map(Number);
-          const deliveryDateTime = new Date();
-          deliveryDateTime.setHours(
-            deliveryHours,
-            deliveryMinutes,
-            deliverySeconds,
-          );
-
-          const [unusedVar2, completedDelivery] = order.completedDelivery.split(' ');
-          const [completedHours, completedMinutes, completedSeconds] =
-            completedDelivery.split(':').map(Number);
-          const completedDateTime = new Date();
-          completedDateTime.setHours(
-            completedHours,
-            completedMinutes,
-            completedSeconds,
-          );
-
-          if (deliveryDateTime > completedDateTime) {
-            hasLateDelivery = true;
+        if (
+          request.distribution === true &&
+          request.documentStatus === 'Доставлено'
+        ) {
+          request.orders.forEach((order) => {
+            if (order.completedDelivery) {
+            const [_, completedDelivery] = order.completedDelivery.split(' ');
+            const [completedHours, completedMinutes, completedSeconds] =
+              completedDelivery.split(':').map(Number);
+            const completedDateTime = new Date();
+            completedDateTime.setHours(
+              completedHours,
+              completedMinutes,
+              completedSeconds,
+            );
+            if (currentTime > completedDateTime) {
+              request.documentStatus = 'Доставлено с опазданием';
+            } else {
+              request.documentStatus = 'Доставлено вовремя';
+            }
           }
-        });
 
-        if (hasLateDelivery) {
-          request.documentStatus = 'Доставлено с опазданием';
-        } else {
-          request.documentStatus = 'Доставлено вовремя';
+          });
         }
-      }
+      });
     });
-  });
 
-  return drivers;
-}
+    return drivers;
+  }
+в этом условии 
+if (
+          request.distribution === true &&
+          request.documentStatus === 'Доставлено'
+        )
+я хочу определять completedDateTime и сравнивать не с currentTime а с deliveryDateTime
+которое используется ранее 
+реализуй это
+
+
+
