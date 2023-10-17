4285a93591461f8f72d0873489fe6d7f57cb63c3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-17 16:04:13 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,35 +1,68 @@
- getDriversStatsByDate(date: string) {
-    const drivers = this.dbService.getDriversStatsByDate(date);
-    const geliosCars = this.geliosService.getCarLocations(
-      GELIOS_PRO_LOGIN,
-      GELIOS_PRO_PASSWORD
-    );
-  
-    // Пройдемся по каждому элементу в geliosCars
-    geliosCars.forEach((geliosCar) => {
-      const { name, latitude, longitude } = geliosCar;
-  
-      // Уберем пробелы из номера и сравним с полем carNumber
-      const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(/\s+/g, '');
-      
-      
-      drivers.forEach((driver) => {
-        if (driver.carNumber === carNumberWithoutSpaces) {
-          // Если номера совпадают, добавим координаты к объекту в массиве drivers
-          driver.coordinates = {
-            latitude,
-            longitude,
-          };
+async updateDeliveryStatus(drivers) {
+    const currentTime = new Date(); // Получение текущего времени
+    currentTime.setMinutes(currentTime.getMinutes() + 15); // Добавление 15 минут запаса на доставку
+
+    drivers.forEach((driver) => {
+      driver.transportRequests.forEach((request) => {
+        if (
+          request.distribution === true &&
+          request.documentStatus === 'Доставляется'
+        ) {
+          request.orders.forEach((order) => {
+            const [_, deliveryTime] = order.Delivery_Time.split(' ');
+            const [deliveryHours, deliveryMinutes, deliverySeconds] =
+              deliveryTime.split(':').map(Number);
+            const deliveryDateTime = new Date();
+            deliveryDateTime.setHours(
+              deliveryHours,
+              deliveryMinutes,
+              deliverySeconds,
+            );
+
+            if (currentTime > deliveryDateTime) {
+              request.documentStatus = 'Доставляется с опазданием';
+            } else {
+              request.documentStatus = 'Доставляется вовремя';
+            }
+          });
         }
+        if (
+          request.distribution === true &&
+          request.documentStatus === 'Доставлено'
+        )
+{
+  // const GoodTime = new Date(); 
+  //   GoodTime.setMinutes(GoodTime.getMinutes() + 15);
+  request.orders.forEach((order) => {
+    const [_, deliveryTime] = order.Delivery_Time.split(' ');
+    const [deliveryHours, deliveryMinutes, deliverySeconds] =
+      deliveryTime.split(':').map(Number);
+    const deliveryDateTime = new Date();
+    deliveryDateTime.setHours(
+      deliveryHours,
+      deliveryMinutes,
+      deliverySeconds,
+    );
+
+    const [_, completedDelivery] = order.completedDelivery.split(' ');
+    const [completedHours, completedMinutes, completedSeconds] =
+      completedDelivery.split(':').map(Number);
+    const completedDateTime = new Date();
+    completedDateTime.setHours(
+      completedHours,
+      completedMinutes,
+      completedSeconds,
+    );
+    if (deliveryDateTime > completedDateTime) {
+      request.documentStatus = 'Доставлено вовремя';
+    } else {
+      request.documentStatus = 'Доставлено с опазданием';
+    }
+  }
+  );
+}
       });
     });
-  
-    // В итоге у вас будет массив drivers с добавленными координатами для совпавших номеров машин
-    console.log(drivers);
+
+    return drivers;
   }
-
-Property 'forEach' does not exist on type 'Promise<any>'.ts(2339)
-
-Property 'forEach' does not exist on type 'Promise<{ totalRequests: number; couriers_online: number; count_rejected: number; percentage_rejected: number; count_transferred: number; percentage_transferred: number; count_delivery_late: number; percentage_delivery_late: number; count_delivery_in_time: number; percentage_delivery_in_time: number; }>'.ts(2339)
-
-
