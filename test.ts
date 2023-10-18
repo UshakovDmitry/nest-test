bccfec76b57d80055b54a5e15bf88479167a83f7
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-18 15:19:52 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,50 +1,19 @@
-import { Injectable } from '@nestjs/common';
-import fetch from 'node-fetch';
-
-@Injectable()
-export class AuthService {
-  async login(username: string, password: string): Promise<any> {
-    const formData = new URLSearchParams();
-    formData.append('client_id', 'DispatcherWorkplace');
-    formData.append('client_secret', 'secret');
-    formData.append('grant_type', 'password');
-    formData.append('username', username);
-    formData.append('password', password);
-    formData.append('scope', 'myAPIs');
-
-    const response = await fetch('https://auth3.next.local/connect/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: formData.toString(),
-    });
-
-    return response.json();
-  }
-
-  async checkToken(token: string): Promise<any> {
-    const response = await fetch('https://auth3.next.local/connect/checktoken', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    return response.json();
-  }
-}
-
-
-
-[Nest] 19464  - 18.10.2023, 13:58:11   ERROR [ExceptionsHandler] request to https://auth3.next.local/connect/token failed, reason: self s
-igned certificate in certificate chain
-FetchError: request to https://auth3.next.local/connect/token failed, reason: self signed certificate in certificate chain
-    at ClientRequest.<anonymous> (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\node-fetch\lib\index.
-js:1501:11)
-    at ClientRequest.emit (node:events:526:28)
-    at TLSSocket.socketErrorListener (node:_http_client:442:9)
-    at TLSSocket.emit (node:events:526:28)
-    at emitErrorNT (node:internal/streams/destroy:157:8)
-    at emitErrorCloseNT (node:internal/streams/destroy:122:3)
-    at processTicksAndRejections (node:internal/process/task_queues:83:21)
+        if (request.distribution === true && request.documentStatus === 'Доставляется') {
+          request.orders.forEach((order) => {
+            const [_, deliveryTime] = order.Delivery_Time.split(' ');
+            const [deliveryHours, deliveryMinutes, deliverySeconds] =
+              deliveryTime.split(':').map(Number);
+            deliveryDateTime = new Date();
+            deliveryDateTime.setHours(
+              deliveryHours,
+              deliveryMinutes,
+              deliverySeconds,
+            );
+  
+            if (currentTime > deliveryDateTime) {
+              request.documentStatus = 'Доставляется с опазданием';
+            } else {
+              request.documentStatus = 'Доставляется вовремя';
+            }
+          });
+        }
