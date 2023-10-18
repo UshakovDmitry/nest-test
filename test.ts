bdc3c88152da0fda23ca46ab3b393b0e1a56c739
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-18 11:57:57 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,29 +1,79 @@
-Я хочу добавить модуль auth на свой сервер на Nest (который будет прослойкой)
-создать два эндпойнта
+import { Module, HttpModule } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
-/login
-который будет принимать login и password 
-затем будет выполнять POST  https://auth3.next.local/connect/token
-
-Content-Type multipart/form-data; boundary=<calculated when request is sent>
-
-и передавать 
-client_id   DispatcherWorkplace
-client_secret secret
-grant_type password
-username login (который присылает пользователь)
-password password (который присылает пользователь)
-scope myAPIs
+@Module({
+  imports: [HttpModule],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
 
 
 
 
 
- /checktoken
-который будет принимать token и 
 
-GET https://auth3.next.local/connect/checktoken
-и в звголовке будет добавлять 
-Authorization со значением  token  (который присылает пользователь)
+import { Controller, Post, Body, Get, Query } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  async login(@Body() body): Promise<any> {
+    const { username, password } = body;
+    return this.authService.login(username, password);
+  }
+
+  @Get('checktoken')
+  async checkToken(@Query('token') token: string): Promise<any> {
+    return this.authService.checkToken(token);
+  }
+}
 
 
+
+
+
+
+
+
+
+
+import { Injectable, HttpService } from '@nestjs/common';
+import { AxiosResponse } from 'axios';
+
+@Injectable()
+export class AuthService {
+  constructor(private readonly httpService: HttpService) {}
+
+  async login(username: string, password: string): Promise<AxiosResponse<any>> {
+    const formData = new URLSearchParams();
+    formData.append('client_id', 'DispatcherWorkplace');
+    formData.append('client_secret', 'secret');
+    formData.append('grant_type', 'password');
+    formData.append('username', username);
+    formData.append('password', password);
+    formData.append('scope', 'myAPIs');
+
+    return this.httpService
+      .post('https://auth3.next.local/connect/token', formData.toString(), {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+      .toPromise();
+  }
+
+  async checkToken(token: string): Promise<AxiosResponse<any>> {
+    return this.httpService
+      .get('https://auth3.next.local/connect/checktoken', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .toPromise();
+  }
+}
