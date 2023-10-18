100565891ff16e9ed07121781ba7754a2d0a2d96
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-18 11:50:56 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,14 +1,29 @@
-GET https://auth3.next.local/connect/checktoken
+Я хочу добавить модуль auth на свой сервер на Nest (который будет прослойкой)
+создать два эндпойнта
 
-Authorization
+/login
+который будет принимать login и password 
+затем будет выполнять POST  https://auth3.next.local/connect/token
 
+Content-Type multipart/form-data; boundary=<calculated when request is sent>
 
-
-POST  https://auth3.next.local/connect/token
-
+и передавать 
 client_id   DispatcherWorkplace
 client_secret secret
 grant_type password
-username ushakov.dmitriy
-password 111111111
+username login (который присылает пользователь)
+password password (который присылает пользователь)
 scope myAPIs
+
+
+
+
+
+ /checktoken
+который будет принимать token и 
+
+GET https://auth3.next.local/connect/checktoken
+и в звголовке будет добавлять 
+Authorization со значением  token  (который присылает пользователь)
+
+
