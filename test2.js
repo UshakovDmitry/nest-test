48942052f9bb9aebd6aee566bd2619dad1448c60
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 14:59:21 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,17 +1,10 @@
-async getCouriersNames(city: string) {
-    const couriers = await this.getCouriers();
-    
-    const couriersNames = couriers
-        .filter(courier => courier.City.toLowerCase() === city.toLowerCase()) // Фильтрация по городу
-        .map(courier => ({  // Преобразование в желаемый формат
-            full_name: courier.Drivers,
-            hiring_type: courier.HiringType,
-            timeWindow: courier.TimeWindow,
-            hard_time_window: courier.HardTimeWindow,
-            return_warehouse: courier.ReturnWarehouse,
-            city: courier.City,
-            car_number: courier.carNumber
-        }));
-        
-    return couriersNames;
-}
+[Nest] 22056  - 19.10.2023, 14:58:34   ERROR [ExceptionsHandler] Cannot read properties of undefined (reading 'toLowerCase')
+TypeError: Cannot read properties of undefined (reading 'toLowerCase')
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\couriers\couriers.service.ts:71:64
+    at Array.filter (<anonymous>)
+    at CouriersService.getCouriersNames (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\couriers\couriers.servi
+ce.ts:71:10)
+    at processTicksAndRejections (node:internal/process/task_queues:96:5)
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\router\router-execution-context.js:
+46:28
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\router\router-proxy.js:9:17
