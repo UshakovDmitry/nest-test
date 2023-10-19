98a90e40934ddd81812194c703c964c1d6f43791
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 16:39:29 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,55 +1,91 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+// transport-requests.dto.ts
+
+import { IsNotEmpty, IsString } from 'class-validator';
+
+// DTO для получения непредсказанных транспортных запросов по дате
+export class GetNotPredictedTransportRequestsByDateDto {
+  @IsNotEmpty()
+  @IsString()
+  date: string;
+}
+
+// DTO для получения транспортных запросов по номеру
+export class GetTransportRequestByNumberDto {
+  @IsNotEmpty()
+  @IsString()
+  number: string;
+}
+
+// DTO для получения транспортных запросов в заданном диапазоне дат
+export class GetTransportRequestsByDateRangeDto {
+  @IsNotEmpty()
+  @IsString()
+  startDate: string;
+
+  @IsNotEmpty()
+  @IsString()
+  endDate: string;
+}
+
+// DTO для получения транспортных запросов по дате
+export class GetTransportRequestsByDateDto {
+  @IsNotEmpty()
+  @IsString()
+  date: string;
+}
+
+
+
+
+
+
+
+
+
+
+// transportrequests.controller.ts
+
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Sse,
+} from '@nestjs/common';
 import { TransportRequestsService } from './transportRequests.service';
-import { ApiTags } from '@nestjs/swagger';
-import { Sse } from '@nestjs/common';
-import { Observable, interval } from 'rxjs';
+import {
+  GetNotPredictedTransportRequestsByDateDto,
+  GetTransportRequestByNumberDto,
+  GetTransportRequestsByDateRangeDto,
+  GetTransportRequestsByDateDto,
+} from './transport-requests.dto';
 
 @ApiTags('getTransportRequests')
 @Controller('/api/getTransportRequests')
 export class TransportRequestsController {
-  constructor(
-    private readonly transportRequestsService: TransportRequestsService,
+  constructor(private readonly transportRequestsService: TransportRequestsService) {}
 
-  ) {}
-
-  @Get()
-  async getAllTransportRequests() {
-    return this.transportRequestsService.getAllTransportRequests();
-  }
-
+  // ... (other methods remain unchanged)
 
   @Post('/not-predicted')
-  async getNotPredictedTransportRequestsByDate(@Body('date') date: string) {
-    return this.transportRequestsService.getNotPredictedTransportRequestsByDate(date);
+  async getNotPredictedTransportRequestsByDate(@Body() dto: GetNotPredictedTransportRequestsByDateDto) {
+    return this.transportRequestsService.getNotPredictedTransportRequestsByDate(dto.date);
   }
-    
-  
 
   @Post('byNumber')
-  async getTransportRequestByNumber(@Body('number') number: string) {
-    return this.transportRequestsService.getTransportRequestByNumber(number);
+  async getTransportRequestByNumber(@Body() dto: GetTransportRequestByNumberDto) {
+    return this.transportRequestsService.getTransportRequestByNumber(dto.number);
   }
 
   @Post('byDateRange')
-  async getTransportRequestsByDateRange(
-    @Body() dateRangeDto: { startDate: string; endDate: string },
-  ) {
-    return this.transportRequestsService.getTransportRequestsByDateRange(
-      dateRangeDto.startDate,
-      dateRangeDto.endDate,
-    );
+  async getTransportRequestsByDateRange(@Body() dto: GetTransportRequestsByDateRangeDto) {
+    return this.transportRequestsService.getTransportRequestsByDateRange(dto.startDate, dto.endDate);
   }
-
 
   @Post('by-date')
-  async getTransportRequestsByDate(@Body('date') date: string) {
-    return this.transportRequestsService.getTransportRequestsByDate(date);
+  async getTransportRequestsByDate(@Body() dto: GetTransportRequestsByDateDto) {
+    return this.transportRequestsService.getTransportRequestsByDate(dto.date);
   }
 
-
-  @Sse('sse')
-  sse(): Observable<MessageEvent> {
-    return this.transportRequestsService.serverSentEvents();
-  }
-
+  // ... (other methods remain unchanged)
 }
