96675ddd0985d557816999eb189ef20155539a99
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 14:48:32 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,11 +1,34 @@
-async getNotPredictedTransportRequestsByDate(date): Promise<any[]> {
-  const requestsByDate = await this.dbService.getTransportRequestsByDate(date);
-  
-  const notPredictedRequests = requestsByDate.filter(request => 
-    request.Driver.trim() === "" && 
-    request.NumberCar.trim() === "" && 
-    (request.CarModel ? request.CarModel.trim() === "" : true) // проверяет CarModel только если оно существует
-  );
-  
-  return notPredictedRequests;
-}
+у меня есть метод который получает city, массив couriers возвращает волдителей 
+я хочу в couriersNames получать водителей у которых "City" совпадает с city и возвращать их массивом 
+в таком виде
+
+    {
+        "full_name": Drivers,
+        "hiring_type": HiringType,
+        "timeWindow": TimeWindow,
+        "hard_time_window": HardTimeWindow,
+        "return_warehouse": ReturnWarehouse,
+        "city": City,
+        "car_number": "carNumber"
+    }
+
+
+ async getCouriersNames(city: string) {
+    const couriers = await this.getCouriers();
+    const couriersNames 
+      
+    return couriers;
+  }
+
+
+
+вот так выглядит элемент массива couriers
+    {
+        "Drivers": "Энсепов Берк Сарсенбаевич",
+        "HiringType": "Водитель-экспедитор",
+        "TimeWindow": "10:00:00-20:00:00",
+        "HardTimeWindow": true,
+        "ReturnWarehouse": false,
+        "City": "Актау",
+        "carNumber": ""
+    }
