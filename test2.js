68830e1c0c07148aa1016ce00ccbfd0c0f8888aa
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 16:12:45 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,24 @@
-@Post('/names')  // Обновлено с @Get на @Post, так как теперь используется тело запроса
-async getCouriersNames(@Body() body: { city: string, date: string }) {
-    const { city, date } = body;
-    return this.couriersService.getCouriersNames(city, date); // Передаем city и date в сервис
+import { Controller, Get,Post,Body } from '@nestjs/common';
+import { CouriersService } from './couriers.service';
+import { ApiTags } from '@nestjs/swagger';
+
+@ApiTags('Couriers')
+@Controller('/api/getCouriers')
+export class CouriersController {
+  constructor(private readonly couriersService: CouriersService) {}
+
+  @Get()
+  getCouriers() {
+    return this.couriersService.getCouriers();
+  }
+  @Post ('/by-date')
+   async getCouriersByDate(@Body('date') date: string) {
+     return this.couriersService.getCouriersByDate(date);
+   }
+   @Post('/names')  
+   async getCouriersNames(@Body() body: { city: string, date: string }) {
+       const { city, date } = body;
+       return this.couriersService.getCouriersNames(city, date);
+   }
+
 }
