a0e175ada285294ca358bb2d0d135523da13eabf
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 12:00:34 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,5 +1,7 @@
-  async getCouriersNames() {
-    const couriers = await this.getCouriers();
-    const couriersNames = couriers.map((courier) => courier.Drivers);
-    return couriersNames;
-  }
+async getCouriersNames() {
+  const couriers = await this.getCouriers();
+  const couriersNames = couriers
+    .filter(courier => courier.Drivers && courier.Drivers.trim() !== '') // фильтрация курьеров без имен
+    .map(courier => courier.Drivers); // маппинг оставшихся курьеров
+  return couriersNames;
+}
