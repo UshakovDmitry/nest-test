4d2cdd20d7f2a50b8b8863eb9b851d84631b9a39
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 16:16:35 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,24 +1,57 @@
-import { Controller, Get,Post,Body } from '@nestjs/common';
+// couriers.dto.ts
+
+import { IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CouriersDateDto {
+  @IsNotEmpty()
+  @IsString()
+  date: string;
+}
+
+export class CouriersNamesDto {
+  @IsNotEmpty()
+  @IsString()
+  city: string;
+
+  @IsOptional()
+  @IsString()
+  date?: string;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// couriers.controller.ts
+
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { CouriersService } from './couriers.service';
 import { ApiTags } from '@nestjs/swagger';
+import { CouriersDateDto } from './dto/couriers-date.dto';
+import { CouriersNamesDto } from './dto/couriers-names.dto';
 
 @ApiTags('Couriers')
 @Controller('/api/getCouriers')
 export class CouriersController {
   constructor(private readonly couriersService: CouriersService) {}
 
-  @Get()
-  getCouriers() {
-    return this.couriersService.getCouriers();
-  }
-  @Post ('/by-date')
-   async getCouriersByDate(@Body('date') date: string) {
-     return this.couriersService.getCouriersByDate(date);
-   }
-   @Post('/names')  
-   async getCouriersNames(@Body() body: { city: string, date: string }) {
-       const { city, date } = body;
-       return this.couriersService.getCouriersNames(city, date);
-   }
+  // ... (остальные методы)
 
+  @Post('/by-date')
+  async getCouriersByDate(@Body() couriersDateDto: CouriersDateDto) {
+    return this.couriersService.getCouriersByDate(couriersDateDto.date);
+  }
+
+  @Post('/names')
+  async getCouriersNames(@Body() couriersNamesDto: CouriersNamesDto) {
+    const { city, date } = couriersNamesDto;
+    return this.couriersService.getCouriersNames(city, date);
+  }
 }
