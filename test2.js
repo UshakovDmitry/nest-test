3e51f7c88f39e196b4e85dbeeb38cb7989c07ce3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-19 11:58:49 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,29 +1,5 @@
-router.beforeEach(async (to, from, next) => {
-  const token = localStorage.getItem("access_token");
-
-  if (token) {
-    try {
-      const response = await fetch('https://auth3.next.local/connect/checktoken', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-
-      if (response.ok) {
-        next(); // Переход к следующему роуту, если токен действителен
-      } else {
-        localStorage.removeItem("access_token"); // Удалить токен, если он недействителен
-        next('/auth'); // Перенаправление на страницу аутентификации
-      }
-    } catch (error) {
-      console.error(error);
-      next('/auth'); // Перенаправление на страницу аутентификации в случае ошибки
-    }
-  } else if (to.meta.public) {
-    next(); // Разрешить доступ к публичным роутам
-  } else {
-    next('/auth'); // Перенаправление на страницу аутентификации, если токен отсутствует
+  async getCouriersNames() {
+    const couriers = await this.getCouriers();
+    const couriersNames = couriers.map((courier) => courier.Drivers);
+    return couriersNames;
   }
-});
-
