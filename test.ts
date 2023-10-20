5a48dacf2926c9fa04d6a62ace00a808ccd7f508
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-20 14:32:19 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,19 @@
 d69d6c2d8e494d47aac84ef650f2cd23
+
+
+
+
+
+
+
+name = "confirm-kaspi-order"
+type = "javascript"
+account_id = "e063b9c38c5012f903d8522ee8b27872"
+
+[vars]
+SECRET_TOKEN = "secret"
+
+[kv_namespaces]
+[[kv_namespaces]]
+binding = "USER_DATA" 
+id = "your_copied_namespace_id" # Сюда вставьте скопированный Namespace ID
