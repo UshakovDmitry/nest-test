d949f1027ebd32fa5d1b07d836422e2cad040084
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-20 12:20:31 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -58,3 +58,41 @@ watch(route, () => {
 }
 </style>
 
+
+
+  В этом коде я напряму лезу в локальное хранилище но у меня есть сервис userCRUDService
+import { IUser } from '../interfaces/user.interface';
+import {
+  storeProviderGetValue,
+  storeProviderRemoveValue,
+  storeProviderSetValue,
+} from '../providers/store.provider';
+
+export class userCRUDService {
+  static USER_FULLNAME = 'user_fullname';
+  static USER_POSITION = 'user_position';
+  static USER_IIN = 'user_iin';
+
+  static createUser(user: IUser) {
+    storeProviderSetValue(this.USER_FULLNAME, user.fullName);
+    storeProviderSetValue(this.USER_POSITION, user.position);
+    storeProviderSetValue(this.USER_IIN, user.iin);
+  }
+  static readUser() {
+    return {
+      fullName: storeProviderGetValue(this.USER_FULLNAME),
+      position: storeProviderGetValue(this.USER_POSITION),
+      iin: storeProviderGetValue(this.USER_IIN),
+    };
+  }
+  static updateUser(user: IUser) {
+    this.createUser(user);
+  }
+  static deleteUser() {
+    storeProviderRemoveValue(this.USER_FULLNAME);
+    storeProviderRemoveValue(this.USER_POSITION);
+    storeProviderRemoveValue(this.USER_IIN);
+  }
+}
+
+примени его 
