186d27c785425a4c9259ce8eaa7f3c3ec6bfd548
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-20 10:34:55 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,6 @@
-вот тут я передаю моковые данные 
 <template>
   <div>
-    <AppHeaderComponent
-      :user="{
-        fullName: 'Иванов Иван Иванович',
-        iin: '123456789012',
-      }"
-      v-if="!isAuthPage"
-    />
+    <AppHeaderComponent :user="user" v-if="!isAuthPage" />
     <div class="main-layout">
       <AppNavbarComponent v-if="!isAuthPage" />
       <div class="content">
@@ -18,13 +11,25 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import AppHeaderComponent from '../components/AppHeaderComponent.vue';
 import AppNavbarComponent from '../components/AppNavbarComponent.vue';
 
 const route = useRoute();
 const isAuthPage = ref(route.path === '/auth');
+
+// Реактивное свойство для хранения данных пользователя
+const user = ref<IUser | null>(null);
+
+onMounted(() => {
+  // Считывание данных пользователя из локального хранилища при монтировании компонента
+  const userData = localStorage.getItem('user');
+  if (userData) {
+    user.value = JSON.parse(userData);
+  }
+});
+
 </script>
 
 <style>
@@ -38,6 +43,3 @@ const isAuthPage = ref(route.path === '/auth');
 }
 </style>
 
-
-
-как мне теперь передать данные из локального хранилища
