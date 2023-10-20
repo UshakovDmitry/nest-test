12d849d63eb7d48dff3b6112f4e24519be2beb81
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-20 10:55:30 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -24,3 +24,35 @@ export interface IUser {
 
 я хочу из этого массива получить одну строку "Отдел разработки ПО, Разработчик ПО"
 тем самы пройтись по всем  элементам массива и сдедлать их одной строкой рзделяя их запятой и пробелом
+
+
+
+вот исходные данные dataToken
+{
+    "client_id": "DispatcherWorkplace",
+    "idp": "local",
+    "locale": "Алматы",
+    "nickname": "",
+    "auth_time": 1697794888,
+    "http://schemas.microsoft.com/ws/2008/06/identity/claims/role": "C8994BAA-BD79-4DA5-AE03-794F5F11C984",
+    "family_name": "Ушаков",
+    "given_name": "Дмитрий",
+    "website": [
+        "Отдел разработки ПО",
+        "Разработчик ПО"
+    ],
+    "profile": "950408050374",
+    "nbf": 1697773287,
+    "exp": 1697816487,
+    "iss": "http://auth3.next.local",
+    "scope": [
+        "myAPIs"
+    ],
+    "aud": [
+        "myAPIs",
+        "http://auth3.next.local/resources"
+    ],
+    "amr": [
+        "pwd"
+    ]
+}
