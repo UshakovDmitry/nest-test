7b2336426edfa17ef38f0b480045542416e4543f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-20 10:03:38 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,18 +1,130 @@
-function parseJwt(token) {
-  try {
-    // Получить payload токена
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    
-    // Декодировать payload
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
-      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-    
-    console.log(JSON.parse(jsonPayload));
-    return JSON.parse(jsonPayload);
-  } catch (error) {
-    console.error("Ошибка при разборе токена:", error);
-    return null;
-  }
+<template>
+  <div class="auth">
+    <div class="auth__content">
+      <img :src="logo" alt="logo" />
+      <Field
+        :config="model.fields[0]"
+        @input="viewModel.setEmail($event)"
+      ></Field>
+      <Field
+        :config="model.fields[1]"
+        @input="viewModel.setPassword($event)"
+      ></Field>
+      <div>
+        <ButtonComponent
+          :config="{
+            type: 'filled',
+            color: 'green',
+            value: 'Войти',
+            border: 'large',
+            width: '350px',
+            disabled: false,
+          }"
+          @on-click="viewModel.handleLogin()"
+        ></ButtonComponent>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import logo from '../../public/icons/logo.svg';
+import ButtonComponent from '../../components/global/button/button.vue';
+import { AuthModel } from './auth.model';
+import { AuthViewModel } from './auth.viewmodel';
+import Field from '../../components/global/fields/fieild/field.vue';
+import { FieldModel } from '../../components/global/fields/fieild/field.model';
+import { ref, Ref } from 'vue';
+
+const model: Ref<AuthModel> = ref(
+  new AuthModel({
+    fields: [
+      new FieldModel({
+        label: 'Email',
+        input: {
+          type: 'email',
+          value: '',
+          placeholder: 'Введите Email',
+          isError: false,
+          isDisabled: false,
+          isTextarea: false,
+          required: true,
+          maxLength: 20,
+        },
+        helper: {
+          isActive: true,
+          value: 'test',
+        },
+      }),
+      new FieldModel({
+        label: 'Пароль',
+        input: {
+          type: 'password',
+          value: '',
+          placeholder: 'Введите пароль',
+          isDisabled: false,
+          isTextarea: false,
+          required: true,
+          maxLength: 20,
+          isError: false,
+        },
+        helper: {
+          isActive: true,
+          value: 'test',
+        },
+      }),
+    ],
+  }),
+);
+const viewModel: Ref<AuthViewModel> = ref(new AuthViewModel(model.value));
+</script>
+<style scoped>
+.auth {
+  background: linear-gradient(108deg, #01a254 0%, #50d17f 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  /* margin-left: -72px;
+  margin-top: -107px; */
+  box-sizing: border-box;
 }
+.auth__content {
+  display: flex;
+  width: 420px;
+  padding: 36px;
+  flex-direction: column;
+  align-items: center;
+  gap: 11px;
+  flex-shrink: 0;
+  border-radius: 16px;
+  box-sizing: border-box;
+  background: #fff;
+}
+</style>
+
+model
+
+export interface IAuthModel {
+    email: string;
+    password: string;
+    isShowedPassword?: boolean;
+    fields?: any[];
+
+  }
+  
+  export class AuthModel implements IAuthModel {
+    email: string;
+    password: string; 
+    isShowedPassword?: boolean;
+    fields?: any[];
+  
+    constructor(obj:any) {
+        this.email = '';
+        this.password = '';
+        this.isShowedPassword = false;
+        this.fields = obj.fields;
+
+    }
+  }
+  
