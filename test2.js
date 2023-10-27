d4646bec258c948e00620ebb417ef98d6bda516a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-27 12:07:02 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -4,34 +4,8 @@ https://confirm-kaspi-order.alser2.workers.dev
 https://kaspi-proxy.alser.kz/
 
 
-тут хочу дклать запрос 
-  transportRequestCorrection(dto: any): Promise<any> {
-    const response = await firstValueFrom.post(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver'));
-  }
-
-вот такой
-POST: http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver
-Body
-{
-    "DocNumber": "№MER000718",
-    "DateDoc": "20230926",
-    "TimeDelivery": "20231017180000",
-    "Driver": "Муравейников Сергей Николаевич",
-    "СarNumber": "A589",
-    "User": "660903402365"
-    "Сomment": "Корретировка диспетчером."
-}
-
-вот код 
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { MessageDocument } from '../schemas/message.shema';
-import { DBService } from '../db/db.service';
-import { map } from 'rxjs/operators';
-import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
-
+// ... (другие импорты)
 
 @Injectable()
 export class TransportRequestsService {
@@ -41,53 +15,30 @@ export class TransportRequestsService {
     private readonly dbService: DBService,
   ) {}
 
-  async getAllTransportRequests(): Promise<any[]> {
-    return await this.dbService.getAllTransportRequests();
-  }
+  // ... (другие методы)
 
-  async getNotPredictedTransportRequestsByDate(date: string): Promise<any[]> {
-    const requestsByDate = await this.dbService.getTransportRequestsByDate(date);
+  async transportRequestCorrection(dto: TransportRequestCorrectionDto): Promise<any> {
+    // Сопоставление полей DTO с ожидаемыми полями в запросе
+    const requestData = {
+      DocNumber: dto.documentNumber,
+      DateDoc: dto.date,
+      TimeDelivery: dto.timeDelivery,
+      Driver: dto.driver,
+      СarNumber: dto.carNumber,
+      User: dto.userIIN, // или другое подходящее поле, если userIIN не подходит
+      Сomment: dto.comment,
+      // ... (другие поля, если они нужны)
+    };
     
-    const notPredictedRequests = requestsByDate.filter(request => 
-      request.Driver.trim() === "" && 
-      request.NumberCar.trim() === "" && 
-      (request.CarModel ? request.CarModel.trim() === "" : true) // проверяет CarModel только если оно существует
-    );
-    
-    return notPredictedRequests;
+    try {
+      const response = await this.httpService.post('http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver', requestData).toPromise();
+      return response.data;
+    } catch (error) {
+      // Обработка ошибки
+      console.error(error);
+      throw error;
+    }
   }
 
-  async getTransportRequestByNumber(number: string): Promise<any> {
-    return await this.dbService.getTransportRequestByNumber(number);
-  }
-
-  async getTransportRequestsByDateRange(
-    startDate: string,
-    endDate: string,
-  ): Promise<any[]> {
-    return await this.dbService.getTransportRequestsByDateRange(
-      startDate,
-      endDate,
-    );
-  }
-
-
-  getTransportRequestsByDate(date: string): Promise<any[]> {
-    return this.dbService.getTransportRequestsByDate(date);
-  }
-
-
-  transportRequestCorrection(dto: any): Promise<any> {
-    const response = await firstValueFrom.post(this.httpService.get('http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver'));
-  }
-
-
-  serverSentEvents(): any {
-    return this.dbService.dataChange$.pipe(
-      map(() => ({ data: { message: 'Pong' } })),
-    );
-  }
-
-
-
+  // ... (другие методы)
 }
