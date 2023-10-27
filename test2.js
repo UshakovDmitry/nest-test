a5587d456bb6950e4251583ce7158d304007fc12
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-27 15:12:54 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -6,46 +6,26 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 
 // ...
 
-  async transportRequestCorrection(dto): Promise<any> {
-    // Сопоставление полей DTO с ожидаемыми полями в запросе
-    const requestData = {
-      DocNumber: dto.documentNumber,
-      DateDoc: dto.date,
-      TimeDelivery: dto.timeDelivery,
-      Driver: dto.driver,
-      СarNumber: dto.carNumber,
-      UserIIN: dto.userIIN,
-      Сomment: dto.comment,
-    };
+import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MessageSchema } from '../schemas/message.shema';
+import { TransportRequestsService } from './transportRequests.service';
+import { TransportRequestsController } from './transportRequests.controller';
+import { DBModule } from '../db/db.module';  
 
-    // Проверка наличия и формата всех необходимых полей
-    const requiredFields = ['DocNumber', 'DateDoc', 'TimeDelivery', 'Driver', 'UserIIN', 'Сomment'];
-    
-    for (const field of requiredFields) {
-      if (!requestData[field]) {
-        throw new HttpException(`Обязательное поле отсутствует: ${field}`, HttpStatus.BAD_REQUEST);
-      }
 
-      if (typeof requestData[field] !== 'string') {
-        throw new HttpException(`Неверный тип данных для поля: ${field}. Ожидалась строка.`, HttpStatus.BAD_REQUEST);
-      }
-    }
-    
-    console.log(requestData, 'корректировка ( отправляю Сане )');
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+    DBModule,
+    HttpModule,
+  ],
+  controllers: [TransportRequestsController],
+  providers: [TransportRequestsService],
+})
+export class TransportRequestsModule {}
 
-    // http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver  боевая
-    try {
-      const response = await firstValueFrom(
-        this.httpService.post(
-          'http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver',
-          requestData,
-        ),
-      );
-      return response.data;
-    } catch (error) {
-      throw new HttpException(
-        `Ошибка при отправке запроса на корректировку транспортной заявки: ${error}`,
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
-}
+
+
+
