8b500a25425662dad6672a9db50959bef4aab5dd
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-10-27 14:10:00 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -3,21 +3,17 @@ https://confirm-kaspi-order.alser2.workers.dev
 
 https://kaspi-proxy.alser.kz/
 
-import { HttpException, HttpStatus } from '@nestjs/common';
-
-// ...
-
-async transportRequestCorrection(dto): Promise<any> {
-  // Сопоставление полей DTO с ожидаемыми полями в запросе
-  const requestData = {
-    DocNumber: dto.documentNumber,
-    DateDoc: dto.date,
-    TimeDelivery: dto.timeDelivery,
-    Driver: dto.driver,
-    СarNumber: dto.carNumber,
-    UserIIN: dto.userIIN,
-    Сomment: dto.comment,
-  };
+  async transportRequestCorrection(dto): Promise<any> {
+    // Сопоставление полей DTO с ожидаемыми полями в запросе
+    const requestData = {
+      DocNumber: dto.documentNumber,
+      DateDoc: dto.date,
+      TimeDelivery: dto.timeDelivery,
+      Driver: dto.driver,
+      СarNumber: dto.carNumber,
+      UserIIN: dto.userIIN,
+      Сomment: dto.comment,
+    };
 
   // Проверка наличия и формата всех необходимых полей
   const requiredFields = ['DocNumber', 'DateDoc', 'TimeDelivery', 'Driver', 'СarNumber', 'UserIIN', 'Сomment'];
@@ -31,20 +27,19 @@ async transportRequestCorrection(dto): Promise<any> {
       throw new HttpException(`Invalid data type for field: ${field}. Expected a string.`, HttpStatus.BAD_REQUEST);
     }
   }
+    console.log(requestData, 'корректировка ( отправляю Сане )');
 
-  console.log(requestData, 'корректировка ( отправляю Сане )');
-
-  // ...
-  try {
-    const response = await firstValueFrom(
-      this.httpService.post(
-        'http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver',
-        requestData,
-      ),
-    );
-    return response.data;
-  } catch (error) {
-    console.error(error);
-    throw error;
+    // http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver  боевая
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(
+          'http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver',
+          requestData,
+        ),
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
-}
