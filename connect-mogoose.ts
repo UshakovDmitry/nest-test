9f0938fe1595fb724f16f3d04fcaebed86ebd125
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-07 10:42:10 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -20,3 +20,46 @@ export class TransportRequestsService {
 
 добавь ее в конструктор
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MessageSchema } from '../schemas/message.shema';
+import { TransportRequestsService } from './transportRequests.service';
+import { TransportRequestsController } from './transportRequests.controller';
+import { DBModule } from '../db/db.module';  
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
+    DBModule,
+    HttpModule,
+  ],
+  controllers: [TransportRequestsController],
+  providers: [TransportRequestsService],
+})
+export class TransportRequestsModule {}
