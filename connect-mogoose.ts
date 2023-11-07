0771f846e7381f9bb9d17b3c7e6ca040b543b811
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-07 10:57:15 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -1,74 +1,57 @@
-  async recordHistoryAction({ name, time, comment }: { name: string; time: string; comment: string; }) {
-    const newHistoryEntry = new this.historyModel({
-      name,
-      time,
-      comment,
-    });
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { firstValueFrom } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { MessageDocument } from '../schemas/message.schema';
+import { HistoryDocument } from '../schemas/history.schema';
+
+@Injectable()
+export class TransportRequestsService {
+  constructor(
+    @InjectModel('Message') private messageModel: Model<MessageDocument>,
+    @InjectModel('History') private historyModel: Model<HistoryDocument>,
+    private readonly httpService: HttpService,
+    // ... возможно, другие зависимости
+  ) {}
+
+  // ... другие методы сервиса
+
+  async recordHistoryAction({ name, time, comment }: { name: string; time: string; comment: string; }): Promise<HistoryDocument> {
+    const newHistoryEntry = new this.historyModel({ name, time, comment });
 
     try {
-      const savedEntry = await newHistoryEntry.save();
-      return savedEntry;
+      return await newHistoryEntry.save();
     } catch (error) {
       throw new HttpException('Не удалось сохранить запись в историю действий', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 
-  async transportRequestCorrection(dto): Promise<any> {
-    // Сопоставление полей DTO с ожидаемыми полями в запросе
+  async transportRequestCorrection(dto: any): Promise<any> {
+    // Подготовка данных из DTO
     const requestData = {
-      DocNumber: dto.documentNumber,
-      DateDoc: dto.date,
-      TimeDelivery: dto.timeDelivery,
-      Driver: dto.driver,
-      СarNumber: dto.carNumber,
-      UserIIN: dto.userIIN,
-      Сomment: dto.comment,
+      // ... другие поля из dto
     };
 
-    // Проверка наличия и формата всех необходимых полей
-    const requiredFields = [
-      'DocNumber',
-      'DateDoc',
-      'TimeDelivery',
-      'Driver',
-      'UserIIN',
-      'Сomment',
-    ];
+    // Проверка полей DTO
+    // ... ваши проверки для requestData
 
-    for (const field of requiredFields) {
-      if (!requestData[field]) {
-        throw new HttpException(
-          `Обязательное поле отсутствует: ${field}`,
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      if (typeof requestData[field] !== 'string') {
-        throw new HttpException(
-          `Неверный тип данных для поля: ${field}. Ожидалась строка.`,
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-    }
-
-    console.log(requestData, 'корректировка ( отправляю Сане )');
-
-    // http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver  боевая
     try {
+      // Регистрация действия в истории перед отправкой запроса на корректировку
+      await this.recordHistoryAction({
+        name: dto.userName, 
+        time: dto.timeDelivery, 
+        comment: dto.comment
+      });
+
+      // Логика отправки запроса на корректировку
       const response = await firstValueFrom(
-        this.httpService.post(
-          // 'http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver', // тестовая
-          'http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver ',
-          requestData,
-        ),
+        this.httpService.post('http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver', requestData)
       );
+
       return response.data;
     } catch (error) {
-      throw new HttpException(
-        `Ошибка при отправке запроса на корректировку транспортной заявки: ${error}`,
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
+      throw new HttpException(`Ошибка при отправке запроса на корректировку транспортной заявки: ${error}`, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-
-Внутри transportRequestCorrection нужно вызвать recordHistoryAction и передать туда  { name: dto.userName , time: dto.timeDelivery , comment: dto.comment }
+}
