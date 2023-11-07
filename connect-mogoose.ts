07f456136567e65674d64e46e8920e75907db925
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-07 10:44:56 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -1,27 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { MessageDocument } from '../schemas/message.shema';
-я добавил новую схему
-import { HistoryDocument } from '../schemas/history.schema';
+import { MessageDocument } from '../schemas/message.schema'; // Убедитесь, что путь к файлу верный
+import { HistoryDocument } from '../schemas/history.schema'; // Убедитесь, что путь к файлу верный
 import { DBService } from '../db/db.service';
-import { map } from 'rxjs/operators';
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
-import { HttpException, HttpStatus } from '@nestjs/common';
 
 @Injectable()
 export class TransportRequestsService {
   constructor(
     @InjectModel('Message') private messageModel: Model<MessageDocument>,
+    @InjectModel('History') private historyModel: Model<HistoryDocument>, // Используйте корректное имя модели, если оно отличается
     private readonly httpService: HttpService,
     private readonly dbService: DBService,
   ) {}
 
-добавь ее в конструктор
+  // ... Остальные методы и логика сервиса
+}
 
 
 
+
+
+
+
+  
 
 
 
