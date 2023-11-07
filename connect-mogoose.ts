16b053b9130bd7771620c26c0ff16a90b27fda48
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-07 10:54:38 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -1,11 +1,3 @@
-import { Injectable } from '@nestjs/common';
-import { HttpException, HttpStatus } from '@nestjs/common';
-// ... другие импорты
-
-@Injectable()
-export class TransportRequestsService {
-  // ... конструктор и другие методы
-
   async recordHistoryAction({ name, time, comment }: { name: string; time: string; comment: string; }) {
     const newHistoryEntry = new this.historyModel({
       name,
@@ -17,55 +9,66 @@ export class TransportRequestsService {
       const savedEntry = await newHistoryEntry.save();
       return savedEntry;
     } catch (error) {
-      // Здесь вы можете обработать ошибку более конкретно, если есть информация об ошибках
       throw new HttpException('Не удалось сохранить запись в историю действий', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-}
 
-  
+  async transportRequestCorrection(dto): Promise<any> {
+    // Сопоставление полей DTO с ожидаемыми полями в запросе
+    const requestData = {
+      DocNumber: dto.documentNumber,
+      DateDoc: dto.date,
+      TimeDelivery: dto.timeDelivery,
+      Driver: dto.driver,
+      СarNumber: dto.carNumber,
+      UserIIN: dto.userIIN,
+      Сomment: dto.comment,
+    };
 
+    // Проверка наличия и формата всех необходимых полей
+    const requiredFields = [
+      'DocNumber',
+      'DateDoc',
+      'TimeDelivery',
+      'Driver',
+      'UserIIN',
+      'Сomment',
+    ];
 
+    for (const field of requiredFields) {
+      if (!requestData[field]) {
+        throw new HttpException(
+          `Обязательное поле отсутствует: ${field}`,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
 
+      if (typeof requestData[field] !== 'string') {
+        throw new HttpException(
+          `Неверный тип данных для поля: ${field}. Ожидалась строка.`,
+          HttpStatus.BAD_REQUEST,
+        );
+      }
+    }
 
+    console.log(requestData, 'корректировка ( отправляю Сане )');
 
+    // http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver  боевая
+    try {
+      const response = await firstValueFrom(
+        this.httpService.post(
+          // 'http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver', // тестовая
+          'http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver ',
+          requestData,
+        ),
+      );
+      return response.data;
+    } catch (error) {
+      throw new HttpException(
+        `Ошибка при отправке запроса на корректировку транспортной заявки: ${error}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MessageSchema } from '../schemas/message.schema'; // Убедитесь, что путь к файлу верный
-import { HistorySchema } from '../schemas/history.schema'; // Убедитесь, что путь к файлу верный
-import { TransportRequestsService } from './transportRequests.service';
-import { TransportRequestsController } from './transportRequests.controller';
-import { DBModule } from '../db/db.module';
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: 'Message', schema: MessageSchema },
-      { name: 'History', schema: HistorySchema }, // Добавленная схема истории
-    ]),
-    DBModule,
-    HttpModule,
-  ],
-  controllers: [TransportRequestsController],
-  providers: [TransportRequestsService],
-})
-export class TransportRequestsModule {}
-
+Внутри transportRequestCorrection нужно вызвать recordHistoryAction и передать туда  { name: dto.userName , time: dto.timeDelivery , comment: dto.comment }
