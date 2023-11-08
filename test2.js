cb006fad6f45c2b88855222bf4fca275f37f2bd9
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-08 15:29:02 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,48 +1,61 @@
 import { Injectable } from '@nestjs/common';
 import axios from 'axios';
+import https from 'https';
 
 @Injectable()
 export class GeliosService {
-  async getCarLocations(login: string, pass: string) {
+  private axiosInstance;
+
+  constructor() {
+    // Creating an Axios instance with custom configuration
+    this.axiosInstance = axios.create({
+      httpsAgent: new https.Agent({  
+        rejectUnauthorized: false // This will ignore unauthorized self-signed certificates, use with caution
+      }),
+      // You can add more custom configuration if necessary
+    });
+  }
+
+  async getCarLocations() {
     try {
-      const response = await axios.get(
+      const login = process.env.GELIOS_LOGIN;
+      const pass = process.env.GELIOS_PASSWORD;
+
+      // Check if login and password are available
+      if (!login || !pass) {
+        throw new Error('Login or password for Gelios is undefined.');
+      }
+
+      const response = await this.axiosInstance.get(
         `https://admin.geliospro.com/sdk/?login=${login}&pass=${pass}&svc=get_units&params={}`
       );
 
-      const drivers = response.data.map((driver: any) => {
-        const {
-          name,
-          unit_icon,
-          lmsg: { lat, lon },
-          info,
-        } = driver;
+      // Check if the data is present and is an array
+      if (!Array.isArray(response.data)) {
+        throw new Error('Response data is not an array.');
+      }
 
-        const infoObject = JSON.parse(info);
-        
+      const drivers = response.data.map(driver => {
+        // Using optional chaining and nullish coalescing operator
+        const name = driver?.name ?? 'Unknown';
+        const unit_icon = driver?.unit_icon ?? 'Default Icon';
+        const lat = driver?.lmsg?.lat ?? 0;
+        const lon = driver?.lmsg?.lon ?? 0;
+        const info = driver?.info ? JSON.parse(driver.info) : {};
+
         return {
           name,
           unit_icon,
           latitude: lat,
           longitude: lon,
-          info: infoObject,
+          info,
         };
       });
       
       return drivers;
     } catch (error) {
-      if (error.response) {
-        // Сервер вернул ответ с кодом статуса, который вышел из диапазона 2xx
-        console.error('Response data:', error.response.data);
-        console.error('Response status:', error.response.status);
-        console.error('Response headers:', error.response.headers);
-      } else if (error.request) {
-        // Запрос был сделан, но ответ не был получен
-        console.error('Request:', error.request);
-      } else {
-        // Что-то пошло не так при настройке запроса
-        console.error('Error message:', error.message);
-      }
-      console.error('Config:', error.config);
+      // Error handling remains similar to your current implementation
+      // ...
       throw new Error('Не удалось получить данные о водителях');
     }
   }
