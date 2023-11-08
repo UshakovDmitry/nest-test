2a4907a35f87d245a413718bb85e8156c84f059c
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-08 15:18:19 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,31 +1,49 @@
-https://confirm-kaspi-order.alser2.workers.dev
+import { Injectable } from '@nestjs/common';
+import axios from 'axios'; // Установите axios, если он не установлен
 
+@Injectable()
+export class GeliosService {
+  async getCarLocations(login: string, pass: string) {
+    try {
+      // Выполните запрос к API GeliosPro для получения данных о координатах водителей
+      const response = await axios.get(
+        `https://admin.geliospro.com/sdk/?login=${login}&pass=${pass}&svc=get_units&params={}`
+      );
 
-https://kaspi-proxy.alser.kz/
-import { HttpException, HttpStatus } from '@nestjs/common';
+      const drivers = response.data.map((driver: any) => {
+        const {
+          name,
+          unit_icon,
+          lmsg: { lat, lon },
+          info,
+        } = driver;
+      
+        // Распарсим поле "info" в объект
+        const infoObject = JSON.parse(info);
+      
+        return {
+          name,
+          unit_icon,
+          "latitude": lat,
+          "longitude": lon,
+          info: infoObject, // Вставляем распарсенный объект "info"
+        };
+      });
+      
+      return drivers;
+    } catch (error) {
+      throw new Error('Не удалось получить данные о водителях');
+    }
+  }
+}
 
-// ...
-
-import { Module } from '@nestjs/common';
-import { HttpModule } from '@nestjs/axios';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MessageSchema } from '../schemas/message.shema';
-import { TransportRequestsService } from './transportRequests.service';
-import { TransportRequestsController } from './transportRequests.controller';
-import { DBModule } from '../db/db.module';  
-
-
-@Module({
-  imports: [
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-    DBModule,
-    HttpModule,
-  ],
-  controllers: [TransportRequestsController],
-  providers: [TransportRequestsService],
-})
-export class TransportRequestsModule {}
-
-
+[Nest] 7612  - 08.11.2023, 15:16:46   ERROR [ExceptionsHandler] Не удалось получить данные о водителях
+Error: Не удалось получить данные о водителях
+    at GeliosService.getCarLocations (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\gelios\gelios.service.ts:35:13)
+    at processTicksAndRejections (node:internal/process/task_queues:95:5)
+    at DriversService.getDriversByDate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\drivers\drivers.service.ts:23:24)
+    at DriversController.getDriversByDate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\drivers\drivers.controller.ts:19:12)
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\router\router-execution-context.js:46:28
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\router\router-proxy.js:9:17
 
 
