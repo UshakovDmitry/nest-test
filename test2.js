c8eef756943dfb80c587c5f24765dd892753a72b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-08 15:21:10 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,11 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import axios from 'axios'; // Установите axios, если он не установлен
+import axios from 'axios';
 
 @Injectable()
 export class GeliosService {
   async getCarLocations(login: string, pass: string) {
     try {
-      // Выполните запрос к API GeliosPro для получения данных о координатах водителей
       const response = await axios.get(
         `https://admin.geliospro.com/sdk/?login=${login}&pass=${pass}&svc=get_units&params={}`
       );
@@ -17,33 +16,35 @@ export class GeliosService {
           lmsg: { lat, lon },
           info,
         } = driver;
-      
-        // Распарсим поле "info" в объект
+
         const infoObject = JSON.parse(info);
-      
+        
         return {
           name,
           unit_icon,
-          "latitude": lat,
-          "longitude": lon,
-          info: infoObject, // Вставляем распарсенный объект "info"
+          latitude: lat,
+          longitude: lon,
+          info: infoObject,
         };
       });
       
       return drivers;
     } catch (error) {
+      if (error.response) {
+        // Сервер вернул ответ с кодом статуса, который вышел из диапазона 2xx
+        console.error('Response data:', error.response.data);
+        console.error('Response status:', error.response.status);
+        console.error('Response headers:', error.response.headers);
+      } else if (error.request) {
+        // Запрос был сделан, но ответ не был получен
+        console.error('Request:', error.request);
+      } else {
+        // Что-то пошло не так при настройке запроса
+        console.error('Error message:', error.message);
+      }
+      console.error('Config:', error.config);
       throw new Error('Не удалось получить данные о водителях');
     }
   }
 }
-
-[Nest] 7612  - 08.11.2023, 15:16:46   ERROR [ExceptionsHandler] Не удалось получить данные о водителях
-Error: Не удалось получить данные о водителях
-    at GeliosService.getCarLocations (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\gelios\gelios.service.ts:35:13)
-    at processTicksAndRejections (node:internal/process/task_queues:95:5)
-    at DriversService.getDriversByDate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\drivers\drivers.service.ts:23:24)
-    at DriversController.getDriversByDate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\src\drivers\drivers.controller.ts:19:12)
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\router\router-execution-context.js:46:28
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\@nestjs\core\router\router-proxy.js:9:17
-
 
