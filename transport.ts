5f9968fcc47c0e1a45b49c2fbafb074b667d29f2
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-09 12:17:30 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,25 +1,25 @@
-  async getDriversByName(name: string, date: string) {
-    const drivers = await this.getAllDrivers();
-  
-    const filteredDrivers = drivers.filter(driver => {
-      if (driver.driver !== name) {
-        return false;
-      }
-  
-      return driver.transportRequests.some(request => {
-        // Извлекаем дату из строки Date_Time_delivery
+async getDriversByName(name: string, date: string) {
+  const drivers = await this.getAllDrivers();
+
+  const filteredDrivers = drivers.reduce((acc, driver) => {
+    if (driver.driver === name) {
+      // Фильтруем заявки, оставляя только те, что соответствуют указанной дате
+      const filteredTransportRequests = driver.transportRequests.filter(request => {
         const deliveryDate = request.contactInformation.Date_Time_delivery.split(' ')[0];
-  
-        // Сравниваем строки дат
         return deliveryDate === date;
       });
-    });
-  
-    return filteredDrivers;
-  }
 
-Метод вовращает неправильные данные 
-В них почему парисутсвуют "Date_Time_delivery": "2023-10-10  До 20:00",
-Не смотря на то что я передаю внутри date 2023-11-09
+      // Если после фильтрации остались заявки, добавляем водителя в аккумулированный массив
+      if (filteredTransportRequests.length > 0) {
+        acc.push({
+          ...driver, // Копируем всю информацию о водителе
+          transportRequests: filteredTransportRequests // Заменяем заявки на отфильтрованные
+        });
+      }
+    }
+    return acc;
+  }, []);
 
-Я хочу чтобы внутри были заявки которые совпадают с числом внутри "Date_Time_delivery"
+  return filteredDrivers;
+}
+
