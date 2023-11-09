e5615cb1a8986d87553b6fc33c13c0241059c985
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-09 12:00:34 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,93 +1,101 @@
-  async authorize(): Promise<boolean> {
-    try {
-      const formData = new FormData();
-      formData.append('client_id', 'DispatcherWorkplace');
-      formData.append('client_secret', 'secret');
-      formData.append('grant_type', 'password');
-      formData.append('username', this.model.email);
-      formData.append('password', this.model.password);
-      formData.append('scope', 'myAPIs');
+  async getDriversByName(name: string , date: string) {
+    const drivers = await this.getAllDrivers();
 
-      const response = await fetch('https://auth3.next.local/connect/token', {
-        method: 'POST',
-        body: formData,
-      });
+    const filteredDrivers = drivers.filter((driver) => driver.driver === name);
 
-      if (response.ok) {
-        const data = await response.json();
-        this.saveToken(data.access_token);
-        const dataToken = this.parseJwt(data.access_token);
-        const transformDataToken: IUser = this.transformDataToken(dataToken);
-        this.saveUser(transformDataToken);
-        router.push('/dashboard');
-        return true;
-      }else {
-        const errorData = await response.json(); // Получение тела ответа в формате JSON
-        console.error('Ошибка HTTP: ' + response.status); // Вывод статуса ответа
-        console.error('Ошибка: ' + errorData.error); // Вывод свойства error
-        console.error('Описание ошибки: ' + errorData.errorDescription); // Вывод свойства errorDescription
-        
-        return false;
-      }
-    } catch (error: Error | any) {
-      console.error('Ошибка при авторизации:', error);
-      return false;
-    }
+    return filteredDrivers;
   }
 
 
+Вот что возвращает эта фуекция 
+[
+  {
+      "driver": "Ибраев Абылайхан Нургожаулы",
+      "carNumber": "M121134",
+      "carModel": "Largus",
+      "transportRequests": [
+          {
+              "number": "№№00148564",
+              "IdYandex": "32bfe3e0-d6153969-856af392-44a076ca",
+              "distribution": true,
+              "date": "03.10.2023 15:36:47",
+              "dateCreated": "4-10-2023",
+              "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+              "documentStatus": "Доставлено вовремя",
+              "ISR": "288519384",
+              "informalDocument": "Заказ покупателя ППО",
+              "filterContractor": "Alser",
+              "loanAgreementStatus": "ПРИНЯТ НАМИ",
+              "typePayment": "Безналичный расчет",
+              "contactInformation": {
+                  "City": "Шымкент",
+                  "Delivery_Condition": "Доставка",
+                  "Date_Time_delivery": "2023-10-04  до 20:00",
+                  "Time_Window": "нет данных",
+                  "Latitude": "42.362533",
+                  "Longitude": "69.613757",
+                  "Street": "нет данных",
+                  "Home": "7",
+                  "Phone": "(777)2400708",
+                  "Apartment": "нет данных",
+                  "Contractor": "BS Kazakhstan BS Kazakhstan BS Kazakhstan",
+                  "_id": "651d19973e71be8042bad459"
+              },
+              "orders": [
+                  {
+                      "NuberPPO": "8780440",
+                      "PPOStatus": "Доставляется",
+                      "SKU": "1379078",
+                      "Pickup_Time": "01.01.0001 9:00:00",
+                      "Delivery_Time": "01.01.0001 11:08:59"
+                  }
+              ]
+          },
+          {
+              "number": "№№00148145",
+              "IdYandex": "260869ea-3d54ecc7-f7af11a5-16e51a44",
+              "distribution": true,
+              "date": "02.10.2023 16:45:39",
+              "dateCreated": "4-10-2023",
+              "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+              "documentStatus": "Отказ клиента от заказа",
+              "ISR": "288316444",
+              "informalDocument": "Заказ покупателя ППО",
+              "filterContractor": "Alser",
+              "loanAgreementStatus": "ОТМЕНЁН",
+              "typePayment": "Кредит",
+              "contactInformation": {
+                  "City": "Шымкент",
+                  "Delivery_Condition": "Доставка",
+                  "Date_Time_delivery": "2023-10-03 До 20:00",
+                  "Time_Window": "нет данных",
+                  "Latitude": "42,374896",
+                  "Longitude": "69,597255",
+                  "Street": "нет данных",
+                  "Home": "5",
+                  "Phone": "(701)2343899",
+                  "Apartment": "нет данных",
+                  "Contractor": "Елемесова Акмарал",
+                  "_id": "651d6d6cceef8fbd8ced2bb3"
+              },
+              "orders": [
+                  {
+                      "NuberPPO": "8778424",
+                      "PPOStatus": "Отменен",
+                      "Pickup_Time": "01.01.0001 9:00:00",
+                      "Delivery_Time": "01.01.0001 10:24:06"
+                  }
+              ]
+          },
+        ]
 
-  validateError = (error: any) => {
-
-  };
-
-
-реалуй валидацию ошибок 
-если  errorData.errorDescription равен invalid_username_or_password то верни строку "Не валидный логин или пароль"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-validateError = (errorData: any) => {
-  switch (errorData.errorDescription) {
-    case 'invalid_username_or_password':
-      return 'Не валидный логин или пароль';
-    
-    // Добавьте дополнительные case для обработки других возможных ошибок, если необходимо
-    
-    default:
-      return 'Произошла неизвестная ошибка'; // Сообщение по умолчанию для ошибок, которые не обрабатываются явно
   }
-};
+]
 
+Я хочу добавить новый фуекционал
+я хочу брать date и сравнивать с каждым элементом массива transportRequests
+А именно contactInformation.Date_Time_delivery 
 
+В данном случае я хочу сравнивать внутри строки  "Date_Time_delivery": "2023-10-03 До 20:00"
 
-
-
-
-
-
-
-else {
-      const errorData = await response.json(); // Получение тела ответа в формате JSON
-      
-      const errorMessage = this.validateError(errorData); // Получение сообщения об ошибке с помощью функции validateError
-      console.error(errorMessage); // Вывод сообщения об ошибке в консоль
-      
-      return false;
-    }
-
+И возвращать только те элементы которые совпадают по дате
