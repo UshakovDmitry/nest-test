0ebef5067d62859541e7f1d177b16a6bcbd903b0
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 11:10:52 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,40 +1,6 @@
-  @Post('call')
-  async addCall(
-    @Body() callData: { name: string; date: string; tel_number: string },
-    @Res() response
-  ) {
-    try {
-      await this.historyService.addCallHistory(callData.name, {
-        date: callData.date,
-        tel_number: callData.tel_number,
-      });
-      return response.status(HttpStatus.OK).json({
-        message: "Звонок успешно записан в БД"
-      });
-    } catch (error) {
-      // Обработка ошибок, например, если не удалось найти пользователя или сохранить данные
-      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
-        message: "Произошла ошибка при записи звонка"
-      });
-    }
-  }
-
-
-
-
-
-
-
-// ... существующий код ...
-
-async addCallHistory(name: string, callData: { date: string; tel_number: string }) {
-  const userAction = await this.historyModel.findOne({ name }).exec();
-  if (userAction) {
-    userAction.callHistory.push(callData);
-    await userAction.save();
-    return true; // Указываем, что операция прошла успешно
-  } else {
-    // Здесь можете выбросить исключение или вернуть false
-    throw new Error('Пользователь не найден');
-  }
-}
+  async recordHistoryAction({ name, time, comment }: { name: string; time: string; comment: string; }) {
+    const newHistoryEntry = new this.historyModel({
+      name,
+      time,
+      comment,
+    });
