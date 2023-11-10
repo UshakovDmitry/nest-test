752b8582b5566ec0143679fa1e22b07221c443b1
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 12:34:19 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,77 +1,65 @@
-  async transportRequestCorrection(dto): Promise<any> {
-    // Сопоставление полей DTO с ожидаемыми полями в запросе
-    const requestData = {
-      DocNumber: dto.documentNumber,
-      DateDoc: dto.date,
-      TimeDelivery: dto.timeDelivery,
-      Driver: dto.driver,
-      СarNumber: dto.carNumber,
-      UserIIN: dto.userIIN,
-      Сomment: dto.comment,
-    };
+async transportRequestCorrection(dto): Promise<any> {
+  // Сопоставление полей DTO с ожидаемыми полями в запросе
+  const requestData = {
+    DocNumber: dto.documentNumber,
+    DateDoc: dto.date,
+    TimeDelivery: dto.timeDelivery,
+    Driver: dto.driver,
+    СarNumber: dto.carNumber,
+    UserIIN: dto.userIIN,
+    Сomment: dto.comment,
+  };
 
-    // Проверка наличия и формата всех необходимых полей
-    const requiredFields = [
-      'DocNumber',
-      'DateDoc',
-      'TimeDelivery',
-      'Driver',
-      'UserIIN',
-      'Сomment',
-    ];
+  // Проверка наличия и формата всех необходимых полей
+  const requiredFields = [
+    'DocNumber',
+    'DateDoc',
+    'TimeDelivery',
+    'Driver',
+    'UserIIN',
+    'Сomment',
+  ];
 
-    for (const field of requiredFields) {
-      if (!requestData[field]) {
-        throw new HttpException(
-          `Обязательное поле отсутствует: ${field}`,
-          HttpStatus.BAD_REQUEST,
-        );
-      }
-
-      if (typeof requestData[field] !== 'string') {
-        throw new HttpException(
-          `Неверный тип данных для поля: ${field}. Ожидалась строка.`,
-          HttpStatus.BAD_REQUEST,
-        );
-      }
+  for (const field of requiredFields) {
+    if (!requestData[field]) {
+      throw new HttpException(
+        `Обязательное поле отсутствует: ${field}`,
+        HttpStatus.BAD_REQUEST,
+      );
     }
 
-    console.log(requestData, 'корректировка ( отправляю Сане )');
-
-    // http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver  боевая
-    try {
-
-
-      const response = await firstValueFrom(
-        this.httpService.post(
-          // 'http://10.0.1.32:8080/1CHS/hs/TMS//ReplaceDriver', // тестовая
-          'http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver ',
-          requestData,
-        ),
-      );
-
-      if (response.data ) {
-        this.actionHistoryService.addCorrectionHistory({
-        name: dto.userName, 
-        time: dto.timeDelivery, 
-        comment: dto.comment
-      });
-      }
-      return response.data;
-    } catch (error) {
+    if (typeof requestData[field] !== 'string') {
       throw new HttpException(
-        `Ошибка при отправке запроса на корректировку транспортной заявки: ${error}`,
-        HttpStatus.INTERNAL_SERVER_ERROR,
+        `Неверный тип данных для поля: ${field}. Ожидалась строка.`,
+        HttpStatus.BAD_REQUEST,
       );
     }
   }
 
-Сделай рефакторинг этого метода
-Если запрос на  'http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver ' то только тогда записывай в бд 
+  // URL боевого сервера
+  const liveServerUrl = 'http://10.0.1.20/1CHS/hs/TMS//ReplaceDriver';
 
-вот запись в бд 
- this.actionHistoryService.addCorrectionHistory({
+  try {
+    // Отправка запроса на корректировку
+    const response = await firstValueFrom(
+      this.httpService.post(liveServerUrl, requestData),
+    );
+
+    // Проверка URL запроса и запись в БД в случае успеха
+    if (response.data && axios.getUri(response.config) === liveServerUrl) {
+      this.actionHistoryService.addCorrectionHistory({
         name: dto.userName, 
         time: dto.timeDelivery, 
         comment: dto.comment
       });
+    }
+
+    return response.data;
+  } catch (error) {
+    throw new HttpException(
+      `Ошибка при отправке запроса на корректировку транспортной заявки: ${error}`,
+      HttpStatus.INTERNAL_SERVER_ERROR,
+    );
+  }
+}
+
