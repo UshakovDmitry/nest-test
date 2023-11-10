e33f5eff015df0044dc228ec4e2be68aa74b45aa
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 15:19:42 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,50 +1,26 @@
-async addHistory(history) {
-this.actionHistoryService.addCorrectionDutyDriversSHistory(history);
-}
-
-
-
-
   async addCorrectionDutyDriversSHistory (data) {
     console.log(data, 'data');
     
-    // let userAction = await this.historyModel.findOne({ name: userName }).exec();
+    let userAction = await this.historyModel.findOne({ name: data.userName }).exec();
 
-    // if (!userAction) {
-    //   userAction = new this.historyModel({ name: userName, transportRequestHistory: [], callHistory: [] ,DutyDriversHistory: [] });
-    // }
+    if (!userAction) {
+      userAction = new this.historyModel({ name: data.userName, transportRequestHistory: [], callHistory: [] ,DutyDriversHistory: [] });
+    }
 
-    // userAction.DutyDriversHistory.push({ date, userIIN });
-    // await userAction.save();
-    // return { status: 'success', message: 'Заявка успешно записана в историю' };
+    userAction.DutyDriversHistory.push({ data.UserIIN, data.date });
+    await userAction.save();
+    return { status: 'success', message: 'Заявка успешно записана в историю' };
   }
+}
 
+Argument of type '{ data: any; "": any; }' is not assignable to parameter of type '{ date: string; userIIN: string; }'.
+  Object literal may only specify known properties, and 'data' does not exist in type '{ date: string; userIIN: string; }'.ts(2345)
+',' expected.ts(1005)
+Translation
+Argument of type '{ data: any; "": any; }' is not assignable to parameter of type '{ date: string; userIIN: string; }'.
+I was expecting { date: string; userIIN: string; }, but you passed { data: any; "": any; }.
 
+Object literal may only specify known properties, and 'data' does not exist in type '{ date: string; userIIN: string; }'.
+You can't pass property data to type { date: string; userIIN: string; }.
 
-
-Приложение запущено на порту 4000
-{
-  UserIIN: '681012301869',
-  UserName: 'Абдуллин Р.Р.',
-  date: '2023-11-02'
-} data
-
-
-Я закоментировал логику пока что и посмотрел что приходит 
-не пойму почему до этого была ошибка
-
-
-
-
-
-
-
-[Nest] 13436  - 10.11.2023, 15:13:12     LOG [RouterExplorer] Mapped {/api/dutydrivers, POST} route +0ms
-[Nest] 13436  - 10.11.2023, 15:13:12     LOG [RouterExplorer] Mapped {/api/dutydrivers/correction, POST} route +0ms
-[Nest] 13436  - 10.11.2023, 15:13:12     LOG [NestApplication] Nest application successfully started +2ms
-Приложение запущено на порту 4000
-{
-  UserIIN: '681012301869',
-  UserName: 'Абдуллин Р.Р.',
-  date: '2023-11-02'
-} data
+(property) data: any
