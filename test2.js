f67fc11bec7e5a14fe22435f02ce049943a9f1ea
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 11:03:11 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,51 +1,40 @@
-@Schema({ versionKey: false })
-export class DispatcherAction {
-  @Prop({ required: true })
-  name: string;
-
-  @Prop({ required: true })
-  time: string;
-
-  @Prop({ required: true })
-  comment: string;
-
-  // Добавьте новое поле для истории звонков
-  @Prop({ default: [] })
-  callHistory: {
-    date: string;
-    tel_number: string;
-  }[];
-}
-
-export const DispatcherActionSchema = SchemaFactory.createForClass(DispatcherAction);
-
-
-
-
-// Метод для добавления записи о звонке
-  async addCallHistory(name: string, callData: { date: string; tel_number: string }) {
-    const userAction = await this.historyModel.findOne({ name }).exec();
-    if (userAction) {
-      userAction.callHistory.push(callData);
-      await userAction.save();
-    } else {
-      // Обработка случая, когда пользователь не найден
+  @Post('call')
+  async addCall(
+    @Body() callData: { name: string; date: string; tel_number: string },
+    @Res() response
+  ) {
+    try {
+      await this.historyService.addCallHistory(callData.name, {
+        date: callData.date,
+        tel_number: callData.tel_number,
+      });
+      return response.status(HttpStatus.OK).json({
+        message: "Звонок успешно записан в БД"
+      });
+    } catch (error) {
+      // Обработка ошибок, например, если не удалось найти пользователя или сохранить данные
+      return response.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        message: "Произошла ошибка при записи звонка"
+      });
     }
   }
 
 
 
-@Controller('api/history')
-export class ActionHistoryController {
-  // ... существующий код ...
 
-  @Post('call') // Используйте декоратор @Post для обработки POST-запросов
-  async addCall(@Body() callData: { name: string; date: string; tel_number: string }) {
-    return this.historyService.addCallHistory(callData.name, {
-      date: callData.date,
-      tel_number: callData.tel_number,
-    });
+
+
+
+// ... существующий код ...
+
+async addCallHistory(name: string, callData: { date: string; tel_number: string }) {
+  const userAction = await this.historyModel.findOne({ name }).exec();
+  if (userAction) {
+    userAction.callHistory.push(callData);
+    await userAction.save();
+    return true; // Указываем, что операция прошла успешно
+  } else {
+    // Здесь можете выбросить исключение или вернуть false
+    throw new Error('Пользователь не найден');
   }
 }
-
-
