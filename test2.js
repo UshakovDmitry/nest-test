ed96a379ca48beaad23849688dc7e45bf8c19b3b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 14:14:20 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,11 +1,20 @@
-Приложение запущено на порту 4000
+мне нужно реализовать вот такой пост запрос на бекенд
+как ты предложишь его написать и как передавать данные
+самое профессиональное решение
 
-C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\document.js:3162
-    this.$__.validationError = new ValidationError(this);
-                               ^
-ValidationError: DispatcherAction validation failed: name: Path `name` is required.
-    at model.Document.invalidate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\document.js:3162:32)
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\document.js:2955:17
-    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\schematype.js:1368:9
-    at processTicksAndRejections (node:internal/process/task_queues:77:11)
-PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> 
+POST http://10.0.1.20/1CHS/hs/TMS/DriverСhange
+
+
+{
+    "Сap": {
+        "DateDoc": "20231102"
+    },
+    "items": [
+        {
+            "Driver": "681012301869",
+            "Duty": true,
+            "Date": "2",
+            "Values": true
+        }
+    ]
+}
