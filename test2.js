ee8c65b3e3fa24254a36a26c2a10a1bd7a78273b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 11:15:05 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,6 +1,27 @@
-  async recordHistoryAction({ name, time, comment }: { name: string; time: string; comment: string; }) {
-    const newHistoryEntry = new this.historyModel({
-      name,
-      time,
-      comment,
-    });
+import { InjectModel } from '@nestjs/mongoose';
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { DispatcherActionDocument } from '../schemas/history.schema';
+
+@Injectable()
+export class ActionHistoryService {
+  constructor(
+    @InjectModel('DispatcherAction')
+    private historyModel: Model<DispatcherActionDocument>,
+  ) {}
+
+  async getAllHistory() {
+    return this.historyModel.find().exec();
+  }
+
+  async addCallHistory(name: string, callData: { date: string; tel_number: string }) {
+    const userAction = await this.historyModel.findOne({ name }).exec();
+    if (userAction) {
+      userAction.callHistory.push(callData);
+      await userAction.save();
+      return true; // Указываем, что операция прошла успешно
+    } else {
+      throw new Error('Пользователь не найден');
+    }
+  }
+}
