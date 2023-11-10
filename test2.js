8d089e64d2e3bdcc78439d883fc7a1b82c0b155d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 14:32:44 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,23 +1,64 @@
+dto
+
+import { IsNotEmpty, IsString } from 'class-validator';
+
 class CapDto {
-  DateDoc: string;
+    @IsNotEmpty()
+    @IsString()
+    DateDoc: string;
+  }
+  
+  class ItemDto {
+    @IsNotEmpty()
+    @IsString()
+    Driver: string;
+
+    @IsNotEmpty()
+    @IsString()
+    Duty: string;
+
+    @IsNotEmpty()
+    @IsString()
+    Date: string;
+
+    @IsNotEmpty()
+    @IsString()
+    Values: string;
+  }
+  
+  class HistoryDto {
+    @IsNotEmpty()
+    @IsString()
+    UserIIN: string;
+
+    @IsNotEmpty()
+    @IsString()
+    UserName: string;
+    
+    @IsNotEmpty()
+    @IsString()
+    date: string;
+  }
+  
+  export class CorrectionDto {
+    Сap: CapDto;
+    items: ItemDto[];
+    history: HistoryDto;
+  } 
+
+Теперь нужно эти данные отправить на другой url внктри this.dutydriversService.setDutyDrivers
+передай туда все кроме HistoryDto
+
+и напиши сам метод 
+
+ @Post('/correction') 
+  async setDutyDrivers(@Body() dto: CorrectionDto) {
+    return this.dutydriversService.setDutyDrivers(
+      {
+        
+      }
+    );
 }
 
-class ItemDto {
-  Driver: string;
-  Duty: string;
-  Date: string;
-  Values: string;
-}
 
-class HistoryDto {
-  UserIIN: string;
-  UserName: string;
-  date: string;
-}
-
-export class CorrectionDto {
-  Сap: CapDto;
-  items: ItemDto[];
-  history: HistoryDto;
-}
 
