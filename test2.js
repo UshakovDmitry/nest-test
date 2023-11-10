6620a33956e12852b3b7dd98fc8f0d47049b8d84
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-10 15:21:51 +0600

    Update test2.js

--- test2.js
+++ test2.js
@@ -1,26 +1,20 @@
-  async addCorrectionDutyDriversSHistory (data) {
-    console.log(data, 'data');
-    
-    let userAction = await this.historyModel.findOne({ name: data.userName }).exec();
+[Nest] 9500  - 10.11.2023, 15:21:28     LOG [RouterExplorer] Mapped {/api/dutydrivers, POST} route +0ms
+[Nest] 9500  - 10.11.2023, 15:21:29     LOG [RouterExplorer] Mapped {/api/dutydrivers/correction, POST} route +1ms
+[Nest] 9500  - 10.11.2023, 15:21:29     LOG [NestApplication] Nest application successfully started +1ms
+Приложение запущено на порту 4000
+{
+  UserIIN: '681012301869',  
+  UserName: 'Абдуллин Р.Р.',
+  date: '2023-11-02'        
+} data
 
-    if (!userAction) {
-      userAction = new this.historyModel({ name: data.userName, transportRequestHistory: [], callHistory: [] ,DutyDriversHistory: [] });
-    }
 
-    userAction.DutyDriversHistory.push({ data.UserIIN, data.date });
-    await userAction.save();
-    return { status: 'success', message: 'Заявка успешно записана в историю' };
-  }
-}
-
-Argument of type '{ data: any; "": any; }' is not assignable to parameter of type '{ date: string; userIIN: string; }'.
-  Object literal may only specify known properties, and 'data' does not exist in type '{ date: string; userIIN: string; }'.ts(2345)
-',' expected.ts(1005)
-Translation
-Argument of type '{ data: any; "": any; }' is not assignable to parameter of type '{ date: string; userIIN: string; }'.
-I was expecting { date: string; userIIN: string; }, but you passed { data: any; "": any; }.
-
-Object literal may only specify known properties, and 'data' does not exist in type '{ date: string; userIIN: string; }'.
-You can't pass property data to type { date: string; userIIN: string; }.
-
-(property) data: any
+C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\document.js:3162
+    this.$__.validationError = new ValidationError(this);
+                               ^
+ValidationError: DispatcherAction validation failed: name: Path `name` is required.
+    at model.Document.invalidate (C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\document.js:3162:32)
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\document.js:2955:17
+    at C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend\node_modules\mongoose\lib\schematype.js:1368:9
+    at processTicksAndRejections (node:internal/process/task_queues:77:11)
+PS C:\Users\ushakov.dmitriy\Desktop\alser.dispatcherworkplaceui\backend> 
