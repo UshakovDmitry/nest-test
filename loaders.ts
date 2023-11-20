9b6b3d5ebbfcc12ee19bcfecfa6317456f98e256
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-20 10:49:28 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,35 +1,107 @@
-export class CouriersViewModel {
-  model: CouriersModel;
-
-  constructor(model: CouriersModel) {
-    this.model = model;
-    this.getCouriers();
+ {
+                "number": "№№00168940",
+                "IdYandex": "1534dddb-b6167af1-5fca2c62-55ae6d31",
+                "IsDelete": false,
+                "distribution": true,
+                "date": "19.11.2023 13:01:16",
+                "dateCreated": "20-11-2023",
+                "organization": "TOO Gulser Computers (Гулсер Компьютерс)",
+                "documentStatus": "Доставлено с опазданием",
+                "completedDelivery": "20.11.2023 10:09:12",
+                "ISR": "301207484",
+                "informalDocument": "Заказ покупателя ППО",
+                "filterContractor": "Kaspi",
+                "loanAgreementStatus": "ПРИНЯТ НАМИ",
+                "typePayment": "Кредит",
   }
 
-  async getCouriers(): Promise<void> {
-    try {
-      const response = await useGetApi('getCouriers');
-      console.log(response, 'response');
+это часть исходных данных
+здесь время в которое нужно было привезти заказ 
+ "date": "19.11.2023 13:01:16"  равно 13:01:16
 
-      // Маппинг ответа в массив объектов ICourier
-      const couriers: ICourier[] = response.map((item: any): ICourier => ({
-        courierFullName: item.Drivers,       // Используем свойства из ответа сервера
-        carNumber: item.carNumber,
-        type: item.HiringType,               // Предполагаю, что это правильное свойство
-        schedule: item.TimeWindow,           // Аналогично, это предположение
-        hardWindow: item.HardTimeWindow ? 'Да' : 'Нет', // Если это булевое значение
-        returnToWarehouse: item.ReturnWarehouse ? 'Да' : 'Нет', // Аналогично
-        city: item.City,
-      }));
+а фактическое время доставки 
+"completedDelivery": "20.11.2023 10:09:12" равно 10:09:12
 
-      // Сохраняем массив couriers в вашей модели (предполагая, что у модели есть такое свойство)
-      this.model.couriers = couriers;
+но почему то статус пишется как с опазданием 
+ "documentStatus": "Доставлено с опазданием"
 
-    } catch (error) {
-      throw error;
-    }
+даже с учетом добавления 15 минут 
+
+вот функция 
+
+  async updateDeliveryStatus(drivers) {
+    // Установка текущего времени и добавление к нему 15 минут
+    const currentTime = new Date();
+    currentTime.setMinutes(currentTime.getMinutes() + 15);
+  
+    // Перебор массива drivers
+    drivers.forEach((driver) => {
+      // Перебор всех transportRequests каждого водителя
+      driver.transportRequests.forEach((request) => {
+        let deliveryDateTime = null; // Переменная для хранения времени доставки
+        let completedDateTime = null; // Переменная для хранения времени завершения доставки
+  
+        // Обработка времени завершения доставки на уровне request
+        if (request.completedDelivery) {
+          const [_, completedDeliveryTime] = request.completedDelivery?.split(' ') || ['', ''];
+          if (completedDeliveryTime) {
+            const [completedHours, completedMinutes, completedSeconds] =
+              completedDeliveryTime.split(':').map(Number);
+            completedDateTime = new Date();
+            completedDateTime.setHours(
+              completedHours,
+              completedMinutes,
+              completedSeconds,
+            );
+          }
+        }
+        // Проверка есть ли в заявке отметка об удалении
+        if (request.IsDelete === true) {
+          request.documentStatus = 'Помечено на удаление';
+        }
+        // Проверка условий для обновления статуса доставки
+        if (
+          request.distribution === true &&
+          (request.documentStatus === 'Доставляется' ||
+            request.documentStatus === 'Забран')
+        ) {
+          // Перебор всех заказов в запросе
+          request.orders.forEach((order) => {
+            if (order.Delivery_Time) {
+              const [_, deliveryTime] = order.Delivery_Time?.split(' ') || ['', ''];
+              if (deliveryTime) {
+                const [deliveryHours, deliveryMinutes, deliverySeconds] =
+                  deliveryTime.split(':').map(Number);
+                deliveryDateTime = new Date();
+                deliveryDateTime.setHours(
+                  deliveryHours,
+                  deliveryMinutes,
+                  deliverySeconds,
+                );
+              }
+  
+              // Сравнение времени доставки с текущим временем для установки статуса
+              if (deliveryDateTime && currentTime > deliveryDateTime) {
+                request.documentStatus = 'Доставляется с опазданием';
+              } else {
+                request.documentStatus = 'Доставляется вовремя';
+              }
+            }
+          });
+        }
+  
+        // Проверка статуса 'Доставлено'
+        if (request.distribution === true && request.documentStatus === 'Доставлено') {
+          // Сравнение времени завершения доставки с временем доставки для установки статуса
+          if (completedDateTime && completedDateTime > deliveryDateTime) {
+            request.documentStatus = 'Доставлено с опазданием';
+          } else {
+            request.documentStatus = 'Доставлено вовремя';
+          }
+        }
+      });
+    });
+  
+    // Возвращение обновленного списка водителей
+    return drivers;
   }
-}
-
-
-
