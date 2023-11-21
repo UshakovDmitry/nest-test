5df6152a04a3f64fe9c8ea6f84e55ca6da85696d
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-21 10:48:57 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,29 +1,32 @@
+setDataDelivery(data: any) {
+  console.log(data , 'data');
+  
+  const numberPPO = data.ArrayStrings.map((item) => item.NuberPPO);
 
+  let deliveryDate = '-/-/-';
+  let deliveryTime = '-/-/-';
 
-  setDataDelivery(data: any) {
-    console.log(data , 'data');
-    
-    const numberPPO = data.ArrayStrings.map((item) => item.NuberPPO);
-
-    return {
-      numberPPO: numberPPO[0],
-      number1C: data.Number,
-      ISR: data.ISR,
-      status: data.DocumentStatus,
-      courier: {
-        name: data.Driver,
-        phone: data.FilterContractor,
-      },
-      delivery: {
-        date: data.CompletedDelivery ? data.CompletedDelivery : '-/-/-',
-        time: data.CompletedDelivery ? data.CompletedDelivery : '-/-/-',
-      },
-    };
+  // Проверяем, существует ли CompletedDelivery и не является ли оно пустым
+  if (data.CompletedDelivery) {
+    const parts = data.CompletedDelivery.split(' '); // Разделяем строку по пробелу
+    if (parts.length === 2) { // Убеждаемся, что присутствуют и дата, и время
+      deliveryDate = parts[0]; // Первая часть - это дата
+      deliveryTime = parts[1]; // Вторая часть - это время
+    }
   }
 
-Есть вот такая функция 
-есть вот такая строка CompletedDelivery: "20.11.2023 18:45:24"
-я хочу заносить в date 20.11.2023
-а в time 18:45:24
-
-исправь преобрахование
+  return {
+    numberPPO: numberPPO[0],
+    number1C: data.Number,
+    ISR: data.ISR,
+    status: data.DocumentStatus,
+    courier: {
+      name: data.Driver,
+      phone: data.FilterContractor,
+    },
+    delivery: {
+      date: deliveryDate,
+      time: deliveryTime,
+    },
+  };
+}
