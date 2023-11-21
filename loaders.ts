a9a73f0e4463b8d172068307c1ff5865f00df063
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-21 11:49:58 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,32 +1,84 @@
-setDataDelivery(data: any) {
-  console.log(data , 'data');
-  
-  const numberPPO = data.ArrayStrings.map((item) => item.NuberPPO);
+  async getTransportRequests(day: string): Promise<void> {
 
-  let deliveryDate = '-/-/-';
-  let deliveryTime = '-/-/-';
+    const today = new Date();
 
-  // Проверяем, существует ли CompletedDelivery и не является ли оно пустым
-  if (data.CompletedDelivery) {
-    const parts = data.CompletedDelivery.split(' '); // Разделяем строку по пробелу
-    if (parts.length === 2) { // Убеждаемся, что присутствуют и дата, и время
-      deliveryDate = parts[0]; // Первая часть - это дата
-      deliveryTime = parts[1]; // Вторая часть - это время
+    if (day === 'tomorrow') {
+      today.setDate(today.getDate() + 1);
+      this.model.isTomorrow = true;
+      this.model.isToday = false;
+      this.model.isYesterday = false;
+    } else if (day === 'yesterday') {
+      today.setDate(today.getDate() - 1);
+      this.model.isTomorrow = false;
+      this.model.isToday = false;
+      this.model.isYesterday = true;
+    } else if (day === 'today') {
+      this.model.isTomorrow = false;
+      this.model.isToday = true;
+      this.model.isYesterday = false;
     }
+
+    const formattedDate = `${today.getFullYear()}-${String(
+      today.getMonth() + 1,
+    ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+    const body = {
+      date: formattedDate,
+    };
+    
+    const response = await usePostApi('getTransportRequestsByDate', body);
+    console.log(response.length, 'кол-во заявок');
+    // console.log(response, 'response');
+    this.model.transportRequests = [];
+    this.model.filterContractors = {};
+    response.forEach((data: any) => {
+      const transformedData = this.transformToTransportRequest(data);
+      const city = this.setCitiesList(transformedData);
+      // Проверяем наличие города в списке и добавляем, если его нет
+      if (!this.model.cities.includes(city)) {
+        this.model.cities.push(city);
+      }
+    
+      
+
+      const contractor = data.FilterContractor
+        ? data.FilterContractor
+        : 'Прочее';
+      if (this.model.filterContractors[contractor]) {
+        this.model.filterContractors[contractor] += 1;
+      } else {
+        this.model.filterContractors[contractor] = 1;
+      }
+      const transformedDataForTable =
+        this.transformToTransportForTable(transformedData);
+
+      this.model.transportRequests.unshift(transformedDataForTable);
+      this.model.filteredTransportRequests = this.model.transportRequests;
+    });
+    
   }
 
-  return {
-    numberPPO: numberPPO[0],
-    number1C: data.Number,
-    ISR: data.ISR,
-    status: data.DocumentStatus,
-    courier: {
-      name: data.Driver,
-      phone: data.FilterContractor,
-    },
-    delivery: {
-      date: deliveryDate,
-      time: deliveryTime,
-    },
-  };
+в жтом методе я выполняю
+ const city = this.setCitiesList(transformedData);
+      // Проверяем наличие города в списке и добавляем, если его нет
+      if (!this.model.cities.includes(city)) {
+        this.model.cities.push(city);
+      }
+наполняю список своих городов из приходящих данных 
+
+затем у меня есть функция где я сортирую по городам 
+  filterRequestsByCity(city: string): void {
+    console.log(city, 'city');
+    
+    this.model.filteredTransportRequests = this.model.transportRequests.filter(
+        request => {
+          
+            const addressParts = request.deliveryAddress.address.split(',');
+            const requestCity = addressParts[0]?.trim(); 
+            return requestCity === city;
+        }
+    );
 }
+
+
+Я хочу добавить "Все города" 
+При клике я хочу отображать все заявки во всех городах
