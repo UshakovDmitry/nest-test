93f21a61f4a6584f3ef1ef5c3bd548550600cb21
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-21 11:52:59 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,84 +1,32 @@
-  async getTransportRequests(day: string): Promise<void> {
+// ... ваш предыдущий код ...
 
-    const today = new Date();
-
-    if (day === 'tomorrow') {
-      today.setDate(today.getDate() + 1);
-      this.model.isTomorrow = true;
-      this.model.isToday = false;
-      this.model.isYesterday = false;
-    } else if (day === 'yesterday') {
-      today.setDate(today.getDate() - 1);
-      this.model.isTomorrow = false;
-      this.model.isToday = false;
-      this.model.isYesterday = true;
-    } else if (day === 'today') {
-      this.model.isTomorrow = false;
-      this.model.isToday = true;
-      this.model.isYesterday = false;
-    }
-
-    const formattedDate = `${today.getFullYear()}-${String(
-      today.getMonth() + 1,
-    ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-    const body = {
-      date: formattedDate,
-    };
-    
-    const response = await usePostApi('getTransportRequestsByDate', body);
-    console.log(response.length, 'кол-во заявок');
-    // console.log(response, 'response');
-    this.model.transportRequests = [];
-    this.model.filterContractors = {};
-    response.forEach((data: any) => {
-      const transformedData = this.transformToTransportRequest(data);
-      const city = this.setCitiesList(transformedData);
-      // Проверяем наличие города в списке и добавляем, если его нет
-      if (!this.model.cities.includes(city)) {
-        this.model.cities.push(city);
-      }
-    
-      
-
-      const contractor = data.FilterContractor
-        ? data.FilterContractor
-        : 'Прочее';
-      if (this.model.filterContractors[contractor]) {
-        this.model.filterContractors[contractor] += 1;
-      } else {
-        this.model.filterContractors[contractor] = 1;
-      }
-      const transformedDataForTable =
-        this.transformToTransportForTable(transformedData);
-
-      this.model.transportRequests.unshift(transformedDataForTable);
-      this.model.filteredTransportRequests = this.model.transportRequests;
-    });
-    
+response.forEach((data: any) => {
+  // ... ваш код обработки данных ...
+  
+  // Добавляем "Все города" в список городов, если его ещё нет
+  if (!this.model.cities.includes("Все города")) {
+    this.model.cities.unshift("Все города");
   }
+});
 
-в жтом методе я выполняю
- const city = this.setCitiesList(transformedData);
-      // Проверяем наличие города в списке и добавляем, если его нет
-      if (!this.model.cities.includes(city)) {
-        this.model.cities.push(city);
-      }
-наполняю список своих городов из приходящих данных 
+// ... ваш код продолжается ...
 
-затем у меня есть функция где я сортирую по городам 
-  filterRequestsByCity(city: string): void {
-    console.log(city, 'city');
-    
+
+
+filterRequestsByCity(city: string): void {
+  console.log(city, 'city');
+
+  if (city === 'Все города') {
+    // Если выбраны "Все города", то показываем все заявки
+    this.model.filteredTransportRequests = this.model.transportRequests;
+  } else {
+    // Фильтрация заявок по выбранному городу
     this.model.filteredTransportRequests = this.model.transportRequests.filter(
-        request => {
-          
-            const addressParts = request.deliveryAddress.address.split(',');
-            const requestCity = addressParts[0]?.trim(); 
-            return requestCity === city;
-        }
+      request => {
+        const addressParts = request.deliveryAddress.address.split(',');
+        const requestCity = addressParts[0]?.trim(); 
+        return requestCity === city;
+      }
     );
+  }
 }
-
-
-Я хочу добавить "Все города" 
-При клике я хочу отображать все заявки во всех городах
