3043ff0dab2820bc7e57a23c0b30436c3740bf87
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-21 10:44:24 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,4 +1,29 @@
-this.model.cities = [
-  ...new Set(this.model.couriers.map((item) => item.city)),
-].sort((a, b) => a.localeCompare(b, 'ru'));
 
+
+  setDataDelivery(data: any) {
+    console.log(data , 'data');
+    
+    const numberPPO = data.ArrayStrings.map((item) => item.NuberPPO);
+
+    return {
+      numberPPO: numberPPO[0],
+      number1C: data.Number,
+      ISR: data.ISR,
+      status: data.DocumentStatus,
+      courier: {
+        name: data.Driver,
+        phone: data.FilterContractor,
+      },
+      delivery: {
+        date: data.CompletedDelivery ? data.CompletedDelivery : '-/-/-',
+        time: data.CompletedDelivery ? data.CompletedDelivery : '-/-/-',
+      },
+    };
+  }
+
+Есть вот такая функция 
+есть вот такая строка CompletedDelivery: "20.11.2023 18:45:24"
+я хочу заносить в date 20.11.2023
+а в time 18:45:24
+
+исправь преобрахование
