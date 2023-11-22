e566c3831ec2b312dfa8fc2b10a3eeddcab84523
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-22 11:12:14 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,48 +1,35 @@
-import { BadRequestException } from '@nestjs/common';
+const amqp = require('amqplib/callback_api');
+import { Subject } from './observer';
 
-@Post('/correction')
-async setDutyDrivers(@Body() dto: CorrectionDto) {
-    // Проверка наличия и содержимого поля 'cap'
-    if (!dto.cap) {
-        throw new BadRequestException('Поле cap отсутствует');
-    }
-    if (!dto.cap.DateDoc || dto.cap.DateDoc.trim() === '') {
-        throw new BadRequestException('Поле DateDoc в cap отсутствует или пусто');
-    }
+export const messageSubject = new Subject();
 
-    // Проверка наличия и содержимого массива 'items'
-    if (!dto.items || !Array.isArray(dto.items) || dto.items.length === 0) {
-        throw new BadRequestException('Поле items отсутствует или пусто');
+amqp.connect(
+  'amqp://tms:26000567855499290979@rabbitmq.next.local',
+  function (errorConnect, connection) {
+    if (errorConnect) {
+      throw 'Ошибка подключения к rabbitMQ: ' + errorConnect;
     }
-    dto.items.forEach((item, index) => {
-        if (!item.Driver || item.Driver.trim() === '') {
-            throw new BadRequestException(`Поле Driver отсутствует или пусто в элементе items[${index}]`);
-        }
-        if (item.Duty === undefined) {
-            throw new BadRequestException(`Поле Duty отсутствует в элементе items[${index}]`);
-        }
-        if (!item.Date || item.Date.trim() === '') {
-            throw new BadRequestException(`Поле Date отсутствует или пусто в элементе items[${index}]`);
-        }
-        if (item.Values === undefined) {
-            throw new BadRequestException(`Поле Values отсутствует в элементе items[${index}]`);
-        }
+    connection.createChannel(function (errorCreateChannel, channel) {
+      if (errorCreateChannel) {
+        throw 'Ошибка создания канала: ' + errorCreateChannel;
+      }
+      const queue = 'TmsQueue';
+
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+
+      channel.consume(
+        queue,
+        function (msg: any) {
+          const messageObj = JSON.parse(msg.content.toString());
+          console.log('Сообщение из TmsQueue', messageObj);
+          messageSubject.notifyObservers(messageObj);
+        },
+        {
+          noAck: true,
+        },
+      );
     });
-
-    // Проверка наличия и содержимого поля 'history'
-    if (!dto.history) {
-        throw new BadRequestException('Поле history отсутствует');
-    }
-    if (!dto.history.UserIIN || dto.history.UserIIN.trim() === '') {
-        throw new BadRequestException('Поле UserIIN в history отсутствует или пусто');
-    }
-    if (!dto.history.UserName || dto.history.UserName.trim() === '') {
-        throw new BadRequestException('Поле UserName в history отсутствует или пусто');
-    }
-    if (!dto.history.date || dto.history.date.trim() === '') {
-        throw new BadRequestException('Поле date в history отсутствует или пусто');
-    }
-
-    this.dutydriversService.addHistory(dto.history);
-    return this.dutydriversService.setDutyDrivers(dto.cap, dto.items);
-}
+  },
+);
