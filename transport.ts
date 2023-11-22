94d52010b8a0a79a4baa010677510665183dfd79
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-22 12:33:40 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,30 +1,36 @@
-        this.actionHistoryService.addCorrectionTransportRequestHistory({
-          name: dto.userName,
-          time: dto.timeDelivery,
-          comment: dto.comment,
-          date: dto.dateCreated
-        });
-  async addCorrectionTransportRequestHistory({ name, time, comment, date }: { name: string; time: string; comment: string; date: string; }) {
-    let userAction = await this.historyModel.findOne({ name }).exec();
+async getCouriersByDate(day: string) {
+  // ... ваш текущий код ...
 
-    if (!userAction) {
-      userAction = new this.historyModel({ name, transportRequestHistory: [], callHistory: [] , DutyDriversHistory: [] });
-    }
-
-    userAction.transportRequestHistory.push({ time, comment, date });
-    await userAction.save();
-    return { status: 'success', message: 'Корректировка транспортной заявки успешно записана в историю' };
-  }
+  // Очистка строки поиска
+  this.$emit('clearSearch');
+}
 
 
 
-Argument of type '{ time: string; comment: string; date: string; }' is not assignable to parameter of type '{ time: string; comment: string; }'.
-  Object literal may only specify known properties, and 'date' does not exist in type '{ time: string; comment: string; }'.ts(2345)
-Translation
-Argument of type '{ time: string; comment: string; date: string; }' is not assignable to parameter of type '{ time: string; comment: string; }'.
-I was expecting { time: string; comment: string; }, but you passed { time: string; comment: string; date: string; }.
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+import IconComponent from '../../../global/icon/icon.component.vue';
 
-Object literal may only specify known properties, and 'date' does not exist in type '{ time: string; comment: string; }'.
-You can't pass property date to type { time: string; comment: string; }.
+const props = defineProps({
+  placeholder: String,
+});
 
-(property) date: string
+const emits = defineEmits(['onSearch']);
+const searchValue = ref('');
+
+const updateSearch = (event: Event) => {
+  const value = (event.target as HTMLInputElement).value;
+  searchValue.value = value;
+  emits('onSearch', value);
+};
+
+// Функция для очистки строки поиска
+const clearSearch = () => {
+  searchValue.value = '';
+};
+
+// Устанавливаем слушатель событий
+onMounted(() => {
+  this.$on('clearSearch', clearSearch);
+});
+</script>
