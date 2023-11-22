8e3e9996bd15404291c8a9195c3b87e351f8ae48
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-22 09:27:17 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,56 +1,14 @@
-  @Post('/correction')
-  async setDutyDrivers(@Body() dto: CorrectionDto) {
-    this.dutydriversService.addHistory(dto.history);
-    return this.dutydriversService.setDutyDrivers(dto.cap, dto.items);
+  @Post('/stats')
+  async getDriversStats(@Body() dto: GetDriversStatsDto) {
+      if (!dto.date) {
+          throw new BadRequestException('Поле date отсутствует');
+      }
+      if (!dto.city) {
+          throw new BadRequestException('Поле city отсутствует');
+      }
+      return this.driversService.getDriversStatsByDate(dto.date, dto.city);
   }
 
-
-import { IsNotEmpty, IsString } from 'class-validator';
-
-class CapDto {
-    @IsNotEmpty()
-    @IsString()
-    DateDoc: string;
-  }
-  
-  class ItemDto {
-    @IsNotEmpty()
-    @IsString()
-    Driver: string;
-
-    @IsNotEmpty()
-    @IsString()
-    Duty: boolean;
-
-    @IsNotEmpty()
-    @IsString()
-    Date: string;
-
-    @IsNotEmpty()
-    @IsString()
-    Values: boolean;
-  }
-  
-  class HistoryDto {
-    @IsNotEmpty()
-    @IsString()
-    UserIIN: string;
-
-    @IsNotEmpty()
-    @IsString()
-    UserName: string;
-    
-    @IsNotEmpty()
-    @IsString()
-    date: string;
-  }
-  
-
-  export class CorrectionDto {
-    cap: CapDto;
-    items: ItemDto[];
-    history: HistoryDto;
-  }
 
 
 Нужно проверять что с фронтенда приходят все эти поля
