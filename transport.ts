7da34b7dda5a833d09396b6fb3e8c2687f5f4b71
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-22 15:20:51 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,151 +1,45 @@
-searchClear(): void {
-  this.model.searchInput = '';
-}
-
-  async getCouriersByDate(day: string) {
-    this.searchClear()
-    const today = new Date();
-
-    if (day === 'tomorrow') {
-      today.setDate(today.getDate() + 1); 
-      this.model.date.isTomorrow = true;
-      this.model.date.isToday = false;
-      this.model.date.isYesterday = false;
-    } else if (day === 'yesterday') {
-      today.setDate(today.getDate() - 1);
-      this.model.date.isTomorrow = false;
-      this.model.date.isToday = false;
-      this.model.date.isYesterday = true;
-    } else if (day === 'today') {
-      this.model.date.isTomorrow = false;
-      this.model.date.isToday = true;
-      this.model.date.isYesterday = false;
-    }
-    const formattedDate = `${today.getFullYear()}-${String(
-      today.getMonth() + 1,
-    ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-    const body = {
-      date: formattedDate,
-    };
-    this.model.currentDate = formattedDate;
-    this.model.couriers = [];
-    this.model.filteredCouriers = [];
-    this.model.cities = [];
-    this.model.currentCity = 'Все города';
-    try {
-      const response = await usePostApi('getCouriersByDate', body);
-      const couriers: ICourier[] = response.map(
-        (item: any): ICourier => ({
-          courierFullName: item.Drivers,
-          carNumber: item.carNumber,
-          hiringType: item.HiringType,
-          schedule: item.TimeWindow,
-          hardWindow: item.HardTimeWindow,
-          returnToWarehouse: item.ReturnWarehouse,
-          city: item.City,
-          goToDetail: '',
-        }),
-      );
-      this.model.couriers = couriers;
-      this.model.filteredCouriers = this.model.couriers;
-      this.model.sortState = 'none';
-      this.model.cities = [
-        ...new Set(this.model.couriers.map((item) => item.city)),
-      ].sort((a, b) => a.localeCompare(b, 'ru'));
-    } catch (error) {
-      throw error;
-    }
-
-  }
-
-Я что заполнил в инпуте 
-затем хочу получить новые данные на страницу и очистить поле поиска вот тут     this.searchClear()
-но он не очищается 
-
-вот компонент поиска 
-<template>
-  <!-- Контейнер для поля поиска и иконки -->
- 
-  <search class="search-container">
-    <div class="icon-wrapper">
-      <IconComponent
-        :сonfig="{
-          name: 'search',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-    <!-- Поле ввода -->
-    <input
-      type="text"
-      data-test="search-input-PPO"
-      :value="searchInput"
-      @input="updateSearch($event)"
-      :placeholder="props.placeholder"
-      class="search-input"
-    />
-  </search>
-
-</template>
-
 <script setup lang="ts">
-import { ref } from 'vue';
+import { watch } from 'vue';
 import IconComponent from '../../../global/icon/icon.component.vue';
-
 
 const props = defineProps({
   searchInput: String,
   placeholder: String
 });
 
-
-// Определяем эмиттеры для событий
 const emits = defineEmits(['updateSearchValue']);
 
-// Состояние для хранения значения поля поиска
+// Локальное состояние для поля ввода
+const inputValue = ref('');
+
+// Отслеживаем изменения searchInput и обновляем inputValue
+watch(() => props.searchInput, (newVal) => {
+  inputValue.value = newVal;
+});
+
 const updateSearch = (event: Event) => {
   const inputElement = event.target as HTMLInputElement;
   if (inputElement) {
     emits('updateSearchValue', inputElement.value);
   }
 };
-
 </script>
 
-<style scoped>
-.search-container {
-  /* Расположение иконки и поля ввода в одной строке */
-  display: flex;
-  align-items: center;
-  position: relative;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  border-radius: 80px;
-  width: 100%;
-  overflow: hidden;
-}
+<template>
+  <search class="search-container">
+    <div class="icon-wrapper">
+      <IconComponent
+        :сonfig="{name: 'search', color: '#23362D4D', width: 24, height: 24}"
+      ></IconComponent>
+    </div>
+    <input
+      type="text"
+      data-test="search-input-PPO"
+      :value="inputValue"
+      @input="updateSearch($event)"
+      :placeholder="props.placeholder"
+      class="search-input"
+    />
+  </search>
+</template>
 
-.icon-wrapper {
-  /* Позиционирование иконки внутри поля ввода */
-  position: absolute;
-  left: 8px; /* Расстояние от левого края поля ввода */
-  z-index: 1; /* Поверх поля ввода */
-}
-
-.search-input {
-  border-radius: 58px;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  background: #fff;
-  border-radius: 16px;
-  padding: 12px 16px;
-  padding-left: 40px; /* Добавляем отступ слева, чтобы текст не перекрывал иконку */
-  font-size: 14px;
-  border: none;
-  outline: transparent;
-  color: hsl(152, 21%, 17%);
-  width: 100%;
-  position: relative;
-  z-index: 0; /* Под иконкой */
-}
-</style>
