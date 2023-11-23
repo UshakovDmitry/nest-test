cfdd27f305b772996b0a85feb8009c1b67309eb5
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-23 12:17:31 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -20,3 +20,41 @@ const clearSearchField = () => {
 
 // ... остальная часть вашего скрипта ...
 </script>
+
+
+
+  <script setup lang="ts">
+// ... остальная часть скрипта ...
+
+const updateSearch = (event: Event) => {
+  const inputElement = event.target as HTMLInputElement;
+  if (inputElement) {
+    emits('input', inputElement.value); // Используйте 'input' для эмита события
+  }
+};
+</script>
+
+
+
+<template>
+  <!-- ... -->
+  <filters-panel-component
+    :config="model.fields[0]"
+    @input="handleInput"
+    <!-- ... остальные пропсы и события ... -->
+  ></filters-panel-component>
+
+  <!-- Вывод текста из поля поиска -->
+  <div>Текст поиска: {{ model.fields[0].input.value }}</div>
+  <!-- ... -->
+</template>
+
+<script setup lang="ts">
+// ... ваш текущий скрипт ...
+
+const handleInput = (newValue) => {
+  model.fields[0].input.value = newValue; // Обновление значения поля поиска
+};
+
+// ... остальная часть вашего скрипта ...
+</script>
