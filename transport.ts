cfb2df992757f8bd9c70599026e0b199ce9e2bf1
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-23 17:45:59 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,23 +1,77 @@
-filterRequestsByContractor(contractor: string): void {
-  let baseRequestsList = this.model.transportRequests;
+<template>
+  <div class="wrapper">
+    <button
+      v-for="(count, contractor) in filterContractors"
+      :key="contractor"
+      class="btn"
+      :class="{ active: activeButton === contractor }"
+      @click="buttonClicked(contractor)"
+    >
+      {{ contractor }} ({{ count }})
+    </button>
+  </div>
+</template>
 
-  // Если уже применен фильтр по городу, используем отфильтрованный список
-  if (this.model.currentCity !== 'Все города' && this.model.currentCity) {
-    baseRequestsList = this.model.filteredTransportRequests;
-  }
+<script setup lang="ts">
+import { ref } from 'vue';
 
-  if (!contractor) { // Если contractor пустая строка, снимаем фильтр
-    this.model.filteredTransportRequests = baseRequestsList;
-  } else if (contractor === "Прочее") {
-    this.model.filteredTransportRequests = baseRequestsList.filter(
-      request => !request.filterContractor || request.filterContractor.trim() === ""
-    );
+defineProps({
+  filterContractors: Object,
+});
+
+const emits = defineEmits(['filterRequestsByContractor']);
+
+const activeButton = ref('');
+
+const buttonClicked = (contractor: string) => {
+  if (activeButton.value === contractor) {
+    activeButton.value = '';
+    emits('filterRequestsByContractor', ''); // Эмитируется событие с пустой строкой, чтобы снять фильтр
   } else {
-    this.model.filteredTransportRequests = baseRequestsList.filter(
-      request => request.filterContractor === contractor
-    );
+    activeButton.value = contractor;
+    emits('filterRequestsByContractor', contractor);
   }
+};
+</script>
 
-  // Обновляем список после фильтрации
-  this.model.lastFilteredTransportRequests = this.model.filteredTransportRequests;
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px;
+  width: 400px;
 }
+
+.btn {
+    font-family: Rubik;
+  font-size: 22px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px;
+  border-radius: 80px;
+  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
+  background: var(--surface-light-mode-background, #fbfbfb);
+  border: 1px solid #e5e5e5;
+  box-sizing: border-box;
+  padding: 6px 16px;
+  font-size: 14px;
+  line-height: 20px;
+  color: #000000;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #eceaea;
+}
+.btn.active {
+  border-radius: 80px;
+  background: var(--primary-dark-mode-main-24, rgba(94, 222, 137, 0.24));
+  box-shadow:
+    0px 2px 6px 2px rgba(204, 204, 204, 0.15),
+    0px 1px 2px 0px rgba(204, 204, 204, 0.3);
+}
+</style>
+
+У меня есть вот такой компонент 
+он не меняет колличество тех или иных элементов при смене города на главной странице
+исправь
