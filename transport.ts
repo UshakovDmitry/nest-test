dc03090b66c835c2045fdea66a33590c0352e3ea
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-23 12:51:07 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,9 +1,17 @@
-  <search-field-component
-        :config="config"
-        :isToday="isToday"
-        :placeholder="placeholder"
-        @onSearch="emits('search', $event)"
-      ></search-field-component>
+<script setup lang="ts">
+import { watch } from 'vue';
+import IconComponent from '../../../global/icon/icon.component.vue';
+
+const { config, isToday } = defineProps({
+  config: Object,
+  isToday: Boolean,
+});
+
+// Отслеживаем изменения в isToday и очищаем поле поиска
+watch(isToday, () => {
+  config.input.value = ''; // Очистка поля поиска
+});
+</script>
 
 <template>
   <!-- Контейнер для поля поиска и иконки -->
@@ -29,61 +37,4 @@
   </search>
 </template>
 
-<script setup lang="ts">
-import IconComponent from '../../../global/icon/icon.component.vue';
-
-// Получение props и определение событий
-const { config } = defineProps({
-  config: Object
-});
-
-const emits = defineEmits(['input', 'onSearch']);
-
-// // Функция для обновления значения поиска и эмита события в родительский компонент
-// const updateSearch = (event: Event) => {
-//   const inputElement = event.target as HTMLInputElement;
-//   if (inputElement) {
-//     emits('input', inputElement.value);
-//   }
-// };
-</script>
-
-<style scoped>
-.search-container {
-  /* Расположение иконки и поля ввода в одной строке */
-  display: flex;
-  align-items: center;
-  position: relative;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  border-radius: 80px;
-  width: 100%;
-  overflow: hidden;
-}
-
-.icon-wrapper {
-  /* Позиционирование иконки внутри поля ввода */
-  position: absolute;
-  left: 8px; /* Расстояние от левого края поля ввода */
-  z-index: 1; /* Поверх поля ввода */
-}
-
-.search-input {
-  border-radius: 58px;
-  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
-  background: #fff;
-  border-radius: 16px;
-  padding: 12px 16px;
-  padding-left: 40px; /* Добавляем отступ слева, чтобы текст не перекрывал иконку */
-  font-size: 14px;
-  border: none;
-  outline: transparent;
-  color: hsl(152, 21%, 17%);
-  width: 100%;
-  position: relative;
-  z-index: 0; /* Под иконкой */
-}
-</style>
-
-
-Я передаю булевое значение isToday
-если оно меняется то я хочу очищать поиск
+<!-- Стили остаются прежними -->
