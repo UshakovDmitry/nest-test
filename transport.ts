f4ebb4e2bd474e90df06ed04d4f21128e6a1c15a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-23 13:42:08 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,18 +1,44 @@
-<script setup lang="ts">
-import { watch } from 'vue';
-import IconComponent from '../../../global/icon/icon.component.vue';
 
-const props = defineProps({
-  config: Object,
-  isToday: Boolean,
-  isYesterday: Boolean,
-  isTomorrow: Boolean,
-});
+const amqp = require('amqplib/callback_api');
+import { Subject } from './observer';
 
-// Отслеживаем изменения в isToday, isYesterday и isTomorrow
-watch([() => props.isToday, () => props.isYesterday, () => props.isTomorrow], () => {
-  props.config.input.value = ''; // Очистка поля поиска при изменении любого из этих значений
-});
+export const messageSubject = new Subject();
 
-const emits = defineEmits(['input', 'onSearch']);
-</script>
+amqp.connect(
+  'amqp://tms:26000567855499290979@rabbitmq.next.local',
+  function (errorConnect, connection) {
+    if (errorConnect) {
+      throw 'Ошибка подключения к rabbitMQ: ' + errorConnect;
+    }
+    connection.createChannel(function (errorCreateChannel, channel) {
+      if (errorCreateChannel) {
+        throw 'Ошибка создания канала: ' + errorCreateChannel;
+      }
+      const queue = 'TmsQueue';
+
+      channel.assertQueue(queue, {
+        durable: true,
+      });
+
+      channel.consume(
+        queue,
+        function (msg: any) {
+          try {
+            const messageObj = JSON.parse(msg.content.toString());
+            console.log('Сообщение из TmsQueue', messageObj);
+            messageSubject.notifyObservers(messageObj);
+          } catch (error) {
+            console.error('Ошибка при обработке сообщения из TmsQueue:', error.message);
+          }
+        },
+        {
+          noAck: true,
+        },
+      );
+    });
+  },
+);
+
+Максимально подробно расскажи про этот код
+как он подключается к очереди и что там за методы
+и как это работает
