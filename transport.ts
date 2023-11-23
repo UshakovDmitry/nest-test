d5d788265ba0abd072f3cb6d12421136eb1ae21a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-23 12:55:10 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,18 +1,3 @@
-<script setup lang="ts">
-import { watch } from 'vue';
-import IconComponent from '../../../global/icon/icon.component.vue';
-
-const { config, isToday } = defineProps({
-  config: Object,
-  isToday: Boolean,
-});
-
-// Отслеживаем изменения в isToday и очищаем поле поиска
-watch(isToday, () => {
-  config.input.value = ''; // Очистка поля поиска
-});
-</script>
-
 <template>
   <!-- Контейнер для поля поиска и иконки -->
   <search class="search-container">
@@ -37,4 +22,86 @@ watch(isToday, () => {
   </search>
 </template>
 
-<!-- Стили остаются прежними -->
+<script setup lang="ts">
+import { watch } from 'vue';
+import IconComponent from '../../../global/icon/icon.component.vue';
+
+// Получение props и определение событий
+const props = defineProps({
+  config: Object,
+  isToday: Boolean,
+});
+
+watch(props.isToday, (newValue) => {
+  config.input.value = ''; // Очистка поля поиска
+});
+
+
+const emits = defineEmits(['input', 'onSearch']);
+
+
+</script>
+
+<style scoped>
+.search-container {
+  /* Расположение иконки и поля ввода в одной строке */
+  display: flex;
+  align-items: center;
+  position: relative;
+  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
+  border-radius: 80px;
+  width: 100%;
+  overflow: hidden;
+}
+
+.icon-wrapper {
+  /* Позиционирование иконки внутри поля ввода */
+  position: absolute;
+  left: 8px; /* Расстояние от левого края поля ввода */
+  z-index: 1; /* Поверх поля ввода */
+}
+
+.search-input {
+  border-radius: 58px;
+  border: 1px solid var(--tertiary-light-mode-border, rgba(35, 54, 45, 0.12));
+  background: #fff;
+  border-radius: 16px;
+  padding: 12px 16px;
+  padding-left: 40px; /* Добавляем отступ слева, чтобы текст не перекрывал иконку */
+  font-size: 14px;
+  border: none;
+  outline: transparent;
+  color: hsl(152, 21%, 17%);
+  width: 100%;
+  position: relative;
+  z-index: 0; /* Под иконкой */
+}
+</style>
+
+
+  No overload matches this call.
+  The last overload gave the following error.ts(2769)
+runtime-core.d.ts(1226, 25): The last overload is declared here.
+const props: {
+    readonly isToday: boolean;
+    readonly config?: Record<string, any>;
+}
+Translation
+No overload matches this call.
+Request a translation for #2769
+
+The last overload gave the following error.
+Request a translation for #2770
+
+
+Cannot find name 'config'.ts(2304)
+any
+Translation
+Cannot find name 'config'.
+I can't find the variable you're trying to access.
+
+
+
+
+
+
