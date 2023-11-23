7040f7e8d676ab89f905a340e4c45eef48d260ac
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-23 12:49:04 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,93 +1,12 @@
-<template>
-  <!-- ... ваш текущий шаблон ... -->
-
-  <!-- Кнопка для очистки поля поиска -->
-  <button @click="clearSearchField">Очистить поиск</button>
-
-  <!-- Вывод текста из поля поиска -->
-  <div>Текст поиска: {{ model.fields[0].input.value }}</div>
-
-  <!-- Ваши другие компоненты ... -->
-</template>
-
-<script setup lang="ts">
-// ... ваш текущий скрипт ...
-
-const clearSearchField = () => {
-  // Обновляем значение поля поиска, устанавливая его в пустую строку
-  model.value.fields[0].input.value = '';
-};
-
-// ... остальная часть вашего скрипта ...
-</script>
-
-
-
-  <script setup lang="ts">
-// ... остальная часть скрипта ...
-
-const updateSearch = (event: Event) => {
-  const inputElement = event.target as HTMLInputElement;
-  if (inputElement) {
-    emits('input', inputElement.value); // Используйте 'input' для эмита события
-  }
-};
-</script>
-
-
+  <search-field-component
+        :config="config"
+        :isToday="isToday"
+        :placeholder="placeholder"
+        @onSearch="emits('search', $event)"
+      ></search-field-component>
 
 <template>
-  <!-- ... -->
-  <filters-panel-component
-    :config="model.fields[0]"
-    @input="handleInput"
-    <!-- ... остальные пропсы и события ... -->
-  ></filters-panel-component>
-
-  <!-- Вывод текста из поля поиска -->
-  <div>Текст поиска: {{ model.fields[0].input.value }}</div>
-  <!-- ... -->
-</template>
-
-<script setup lang="ts">
-// ... ваш текущий скрипт ...
-
-const handleInput = (newValue) => {
-  model.fields[0].input.value = newValue; // Обновление значения поля поиска
-};
-
-// ... остальная часть вашего скрипта ...
-</script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <template>
   <!-- Контейнер для поля поиска и иконки -->
- 
   <search class="search-container">
     <div class="icon-wrapper">
       <IconComponent
@@ -103,64 +22,30 @@ const handleInput = (newValue) => {
     <input
       type="text"
       data-test="search-input-PPO"
-      :value="config.input.value"
-      @input="
-        emits(
-          'input',
-          ($event.target as HTMLInputElement | HTMLTextAreaElement).value,
-        );"
-        :placeholder="config.input.placeholder"
+      v-model="config.input.value"
+      :placeholder="config.input.placeholder"
       class="search-input"
     />
   </search>
-
 </template>
 
 <script setup lang="ts">
 import IconComponent from '../../../global/icon/icon.component.vue';
 
+// Получение props и определение событий
+const { config } = defineProps({
+  config: Object
+});
 
+const emits = defineEmits(['input', 'onSearch']);
 
-
-defineProps<{
-  config: any;
-}>();
-
-
-
-
-const emits = defineEmits(['onSearch','input']);
-
-
-const updateSearch = (event: Event) => {
-  const inputElement = event.target as HTMLInputElement;
-  if (inputElement) {
-    emits('input', inputElement.value); // Используйте 'input' для эмита события
-  }
-};
-
-
-
-
-
-// const props = defineProps<{
-//   placeholder: string;
-// }>();
-// const emits = defineEmits(['onSearch']);
-
-// const searchValue = ref('');
-
+// // Функция для обновления значения поиска и эмита события в родительский компонент
 // const updateSearch = (event: Event) => {
-//   const value = (event.target as HTMLInputElement).value;
-
-//   searchValue.value = value;
-
-//   emits('onSearch', value);
+//   const inputElement = event.target as HTMLInputElement;
+//   if (inputElement) {
+//     emits('input', inputElement.value);
+//   }
 // };
-
-
-
-
 </script>
 
 <style scoped>
@@ -200,69 +85,5 @@ const updateSearch = (event: Event) => {
 </style>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  <template>
-  <!-- Контейнер для поля поиска и иконки -->
-  <search class="search-container">
-    <div class="icon-wrapper">
-      <IconComponent
-        :сonfig="{
-          name: 'search',
-          color: '#23362D4D',
-          width: 24,
-          height: 24,
-        }"
-      ></IconComponent>
-    </div>
-    <!-- Поле ввода -->
-    <input
-      type="text"
-      data-test="search-input-PPO"
-      :value="config.input.value"
-      @input="updateSearch" <!-- Используйте функцию updateSearch здесь -->
-      :placeholder="config.input.placeholder"
-      class="search-input"
-    />
-  </search>
-</template>
-
-<script setup lang="ts">
-import IconComponent from '../../../global/icon/icon.component.vue';
-
-// Получение props и определение событий
-const { config } = defineProps({
-  config: Object
-});
-
-const emits = defineEmits(['input', 'onSearch']);
-
-// Функция для обновления значения поиска и эмита события в родительский компонент
-const updateSearch = (event: Event) => {
-  const inputElement = event.target as HTMLInputElement;
-  if (inputElement) {
-    emits('input', inputElement.value);
-  }
-};
-</script>
-
-<style scoped>
-/* Ваши стили */
-</style>
+Я передаю булевое значение isToday
+если оно меняется то я хочу очищать поиск
