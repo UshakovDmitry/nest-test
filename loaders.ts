08a85a1533b7d1a53f054dd9473d6bd3fea8ed7b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-24 11:06:35 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,31 +1,7 @@
-async getDriversByName(name: string, date: string) {
-    const drivers = await this.dbService.getDriversByName(name, date);
-    const geliosCars = await this.geliosService.getCarLocations(
-      GELIOS_PRO_LOGIN,
-      GELIOS_PRO_PASSWORD,
-    );
-
-    // Устанавливаем пустые координаты для каждого водителя
-    drivers.forEach((driver) => {
-      driver.coordinates = {
-        latitude: '',
-        longitude: '',
-      };
-    });
-
-    geliosCars.forEach((geliosCar) => {
-      const { latitude, longitude } = geliosCar;
-      const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(/\s+/g, '');
-
-      drivers.forEach((driver) => {
-        if (driver.carNumber === carNumberWithoutSpaces) {
-          driver.coordinates = {
-            latitude,
-            longitude,
-          };
-        }
-      });
-    });
-    return drivers;
-}
-
+const props = defineProps({
+  config: Object,
+  isToday: Boolean,
+  isYesterday: Boolean,
+  isTomorrow: Boolean,
+  currentCity: String,
+});
