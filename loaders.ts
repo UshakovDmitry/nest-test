61982f37c639276e50388972cf9d62a50e6ddadf
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-24 09:26:31 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -1,16 +1,21 @@
-  async getDriversByName(name: string, date: string) {
+async getDriversByName(name: string, date: string) {
     const drivers = await this.dbService.getDriversByName(name, date);
     const geliosCars = await this.geliosService.getCarLocations(
       GELIOS_PRO_LOGIN,
       GELIOS_PRO_PASSWORD,
     );
 
+    // Устанавливаем пустые координаты для каждого водителя
+    drivers.forEach((driver) => {
+      driver.coordinates = {
+        latitude: '',
+        longitude: '',
+      };
+    });
+
     geliosCars.forEach((geliosCar) => {
       const { latitude, longitude } = geliosCar;
-      const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(
-        /\s+/g,
-        '',
-      );
+      const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(/\s+/g, '');
 
       drivers.forEach((driver) => {
         if (driver.carNumber === carNumberWithoutSpaces) {
@@ -18,21 +23,9 @@
             latitude,
             longitude,
           };
-        } else {
-          driver.coordinates = {
-            latitude: '',
-            longitude: '',
-          };
         }
       });
     });
     return drivers;
-  }
+}
 
-тут я хочу если даже если это ксловие не пройдет  if (driver.carNumber === carNumberWithoutSpaces)
-то все рвано присвоит для каждого элемента  driver.coordinates = {
-            latitude: '',
-            longitude: '',
-          }; с  пустыми строками
-
-верно описана логика?
