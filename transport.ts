65d6528ed28fe26524b6b7f5419fd989919ca867
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-27 15:33:47 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,3 +1,88 @@
+<template>
+  <div class="chronologies__wrapper">
+    <h3 class="chronologies__title">Хронология ППО</h3>
+
+    <div class="chronologies">
+      <div class="chronologies__item" v-for="(statusPPO, index) in chronologies" :key="index">
+        <p class="numberPPO">{{ statusPPO.PPO }}</p>
+
+        <div class="chronologies-wrapper">
+          <div class="item__border" v-for="(item, index) in statusPPO.Chronology" :key="index">
+            <p>{{ item.TypeOperation }}</p>
+
+            <ul class="default__chronologies">
+              <li class="default__chronologies-status" :class="{ 'status--active': isStatusInChronology(status, item.statuses) }" v-for="status in defaultStatusesPPO" :key="status">{{ status }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, PropType } from 'vue';
+
+interface ChronologyItem {
+  PPO: string;
+  Chronology: {
+    TypeOperation: string;
+    statuses: string[];
+  }[];
+}
+
+const props = defineProps({
+  chronologies: {
+    type: Array as PropType<ChronologyItem[]>,
+    required: true,
+  },
+  defaultStatusesPPO: {
+    type: Array as PropType<string[]>,
+    required: true,
+  },
+  currentStatusTR: {
+    type: String,
+    required: true,
+  },
+});
+
+const isStatusInChronology = (status, statuses) => {
+  return statuses.includes(status);
+};
+</script>
+
+<style scoped>
+  /* Ваши стили */
+  .default__chronologies-status.status--active {
+    color: #259451; /* Зеленый цвет для активных статусов */
+  }
+</style>
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 [
     {
         "PPO": "8942965",
