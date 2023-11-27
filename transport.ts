5f79fa8b11251651b75e015d71f15db8cb5c404f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-27 16:18:13 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,81 +1,26 @@
-<template>
-  <div class="chronologies__wrapper">
-    <h3 class="chronologies__title">Хронология ППО</h3>
+[
+    {
+        "PPO": "8945070",
+        "Chronology": [
+            {
+                "TypeOperation": "Самовывоз",
+                "statuses": [
+                    "Ожидает клиента"
+                ]
+            },
+            {
+                "TypeOperation": "Доставка",
+                "statuses": [
+                    "Доставляется до клиента (на складе отгрузки)"
+                ]
+            }
+        ]
+    }
+]
 
-    <div class="chronologies">
-      <div class="chronologies__item" v-for="(statusPPO, index) in chronologies" :key="index">
-        <p class="numberPPO">{{ statusPPO.PPO }}</p>
 
-        <div class="chronologies-wrapper">
-          <div class="item__border" v-for="(item, index) in statusPPO.Chronology" :key="index">
-            <p>{{ item.TypeOperation }}</p>
-
-            <ul class="default__chronologies">
-              <li class="default__chronologies-status" :class="{ 'status--active': isStatusInChronology(status, item.statuses) }" v-for="status in defaultStatusesPPO" :key="status">
-                {{ status }}
-                <div class="status-bar" :class="{ 'bar--active': isStatusInChronology(status, item.statuses) }"></div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
-
-<script setup lang="ts">
-import { defineProps, PropType } from 'vue';
-
-interface ChronologyItem {
-  PPO: string;
-  Chronology: {
-    TypeOperation: string;
-    statuses: string[];
-  }[];
-}
-
-const props = defineProps({
-  chronologies: {
-    type: Array as PropType<ChronologyItem[]>,
-    required: true,
-  },
-  defaultStatusesPPO: {
-    type: Array as PropType<string[]>,
-    required: true,
-  },
-  currentStatusTR: {
-    type: String,
-    required: true,
-  },
-});
-
-const isStatusInChronology = (status, statuses) => {
-  return statuses.includes(status);
-};
-</script>
-
-<style scoped>
-  .default__chronologies-status {
-    color: grey; /* Серый цвет по умолчанию */
-    position: relative; /* Для позиционирования полоски */
-    padding-bottom: 16px; /* Отступ для полоски */
-  }
-
-  .default__chronologies-status.status--active {
-    color: #259451; /* Зеленый цвет для активных статусов */
-  }
-
-  .status-bar {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 12px;
-    background-color: grey; /* Серый цвет полоски */
-    border-radius: 6px; /* Закругленные углы */
-  }
-
-  .status-bar.bar--active {
-    background-color: #259451; /* Зеленый цвет для активной полоски */
-  }
-</style>
+теперь приходят вот такие данные 
+и я нашел проблему
+у меня отображается хронология и для Самовывоз и для Доставка
+а должна отображаться одна хронология для  "PPO": "8945070" из дефолтных статусов с той же логикой 
+нужно смотреть внуть "statuses" 
