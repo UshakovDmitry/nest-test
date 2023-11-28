698e13b1afc0a498a48ed60a62da0db7be59c6fe
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 17:55:03 +0600

    Update message.ts

--- message.ts
+++ message.ts
@@ -13,12 +13,9 @@ const dateSelection = ref();
 // Отслеживание изменений в isToday, isYesterday, и isTomorrow
 watch(
   [() => props.isToday, () => props.isYesterday, () => props.isTomorrow],
-  (newValues, prevValues) => {
-    const [newIsToday, newIsYesterday, newIsTomorrow] = newValues;
-    const [prevIsToday, prevIsYesterday, prevIsTomorrow] = prevValues;
-
-    // Проверка, изменилось ли любое из свойств
-    if (newIsToday !== prevIsToday || newIsYesterday !== prevIsYesterday || newIsTomorrow !== prevIsTomorrow) {
+  ([newIsToday, newIsYesterday, newIsTomorrow]) => {
+    // Проверка, стало ли одно из свойств true
+    if (newIsToday || newIsYesterday || newIsTomorrow) {
       dateSelection.value = null; // Сбрасываем выбор дат
       emits('clearDateSelection'); // Вызываем событие для сброса даты в viewModel, если необходимо
     }
