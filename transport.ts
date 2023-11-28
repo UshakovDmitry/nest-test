a223fcfe2ba4a481475bbba1af66046834a4cd77
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 10:22:10 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,70 +1,48 @@
-<template>
-  <div class="chronologies__wrapper">
-    <h3 class="chronologies__title">Хронология ППО</h3>
+  async getDriversByDate(date: string) {
+    const drivers = await this.dbService.getDriversByDate(date);
+    const geliosCars = await this.geliosService.getCarLocations(
+      GELIOS_PRO_LOGIN,
+      GELIOS_PRO_PASSWORD,
+    );
 
-    <div class="chronologies">
-      <div class="chronologies__item" v-for="(statusPPO, index) in chronologies" :key="index">
-        <p class="numberPPO">{{ statusPPO.PPO }}</p>
 
-        <div class="chronologies-wrapper">
-          <div class="item__border" v-if="statusPPO.Chronology.length">
-            <p>{{ findDeliveryOperation(statusPPO.Chronology).TypeOperation }}</p>
+      
 
-            <ul class="default__chronologies">
-              <li class="default__chronologies-status" 
-                  :class="{ 'status--active': isStatusActive(status, statusPPO.Chronology) }" 
-                  v-for="status in defaultStatusesPPO" 
-                  :key="status">
-                {{ status }}
-                <div class="status-bar" 
-                     :class="{ 'bar--active': isStatusActive(status, statusPPO.Chronology) }">
-                </div>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
+    geliosCars.forEach((geliosCar) => {
+      const { latitude, longitude } = geliosCar;
+      
+      const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(
+        /\s+/g,
+        '',
+      );
 
-<script setup lang="ts">
-import { defineProps, PropType } from 'vue';
 
-interface ChronologyItem {
-  PPO: string;
-  Chronology: {
-    TypeOperation: string;
-    statuses: string[];
-  }[];
-}
 
-const props = defineProps({
-  chronologies: {
-    type: Array as PropType<ChronologyItem[]>,
-    required: true,
-  },
-  defaultStatusesPPO: {
-    type: Array as PropType<string[]>,
-    required: true,
-  },
-});
-
-const findDeliveryOperation = (chronology) => {
-  return chronology.find((item) => item.TypeOperation === "Доставка") || {};
-};
-
-const isStatusActive = (status, chronology) => {
-  const delivery = findDeliveryOperation(chronology);
-  const hasPickup = chronology.some(item => item.TypeOperation === "Самовывоз");
-  
-  if (status === "доставляется" && hasPickup) {
-    return true;
+      drivers.forEach((driver) => {
+     
+        
+        if (driver.carNumber === carNumberWithoutSpaces) {
+          driver.coordinates = {
+            latitude,
+            longitude,
+          };
+        } else {
+          driver.coordinates = {
+            latitude: '',
+            longitude: '',
+          };
+        }
+      });
+    });
+    return drivers;
   }
-  return delivery.statuses && delivery.statuses.includes(status);
-};
-</script>
 
-<style scoped>
-  /* Ваши стили */
-</style>
+я хочу присваивать значение координат при совпадении автомобиля
+в ином случае присваивать пустые строки к этому элемиенту
+я добавил  else {
+          driver.coordinates = {
+            latitude: '',
+            longitude: '',
+          };
+        }
+и они все стали пустыми строками
