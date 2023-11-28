fe718aac1ec10f65d331bbd90b76efdadee632ba
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 12:47:41 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,161 +1,137 @@
+[plugin:vite:import-analysis] Failed to resolve import "../../components/default_modals/no-access.component.vue" from "src\pages\auth\auth.component.vue". Does the file exist?
+C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/src/pages/auth/auth.component.vue:45:21
+5  |  import { AuthViewModel } from "./auth.viewmodel";
+6  |  import NotValid from "../../components/default_modals/not-valid.component.vue";
+7  |  import NoAccess from "../../components/default_modals/no-access.component.vue";
+   |                        ^
+8  |  import Field from "../../components/global/fields/fieild/field.vue";
+9  |  import { FieldModel } from "../../components/global/fields/fieild/field.model";
+    at formatError (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:43993:46)
+    at TransformContext.error (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:43989:19)
+    at normalizeUrl (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:41801:33)
+    at process.processTicksAndRejections (node:internal/process/task_queues:95:5)
+    at async file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:41945:47
+    at async Promise.all (index 6)
+    at async TransformContext.transform (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:41870:13)
+    at async Object.transform (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:44283:30)
+    at async loadAndTransform (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:54950:29)
+    at async viteTransformMiddleware (file:///C:/Users/ushakov.dmitriy/Desktop/alser.dispatcherworkplaceui/frontend/node_modules/vite/dist/node/chunks/dep-df561101.js:64345:32
+Click outside, press Esc key, or fix the code to dismiss.
+You can also disable this overlay by setting server.hmr.overlay to false in vite.config.js.
+
+<template>
+  <div class="auth">
+    <div class="auth__content">
+      <img :src="logo" alt="logo" />
+      <Field
+        :config="model.fields[0]"
+        @input="viewModel.setEmail($event)"
+        @on-enter="viewModel.handleLogin()"
+      ></Field>
+      <Field
+        :config="model.fields[1]"
+        @input="viewModel.setPassword($event)"
+        @on-enter="viewModel.handleLogin()"
+      ></Field>
+      <div>
+        <ButtonComponent
+          :config="{
+            type: 'filled',
+            color: 'green',
+            value: 'Войти',
+            border: 'large',
+            width: '350px',
+            disabled: model.email.length === 0 || model.password.length === 0,
+          }"
+          @on-click="viewModel.handleLogin()"
+        ></ButtonComponent>
+      </div>
+      <not-valid 
+      v-if="model.isModalError"
+      @close="viewModel.closeErrorModal()"
+      ></not-valid>
+      <no-access 
+      v-if="model.isErrorNoAccess"
+      @close="viewModel.closeErrorModal()"
+      ></no-access>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import logo from '../../public/icons/logo.svg';
+import ButtonComponent from '../../components/global/button/button.vue';
 import { AuthModel } from './auth.model';
-import { IUser } from '../../domain/interfaces/user.interface';
-import { User } from '../../domain/entities/user';
-import { tokenCRUDService } from '../../domain/services/tokenCRUD.service';
-import { userCRUDService } from '../../domain/services/userCRUD.service';
-import { useGetApi } from '../../domain/services/getHTTP.service';
-import { usePostApi } from '../../domain/services/postHTTP.service';
-import { storeProviderSetValue } from '../../domain/providers/store.provider';
+import { AuthViewModel } from './auth.viewmodel';
+import NotValid from '../../components/default_modals/not-valid.component.vue';
+import NoAccess from '../../components/default_modals/no-access.component.vue';
+import Field from '../../components/global/fields/fieild/field.vue';
+import { FieldModel } from '../../components/global/fields/fieild/field.model';
+import { ref, Ref } from 'vue';
 
-import router from '../../router';
-export class AuthViewModel {
-  model: AuthModel;
-
-  constructor(model: AuthModel) {
-    this.model = model;
-  }
-
-  async authorize(): Promise<boolean> {
-    console.log(this.model.email, this.model.password);
-    
-    try {
-      const formData = new FormData();
-      formData.append('client_id', 'DispatcherWorkplace');
-      formData.append('client_secret', 'secret');
-      formData.append('grant_type', 'password');
-      formData.append('username', this.model.email);
-      formData.append('password', this.model.password);
-      formData.append('scope', 'myAPIs');
-
-      const response = await fetch('https://auth3.next.local/connect/token', {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        const data = await response.json();
-        this.saveToken(data.access_token);
-        const dataToken = this.parseJwt(data.access_token);
-        console.log(dataToken, 'dataToken');
-        
-        const transformDataToken: IUser = this.transformDataToken(dataToken);
-        this.saveUser(transformDataToken);
-        router.push('/dashboard');
-        this.model.email = '';
-        this.model.password = '';
-        return true;
-      }else {
-        const errorData = await response.json();
-        // Получение тела ответа в формате JSON
-        
-        const errorMessage = this.validateError(errorData); // Получение сообщения об ошибке с помощью функции validateError
-        this.model.isModalError = true;
-        this.model.errorMsg = errorMessage;
-        console.error(errorMessage); // Вывод сообщения об ошибке в консоль
-        
-        return false;
-      }
-    } catch (error: Error | any) {
-      console.error('Ошибка при авторизации:', error);
-      return false;
-    }
-  }
-
-
-  validateError = (errorData: any) => {
-    switch (errorData.errorDescription) {
-      case 'invalid_username_or_password':
-        return 'Не валидный логин или пароль';
-      
-      // Добавьте дополнительные case для обработки других возможных ошибок, если необходимо
-      
-      default:
-        return 'Произошла неизвестная ошибка'; // Сообщение по умолчанию для ошибок, которые не обрабатываются явно
-    }
-  };
-
-  closeErrorModal () {
-    this.model.isModalError = false;
-  }
-
-  saveToken(token) {
-    tokenCRUDService.createToken(token);
-  }
-  saveUser(user: IUser) {
-    userCRUDService.createUser(user);
-  }
-
-  transformDataToken(dataToken: any) {
-    let position = '';
-    if (Array.isArray(dataToken.website) && dataToken.website.length > 0) {
-      position = dataToken.website.join(', ');
-    }
-  
-    // Проверка наличия свойства role в токене
-    const roleClaim = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
-    if (!dataToken[roleClaim]) {
-      throw new Error('В токене отсутствует свойство role, что означает отсутствие роли у пользователя');
-        }
-  
-    const user: IUser = {
-      fullName: `${dataToken.family_name} ${dataToken.given_name}`,
-      position: position,
-      iin: dataToken.profile,
-      role: dataToken[roleClaim], // Добавление свойства role
-    };
-    return user;
-  }
-  setEmail(value: string) {
-    this.model.email = value;
-  }
-  setPassword(value: string) {
-    this.model.password = value;
-  }
-
-  handleLogin = () => {
-    // Проверка полей на правильность заполнения
-    let allFieldsValid = true;
-    this.model.fields.forEach((field) => {
-      if (field.isEmpty()) {
-        allFieldsValid = false;
-      }
-    });
-
-    // Если все поля заполнены верно, продолжаем вход
-    if (allFieldsValid) {
-      this.authorize();
-    }
-  };
-
-  parseJwt(token) {
-    try {
-      // Получить payload токена
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-
-      // Декодировать payload
-      const jsonPayload = decodeURIComponent(
-        atob(base64)
-          .split('')
-          .map(function (c) {
-            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-          })
-          .join(''),
-      );
-
-      return JSON.parse(jsonPayload);
-    } catch (error) {
-      console.error('Ошибка при разборе токена:', error);
-      return null;
-    }
-  }
+const model: Ref<AuthModel> = ref(
+  new AuthModel({
+    fields: [
+      new FieldModel({
+        label: 'Email',
+        input: {
+          type: 'email',
+          value: '',
+          placeholder: 'Введите Email',
+          isError: false,
+          isDisabled: false,
+          isTextarea: false,
+          required: true,
+          maxLength: 20,
+        },
+        helper: {
+          isActive: true,
+          value: 'test',
+        },
+      }),
+      new FieldModel({
+        label: 'Пароль',
+        input: {
+          type: 'password',
+          value: '',
+          placeholder: 'Введите пароль',
+          isDisabled: false,
+          isTextarea: false,
+          required: true,
+          maxLength: 20,
+          isError: false,
+        },
+        helper: {
+          isActive: true,
+          value: 'test',
+        },
+      }),
+    ],
+  }),
+);
+const viewModel: Ref<AuthViewModel> = ref(new AuthViewModel(model.value));
+</script>
+<style scoped>
+.auth {
+  background: linear-gradient(108deg, #01a254 0%, #50d17f 100%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+  /* margin-left: -72px;
+  margin-top: -107px; */
+  box-sizing: border-box;
 }
-
-
-Я хочу если срабатывает это условие 
-if (!dataToken[roleClaim]) {
-      throw new Error('В токене отсутствует свойство role, что означает отсутствие роли у пользователя');
-        }
-то выдавать ошибку авторизации и запрещать дальнейшее продвижение пользователя в приложении 
-также хочу чтобы this.model.isErrorNoAccess = true;
-то 
+.auth__content {
+  display: flex;
+  width: 420px;
+  padding: 36px;
+  flex-direction: column;
+  align-items: center;
+  gap: 11px;
+  flex-shrink: 0;
+  border-radius: 16px;
+  box-sizing: border-box;
+  background: #fff;
+}
+</style>
