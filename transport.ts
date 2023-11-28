dd05813d2db7430d3f149da2f3e0a88043337005
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 17:29:40 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,23 +1,11 @@
-class TransportRequestsViewModel {
-  // ... остальные свойства и методы ...
-
-  setDateSelection(timeRange) {
-    const formattedDates = timeRange.map(date => date.toISOString().split('T')[0]);
-    this.getTimeRange(formattedDates);
-    // Вы можете добавить здесь любую другую логику, связанную с выбором даты
-  }
-
-  // ... другие методы ...
-}
+      <VueDatePicker
+        v-model="dateSelection"
+        locale="ru"
+        range
+        :format="'dd.MM'"
+        :placeholder="'Выберите диапазон дат'"
+        class="date_picker"
+        @update:model-value="handleDate"
+      ></VueDatePicker>
 
 
-
-<VueDatePicker
-  v-model="dateSelection"
-  locale="ru"
-  range
-  :format="'dd.MM'"
-  :placeholder="'Выберите диапазон дат'"
-  class="date_picker"
-  @update:model-value="emits('setDateSelection', $event)"
-></VueDatePicker>
