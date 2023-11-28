eda42bb4c8c198925646913cac6392af1e9a69bf
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 12:28:52 +0600

    Update message.ts

--- message.ts
+++ message.ts
@@ -1,148 +1,20 @@
-rabbitmq.service
-
-import { Injectable } from '@nestjs/common';
-import {
-  MessagePattern,
-  Payload,
-  Ctx,
-  RmqContext,
-} from '@nestjs/microservices';
-import { MessageService } from '../message/message.service';
-
-@Injectable()
-export class RabbitMQService {
-  constructor(private messageService: MessageService) {}
-
-  @MessagePattern()
-  async handleData(@Payload() data: any, @Ctx() context: RmqContext) {
-    const channel = context.getChannelRef();
-    const originalMsg = context.getMessage();
-    console.log('Сообщение получено:', data);
-    try {
-      await this.messageService.create(data);
-      console.log('Сообщение сохранено');
-      channel.ack(originalMsg);
-    } catch (error) {
-      console.error('Ошибка при сохранении', error);
-    }
+transformDataToken(dataToken: any) {
+  let position = '';
+  if (Array.isArray(dataToken.website) && dataToken.website.length > 0) {
+    position = dataToken.website.join(', ');
   }
+
+  // Проверка наличия свойства role в токене
+  const roleClaim = "http://schemas.microsoft.com/ws/2008/06/identity/claims/role";
+  if (!dataToken[roleClaim]) {
+    throw new Error("Отсутствует свойство role в токене");
+  }
+
+  const user: IUser = {
+    fullName: `${dataToken.family_name} ${dataToken.given_name}`,
+    position: position,
+    iin: dataToken.profile,
+    role: dataToken[roleClaim], // Добавление свойства role
+  };
+  return user;
 }
-
-
-rabbitmq.module
-import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { RabbitMQService } from './rabbitmq.service';
-import { MessageService } from '../message/message.service';
-import { RabbitMQController } from './rabbitmq.controller';
-
-@Module({
-  imports: [
-    ClientsModule.register([
-      {
-        name: 'RABBITMQ_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
-          queue: 'TmsQueue',
-          queueOptions: { durable: true },
-        },
-      },
-    ]),
-  ],
-  providers: [RabbitMQService, MessageService, RabbitMQController],
-})
-export class RabbitMQModule {}
-
-app.module
-import { Module } from '@nestjs/common';
-import { RabbitMQService } from './rabbitmq/rabbitmq.service';
-import { RabbitMQController } from './rabbitmq/rabbitmq.controller';
-import { MessageService } from './message/message.service';
-import { MessageSchema } from './message/message.shema';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { connectMongoose } from './connect-mongoose';
-
-@Module({
-  imports: [
-    MongooseModule.forRoot(connectMongoose()),
-    MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }]),
-    ClientsModule.register([
-      {
-        name: 'RABBITMQ_SERVICE',
-        transport: Transport.RMQ,
-        options: {
-          urls: ['amqp://tms:26000567855499290979@rabbitmq.next.local'],
-          queue: 'TmsQueue',
-          queueOptions: {
-            durable: true,
-          },
-        },
-      },
-    ]),
-  ],
-  controllers: [RabbitMQController],
-  providers: [RabbitMQService, MessageService],
-})
-export class AppModule {}
-
-
-main.ts
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as basicAuth from 'express-basic-auth';
-import * as compression from 'compression';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { config } from 'dotenv';
-
-// Инициализация dotenv
-config();
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.RMQ,
-    options: {
-      urls: [process.env.RABBITMQ_URL || ''],
-      queue: 'TmsQueue',
-      queueOptions: {
-        durable: true,
-      },
-    },
-  });
-
-  // SWAGGER CONFIGURATION
-  app.use(
-    ['/swagger', '/swagger-stats'],
-    basicAuth({
-      challenge: true,
-      users: {
-        [process.env.SWAGGER_USER || '']: process.env.SWAGGER_PASSWORD || '',
-      },
-    }),
-  );
-  const config = new DocumentBuilder()
-    .setTitle('Описание всех контроллеров REST API')
-    .setDescription(
-      'Внимание! Некоторые методы могут изменять данные в базе данных!',
-    )
-    .setVersion('1.0')
-    .addTag('API для TMS')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
-
-  app.enableCors();
-  app.use(compression());
-
-  await app.startAllMicroservices();
-  console.log('Microservices started');
-
-  const port = parseInt(process.env.PORT || '4000', 10);
-  await app.listen(port);
-  console.log(`Application is listening on port ${port}`);
-}
-bootstrap();
