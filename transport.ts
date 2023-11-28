bce745daf461802fc146bfc5b437c17432d435f2
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 10:25:03 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,48 +1,31 @@
-  async getDriversByDate(date: string) {
-    const drivers = await this.dbService.getDriversByDate(date);
-    const geliosCars = await this.geliosService.getCarLocations(
-      GELIOS_PRO_LOGIN,
-      GELIOS_PRO_PASSWORD,
-    );
+async getDriversByDate(date: string) {
+  const drivers = await this.dbService.getDriversByDate(date);
+  const geliosCars = await this.geliosService.getCarLocations(
+    GELIOS_PRO_LOGIN,
+    GELIOS_PRO_PASSWORD,
+  );
 
+  // Инициализация координат водителей
+  drivers.forEach(driver => {
+    driver.coordinates = {
+      latitude: '',
+      longitude: '',
+    };
+  });
 
-      
+  geliosCars.forEach(geliosCar => {
+    const { latitude, longitude } = geliosCar;
+    const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(/\s+/g, '');
 
-    geliosCars.forEach((geliosCar) => {
-      const { latitude, longitude } = geliosCar;
-      
-      const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(
-        /\s+/g,
-        '',
-      );
-
-
-
-      drivers.forEach((driver) => {
-     
-        
-        if (driver.carNumber === carNumberWithoutSpaces) {
-          driver.coordinates = {
-            latitude,
-            longitude,
-          };
-        } else {
-          driver.coordinates = {
-            latitude: '',
-            longitude: '',
-          };
-        }
-      });
+    drivers.forEach(driver => {
+      if (driver.carNumber.replace(/\s+/g, '') === carNumberWithoutSpaces) {
+        driver.coordinates = {
+          latitude,
+          longitude,
+        };
+      }
     });
-    return drivers;
-  }
+  });
 
-я хочу присваивать значение координат при совпадении автомобиля
-в ином случае присваивать пустые строки к этому элемиенту
-я добавил  else {
-          driver.coordinates = {
-            latitude: '',
-            longitude: '',
-          };
-        }
-и они все стали пустыми строками
+  return drivers;
+}
