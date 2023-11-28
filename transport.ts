ca1d4d94327e557cf06f6b874cf9a4472697fd27
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 11:15:44 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,15 +1,18 @@
-  search(value: string) {
-    console.log(this.model.transportRequests, 'заявки');
-    
-    if (this.model.currentCity !== 'Все города' && this.model.currentCity !== '') {
+search(value: string) {
+  console.log(this.model.transportRequests, 'заявки');
+
+  const searchQuery = value.toLowerCase();
+
+  if (this.model.currentCity !== 'Все города' && this.model.currentCity !== '') {
     this.model.filteredTransportRequests = this.model.lastFilteredTransportRequests.filter(item => {
-      return item.request.number.toLowerCase().includes(value.toLowerCase());
+      return item.request.number.toLowerCase().includes(searchQuery) ||
+             item.document.numberPPO.toLowerCase().includes(searchQuery);
     });
   } else {
     this.model.filteredTransportRequests = this.model.transportRequests.filter(item => {
-      return item.request.number.toLowerCase().includes(value.toLowerCase());
+      return item.request.number.toLowerCase().includes(searchQuery) ||
+             item.document.numberPPO.toLowerCase().includes(searchQuery);
     });
   }
 }
 
-Добавь также поиск помимо item.request.number еще и item.document.numberPPO
