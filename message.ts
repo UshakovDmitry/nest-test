8f9127a5729fa921661678f8874b067c211686c3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 15:16:06 +0600

    Update message.ts

--- message.ts
+++ message.ts
@@ -1,36 +1,8 @@
-<script setup lang="ts">
-import { ref, watch } from 'vue';
-// ... остальные импорты ...
-
-const props = defineProps({
-  // ... ваши пропсы ...
-  isToday: Boolean,
-  isYesterday: Boolean,
-  isTomorrow: Boolean,
-});
-
-const dateSelection = ref();
-
-// Отслеживание изменений в isYesterday, isToday, isTomorrow
-watch(
-  [() => props.isYesterday, () => props.isToday, () => props.isTomorrow],
-  ([newIsYesterday, newIsToday, newIsTomorrow], [oldIsYesterday, oldIsToday, oldIsTomorrow]) => {
-    if (newIsYesterday !== oldIsYesterday || newIsToday !== oldIsToday || newIsTomorrow !== oldIsTomorrow) {
-      dateSelection.value = null;
-    }
+// Отслеживание изменений в dateSelection
+watch(dateSelection, (newValue, oldValue) => {
+  if (newValue !== oldValue) {
+    if (props.isToday) props.isToday = false;
+    if (props.isYesterday) props.isYesterday = false;
+    if (props.isTomorrow) props.isTomorrow = false;
   }
-);
-
-const handleDate = (timeRange) => {
-  dateSelection.value = timeRange;
-  const formattedDates = timeRange.map(
-    (date) => date.toISOString().split('T')[0],
-  );
-  emits('setTimeRange', formattedDates);
-};
-
-// ... остальной код ...
-</script>
-
-я хочу сбрасывать dateSelection когда меняются значения isYesterday,isToday , isTomorrow
-сделай это через watch
+});
