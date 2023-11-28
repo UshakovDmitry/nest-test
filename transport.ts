17c7c310de64d6a18fbd4d54be1fa96dbe6419ea
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-28 11:46:17 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,120 +1,35 @@
-  async getTransportRequests(day: string): Promise<void> {
-    const today = new Date();
-
-    if (day === 'tomorrow') {
-      today.setDate(today.getDate() + 1);
-      this.model.isTomorrow = true;
-      this.model.isToday = false;
-      this.model.isYesterday = false;
-    } else if (day === 'yesterday') {
-      today.setDate(today.getDate() - 1);
-      this.model.isTomorrow = false;
-      this.model.isToday = false;
-      this.model.isYesterday = true;
-    } else if (day === 'today') {
-      this.model.isTomorrow = false;
-      this.model.isToday = true;
-      this.model.isYesterday = false;
-    }
-
-    const formattedDate = `${today.getFullYear()}-${String(
-      today.getMonth() + 1,
-    ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
-    const body = {
-      date: formattedDate,
-    };
-    
-    const response = await usePostApi('getTransportRequestsByDate', body);
-    console.log(response.length, 'кол-во заявок');
-    // console.log(response, 'response');
-    this.model.transportRequests = [];
-    this.model.filterContractors = {};
-    response.forEach((data: any) => {
-      const transformedData = this.transformToTransportRequest(data);
-      const city = this.setCitiesList(transformedData);
-      // Проверяем наличие города в списке и добавляем, если его нет
-      if (!this.model.cities.includes(city)) {
-        this.model.cities.push(city);
-      }
-      if (!this.model.cities.includes("Все города")) {
-        this.model.cities.unshift("Все города");
-      }
-    
-      
-
-      const contractor = data.FilterContractor
-        ? data.FilterContractor
-        : 'Прочее';
-      if (this.model.filterContractors[contractor]) {
-        this.model.filterContractors[contractor] += 1;
-      } else {
-        this.model.filterContractors[contractor] = 1;
-      }
-      const transformedDataForTable =
-        this.transformToTransportForTable(transformedData);
-
-      this.model.transportRequests.unshift(transformedDataForTable);
-      this.model.filteredTransportRequests = this.model.transportRequests;
-    });
-    
-  }
-
-
-  filterRequestsByContractor(contractor: string): void {
-    let baseRequestsList = this.model.transportRequests;
-  
-    // Если уже применен фильтр по городу, используем отфильтрованный список
-    if (this.model.currentCity !== 'Все города' && this.model.currentCity) {
-      baseRequestsList = this.model.filteredTransportRequests;
-    }
-  
-    if (!contractor) { // Если contractor пустая строка, снимаем фильтр
-      this.model.filteredTransportRequests = baseRequestsList;
-    } else if (contractor === "Прочее") {
-      this.model.filteredTransportRequests = baseRequestsList.filter(
-        request => !request.filterContractor || request.filterContractor.trim() === ""
-      );
-    } else {
-      this.model.filteredTransportRequests = baseRequestsList.filter(
-        request => request.filterContractor === contractor
-      );
-    }
-  
-    // Обновляем список после фильтрации
-    this.model.lastFilteredTransportRequests = this.model.filteredTransportRequests;
-  }
-
-при загрузке данных я выполняю следующие действия 
- const contractor = data.FilterContractor
-        ? data.FilterContractor
-        : 'Прочее';
-      if (this.model.filterContractors[contractor]) {
-        this.model.filterContractors[contractor] += 1;
-      } else {
-        this.model.filterContractors[contractor] = 1;
-      }
-
-а за тем использую это в компоненте функцией filterRequestsByContractor
-
-
-Но есть проблема 
-при смене города эти данные не обновляются 
-вот функция смены города
-я хочу чтобы был реализован пересчет при смене города
-
 filterRequestsByCity(city: string): void {
-    this.model.currentCity = city;
-    if (city === 'Все города') {
-      this.model.filteredTransportRequests = this.model.transportRequests;
-    } else {
-      this.model.filteredTransportRequests = this.model.transportRequests.filter(
-        request => {
-          const addressParts = request.deliveryAddress.address.split(',');
-          const requestCity = addressParts[0]?.trim(); 
-          return requestCity === city;
-        }
-      );
-    }
-        // Сохраняем состояние последнего примененного фильтра
-        this.model.lastFilteredTransportRequests = this.model.filteredTransportRequests;
+  this.model.currentCity = city;
+  if (city === 'Все города') {
+    this.model.filteredTransportRequests = this.model.transportRequests;
+  } else {
+    this.model.filteredTransportRequests = this.model.transportRequests.filter(
+      request => {
+        const addressParts = request.deliveryAddress.address.split(',');
+        const requestCity = addressParts[0]?.trim(); 
+        return requestCity === city;
+      }
+    );
   }
+
+  // Сохраняем состояние последнего примененного фильтра
+  this.model.lastFilteredTransportRequests = this.model.filteredTransportRequests;
+  
+  // Обновляем данные о подрядчиках
+  this.updateContractorsData();
+}
+
+updateContractorsData(): void {
+  // Сбрасываем текущие данные о подрядчиках
+  this.model.filterContractors = {};
+
+  // Пересчитываем данные на основе отфильтрованного списка заявок
+  this.model.filteredTransportRequests.forEach(request => {
+    const contractor = request.filterContractor ? request.filterContractor : 'Прочее';
+    if (this.model.filterContractors[contractor]) {
+      this.model.filterContractors[contractor] += 1;
+    } else {
+      this.model.filterContractors[contractor] = 1;
+    }
+  });
+}
