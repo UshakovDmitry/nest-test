3f8a2c030dc40a2821df9a674a9e5a9795f31d02
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-29 12:24:27 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,35 +1,35 @@
-  async getDriversByDate(date: string) {
-    console.log('date', date);
-    
-    const allDrivers = await this.getAllDrivers()
-    const drivers = await this.updateDriversWithExternalData(allDrivers)
+async getDriversByDate(date: string) {
+  console.log('date', date);
+  
+  const allDrivers = await this.getAllDrivers();
+  
+  // Получение сегодняшней даты в нужном формате
+  const today = new Date();
+  const formattedToday = `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
 
-    const filteredDrivers = drivers
-      .map((driver) => {
-        const filteredRequests = driver.transportRequests.filter(
-          (transportRequest) => {
-            return (
-              transportRequest.contactInformation.Date_Time_delivery.split(
-                ' ',
-              )[0] === date
-            );
-          },
-        );
-
-        return { ...driver, transportRequests: filteredRequests };
-      })
-      .filter((driver) => driver.transportRequests.length > 0);
-
-    const updatedDrivers = this.setCountOrdersStatus(filteredDrivers, date);
-    return updatedDrivers;
+  // Обновление данных водителей, если переданная дата совпадает с сегодняшней
+  let drivers = allDrivers;
+  if (date === formattedToday) {
+    drivers = await this.updateDriversWithExternalData(allDrivers);
   }
 
-мне нужно кое-что поменять 
-мне приходит дата в функцию 
-я хочу выполнять  const drivers = await this.updateDriversWithExternalData(allDrivers)
-только тогда когда дата равна сегодняшнему дню
-во всех остальных случаях я не хочу выполнять updateDriversWithExternalData 
+  const filteredDrivers = drivers
+    .map((driver) => {
+      const filteredRequests = driver.transportRequests.filter(
+        (transportRequest) => {
+          return (
+            transportRequest.contactInformation.Date_Time_delivery.split(
+              ' ',
+            )[0] === date
+          );
+        },
+      );
 
-вот в таком виде приходит дата год-месяц-число
-date 2023-11-30
+      return { ...driver, transportRequests: filteredRequests };
+    })
+    .filter((driver) => driver.transportRequests.length > 0);
+
+  const updatedDrivers = this.setCountOrdersStatus(filteredDrivers, date);
+  return updatedDrivers;
+}
 
