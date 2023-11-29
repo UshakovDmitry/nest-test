591a029fd4d1390243ca78d08bec25f061831764
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-29 11:01:19 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -6,7 +5,8 @@ import { MessageDocument } from '../schemas/message.shema';
 import { DBService } from '../db/db.service';
 import { GeliosService } from '../gelios/gelios.service';
 import { GELIOS_PRO_LOGIN, GELIOS_PRO_PASSWORD } from '../config';
-import e from 'express';
+import axios from 'axios';
+
 @Injectable()
 export class DriversService {
   constructor(
@@ -15,72 +15,31 @@ export class DriversService {
     private readonly geliosService: GeliosService,
   ) {}
 
-  async getDrivers() {
-    return await this.dbService.getAllDrivers();
-  }
+  // Остальные методы...
 
+  async getDriversByDate(date: string) {
+    let drivers = await this.dbService.getDriversByDate(date);
 
-// Не приходят координаты
-async getDriversByDate(date: string) {
-  const drivers = await this.dbService.getDriversByDate(date);
-console.log(drivers, 'drivers');
-
-
-
-  const geliosCars = await this.geliosService.getCarLocations(
-    GELIOS_PRO_LOGIN,
-    GELIOS_PRO_PASSWORD,
-  );
-
-  // Инициализация координат водителей
-  drivers.forEach(driver => {
-    driver.coordinates = {
-      latitude: '',
-      longitude: '',
-    };
-  });
-
-  geliosCars.forEach(geliosCar => {
-    const { latitude, longitude } = geliosCar;
-    const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(/\s+/g, '');
-
-    drivers.forEach(driver => {
-      if (driver.carNumber.replace(/\s+/g, '') === carNumberWithoutSpaces) {
-        driver.coordinates = {
-          latitude,
-          longitude,
-        };
-      }
-    });
-  });
-
-  return drivers;
-}
-
-
-
-  async getDriversByName(name: string, date: string) {
-    const drivers = await this.dbService.getDriversByName(name, date);
+    // Получение данных о местоположении автомобилей
     const geliosCars = await this.geliosService.getCarLocations(
       GELIOS_PRO_LOGIN,
       GELIOS_PRO_PASSWORD,
     );
 
-    // Устанавливаем пустые координаты для каждого водителя
-    drivers.forEach((driver) => {
+    // Инициализация координат водителей
+    drivers.forEach(driver => {
       driver.coordinates = {
         latitude: '',
         longitude: '',
       };
     });
 
-    geliosCars.forEach((geliosCar) => {
+    geliosCars.forEach(geliosCar => {
       const { latitude, longitude } = geliosCar;
       const carNumberWithoutSpaces = geliosCar.info.numberPlate.replace(/\s+/g, '');
 
-      drivers.forEach((driver) => {
-     
-        if (driver.carNumber === carNumberWithoutSpaces) {
+      drivers.forEach(driver => {
+        if (driver.carNumber.replace(/\s+/g, '') === carNumberWithoutSpaces) {
           driver.coordinates = {
             latitude,
             longitude,
@@ -88,10 +47,33 @@ console.log(drivers, 'drivers');
         }
       });
     });
-    return drivers;
-}
 
-  async getDriversStatsByDate(date: string, city: string) {
-    return await this.dbService.getDriversStatsByDate(date, city);
+    // Получение и обновление данных водителей из внешнего источника
+    drivers = await this.updateDriversWithExternalData(drivers);
+
+    return drivers;
+  }
+
+  // Добавленная функция
+  async updateDriversWithExternalData(drivers) {
+    try {
+      const response = await axios.get('http://10.0.1.20/1CHS/hs/Yandex_Go/ListDiliveryBlocked');
+      const externalData = response.data.data;
+
+      drivers.forEach(driver => {
+        const matchedDriver = externalData.find(externalDriver => 
+          externalDriver.DriverName.trim() === driver.driver.trim()
+        );
+
+        if (matchedDriver && matchedDriver.CarModel && matchedDriver.RegistrationNumber) {
+          driver.carModel = matchedDriver.CarModel;
+          driver.carNumber = matchedDriver.RegistrationNumber;
+        }
+      });
+    } catch (error) {
+      console.error('Ошибка при получении данных из внешнего источника:', error);
+    }
+
+    return drivers;
   }
 }
