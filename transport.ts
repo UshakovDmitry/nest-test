ed9e240e11d3c2a057bc5d5cc16fb9a46059a06b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-30 16:06:41 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,13 +1,24 @@
-<div v-else-if="config.value === ''" class="default">
-    <div>
-      <IconComponent
-        :сonfig="{
-          name: 'cancel',
-          color: 'red',
-          width: 22,
-          height: 22,
-        }"
-      ></IconComponent>
-    </div>
-    <p class="cell__text-default">Без статуса</p>
-  </div>
+функуия которая отображает только те заявке которые совпадают с тем что выбрал позьзователь
+
+  filterTableByCity(city: string): void {
+
+    this.model.currentCity = city;
+    if (city === 'Все города') {
+      this.model.filteredCouriers = this.model.couriers;
+      return;
+    }
+    this.model.filteredCouriers = this.model.couriers.filter(
+      (item) => item.city === city,
+    );
+  }
+
+также есть функция для поиска 
+  search(value: string): void {
+    this.model.filteredCouriers = this.model.couriers.filter((item) => {
+      return item.courierFullName.toLowerCase().includes(value.toLowerCase());
+    });
+  }
+
+
+и проблема в том что когда я выбираю город и отображается только те данные которые совпадают с городо
+а поиск осуществляется по всем данным и не учитывает 
