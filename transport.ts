10f219e1cbd9f937df188d042bc8a86d7bf3d49b
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-30 14:54:54 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -61,23 +61,23 @@ async getTransportRequestsByDateRange(startDate: string, endDate: string): Promi
   const start = new Date(startDate);
   const end = new Date(endDate);
 
-  // Установка времени на начало дня (00:00:00 UTC)
-  start.setUTCHours(0, 0, 0, 0);
-  end.setUTCHours(23, 59, 59, 999); // Установка времени на конец дня (23:59:59 UTC)
+  // Установка времени на начало и конец дня для учета всего диапазона дат
+  start.setUTCHours(0, 0, 0, 0); // начало дня для startDate
+  end.setUTCHours(23, 59, 59, 999); // конец дня для endDate
 
-  // Поиск заявок в базе данных, где Date_Time_delivery находится в диапазоне между startDate и endDate
-  return await this.messageModel
-    .find({
-      'ContactInformation.Date_Time_delivery': {
-        $gte: start.toISOString(),
-        $lte: end.toISOString(),
-      },
-    })
-    .exec();
+  // Преобразование объектов Date обратно в строки для поиска в базе данных
+  const formattedStartDate = start.toISOString().split('T')[0];
+  const formattedEndDate = end.toISOString().split('T')[0];
+
+  // Логирование для отладки
+  console.log('Ищем заявки с:', formattedStartDate, 'по', formattedEndDate);
+
+  // Поиск заявок в базе данных в указанном диапазоне дат
+  return await this.messageModel.find({
+    'ContactInformation.Date_Time_delivery': {
+      $gte: formattedStartDate,
+      $lte: formattedEndDate,
+    },
+  }).exec();
 }
 
-
-
-проблема в том что если я пришлю startDate: 2023-11-22
-endDate: 2023-11-24 то он не учитывает 22 число
-я хочу чтобы он включал эту дату (startDate) в логику поиска данных
