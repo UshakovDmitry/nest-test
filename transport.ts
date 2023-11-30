c1a52b6426103488275f368bb6976e22fa7f8a5f
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-30 11:22:45 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,5 +1,28 @@
-console.log('Before sorting', this.model.filteredTransportRequests.map(item => item.request.status));
-this.model.filteredTransportRequests.sort((a, b) => {
-  return a[propertyToSortBy] === b[propertyToSortBy] ? 0 : a[propertyToSortBy] ? -1 : 1;
-});
-console.log('After sorting', this.model.filteredTransportRequests.map(item => item.request.status));
+sortRequests(index) {
+  const propertyToSortBy = this.getPropertyByIndex(index);
+
+  if (this.model.sortState === 'none' || this.model.sortState === 'descending') {
+    this.model.sortState = 'ascending';
+    // Логика сортировки по возрастанию
+  } else if (this.model.sortState === 'ascending') {
+    this.model.sortState = 'descending';
+    // Логика сортировки по убыванию
+  }
+  
+  // Осуществление сортировки в зависимости от состояния
+  this.model.filteredTransportRequests.sort((a, b) => {
+    if (propertyToSortBy === 'request.status') {
+      if (this.model.sortState === 'ascending') {
+        return (a.request.status === b.request.status) ? 0 : a.request.status ? 1 : -1;
+      } else {
+        return (a.request.status === b.request.status) ? 0 : a.request.status ? -1 : 1;
+      }
+    } else {
+      if (this.model.sortState === 'ascending') {
+        return (a[propertyToSortBy] < b[propertyToSortBy]) ? -1 : (a[propertyToSortBy] > b[propertyToSortBy]) ? 1 : 0;
+      } else {
+        return (a[propertyToSortBy] > b[propertyToSortBy]) ? -1 : (a[propertyToSortBy] < b[propertyToSortBy]) ? 1 : 0;
+      }
+    }
+  });
+}
