99bdb29de014c0edbec7b1ea0aa88cd93dac1b02
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2023-11-30 14:25:28 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -49,6 +49,35 @@ export class GetTransportRequestsByDateRangeDto {
 
 
 Этот эндпоинт возвращает данные за период времени
+
+
+
+
+
+
+
+async getTransportRequestsByDateRange(startDate: string, endDate: string): Promise<any[]> {
+  // Преобразование дат из строки в объект Date
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  // Установка времени на начало дня (00:00:00 UTC)
+  start.setUTCHours(0, 0, 0, 0);
+  end.setUTCHours(23, 59, 59, 999); // Установка времени на конец дня (23:59:59 UTC)
+
+  // Поиск заявок в базе данных, где Date_Time_delivery находится в диапазоне между startDate и endDate
+  return await this.messageModel
+    .find({
+      'ContactInformation.Date_Time_delivery': {
+        $gte: start.toISOString(),
+        $lte: end.toISOString(),
+      },
+    })
+    .exec();
+}
+
+
+
 проблема в том что если я пришлю startDate: 2023-11-22
 endDate: 2023-11-24 то он не учитывает 22 число
 я хочу чтобы он включал эту дату (startDate) в логику поиска данных
