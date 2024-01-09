d507c7560cfd565eb8f2e11dc60ececfef7c072a
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-09 11:50:59 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,6 +1,6 @@
-export enum methodsMapEnum {
-    GET = "get",
-    SET = "set",
-    REMOVE = "remove",
-    CLEAR = "clear",
-}
+export const methodsMapEnum = {
+    GET: "get",
+    SET: "set",
+    REMOVE: "remove",
+    CLEAR: "clear",
+} as const;
