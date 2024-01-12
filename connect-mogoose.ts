100acbe782b42f8af7c53db5708ed3f24e709415
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-12 12:32:16 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -1,16 +1,25 @@
-router.beforeEach(async (to, from, next) => {
-  const tokenLocalStorage = localStorage.getItem('token');
-  const tokenSessionStorage = sessionStorage.getItem('token');
+export const headersMapEnum = {
+  HEADER_AUTHORIZATION : "authorization",
+  HEADER_LANGUAGE :"language",
+  HEADER_LOCATION_ID : "location-id",
+} as const;
 
-  const isAuthenticated = tokenLocalStorage || tokenSessionStorage;
 
-  if (to.name !== 'login' && !isAuthenticated) {
-    return next({ name: 'login' });
-  }
+//FIXME: token
+const tokenLocalStorage = localStorage.getItem("token");
+const tokenSessionStorage = sessionStorage.getItem("token");
+const token = tokenLocalStorage || tokenSessionStorage;
+const parsedToken = token ? JSON.parse(token) : null;
 
-  if (to.name === 'login' && isAuthenticated) {
-    return next({ name: 'orders' });
-  }
 
-  next();
-});
+export const headersMap = new Map([
+  [headersMapEnum.HEADER_AUTHORIZATION, `Bearer ${JSON.parse(parsedToken)}`],
+  [headersMapEnum.HEADER_LANGUAGE, "ru"],
+  [headersMapEnum.HEADER_LOCATION_ID, "8"],
+]);
+
+
+мне нужно присваивать токент в заголовках 
+он может лежать либо в локальном либо в сессион хранилище
+также его надо парсить напиши правильною логику учитывая все варианты
+код должен быть максимально проффесиональным и использовать последние возможности JS чтобы не рефакторить
