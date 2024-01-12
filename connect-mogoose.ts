0a5821e1e015e7c10abe5ccfd44c9de5a8150d97
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-12 13:58:55 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -4,14 +4,20 @@ router.beforeEach(async (to, from, next) => {
 
   const isAuth = tokenLocalStorage || tokenSessionStorage;
 
-  if (to.name !== 'login' && to.name !== 'singup' && !isAuth) {
+  // Проверка, находится ли пользователь на странице login или signup
+  const isOnAuthPages = to.name === 'login' || to.name === 'signup';
+
+  if (!isAuth && !isOnAuthPages) {
+    // Если пользователь не авторизован и не на страницах login или signup, перенаправляем на login
     return next({ name: 'login' });
   }
 
-  if (to.name === 'login' && isAuth) {
+  if (isOnAuthPages && isAuth) {
+    // Если пользователь авторизован и пытается зайти на страницу login или signup, перенаправляем на orders
     return next({ name: 'orders' });
   }
 
+  // В остальных случаях пропускаем пользователя на запрашиваемый роут
   next();
 });
 
