eae6127b82dd99860d02c51762cf977651509357
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-15 17:24:12 +0600

    Update loaders.ts

--- loaders.ts
+++ loaders.ts
@@ -6,16 +6,19 @@
       :label="'Имя'"
       :required="true"
       :disabled="false"
-      :value="user_name"
-      :hint-text="hint_user_name"
+      :value="name"
+      :hint-text="''"
+      @onChange="(value: string) => name = value"
     ></alser-default-field>
     <alser-default-field
       :placeholder="'Фамилия'"
       :label="'Фамилия'"
       :required="true"
       :disabled="false"
-      :value="user_surname"
+      :value="surname"
       :hint-text="''"
+      @onChange="(value: string) => surname = value"
+      
     ></alser-default-field>
   </div>
     <div class="flex-row">
@@ -26,6 +29,7 @@
         :disabled="false"
         :value="phone_number"
         :hint-text="''"
+        @onChange="(value: string) => phone_number = value"
       ></alser-tel-field>
       <alser-email-field
         :placeholder="'user@email.com'"
@@ -33,7 +37,8 @@
         :required="true"
         :disabled="false"
         :value="email"
-        :hint-text="''"
+        :hint-text="hint_email"
+        @onChange="(value: string) => email = value"
       ></alser-email-field>
     </div>
     <alser-default-field
@@ -43,6 +48,7 @@
       :disabled="false"
       :value="job_title"
       :hint-text="''"
+      @onChange="(value: string) => job_title = value"
     ></alser-default-field>
 
     <div>
@@ -58,6 +64,7 @@
       :disabled="false"
       :value="''"
       :hint-text="''"
+      @onChange="(value: string) => password = value"
     ></alser-password-field>
     <alser-password-field
       :placeholder="'Повторите пароль'"
@@ -65,6 +72,7 @@
       :disabled="false"
       :value="''"
       :hint-text="''"
+      @onChange="(value: string) => password_confirmation = value"
     ></alser-password-field>
   </div>
 </template>
@@ -78,19 +86,29 @@ import { AlserTelField } from "alser.vue.library";
 import { AlserEmailField } from "alser.vue.library";
 //@ts-ignore
 import { AlserPasswordField } from "alser.vue.library";
-import { computed, ref } from "vue";
+import { computed, ref, watch } from "vue";
 
-const user_name = ref('');
-const user_surname = ref('');
+const name = ref('');
+const surname = ref('');
 const phone_number = ref('');
 const email = ref('');
 const job_title = ref('');
+const password = ref('');
+const password_confirmation = ref('');
 
-const hint_user_name = computed(() => {
-  if (user_name.value.length > 0 && user_name.value.length < 12) {
-    return 'Имя должно быть длиннее 12 символов';
-  }
-  return '';
+
+
+const hint_email = computed(() => {
+if (email.value.length > 0 && !email.value.includes("@")) {
+  return "Неверный формат email";
+}
+return "";
+});
+
+watch([name, surname, phone_number, email, job_title, password, password_confirmation], () => {
+ if (name.value.length > 0 && surname.value.length > 0 && phone_number.value.length > 0 && email.value.length > 0 && job_title.value.length > 0 && password.value.length > 0 && password_confirmation.value.length > 0) {
+   console.log('all fields are filled');
+ }
 });
 
 </script>
