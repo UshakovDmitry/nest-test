3fad57fc76d681b92ca8c9f5b9c4cb03919befb4
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-16 12:24:43 +0600

    Update transport.ts

--- transport.ts
+++ transport.ts
@@ -1,30 +1,40 @@
-    <div class="form-component__body">
+<div class="form-component__body">
+    <transition name="fade">
+      <company-fields-step v-if="_first_step"></company-fields-step>
+    </transition>
+    
+    <transition name="slide">
+      <person-fields-step v-if="_second_step"></person-fields-step>
+    </transition>
 
-      <company-fields-step
-        v-if="_first_step && !_second_step && !_third_step"
-        @changeBIN="(value:string) => bin = value"
-        @changeCompanyName="(value:string) => company_name = value"
-        @changeLegalAddress="(value:string) => legal_address = value"
-        @changeBIC="(value:string) => bic = value"
-        @changeBankName="(value:string) =>bank_name = value"
-        @changeIBAN="(value:string) => iban = value"
-        @changeCBE="(value:string) => cbe = value"
-        @changeDirectorName="(value:string) => director_name = value"
-        @changeDirectorPhoneNumber="(value:string) => director_phone_number = value"
-        @validate="(value:boolean) => btn_state_step1 = value"
-      ></company-fields-step>
-      
-      <person-fields-step
-        v-if="_second_step && !_first_step && !_third_step"
-        @changeUserName="(value:string) => user_name = value"
-        @changeUserSurname="(value:string) => user_surname = value"
-        @changePhoneNumber="(value:string) => phone_number = value"
-        @changeEmail="(value:string) => email = value"
-        @changeJobTitle="(value:string) => job_title = value"
-        @changePassword="(value:string) => password = value"
-        @changePasswordConfirmation="(value:string) => password_confirmation = value"
-        @validForm="(value:boolean) => btn_step2 = value"
-      ></person-fields-step>
+    <transition name="zoom">
+      <success-step v-if="_third_step"></success-step>
+    </transition>
+</div>
 
-      <success-step v-if="_third_step && !_first_step && !_second_step"></success-step>
-    </div>
+
+
+/* Общие стили перехода */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */ {
+  opacity: 0;
+}
+
+.slide-enter-active, .slide-leave-active {
+  transition: transform 0.5s;
+}
+.slide
+-enter, .slide-leave-to {
+transform: translateY(20px);
+opacity: 0;
+}
+
+.zoom-enter-active, .zoom-leave-active {
+transition: transform 0.5s, opacity 0.5s;
+}
+.zoom-enter, .zoom-leave-to {
+transform: scale(0.5);
+opacity: 0;
+}
