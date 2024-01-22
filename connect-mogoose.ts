a7c13189b94436b5f814ecf133913d7d98f3a7f3
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-22 10:23:12 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -1,24 +1,167 @@
-router.beforeEach(async (to, from, next) => {
-  const tokenLocalStorage = localStorage.getItem('token');
-  const tokenSessionStorage = sessionStorage.getItem('token');
+<template>
+  <div class="alser-ui-library-field">
+    <label v-if="label" class="label">
+      <p
+        class="lable__text"
+        :class="{
+          lable__text_required: disabled,
+        }"
+      >
+        {{ label }}
+      </p>
+      <p v-if="required" class="label__mark">*</p>
+    </label>
 
-  const isAuth = tokenLocalStorage || tokenSessionStorage;
+    <div
+      class="input-wrapper"
+      :class="{
+        'input-wrapper_error': errorText,
+        'input-wrapper_disabled': disabled,
+      }"
+    >
+      <div class="base-input">
+        <BaseInput
+          :type="type"
+          :placeholder="placeholder"
+          :required="required"
+          :minLength="minLength"
+          :maxLength="maxLength"
+          :disabled="disabled"
+          :readonly="readonly"
+          :icon="icon"
+          @onChange="emits('onChange', $event)"
+        ></BaseInput>
+      </div>
+      <div class="button-wrapper">
+        <button class="field-btn">Button</button>
+      </div>
+    </div>
 
-  // Проверка, находится ли пользователь на странице login или signup
-  const isOnAuthPages = to.name === 'login' || to.name === 'signup';
+    <span :class="errorText ? 'error-text' : 'hidden-span'">{{
+      errorText
+    }}</span>
+    <span :class="hintText ? 'hint-text' : 'hidden-span'">{{ hintText }}</span>
+  </div>
+</template>
 
-  if (!isAuth && !isOnAuthPages) {
-    // Если пользователь не авторизован и не на страницах login или signup, перенаправляем на login
-    return next({ name: 'login' });
+<script setup lang="ts">
+  import BaseInput from '@/components/BaseInput.vue'
+
+  defineSlots<{
+    iconPrefix?: (props) => any
+    iconSufix?: (props) => any
+  }>()
+
+  defineProps({
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    required: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    minLength: {
+      type: Number,
+      required: false,
+    },
+    maxLength: {
+      type: Number,
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+    readonly: {
+      type: Boolean,
+      required: false,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    type: {
+      type: String,
+      required: false,
+      default: 'text',
+    },
+    errorText: {
+      type: String,
+      required: false,
+      default: '',
+    },
+    hintText: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  })
+
+  const emits = defineEmits(['onChange'])
+</script>
+
+<style scoped>
+  .input-wrapper {
+    display: flex;
+    align-items: stretch;
+    justify-content: space-between;
+    padding-left: 16px;
+    /* padding: var(--spacing-md, 8px) var(--spacing-lg, 12px); */
+    border-radius: 16px;
+    /* gap: 0.5rem; */
+    border: 1px solid var(--Tertiary-LightMode-Border, rgba(35, 54, 45, 0.12));
+    background: #fff;
+    overflow: hidden;
   }
 
-  if (isOnAuthPages && isAuth) {
-    // Если пользователь авторизован и пытается зайти на страницу login или signup, перенаправляем на orders
-    return next({ name: 'orders' });
+  .base-input {
+    flex: 1;
+    border: none;
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    padding: 8px 12px 8px 0;
   }
 
-  // В остальных случаях пропускаем пользователя на запрашиваемый роут
-  next();
-});
+  .button-wrapper {
+    display: flex;
+    align-items: stretch;
+    justify-content: stretch;
+    min-width: 119px;
+    width: 15%;
+    border: none;
+    background: var(--bg-secondary, #f8f9fa);
+    border-left: 1px solid var(--border-tertiary, #e6e9e6);
+    color: #fff;
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+  }
 
-export default router;
+  .field-btn {
+    width: 100%;
+    border: none;
+    background: var(--bg-secondary, #f8f9fa);
+    border-left: 1px solid var(--border-tertiary, #e6e9e6);
+    color: #fff;
+    cursor: pointer;
+    margin: 0;
+    padding: 0;
+    color: black;
+    outline: none;
+  }
+
+  .field-btn:focus  {
+    border: 1px solid  #46e646;
+    background: var(--colors-palette-basic-white, #fff) !important;
+    box-shadow: var(--focus-rings-ring-error) !important;
+  }
