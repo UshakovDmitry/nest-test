dc37ca217aea4e0dc7d07ae6e6c6153e9e807995
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-01-24 11:51:22 +0600

    Update connect-mogoose.ts

--- connect-mogoose.ts
+++ connect-mogoose.ts
@@ -35,3 +35,103 @@ import FooterComponent from "./ui/layout/footer.vue";
 }
 </style>
 
+
+
+
+///////////
+
+import { createRouter, createWebHistory, RouteRecordRaw, } from "vue-router";
+
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "catalog",
+    component: () => import("@/ui/pages/catalog/сatalog.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("@/ui/pages/login/login.vue"),
+  },
+  {
+    path: "/signup",
+    name: "signup",
+    component: () => import("@/ui/pages/sign-up/sign-up.vue"),
+  },
+  {
+    path: "/orders",
+    name: "orders",
+    component: () => import("@/ui/pages/orders/orders.vue"),
+  },
+  {
+    path: "/service",
+    name: "service",
+    component: () => import("@/ui/pages/service/service.vue"),
+  },
+  {
+    path: "/promotions",
+    name: "promotions",
+    component: () => import("@/ui/pages/promotions/promotions.vue"),
+  },
+
+  {
+    path: '/compare',
+    name: 'compare',
+    component: () => import("@/ui/pages/compare/compare.vue"),
+
+  },
+  {
+    path: "/favorites",
+    name: "favorites",
+    component: () => import("@/ui/pages/favorites/favorites.vue"),
+  },
+  {
+    path: "/cart",
+    name: "cart",
+    component: () => import("@/ui/pages/cart/cart.vue"),
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: () => import("@/ui/pages/profile/profile.vue"),
+  },
+
+
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+  linkActiveClass: 'router-link-active',
+});
+
+// FIXME: update
+router.beforeEach(async (to, from, next) => {
+  console.log(from);
+  
+  const tokenLocalStorage = localStorage.getItem('token');
+  const tokenSessionStorage = sessionStorage.getItem('token');
+
+  const isAuth = tokenLocalStorage || tokenSessionStorage;
+
+  // Проверка, находится ли пользователь на странице login или signup
+  const isOnAuthPages = to.name === 'login' || to.name === 'signup';
+
+  if (!isAuth && !isOnAuthPages) {
+    // Если пользователь не авторизован и не на страницах login или signup, перенаправляем на login
+    return next({ name: 'login' });
+  }
+
+  if (isOnAuthPages && isAuth) {
+    // Если пользователь авторизован и пытается зайти на страницу login или signup, перенаправляем на orders
+    return next({ name: 'catalog' });
+  }
+
+  // В остальных случаях пропускаем пользователя на запрашиваемый роут
+  next();
+});
+
+export default router;
+
+
