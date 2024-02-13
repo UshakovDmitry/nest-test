713ab2c48fe1362f73193760b301b52521238bbc
Ushakov Dmitry <86829183+UshakovDmitry@users.noreply.github.com>
2024-02-13 10:00:21 +0600

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,48 +1,148 @@
-d69d6c2d8e494d47aac84ef650f2cd23
-ixiMAWaXXOQ4-ZCx-F4zmlTjLil2-iFqeL76NRK3
+<template>
+  <input
+    class="base-input"
+    v-model="value"
+    :type="type"
+    :placeholder="placeholder"
+    :required="required"
+    :minLength="minLength"
+    :maxlength="maxLength"
+    :disabled="disabled"
+    :readonly="readonly"
+    autocomplete="off"
+    @input="onChange"
+    @blur="emits('blur', $event)"
+    @focus="emits('focus', $event)"
+  />
+</template>
 
-f1h2ajZshnZu-m5GnMGYT9zhz-o2XS-k_e6XSsK4
+<script setup lang="ts">
+  import { ref } from 'vue'
+  defineProps({
+    type: {
+      type: String,
+      required: false,
+      default: 'text',
+    },
+    placeholder: {
+      type: String,
+      required: false,
+    },
+    required: {
+      type: Boolean,
+      required: false,
+    },
+    minLength: {
+      type: Number,
+      required: false,
+    },
+    maxLength: {
+      type: Number,
+      required: false,
+    },
+    disabled: {
+      type: Boolean,
+      required: false,
+    },
+    readonly: {
+      type: Boolean,
+      required: false,
+    },
+    label: {
+      type: String,
+      required: false,
+      default: '',
+    },
+  })
 
-  @Post('correction')
-  async transportRequestcorrection(@Body() dto: TransportRequestCorrectionDto ) {
-    return this.transportRequestsService.transportRequestCorrection(dto: TransportRequestCorrectionDto);
+  const emits = defineEmits(['onChange','blur','focus'])
+
+  const value = ref('')
+
+  const onChange = (): void => {
+    emits('onChange', value.value)
+  }
+</script>
+<style scoped>
+  /* TODO Некоторые браузеры могут игнорировать autocomplete="off" из соображений удобства пользователя или безопасности. */
+  .base-input {
+    border: none;
+    outline: none;
+    background-color: transparent;
+    width: 100%;
+    /* height: 100%; */
+    font-family: 'Rubik';
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px; /* 150% */
+    letter-spacing: 0.4px;
+
+    /* Стили для type="password" и type="email" */
+    &[type='password'],
+    &[type='email'] {
+      /* Удаление вебкит-специфичных стилей для password и email типов */
+      &::-webkit-outer-spin-button,
+      &::-webkit-inner-spin-button,
+      &::-webkit-clear-button {
+        -webkit-appearance: none;
+        margin: 0;
+      }
+
+      /* Фикс для Firefox */
+      &::-moz-placeholder {
+        opacity: 1; 
+      }
+
+      /* Стили для IE */
+      &::-ms-clear,
+      &::-ms-reveal {
+        display: none;
+        width: 0;
+        height: 0;
+      }
+    }
+
+    /* Для type="search" */
+    &::-webkit-search-cancel-button {
+      -webkit-appearance: none; /* Убрать стандартный вид кнопки */
+      appearance: none; /* Для всех браузеров поддерживающих стандарт */
+      margin: 0; /* Удалить любые внешние отступы */
+    }
+
+    /* Удаление дополнительных элементов декорации в полях поиска в браузерах WebKit */
+    &::-webkit-search-decoration,
+    &::-webkit-search-results-button,
+    &::-webkit-search-results-decoration {
+      display: none; /* Скрыть элементы */
+    }
+
+    /* Удаление кнопки очистки поля в Internet Explorer и Microsoft Edge */
+    &::-ms-clear,
+    &::-ms-reveal {
+      display: none; /* Скрыть кнопку */
+      width: 0; /* Обнулить ширину */
+      height: 0; /* Обнулить высоту */
+    }
+
+    /* Удаление стандартной обводки при фокусе (для всех браузеров) */
+    &:focus {
+      outline: none; /* Убрать стандартную обводку при фокусе */
+    }
   }
 
-// DTO для корректировки транспортной заявки
-export class TransportRequestCorrectionDto {
-  @IsNotEmpty()
-  @IsString()
-  documentNumber: string;
 
-  @IsNotEmpty()
-  @IsString()
-  driver: string;
+  .base-input::placeholder {
+    color: var(--colors-text-placeholder, #c3c8c2);
+    font-family: 'Rubik';
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px; 
+    letter-spacing: 0.4px;
+  }
 
-  @IsNotEmpty()
-  @IsString()
-  timeDelivery: string;
-
-  @IsNotEmpty()
-  @IsString()
-  date: string;
-
-  @IsNotEmpty()
-  @IsString()
-  userIIN: string;
-
-  @IsNotEmpty()
-  @IsString()
-  userName: string;
-
-  @IsNotEmpty()
-  @IsString()
-  userPosition: string;
-
-  @IsNotEmpty()
-  @IsString()
-  carNumber: string;
-
-  @IsNotEmpty()
-  @IsString()
-  comment: string;
-}
+  .base-input:disabled {
+    cursor: not-allowed;
+  }
+</style>
